6166233e05b821e55bcca2bba2d9adad16347f82
Chao Liu <lc.roy86@gmail.com>
2019-04-03 10:36:18 -0500

    add script to extrac asm on hip

---
+++ script/compile-hip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export KMDUMPISA=1
+export KMDUMPLLVM=1
+
+make -j driver
+/opt/rocm/hcc/bin/llvm-objdump -mcpu=gfx906 -source -line-numbers driver/dump-gfx906.isabin > driver/dump-gfx906.isabin.isa
