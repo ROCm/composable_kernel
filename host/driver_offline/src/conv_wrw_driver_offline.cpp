2d194c52b6833c95a8ea6d94edf264abb1ecd74f
ltqin <letaoqin@amd.com>
2021-08-19 11:44:32 +0800

    input hack

--- host/driver_offline/src/conv_wrw_driver_offline.cpp
+++ host/driver_offline/src/conv_wrw_driver_offline.cpp
@@ -201,8 +201,8 @@ int main(int argc, char* argv[])
         out.GenerateTensorValue(GeneratorTensor_2{-5, 5}, num_thread);
         break;
     case 5:
-        in.GenerateTensorValue(GeneratorTensor_3<float>{0.0, 1.0}, num_thread);
-        out.GenerateTensorValue(GeneratorTensor_3<float>{-0.5, 0.5}, num_thread);
+        in.GenerateTensorValue(GeneratorTensor_3<float>{0.0, 0.01}, num_thread);
+        out.GenerateTensorValue(GeneratorTensor_3<float>{0.0, 0.01}, num_thread);
         break;
     default:
         in.GenerateTensorValue(GeneratorTensor_2{1, 5}, num_thread);
