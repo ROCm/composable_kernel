3c326d3ce4912022a68efd6a6d0302c1f37ac393
j4yan <jfyan008@gmail.com>
2021-10-22 17:37:02 -0400

    WIP: conv3d_fwd_v4r4r4_xdlops using coord transforms works, but subject to 2G limitation

--- host/host_tensor/include/host_conv.hpp
+++ host/host_tensor/include/host_conv.hpp
@@ -48,3 +48,61 @@ void host_conv_nchw_kcyx_nkhw(const Tensor<TIn>& in,
                                out.mDesc.GetLengths()[2],
                                out.mDesc.GetLengths()[3])(std::thread::hardware_concurrency());
 }
+
+void host_direct_convolution3d(const Tensor<TIn>& in,
+                               const Tensor<TWei>& wei,
+                               Tensor<TOut>& out,
+                               const ConvStrides& conv_strides,
+                               const ConvDilations& conv_dilations,
+                               const InLeftPads& in_left_pads,
+                               const InRightPads&,
+                               const ConvTensorLayout layout = ConvTensorLayout::NDHWC)
+{
+    using namespace ck;
+
+    constexpr auto I0 = Number<0>{};
+    constexpr auto I1 = Number<1>{};
+    constexpr auto I2 = Number<2>{};
+
+    auto f_ndhwc = [&](auto n, auto do_, auto ho, auto wo, auto k) {
+        double v = 0;
+        for(int c = 0; c < wei.mDesc.GetLengths()[4]; ++c)
+        {
+            for(int z = 0; z < wei.mDesc.GetLengths()[1]; ++z)
+            {
+                int di = do_ * conv_strides[I0] + z * conv_dilations[I0] - in_left_pads[I0];
+                for(int y = 0; y < wei.mDesc.GetLengths()[2]; ++y)
+                {
+                    int hi = ho * conv_strides[I1] + y * conv_dilations[I1] - in_left_pads[I1];
+                    for(int x = 0; x < wei.mDesc.GetLengths()[3]; ++x)
+                    {
+                        int wi = wo * conv_strides[I2] + x * conv_dilations[I2] - in_left_pads[I2];
+                        if(di >= 0 && di < in.mDesc.GetLengths()[1] &&  
+                           hi >= 0 && hi < in.mDesc.GetLengths()[2] && 
+                           wi >= 0 && wi < in.mDesc.GetLengths()[3])
+                        {
+                            v += static_cast<const double>(in(n, di, hi, wi, c)) *
+                                static_cast<const double>(wei(k, z, y, x, c));
+                        }
+                    }
+                }
+            }
+        }
+        out(n, do_, ho, wo, k) = v;
+    };
+
+    if(layout == ConvTensorLayout::NDHWC)
+    {
+        make_ParallelTensorFunctor(f_ndhwc,
+                                   out.mDesc.GetLengths()[0],
+                                   out.mDesc.GetLengths()[1],
+                                   out.mDesc.GetLengths()[2],
+                                   out.mDesc.GetLengths()[3],
+                                   out.mDesc.GetLengths()[4])(std::thread::hardware_concurrency());
+    }
+    else
+    {
+        throw std::runtime_error("wrong! not supported layout");
+    }
+}
+
