b7c81969cf56b8886a4f69809f4c57d593706468
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2021-12-28 15:45:01 +0000

    clang-format some files again

--- device_operation/include/device_reduce.hpp
+++ device_operation/include/device_reduce.hpp
@@ -12,30 +12,50 @@ namespace ck {
 namespace tensor_operation {
 namespace device {
 
-template <typename inType, typename compType, typename outType, int rank, typename reduceDims, 
-	  ReduceTensorOp_t reduceOp, NanPropagation_t nanOpt, ReduceTensorIndices_t indicesOpt>
+template <typename inType,
+          typename compType,
+          typename outType,
+          int rank,
+          typename reduceDims,
+          ReduceTensorOp_t reduceOp,
+          NanPropagation_t nanOpt,
+          ReduceTensorIndices_t indicesOpt>
 struct DeviceReduce : public BaseOperator
 {
-    virtual size_t getWorkspaceSize(const std::vector<int> & inLengths) = 0; 
+    virtual size_t getWorkspaceSize(const std::vector<int>& inLengths) = 0;
 
-    virtual bool hasFurtherCall() { return(false); };  
+    virtual bool hasFurtherCall() { return (false); };
 
-    virtual std::vector<int> getWorkspace2dLengths(const BaseArgument *argPtr) 
-    { 
-        (void)argPtr; 	
-        return( std::vector<int>{0, 0} ); 
-    }; 
+    virtual std::vector<int> getWorkspace2dLengths(const BaseArgument* argPtr)
+    {
+        (void)argPtr;
+        return (std::vector<int>{0, 0});
+    };
 
-    virtual std::unique_ptr<BaseArgument> MakeArgumentPointer(const std::vector<int> & inLengths, const std::vector<int> & inStrides,
-		                                              const std::vector<int> & outLengths, const std::vector<int> & outStrides, 
-		                                              float alpha, float beta, const void *in_dev, void *out_dev, void *out_indices_dev, void *workspace_dev) = 0; 
+    virtual std::unique_ptr<BaseArgument> MakeArgumentPointer(const std::vector<int>& inLengths,
+                                                              const std::vector<int>& inStrides,
+                                                              const std::vector<int>& outLengths,
+                                                              const std::vector<int>& outStrides,
+                                                              float alpha,
+                                                              float beta,
+                                                              const void* in_dev,
+                                                              void* out_dev,
+                                                              void* out_indices_dev,
+                                                              void* workspace_dev) = 0;
 
     virtual std::unique_ptr<BaseInvoker> MakeInvokerPointer() = 0;
 };
 
-template <typename inType, typename compType, typename outType, int rank, typename reduceDims, 
-	  ReduceTensorOp_t reduceOp, NanPropagation_t nanOpt, ReduceTensorIndices_t indicesOpt>
-using DeviceReducePtr = std::unique_ptr< DeviceReduce<inType, compType, outType, rank, reduceDims, reduceOp, nanOpt, indicesOpt> >;
+template <typename inType,
+          typename compType,
+          typename outType,
+          int rank,
+          typename reduceDims,
+          ReduceTensorOp_t reduceOp,
+          NanPropagation_t nanOpt,
+          ReduceTensorIndices_t indicesOpt>
+using DeviceReducePtr = std::unique_ptr<
+    DeviceReduce<inType, compType, outType, rank, reduceDims, reduceOp, nanOpt, indicesOpt>>;
 
 } // namespace device
 } // namespace tensor_operation
--- device_operation/include/device_reduce_instance_impl_common.hpp
+++ device_operation/include/device_reduce_instance_impl_common.hpp
@@ -9,84 +9,105 @@ namespace device_reduce_instance {
 template <int blockSize, int dim0_thread_cluster_size, int dim1_thread_cluster_size>
 struct ReductionConfiguration_1
 {
-    static_assert(blockSize == dim0_thread_cluster_size * dim1_thread_cluster_size, "Invalid Configuration!");
+    static_assert(blockSize == dim0_thread_cluster_size * dim1_thread_cluster_size,
+                  "Invalid Configuration!");
 
-    static constexpr int blockSize_ = blockSize;
+    static constexpr int blockSize_                = blockSize;
     static constexpr int dim0_thread_cluster_size_ = dim0_thread_cluster_size;
     static constexpr int dim1_thread_cluster_size_ = dim1_thread_cluster_size;
 };
 
-template<int dim0_max_vector_size, int dim1_max_vector_size, int dim0_thread_slice_size, int dim1_thread_slice_size>
+template <int dim0_max_vector_size,
+          int dim1_max_vector_size,
+          int dim0_thread_slice_size,
+          int dim1_thread_slice_size>
 struct ReductionConfiguration_2
 {
-    static constexpr int dim0_max_vector_size_ = dim0_max_vector_size;
-    static constexpr int dim1_max_vector_size_ = dim1_max_vector_size;
+    static constexpr int dim0_max_vector_size_   = dim0_max_vector_size;
+    static constexpr int dim1_max_vector_size_   = dim1_max_vector_size;
     static constexpr int dim0_thread_slice_size_ = dim0_thread_slice_size;
     static constexpr int dim1_thread_slice_size_ = dim1_thread_slice_size;
 };
 
-using reduce_configuration_1_instances = std::tuple<
-    ReductionConfiguration_1<256, 128, 2>,
-    ReductionConfiguration_1<256, 64, 4>,
-    ReductionConfiguration_1<256, 32, 8>,
-    ReductionConfiguration_1<256, 16, 16>,
-    ReductionConfiguration_1<256, 8, 32>,
-    ReductionConfiguration_1<256, 4, 64>,
-    ReductionConfiguration_1<256, 2, 128>,
-    ReductionConfiguration_1<256, 1, 256>
-    >;
+using reduce_configuration_1_instances = std::tuple<ReductionConfiguration_1<256, 128, 2>,
+                                                    ReductionConfiguration_1<256, 64, 4>,
+                                                    ReductionConfiguration_1<256, 32, 8>,
+                                                    ReductionConfiguration_1<256, 16, 16>,
+                                                    ReductionConfiguration_1<256, 8, 32>,
+                                                    ReductionConfiguration_1<256, 4, 64>,
+                                                    ReductionConfiguration_1<256, 2, 128>,
+                                                    ReductionConfiguration_1<256, 1, 256>>;
 
 #define QUICK_REDUCE_TEST 1
 
 #ifdef QUICK_REDUCE_TEST
-using reduce_configuration_2_instances = std::tuple<
-    ReductionConfiguration_2<1, 1, 1, 1>
-    >;
+using reduce_configuration_2_instances = std::tuple<ReductionConfiguration_2<1, 1, 1, 1>>;
 #else
-using reduce_configuration_2_instances = std::tuple<
-    ReductionConfiguration_2<8, 1, 8, 1>,
-    ReductionConfiguration_2<8, 1, 4, 1>,
-    ReductionConfiguration_2<8, 1, 2, 1>,
-    ReductionConfiguration_2<8, 1, 1, 1>,
+using reduce_configuration_2_instances = std::tuple<ReductionConfiguration_2<8, 1, 8, 1>,
+                                                    ReductionConfiguration_2<8, 1, 4, 1>,
+                                                    ReductionConfiguration_2<8, 1, 2, 1>,
+                                                    ReductionConfiguration_2<8, 1, 1, 1>,
 
-    ReductionConfiguration_2<4, 1, 4, 1>,
-    ReductionConfiguration_2<4, 1, 2, 1>,
-    ReductionConfiguration_2<4, 1, 1, 1>,
+                                                    ReductionConfiguration_2<4, 1, 4, 1>,
+                                                    ReductionConfiguration_2<4, 1, 2, 1>,
+                                                    ReductionConfiguration_2<4, 1, 1, 1>,
 
-    ReductionConfiguration_2<2, 1, 2, 1>,
-    ReductionConfiguration_2<2, 1, 1, 1>,
-    ReductionConfiguration_2<1, 1, 1, 1>,
-    ReductionConfiguration_2<1, 2, 1, 2>,
-    ReductionConfiguration_2<1, 2, 1, 1>,
-    ReductionConfiguration_2<1, 4, 1, 4>,
-    ReductionConfiguration_2<1, 4, 1, 2>,
-    ReductionConfiguration_2<1, 4, 1, 1>,
+                                                    ReductionConfiguration_2<2, 1, 2, 1>,
+                                                    ReductionConfiguration_2<2, 1, 1, 1>,
+                                                    ReductionConfiguration_2<1, 1, 1, 1>,
+                                                    ReductionConfiguration_2<1, 2, 1, 2>,
+                                                    ReductionConfiguration_2<1, 2, 1, 1>,
+                                                    ReductionConfiguration_2<1, 4, 1, 4>,
+                                                    ReductionConfiguration_2<1, 4, 1, 2>,
+                                                    ReductionConfiguration_2<1, 4, 1, 1>,
 
-    ReductionConfiguration_2<1, 8, 1, 8>,
-    ReductionConfiguration_2<1, 8, 1, 4>,
-    ReductionConfiguration_2<1, 8, 1, 2>,
-    ReductionConfiguration_2<1, 8, 1, 1>,
+                                                    ReductionConfiguration_2<1, 8, 1, 8>,
+                                                    ReductionConfiguration_2<1, 8, 1, 4>,
+                                                    ReductionConfiguration_2<1, 8, 1, 2>,
+                                                    ReductionConfiguration_2<1, 8, 1, 1>,
 
-    // special instances
-    ReductionConfiguration_2<1, 1, 3, 1>,
-    ReductionConfiguration_2<1, 1, 5, 1>,
-    ReductionConfiguration_2<1, 1, 7, 1>,
-    ReductionConfiguration_2<1, 1, 11, 1>,
+                                                    // special instances
+                                                    ReductionConfiguration_2<1, 1, 3, 1>,
+                                                    ReductionConfiguration_2<1, 1, 5, 1>,
+                                                    ReductionConfiguration_2<1, 1, 7, 1>,
+                                                    ReductionConfiguration_2<1, 1, 11, 1>,
 
-    ReductionConfiguration_2<1, 1, 1, 3>,
-    ReductionConfiguration_2<1, 1, 1, 5>,
-    ReductionConfiguration_2<1, 1, 1, 7>,
-    ReductionConfiguration_2<1, 1, 1, 11>
-    >;
-#endif    
+                                                    ReductionConfiguration_2<1, 1, 1, 3>,
+                                                    ReductionConfiguration_2<1, 1, 1, 5>,
+                                                    ReductionConfiguration_2<1, 1, 1, 7>,
+                                                    ReductionConfiguration_2<1, 1, 1, 11>>;
+#endif
 
-#define ADD_INST_BY_TYPE(key,inT,compT,outT,reduceOp,nanOpt,indicesOpt,rank,...) \
-        template void add_device_reduce_instance_##key<inT,compT,outT,rank,Sequence<__VA_ARGS__>,reduceOp,nanOpt,indicesOpt>( \
-                        std::vector<DeviceReducePtr<inT,compT,outT,rank,Sequence<__VA_ARGS__>,reduceOp,nanOpt,indicesOpt>> & device_op_instances)
+#define ADD_INST_BY_TYPE(key, inT, compT, outT, reduceOp, nanOpt, indicesOpt, rank, ...) \
+    template void add_device_reduce_instance_##key<inT,                                  \
+                                                   compT,                                \
+                                                   outT,                                 \
+                                                   rank,                                 \
+                                                   Sequence<__VA_ARGS__>,                \
+                                                   reduceOp,                             \
+                                                   nanOpt,                               \
+                                                   indicesOpt>(                          \
+        std::vector<DeviceReducePtr<inT,                                                 \
+                                    compT,                                               \
+                                    outT,                                                \
+                                    rank,                                                \
+                                    Sequence<__VA_ARGS__>,                               \
+                                    reduceOp,                                            \
+                                    nanOpt,                                              \
+                                    indicesOpt>> &                                       \
+        device_op_instances)
 
-#define ADD_INST_BY_ID(key,inT,compT,outT,reduceOp,nanOpt,indicesOpt,rank,...) \
-        ADD_INST_BY_TYPE(key,inT,compT,outT,static_cast<ReduceTensorOp_t>(reduceOp),static_cast<NanPropagation_t>(nanOpt),static_cast<ReduceTensorIndices_t>(indicesOpt),rank,__VA_ARGS__) 
-    
+#define ADD_INST_BY_ID(key, inT, compT, outT, reduceOp, nanOpt, indicesOpt, rank, ...) \
+    ADD_INST_BY_TYPE(key,                                                              \
+                     inT,                                                              \
+                     compT,                                                            \
+                     outT,                                                             \
+                     static_cast<ReduceTensorOp_t>(reduceOp),                          \
+                     static_cast<NanPropagation_t>(nanOpt),                            \
+                     static_cast<ReduceTensorIndices_t>(indicesOpt),                   \
+                     rank,                                                             \
+                     __VA_ARGS__)
+
 } // namespace device_reduce_instance
 } // namespace device
 } // namespace tensor_operation
@@ -94,5 +115,3 @@ using reduce_configuration_2_instances = std::tuple<
 } // namespace ck
 
 #endif
-
-
