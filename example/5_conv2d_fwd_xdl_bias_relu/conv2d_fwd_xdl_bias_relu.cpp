9dae287ec578d2ca73bd82ffc24f64783c536938
ltqin <letaoqin@amd.com>
2022-01-28 20:53:45 +0800

    add reference

--- example/5_conv2d_fwd_xdl_bias_relu/conv2d_fwd_xdl_bias_relu.cpp
+++ example/5_conv2d_fwd_xdl_bias_relu/conv2d_fwd_xdl_bias_relu.cpp
@@ -13,6 +13,7 @@
 #include "tensor_layout.hpp"
 #include "device_conv2d_fwd_xdl_c_shuffle_bias_activation_nhwc_kyxc_nhwk.hpp"
 #include "element_wise_operation.hpp"
+#include "reference_conv2d_fwd_bias_activation_nhwc_kyxc_nhwk.hpp"
 
 using InDataType  = ck::half_t;
 using WeiDataType = ck::half_t;
@@ -44,6 +45,10 @@ using DeviceConvFwdInstance = ck::tensor_operation::device::
 //      |          |            |            |            |   Operation|   Operation|     Operation| DataOperation|               |      |      |      |      |   |     |     | Wave| Wave| Lengths_K0_M_K1|   ArrangeOrder|               |               |      PerVector|   PerVector_K1|          | Lengths_K0_N_K1|   ArrangeOrder|               |              |      PerVector|   PerVector_K1|          |  PerShuffle|  PerShuffle| _NBlock_NXdlPerWave_NWaveNPerXdl|   _NWaveNPerXdl|
 //      |          |            |            |            |            |            |              |              |               |      |      |      |      |   |     |     |     |     |                |               |               |               |               |               |          |                |               |               |              |               |               |          |            |            |                                 |                |
         <InDataType, WeiDataType, OutDataType, AccDataType, InElementOp, WeiElementOp, OutElementOp,     MemorySet, ConvFwdDefault,   256,   128,   256,     4,  8,   32,   32,    2,    4,     S<4, 64, 1>,     S<1, 0, 2>,     S<1, 0, 2>,              2,              8,              8,      true,     S<4, 64, 1>,     S<1, 0, 2>,     S<1, 0, 2>,             2,              8,              8,      true,           1,           1,             S<1, 1, 32, 1, 1, 8>,               8>;
+
+using ReferenceConvFwdInstance = ck::tensor_operation::host::
+    ReferenceConv2dFwd_Bias_Activation_Input_N_Hi_Wi_C_Weight_K_Y_X_C_Output_N_Ho_Wo_K
+        < InDataType,  WeiDataType,  OutDataType,  AccDataType, InElementOp, WeiElementOp, OutElementOp>;
 // clang-format on
 
 template <typename TIn,
@@ -277,17 +282,21 @@ int main(int argc, char* argv[])
 
     if(do_verification)
     {
-        host_reference_calculation(in_n_c_hi_wi,
-                                   wei_k_c_y_x,
-                                   out_n_k_ho_wo_host_result,
-                                   bias_k,
-                                   conv_filter_strides,
-                                   conv_filter_dilations,
-                                   input_left_pads,
-                                   input_right_pads,
-                                   InElementOp{},
-                                   WeiElementOp{},
-                                   OutElementOp{});
+        auto refConv    = ReferenceConvFwdInstance{};
+        auto refInvoker = refConv.MakeInvoker();
+
+        auto refArgument = refConv.MakeArgument(in_n_c_hi_wi,
+                                                wei_k_c_y_x,
+                                                out_n_k_ho_wo_host_result,
+                                                bias_k,
+                                                conv_filter_strides,
+                                                conv_filter_dilations,
+                                                input_left_pads,
+                                                input_right_pads,
+                                                InElementOp{},
+                                                WeiElementOp{},
+                                                OutElementOp{});
+        refInvoker.Run(refArgument);
 
         out_device_buf.FromDevice(out_n_k_ho_wo_device_result.mData.data());
 
