719b62e3a0de861e2ef2f658e3b813c0ca33db10
Jianfeng yan <jfyan008@gmail.com>
2022-02-23 21:04:44 +0000

    add functions for backward and forward step; hard coded results in unit test

--- composable_kernel/include/utility/tensor_space_filling_curve.hpp
+++ composable_kernel/include/utility/tensor_space_filling_curve.hpp
@@ -6,19 +6,19 @@
 
 namespace ck {
 
-template <typename SliceLengths,
+template <typename TensorLengths,
           typename DimAccessOrder,
           typename ScalarsPerAccess> // # of scalars per access in each dimension
 struct SpaceFillingCurve
 {
-    static constexpr index_t nDim = SliceLengths::Size();
+    static constexpr index_t nDim = TensorLengths::Size();
 
     using Index = MultiIndex<nDim>;
 
     static constexpr index_t ScalarPerVector =
         reduce_on_sequence(ScalarsPerAccess{}, math::multiplies{}, Number<1>{});
 
-    static constexpr auto access_lengths   = SliceLengths{} / ScalarsPerAccess{};
+    static constexpr auto access_lengths   = TensorLengths{} / ScalarsPerAccess{};
     static constexpr auto dim_access_order = DimAccessOrder{};
     static constexpr auto ordered_access_lengths =
         container_reorder_given_new2old(access_lengths, dim_access_order);
@@ -31,37 +31,58 @@ struct SpaceFillingCurve
     static constexpr auto I0 = Number<0>{};
     static constexpr auto I1 = Number<1>{};
 
-    __device__ __host__ static constexpr index_t GetNumOfAccess()
+    __host__ __device__ static constexpr index_t GetNumOfAccess()
     {
-        return reduce_on_sequence(SliceLengths{}, math::multiplies{}, Number<1>{}) /
+        return reduce_on_sequence(TensorLengths{}, math::multiplies{}, Number<1>{}) /
                ScalarPerVector;
     }
 
-    template <int ii>
-    static __device__ __host__ constexpr Index GetIndex(Number<ii>)
+    template <index_t AccessIdx1d>
+    static __device__ __host__ constexpr auto GetForwardStep(Number<AccessIdx1d>)
+    {
+
+        constexpr auto idx_curr = GetIndex(Number<AccessIdx1d>{});
+        constexpr auto idx_next = GetIndex(Number<AccessIdx1d>{} + Number<1>{});
+        return idx_next - idx_curr;
+    }
+
+    template <index_t AccessIdx1d>
+    static __device__ __host__ constexpr auto GetBackwardStep(Number<AccessIdx1d>)
+    {
+        static_assert(AccessIdx1d > 0, "1D index should be larger than 0");
+
+        constexpr auto idx_curr = GetIndex(Number<AccessIdx1d>{});
+        constexpr auto idx_prev = GetIndex(Number<AccessIdx1d>{} - Number<1>{});
+        return idx_prev - idx_curr;
+    }
+
+    template <index_t AccessIdx1d>
+    static __device__ __host__ constexpr Index GetIndex(Number<AccessIdx1d>)
     {
 #if 0
         /*
          * \todo: TensorAdaptor::CalculateBottomIndex does NOT return constexpr as expected.
          */
-        constexpr auto ordered_access_idx = to_index_adaptor.CalculateBottomIndex(make_multi_index(Number<ii>{}));
+        constexpr auto ordered_access_idx = to_index_adaptor.CalculateBottomIndex(make_multi_index(Number<AccessIdx1d>{}));
 #else
 
         constexpr auto access_strides = container_reverse_exclusive_scan(
             ordered_access_lengths, math::multiplies{}, Number<1>{});
 
-        constexpr auto Id = Number<ii>{};
-        // all constexpr has to be captured by VALUE.
-        constexpr auto compute_index = [ Id, access_strides ](auto idim) constexpr
+        constexpr auto idx_1d = Number<AccessIdx1d>{};
+        // Given tensor strides \p access_lengths, and 1D index of space-filling-curve, compute the
+        // idim-th element of multidimensional index.
+        // All constexpr variables have to be captured by VALUE.
+        constexpr auto compute_index = [ idx_1d, access_strides ](auto idim) constexpr
         {
-            constexpr auto compute_index_impl = [ Id, access_strides ](auto jdim) constexpr
+            constexpr auto compute_index_impl = [ idx_1d, access_strides ](auto jdim) constexpr
             {
-                auto res = Id.value;
+                auto res = idx_1d.value;
                 auto id  = 0;
 
-                static_for<0, jdim.value + 1, 1>{}([&](auto jj) {
-                    id = res / access_strides[jj].value;
-                    res -= id * access_strides[jj].value;
+                static_for<0, jdim.value + 1, 1>{}([&](auto kdim) {
+                    id = res / access_strides[kdim].value;
+                    res -= id * access_strides[kdim].value;
                 });
 
                 return id;
@@ -74,7 +95,6 @@ struct SpaceFillingCurve
         constexpr auto ordered_access_idx = generate_tuple(compute_index, Number<nDim>{});
 #endif
         constexpr auto forward_sweep = [&]() {
-            // auto forward_sweep = [&]() {
             StaticallyIndexedArray<bool, nDim> forward_sweep_;
 
             forward_sweep_(I0) = true;
@@ -91,9 +111,8 @@ struct SpaceFillingCurve
             return forward_sweep_;
         }();
 
-        // calculate data index
-        auto data_idx = [&]() {
-            // constexpr auto data_idx = [&]() {
+        // calculate multi-dim tensor index
+        auto idx_md = [&]() {
             Index ordered_idx;
 
             static_for<0, nDim, 1>{}([&](auto idim) {
@@ -105,7 +124,7 @@ struct SpaceFillingCurve
             return container_reorder_given_old2new(ordered_idx, dim_access_order) *
                    ScalarsPerAccess{};
         }();
-        return data_idx;
+        return idx_md;
     }
 };
 
--- test/space_filling_curve/space_filling_curve.cpp
+++ test/space_filling_curve/space_filling_curve.cpp
@@ -7,7 +7,7 @@
 
 using namespace ck;
 
-__global__ void traverse_using_space_filling_curve();
+void traverse_using_space_filling_curve();
 
 int main(int argc, char** argv)
 {
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
     (void)argv;
 
     {
-        traverse_using_space_filling_curve<<<1, 1>>>();
+        traverse_using_space_filling_curve();
         auto err = hipDeviceSynchronize();
         (void)err;
         assert(err == hipSuccess);
@@ -23,45 +23,109 @@ int main(int argc, char** argv)
     return 0;
 }
 
-__global__ void traverse_using_space_filling_curve()
+void traverse_using_space_filling_curve()
 {
     constexpr auto I0 = Number<0>{};
+    constexpr auto I1 = Number<1>{};
+    constexpr auto I2 = Number<2>{};
 
-    using SliceLengths      = Sequence<4, 3, 6>;
-    using DimAccessOrder    = Sequence<2, 1, 0>;
-    using ScalarsPerAccess  = Sequence<2, 1, 2>;
-    using SpaceFillingCurve = SpaceFillingCurve<SliceLengths, DimAccessOrder, ScalarsPerAccess>;
+    using TensorLengths     = Sequence<4, 10, 9>;
+    using DimAccessOrder    = Sequence<2, 0, 1>;
+    using ScalarsPerAccess  = Sequence<1, 2, 3>;
+    using SpaceFillingCurve = SpaceFillingCurve<TensorLengths, DimAccessOrder, ScalarsPerAccess>;
+
+    constexpr auto expected = make_tuple(make_tuple(0, 0, 0),
+                                         make_tuple(0, 2, 0),
+                                         make_tuple(0, 4, 0),
+                                         make_tuple(0, 6, 0),
+                                         make_tuple(0, 8, 0),
+                                         make_tuple(1, 8, 0),
+                                         make_tuple(1, 6, 0),
+                                         make_tuple(1, 4, 0),
+                                         make_tuple(1, 2, 0),
+                                         make_tuple(1, 0, 0),
+                                         make_tuple(2, 0, 0),
+                                         make_tuple(2, 2, 0),
+                                         make_tuple(2, 4, 0),
+                                         make_tuple(2, 6, 0),
+                                         make_tuple(2, 8, 0),
+                                         make_tuple(3, 8, 0),
+                                         make_tuple(3, 6, 0),
+                                         make_tuple(3, 4, 0),
+                                         make_tuple(3, 2, 0),
+                                         make_tuple(3, 0, 0),
+                                         make_tuple(3, 0, 3),
+                                         make_tuple(3, 2, 3),
+                                         make_tuple(3, 4, 3),
+                                         make_tuple(3, 6, 3),
+                                         make_tuple(3, 8, 3),
+                                         make_tuple(2, 8, 3),
+                                         make_tuple(2, 6, 3),
+                                         make_tuple(2, 4, 3),
+                                         make_tuple(2, 2, 3),
+                                         make_tuple(2, 0, 3),
+                                         make_tuple(1, 0, 3),
+                                         make_tuple(1, 2, 3),
+                                         make_tuple(1, 4, 3),
+                                         make_tuple(1, 6, 3),
+                                         make_tuple(1, 8, 3),
+                                         make_tuple(0, 8, 3),
+                                         make_tuple(0, 6, 3),
+                                         make_tuple(0, 4, 3),
+                                         make_tuple(0, 2, 3),
+                                         make_tuple(0, 0, 3),
+                                         make_tuple(0, 0, 6),
+                                         make_tuple(0, 2, 6),
+                                         make_tuple(0, 4, 6),
+                                         make_tuple(0, 6, 6),
+                                         make_tuple(0, 8, 6),
+                                         make_tuple(1, 8, 6),
+                                         make_tuple(1, 6, 6),
+                                         make_tuple(1, 4, 6),
+                                         make_tuple(1, 2, 6),
+                                         make_tuple(1, 0, 6),
+                                         make_tuple(2, 0, 6),
+                                         make_tuple(2, 2, 6),
+                                         make_tuple(2, 4, 6),
+                                         make_tuple(2, 6, 6),
+                                         make_tuple(2, 8, 6),
+                                         make_tuple(3, 8, 6),
+                                         make_tuple(3, 6, 6),
+                                         make_tuple(3, 4, 6),
+                                         make_tuple(3, 2, 6),
+                                         make_tuple(3, 0, 6));
 
     constexpr index_t num_accesses = SpaceFillingCurve::GetNumOfAccess();
-    static_assert(num_accesses == reduce_on_sequence(SliceLengths{} / ScalarsPerAccess{},
+
+    static_assert(num_accesses == reduce_on_sequence(TensorLengths{} / ScalarsPerAccess{},
                                                      math::multiplies{},
                                                      Number<1>{}));
 
-    auto idx0 = SpaceFillingCurve::GetIndex(I0);
-
     static_for<1, num_accesses, 1>{}([&](auto i) {
-        constexpr auto idx1 = SpaceFillingCurve::GetIndex(i);
-        const auto idx_diff = idx1 - idx0;
-        index_t diff        = 0;
-        static_for<0, SliceLengths::Size(), 1>{}([&](auto idim) {
-            diff += abs(idx_diff[idim]) / ScalarsPerAccess{}[idim];
+        constexpr auto idx_curr = SpaceFillingCurve::GetIndex(i);
 
-        });
+        static_assert(idx_curr[I0] == expected[i][I0]);
+        static_assert(idx_curr[I1] == expected[i][I1]);
+        static_assert(idx_curr[I2] == expected[i][I2]);
 
-        if(diff != 1)
-        {
-            constexpr auto I1 = Number<1>{};
-            constexpr auto I2 = Number<2>{};
+        constexpr auto backward_step = SpaceFillingCurve::GetBackwardStep(i);
+        constexpr auto expected_step = expected[i - I1] - expected[i];
+        static_assert(backward_step[I0] == expected_step[I0]);
+        static_assert(backward_step[I1] == expected_step[I1]);
+        static_assert(backward_step[I2] == expected_step[I2]);
+    });
 
-            printf("Error: \n");
-            printf("idx(%d) = [%d, %d, %d]\n", i.value - 1, idx0[I0], idx0[I1], idx0[I2]);
-            printf("idx(%d) = [%d, %d, %d]\n", i.value, idx1[I0], idx1[I1], idx1[I2]);
-            printf("delta_idx / ScalarsPerAccess = [%d, %d, %d]\n",
-                   idx_diff[I0] / ScalarsPerAccess{}[I0],
-                   idx_diff[I1] / ScalarsPerAccess{}[I1],
-                   idx_diff[I2] / ScalarsPerAccess{}[I2]);
-        }
-        idx0 = idx1;
+    static_for<0, num_accesses - 1, 1>{}([&](auto i) {
+        constexpr auto idx_curr = SpaceFillingCurve::GetIndex(i);
+
+        static_assert(idx_curr[I0] == expected[i][I0]);
+        static_assert(idx_curr[I1] == expected[i][I1]);
+        static_assert(idx_curr[I2] == expected[i][I2]);
+
+        constexpr auto forward_step  = SpaceFillingCurve::GetForwardStep(i);
+        constexpr auto expected_step = expected[i + I1] - expected[i];
+        static_assert(forward_step[I0] == expected_step[I0]);
+        static_assert(forward_step[I1] == expected_step[I1]);
+        static_assert(forward_step[I2] == expected_step[I2]);
     });
 }
-
