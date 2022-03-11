87b9fbd8c08e296d4cd75d088f4eefc712dc3d86
rocking <chunylai@amd.com>
2022-03-11 23:49:29 +0800

    Add gemm fp16 test

--- test/include/test_util.hpp
+++ test/include/test_util.hpp
@@ -54,6 +54,49 @@ check_err(const std::vector<T>& out,
     return res;
 }
 
+bool check_err(const std::vector<_Float16>& out,
+                   const std::vector<_Float16>& ref,
+                   const std::string& msg,
+                   _Float16 rtol = static_cast<_Float16>(1e-5),
+                   _Float16 atol = static_cast<_Float16>(1e-8))
+{
+    if(out.size() != ref.size())
+    {
+        std::cout << "out.size() != ref.size(), :" << out.size() << " != " << ref.size()
+                  << std::endl
+                  << msg << std::endl;
+        return false;
+    }
+
+    bool res{true};
+    int err_count = 0;
+    double err         = 0;
+    double max_err     = std::numeric_limits<_Float16>::min();
+    for(std::size_t i = 0; i < ref.size(); ++i)
+    {
+        double out_ = double(out[i]);
+        double ref_ = double(ref[i]);
+        err = std::abs(out_ - ref_);
+        if(err > atol + rtol * std::abs(ref_) || !std::isfinite(out_) || !std::isfinite(ref_))
+        {
+            max_err = err > max_err ? err : max_err;
+            err_count++;
+            if(err_count < 5)
+            {
+                std::cout << std::setw(12) << std::setprecision(7) << "out[" << i << "] != ref["
+                          << i << "]: " << out_ << "!=" << ref_ << std::endl
+                          << msg << std::endl;
+            }
+            res = false;
+        }
+    }
+    if(!res)
+    {
+        std::cout << std::setw(12) << std::setprecision(7) << "max err: " << max_err << std::endl;
+    }
+    return res;
+}
+
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, bool>::type check_err(
     const std::vector<T>& out, const std::vector<T>& ref, const std::string& msg, T = 0, T = 0)
