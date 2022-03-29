25afb690ddbdb83306477b63815a42df23aef544
Jianfeng yan <jfyan008@gmail.com>
2022-03-29 23:04:22 +0000

    batched_gemm_reduce: add test

--- example/01_gemm/gemm_xdl_int8.cpp
+++ example/01_gemm/gemm_xdl_int8.cpp
@@ -5,11 +5,9 @@
 #include <stdlib.h>
 #include <half.hpp>
 #include "config.hpp"
-#include "print.hpp"
 #include "device.hpp"
 #include "host_tensor.hpp"
 #include "host_tensor_generator.hpp"
-#include "host_gemm.hpp"
 #include "device_tensor.hpp"
 #include "device_gemm_xdl.hpp"
 #include "device_gemm_xdl_c_shuffle.hpp"
