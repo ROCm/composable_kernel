494d923ca892609042a75579f0581f5ff34fe06a
Adam Osewski <aosewski@amd.com>
2022-03-30 09:51:46 +0200

    Remove unused check_indices restored by previous merge.

--- library/include/ck/library/host_tensor/host_tensor.hpp
+++ library/include/ck/library/host_tensor/host_tensor.hpp
@@ -349,28 +349,4 @@ float check_error(const Tensor<T>& ref, const Tensor<T>& result)
     return linf_error;
 }
 
-template <typename T>
-void check_indices(const Tensor<T>& ref, const Tensor<T>& result)
-{
-    bool has_error  = false;
-    int error_count = 0;
-
-    for(int i = 0; i < ref.mData.size(); ++i)
-    {
-        if(ref.mData[i] != result.mData[i])
-        {
-            std::cerr << std::endl
-                      << "Indices different at position " << i << " (ref: " << ref.mData[i]
-                      << ", result: " << result.mData[i] << ")" << std::endl;
-            has_error = true;
-            error_count++;
-            if(error_count == 20)
-                break;
-        };
-    }
-
-    if(!has_error)
-        std::cout << std::endl << "Indices result is completely acccurate!" << std::endl;
-}
-
 #endif
