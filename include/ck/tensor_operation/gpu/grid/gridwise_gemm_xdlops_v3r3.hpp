3b573bb9ed374a8b00f0b9fe1d89e2db46e7da01
illsilin <Illia.Silin@amd.com>
2022-04-08 18:05:27 -0700

    change the target condition

--- include/ck/tensor_operation/gpu/grid/gridwise_gemm_xdlops_v3r3.hpp
+++ include/ck/tensor_operation/gpu/grid/gridwise_gemm_xdlops_v3r3.hpp
@@ -49,7 +49,7 @@ __global__ void
             const CElementwiseOperation c_element_op,
             const Block2CTileMap block_2_ctile_map)
 {
-#if (defined(!__HIP_DEVICE_COMPILE__  || __gfx908__) || defined(__gfx90a__))
+#if (!defined(__HIP_DEVICE_COMPILE__)  || defined(__gfx908__) || defined(__gfx90a__))
     __shared__ char p_shared[GridwiseGemm::GetSharedMemoryNumberOfByte()];
 
     GridwiseGemm::template Run<HasMainK0BlockLoop>(
