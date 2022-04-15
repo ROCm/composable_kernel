4b2bfee388ccb725da052997afc14a083d13b446
Illia Silin <98187287+illsilin@users.noreply.github.com>
2022-04-15 12:17:28 -0700

    Compile CK for all targets (#188)
    * compile ck for all targets
    
    * update the target criteria
    
    * change the target condition
    
    * fixed some typos
    
    * fixed missed file
    
    * revert changes in README
    
    * revert device_conv3d_fwd_xdl_...
    
    * update device_conv3d_fwd_xdl_...
    
    * update device_batched_gemm_reduce...
    
    * test the unused arguments fix
    
    * test the warning suppression
    
    * try suppress warnings in device_batched_gemm_reduce_xdl...
    
    * fix the last warnings
    
    * replace UNUSED with std::ignore
    
    * fix a typo
    
    * replaced std::ignore with ignore
    
    * add igonre header to common_header
    
    * refactor atomicAdd
    
    Co-authored-by: Chao Liu <chao.liu2@amd.com>
    
    [ROCm/composable_kernel commit: 4221505d3e579e27d2ead8c59fba9f57b4979052]

--- include/ck/tensor_operation/gpu/device/device_batched_gemm_xdl.hpp
+++ include/ck/tensor_operation/gpu/device/device_batched_gemm_xdl.hpp
@@ -46,6 +46,7 @@ __global__ void
             const ComputeBasePrtOfBatch compute_base_ptr_of_batch_,
             const Block2CTileMap block_2_ctile_map)
 {
+#if(!defined(__HIP_DEVICE_COMPILE__) || defined(__gfx908__) || defined(__gfx90a__))
     const index_t num_blocks_per_batch =
         __builtin_amdgcn_readfirstlane(get_grid_size() / batch_count);
     const index_t g_idx = __builtin_amdgcn_readfirstlane(get_block_1d_id() / num_blocks_per_batch);
@@ -70,6 +71,20 @@ __global__ void
                                                   b_element_op,
                                                   c_element_op,
                                                   block_2_ctile_map);
+#else
+    ignore = p_a_grid;
+    ignore = p_b_grid;
+    ignore = p_c_grid;
+    ignore = batch_count;
+    ignore = a_grid_desc_k0_m_k1;
+    ignore = b_grid_desc_k0_n_k1;
+    ignore = c_grid_desc_m0_n0_m1_n1_m2_m3_m4_n2;
+    ignore = a_element_op;
+    ignore = b_element_op;
+    ignore = c_element_op;
+    ignore = compute_base_ptr_of_batch_;
+    ignore = block_2_ctile_map;
+#endif // end of if (defined(__gfx908__) || defined(__gfx90a__))
 }
 
 template <typename ADataType,
