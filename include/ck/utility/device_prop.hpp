6baedf3fc7da0f04137db75e04b93f20c519afd4
j4yan <jfyan008@gmail.com>
2022-04-19 21:04:57 -0500

    minor changes

--- include/ck/utility/device_prop.hpp
+++ include/ck/utility/device_prop.hpp
@@ -7,6 +7,8 @@ namespace ck {
 std::string get_device_name()
 {
     hipDeviceProp_t props{};
+    int device;
+    hipGetDevice(&device);
     hipGetDeviceProperties(&props, device);
     const std::string name(props.gcnArchName);
 
