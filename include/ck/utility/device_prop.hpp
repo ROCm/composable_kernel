1bcb8cd537c756775b32062aaf9986759a3387ba
j4yan <jfyan008@gmail.com>
2022-04-19 21:06:20 -0500

    minor changes

--- include/ck/utility/device_prop.hpp
+++ include/ck/utility/device_prop.hpp
@@ -9,6 +9,11 @@ std::string get_device_name()
     hipDeviceProp_t props{};
     int device;
     hipGetDevice(&device);
+    if(status != hipSuccess)
+    {
+        return std::string();
+    }
+
     hipGetDeviceProperties(&props, device);
     const std::string name(props.gcnArchName);
 
