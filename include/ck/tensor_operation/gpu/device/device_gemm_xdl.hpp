c695dfa1a8784182ee5e513fd84e18ead7f41a8e
j4yan <jfyan008@gmail.com>
2022-04-21 15:33:49 -0500

    minor changes

--- include/ck/tensor_operation/gpu/device/device_gemm_xdl.hpp
+++ include/ck/tensor_operation/gpu/device/device_gemm_xdl.hpp
@@ -404,20 +404,18 @@ struct DeviceGemmXdl
 
     static bool IsSupportedArgument(const Argument& arg)
     {
-        std::cout << ck::get_device_name() << std::endl;
-
-        // if (ck::get_device_name() == "gfx1030")
-        // {
-        // return GridwiseGemm::CheckValidity(arg.a_grid_desc_k0_m_k1_,
-        //                                    arg.b_grid_desc_k0_n_k1_,
-        //                                    arg.c_grid_desc_m_n_,
-        //                                    arg.M01_,
-        //                                    arg.N01_);
-        // }
-        // else
-        // {
-        //     return false;
-        // }
+        if (ck::get_device_name() == "gfx1030")
+        {
+            return false;
+        }
+        else
+        {
+            return GridwiseGemm::CheckValidity(arg.a_grid_desc_k0_m_k1_,
+                                               arg.b_grid_desc_k0_n_k1_,
+                                               arg.c_grid_desc_m_n_,
+                                               arg.M01_,
+                                               arg.N01_);
+        }
     }
 
     // polymorphic
