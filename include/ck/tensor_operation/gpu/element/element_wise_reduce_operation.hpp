fbede07a01b8ed1a6b2ebf6c0ae345e8bb9df927
Qianfeng <qianfeng.zhang@amd.com>
2022-04-30 00:35:25 +0800

    Update to gemm_reduce and batched_gemm_reduce (#213)
    * [Experimental] Change to gemm+reduce and batched-gemm+reduce
    
    * Use threadwise-reduce function to improve the gridwise_gemm_reduce_xdl_cshuffle kernel
    
    * Tiny fix in device_batched_gemm_xdl.hpp
    
    * clang-format library/src/utility/conv_fwd_util.cpp
    
    [ROCm/composable_kernel commit: c77ae65d40b1316dac02c4decf02d8517c840be2]

--- include/ck/tensor_operation/gpu/element/element_wise_reduce_operation.hpp
+++ include/ck/tensor_operation/gpu/element/element_wise_reduce_operation.hpp
@@ -5,20 +5,6 @@ namespace ck {
 namespace tensor_operation {
 namespace element_wise {
 
-struct ReduceSum
-{
-    __host__ __device__ static constexpr float GetReduceZeroValue() { return float(0); }
-
-    __host__ __device__ void Reduce(float& acc, float v) const { acc += v; }
-};
-
-struct ReduceSquareSum
-{
-    __host__ __device__ static constexpr float GetReduceZeroValue() { return float(0); }
-
-    __host__ __device__ void Reduce(float& acc, float v) const { acc += v * v; }
-};
-
 } // namespace element_wise
 } // namespace tensor_operation
 } // namespace ck
