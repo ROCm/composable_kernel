d7dca21c347fd6ffba13bfeb83445ba5dc12ac4d
rocking <chunylai@amd.com>
2022-05-19 00:18:21 +0800

    Support double

--- include/ck/tensor_operation/gpu/element/binary_element_wise_operation.hpp
+++ include/ck/tensor_operation/gpu/element/binary_element_wise_operation.hpp
@@ -8,6 +8,12 @@ namespace binary_element_wise {
 struct Add
 {
     __host__ __device__ constexpr void
+    operator()(double& dst, const double& src1, const double& src2) const
+    {
+        dst = src1 + src2;
+    }
+
+    __host__ __device__ constexpr void
     operator()(float& dst, const float& src1, const float& src2) const
     {
         dst = src1 + src2;
