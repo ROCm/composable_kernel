ee060994463e472cf64010d87f5ba3fc3b0c624e
myamlak <myamlak@gmail.com>
2022-05-24 14:26:22 +0000

    Review remarks from aosew addressed

--- library/include/ck/library/reference_tensor_operation/cpu/reference_cgemm.hpp
+++ library/include/ck/library/reference_tensor_operation/cpu/reference_cgemm.hpp
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 #pragma once
 #include <iostream>
 #include <sstream>
@@ -59,14 +84,14 @@ struct ReferenceCGemm : public device::BaseOperator
 
         float Run(const Argument& arg)
         {
+            const std::size_t K = arg.a_m_k_real_.mDesc.GetLengths()[1];
+
+            if(K != arg.a_m_k_imag_.mDesc.GetLengths()[1])
+            {
+                throw std::runtime_error("wrong! Incompatible real and imag sizes in CGEMM");
+            }
+
             auto f_mk_kn_mn_real = [&](auto m, auto n) {
-                const std::size_t K = arg.a_m_k_real_.mDesc.GetLengths()[1];
-
-                if(K != arg.a_m_k_imag_.mDesc.GetLengths()[1])
-                {
-                    throw std::runtime_error("wrong! Incompatible real and imag sizes in CGEMM");
-                }
-
                 float v_acc = 0;
 
                 for(std::size_t k = 0; k < K; ++k)
@@ -92,13 +117,6 @@ struct ReferenceCGemm : public device::BaseOperator
             };
 
             auto f_mk_kn_mn_imag = [&](auto m, auto n) {
-                const std::size_t K = arg.a_m_k_real_.mDesc.GetLengths()[1];
-
-                if(K != arg.a_m_k_imag_.mDesc.GetLengths()[1])
-                {
-                    throw std::runtime_error("wrong! Incompatible real and imag sizes in CGEMM");
-                }
-
                 float v_acc = 0;
 
                 for(std::size_t k = 0; k < K; ++k)
