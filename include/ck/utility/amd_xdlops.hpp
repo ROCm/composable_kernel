3e6c2610ae9256dc7e4118dbf2074e97487babe3
ltqin <letao.qin@amd.com>
2022-05-27 03:48:57 +0800

    Add FP64 XDL GEMM built-in function (#199)
    * add intrin_mfma_f64_16x16x4f64
    
    * add example
    
    * gemm reference add double data type
    
    * chang init data
    
    * fix M N PerXdlops
    
    * fix ifdef
    
    * add comparsion config
    
    * add conv fwd example
    
    * format log out
    
    * change rc matrix egister layout
    
    * reorganize example
    
    * reorganize example 2
    
    * format,because merge develop
    
    * fix call impl adding acc data type
    
    * lost ;
    
    * add compiler warning
    
    * change example tunning parameters
    
    * add test for fp64
    
    * add instance
    
    * add test/gemm/gemm_fp64.cpp
    
    * fix get name issue
    
    * remove some tunning parameter
    
    * fix conflict
    
    * format
    
    * use integer value for GEMM test
    
    * add acc data type
    
    * remove typeid because fp16
    
    * fix streamconfig etc bug from merging develop
    
    * format
    
    * remove test_gemm_xdl_fp64
    
    * add AccDataType
    
    * AccDataType problem
    
    Co-authored-by: qinletao <letaoqin@amd.com>
    Co-authored-by: Chao Liu <chao.liu2@amd.com>

--- include/ck/utility/amd_xdlops.hpp
+++ include/ck/utility/amd_xdlops.hpp
@@ -294,5 +294,24 @@ struct intrin_mfma_i32_16x16x16i8<16, 16>
     }
 };
 
+template <index_t MPerWave, index_t NPerWave>
+struct intrin_mfma_f64_16x16x4f64;
+
+template <>
+struct intrin_mfma_f64_16x16x4f64<16, 16>
+{
+    template <class FloatC>
+    __device__ static void Run(const double& reg_a, const double& reg_b, FloatC& reg_c)
+    {
+#ifdef __gfx90a__
+        reg_c.template AsType<double4_t>()(Number<0>{}) = __builtin_amdgcn_mfma_f64_16x16x4f64(
+            reg_a, reg_b, reg_c.template AsType<double4_t>()[Number<0>{}], 0, 0, 0);
+#else
+        ignore = reg_a;
+        ignore = reg_b;
+        ignore = reg_c;
+#endif
+    }
+};
 } // namespace ck
 #endif
