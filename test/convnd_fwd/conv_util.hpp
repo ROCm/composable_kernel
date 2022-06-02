bc99d67e3a3113d0a2c523a47a81facefcf4329e
Adam Osewski <aosewski@amd.com>
2022-06-02 11:42:15 +0200

    Refine test instances.
    * Properly set accumulator data type.
    * Add another "big" instance.

--- test/convnd_fwd/conv_util.hpp
+++ test/convnd_fwd/conv_util.hpp
@@ -4,6 +4,7 @@
 #include <tuple>
 
 #include "config.hpp"
+#include "data_type.hpp"
 #include "device_convnd_fwd_xdl_nhwc_kyxc_nhwk.hpp"
 #include "element_wise_operation.hpp"
 #include "host_tensor.hpp"
@@ -26,13 +27,13 @@ static constexpr auto ConvFwdDefault =
     ck::tensor_operation::device::ConvolutionForwardSpecialization::Default;
 
 template <ck::index_t SpatialDims, typename InDataType, typename WeiDataType, typename OutDataType>
-using DeviceConvNDFwdInstance = ck::tensor_operation::device::
+using DeviceConvNDFwdInstanceSmallBlockSize = ck::tensor_operation::device::
     DeviceConvNDFwdXdl_Input_N_Hi_Wi_C_Weight_K_Y_X_C_Output_N_Ho_Wo_K<
         // clang-format off
         InDataType,         // 
         WeiDataType,        //
         OutDataType,        //
-        InDataType,         // 
+        typename ck::AccumulatorDataType<InDataType>::type, // Accumulator data type.
         InElementOp,        // Input Elementwise Operation
         WeiElementOp,       // Weights Elementwise Operation
         OutElementOp,       // Output Elementwise Operation
@@ -65,14 +66,67 @@ using DeviceConvNDFwdInstance = ck::tensor_operation::device::
         1>;                 // CThreadTransferDstScalarPerVector
 // clang-format on
 
+// This works for fp16
+template <ck::index_t SpatialDims, typename InDataType, typename WeiDataType, typename OutDataType>
+using DeviceConvNDFwdInstanceBigBlockSize = ck::tensor_operation::device::
+    DeviceConvNDFwdXdl_Input_N_Hi_Wi_C_Weight_K_Y_X_C_Output_N_Ho_Wo_K<
+        // clang-format off
+        InDataType,         // 
+        WeiDataType,        //
+        OutDataType,        //
+        // typename ck::AccumulatorDataType<InDataType>::type, // Accumulator data type.
+        float,
+        InElementOp,        // Input Elementwise Operation
+        WeiElementOp,       // Weights Elementwise Operation
+        OutElementOp,       // Output Elementwise Operation
+        ConvFwdDefault,     // ConvForwardSpecialization
+        SpatialDims,        // SptialDims
+        256,                // BlockSize
+        128,                // MPerBlock
+        256,                // NPerBlock
+        4,                  // K0PerBlock
+        8,                  // K1
+        32,                 // MPerXdl
+        32,                 // NPerXdl
+        2,                  // MXdlPerWave
+        4,                  // NXdlPerWave
+        S<4, 64, 1>,        // ABlockTransferThreadClusterLengths_K0_M_K1
+        S<1, 0, 2>,         // ABlockTransferThreadClusterArrangeOrder
+        S<1, 0, 2>,         // ABlockTransferSrcAccessOrder
+        2,                  // ABlockTransferSrcVectorDim
+        8,                  // ABlockTransferSrcScalarPerVector
+        8,                  // ABlockTransferDstScalarPerVector_K1
+        true,               // ABlockLdsAddExtraM
+        S<4, 64, 1>,        // BBlockTransferThreadClusterLengths_K0_N_K1
+        S<1, 0, 2>,         // BBlockTransferThreadClusterArrangeOrder
+        S<1, 0, 2>,         // BBlockTransferSrcAccessOrder
+        2,                  // BBlockTransferSrcVectorDim
+        8,                  // BBlockTransferSrcScalarPerVector
+        8,                  // BBlockTransferDstScalarPerVector_K1
+        true,               // BBlockLdsAddExtraN
+        7,                  // CThreadTransferSrcDstVectorDim
+        1>;                // CThreadTransferDstScalarPerVector
+// clang-format on
+
 template <ck::index_t NDim,
           typename InDataType  = float,
           typename WeiDataType = float,
           typename OutDataType = float>
-void get_test_convolution_fwd_instance(std::vector<DeviceConvFwdNoOpPtr>& instances)
+void get_test_convolution_fwd_instance(std::vector<DeviceConvFwdNoOpPtr>& instances,
+                                       bool small_block_size = true)
 {
-    using ConvInstanceT = DeviceConvNDFwdInstance<NDim, InDataType, WeiDataType, OutDataType>;
-    instances.emplace_back(std::make_unique<ConvInstanceT>());
+    if(small_block_size)
+    {
+        using ConvInstanceT =
+            DeviceConvNDFwdInstanceSmallBlockSize<NDim, InDataType, WeiDataType, OutDataType>;
+        instances.emplace_back(std::make_unique<ConvInstanceT>());
+    }
+    else
+    {
+        using ConvInstanceT =
+            DeviceConvNDFwdInstanceBigBlockSize<NDim, InDataType, WeiDataType, OutDataType>;
+        instances.emplace_back(std::make_unique<ConvInstanceT>());
+    }
 }
 
 } // namespace conv
