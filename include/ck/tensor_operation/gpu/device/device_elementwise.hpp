49a719dd7bd9b36802619304c117b17f848a8372
rocking <chunyu.lai@amd.com>
2022-06-16 11:06:16 +0000

    Extract base class for elementwise

---
+++ include/ck/tensor_operation/gpu/device/device_elementwise.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "device_base.hpp"
+
+namespace ck {
+namespace tensor_operation {
+namespace device {
+
+template <typename ElementwiseFunctor>
+struct DeviceElementwise : public BaseOperator
+{
+    virtual std::unique_ptr<BaseArgument>
+    MakeArgumentPointer(const void* p_input,
+                        void* p_output,
+                        std::vector<index_t> lengths,
+                        std::vector<std::vector<index_t>> input_strides,
+                        std::vector<std::vector<index_t>> output_strides,
+                        ElementwiseFunctor functor) = 0;
+
+    virtual std::unique_ptr<BaseInvoker> MakeInvokerPointer() = 0;
+};
+
+template <typename ElementwiseFunctor>
+using DeviceElementwisePtr = std::unique_ptr<DeviceElementwise<ElementwiseFunctor>>;
+
+} // namespace device
+} // namespace tensor_operation
+} // namespace ck
