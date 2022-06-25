d105364276f782ecea0dc7c17393ea9a67ddd171
Chao Liu <chao.liu2@amd.com>
2022-06-25 02:25:24 +0000

    add license in file

--- library/src/host_tensor/device_memory.cpp
+++ library/src/host_tensor/device_memory.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
+
 #include "ck/device_utility/hip_check_error.hpp"
 #include "ck/library/host_tensor/device_memory.hpp"
 
