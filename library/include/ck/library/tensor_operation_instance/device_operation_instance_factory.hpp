1deb01b2baf66a683b09a0225946f701a49eac7c
Chao Liu <chao.liu2@amd.com>
2022-07-06 03:17:22 -0400

    contraction+scale

--- library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
+++ library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
@@ -16,6 +16,8 @@ using F32  = float;
 using F16  = ck::half_t;
 using BF16 = ck::bhalf_t;
 
+using EMPTY_TUPLE = ck::Tuple<>;
+
 using F16_TUPLE     = ck::Tuple<F16>;
 using F16_F16_TUPLE = ck::Tuple<F16, F16>;
 
@@ -25,6 +27,7 @@ using Row = ck::tensor_layout::gemm::RowMajor;
 using Col = ck::tensor_layout::gemm::ColumnMajor;
 
 using PassThrough    = ck::tensor_operation::element_wise::PassThrough;
+using Scale          = ck::tensor_operation::element_wise::Scale;
 using Bilinear       = ck::tensor_operation::element_wise::Bilinear;
 using AddAddFastGelu = ck::tensor_operation::element_wise::AddAddFastGelu;
 
