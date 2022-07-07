4fe9c393b81914a8f66517b3eab5fbe926d837ab
Chao Liu <chao.liu2@amd.com>
2022-07-07 14:31:11 -0500

    N-D Tensor Contraction example, instance, and client example (#270)
    * adding contraction
    
    * add contraction example
    
    * update examle
    
    * update example
    
    * format
    
    * update readme
    
    * clean header
    
    * clean header
    
    * contraction with multiple D
    
    * rename
    
    * fix naming issue; add instances for contraction+bilinear
    
    * change assumed virtual layout of contraction; add client example
    
    * update example
    
    * update
    
    * contraction+scale
    
    * use type_convert
    
    * rename

--- include/ck/ck.hpp
+++ include/ck/ck.hpp
@@ -102,7 +102,12 @@
 #define CK_EXPERIMENTAL_STATIC_TENSOR_DESCRIPTOR 0
 
 // experimental feature: buffer load/store/atomic-add/ OOB trick
+// This (ifndef) is a hack to use customized behavior for buffer load rather than using default
+// setting Don't use this hack unless absolutely necessary!
+// FIXME: make the behavior of buffer load a configurable (template) parameter of each device op
+#ifndef CK_EXPERIMENTAL_USE_BUFFER_LOAD_OOB_CHECK_OFFSET_TRICK
 #define CK_EXPERIMENTAL_USE_BUFFER_LOAD_OOB_CHECK_OFFSET_TRICK 0
+#endif
 #define CK_EXPERIMENTAL_USE_BUFFER_STORE_OOB_CHECK_OFFSET_TRICK 1
 #define CK_EXPERIMENTAL_USE_BUFFER_ATOMIC_ADD_OOB_CHECK_OFFSET_TRICK 1
 #define CK_EXPERIMENTAL_USE_BUFFER_ATOMIC_MAX_OOB_CHECK_OFFSET_TRICK 1
