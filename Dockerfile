a694e9fe6a2b2fc727ee4b03528e97dedf29a092
Illia Silin <98187287+illsilin@users.noreply.github.com>
2022-07-13 07:27:43 -0700

    Add switch between compilers, make 9110 compiler default, add full QA scripts. (#322)
    * adding scripts for full perf test suite
    
    * uncomment the sql queries
    
    * fix typo and chmod a+x for scripts
    
    * dos2unix for all new scripts
    
    * disable verification in full performance test
    
    * fix reduction scripts, add gfrouped_gemm hotfix
    
    * fix the grouped_gemm hotfix and only run reduction for fp16
    
    * change compiler flag syntax
    
    * fix syntax
    
    * add predefinition of dockerArgs
    
    * avoid redefinitions of dockerArgs
    
    * add blank space at the end of dockerArgs
    
    * try to build with release compiler
    
    * adding spaces inside if condition
    
    * limit the number of threads for building 9110 compiler
    
    * change the way HIP_CLANG_PATH is set
    
    * remove the export command
    
    * change the conditional ENV syntax
    
    * set HIP_CLANG_PATH at docker run time
    
    * update scripts for full qa
    
    * enable the sql write query
    
    * fix typo
    
    * remove a comment from a script
    
    [ROCm/composable_kernel commit: 39acaea36d7f9af41f3587bad9fba3dd5d370426]

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:18.04
 
 ARG ROCMVERSION=5.1
 ARG OSDB_BKC_VERSION
+ARG compiler_version
 
 RUN set -xe
 
@@ -93,3 +94,19 @@ RUN groupadd -f render
 RUN git clone -b master https://github.com/RadeonOpenCompute/rocm-cmake.git  && \
   cd rocm-cmake && mkdir build && cd build && \
   cmake  .. && cmake --build . && cmake --build . --target install
+
+WORKDIR /
+
+ENV compiler_version=$compiler_version
+RUN sh -c "echo compiler version = '$compiler_version'"
+
+RUN if [ "$compiler_version" = "9110" ]; then \
+        git clone -b ck-9110 https://github.com/RadeonOpenCompute/llvm-project.git && \
+        cd llvm-project && mkdir build && cd build && \
+        cmake -DCMAKE_INSTALL_PREFIX=/opt/rocm/llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=1 -DLLVM_TARGETS_TO_BUILD="AMDGPU;X86" -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt" ../llvm && \
+        make -j 8 ; \
+    else echo "using the release compiler"; \
+    fi
+
+#ENV HIP_CLANG_PATH='/llvm-project/build/bin'
+#RUN sh -c "echo HIP_CLANG_PATH = '$HIP_CLANG_PATH'"
---
+++ script/profile_gemm_bias_relu_add.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+ 
+## GPU visibility
+export HIP_VISIBLE_DEVICES=0
+DRIVER="../build/bin/ckProfiler"
+OP=$1
+DATATYPE=$2
+LAYOUT=$3
+VERIFY=$4
+INIT=$5
+LOG=$6
+REPEAT=$7
+ 
+########  op  datatype  layout  verify  init  log  repeat  M___ N___ K___  StrideA StrideB StrideC StrideC1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT  960  1024 1024       -1     -1      -1        -1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 1920  2048 2048       -1     -1      -1        -1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 3840  4096 4096       -1     -1      -1        -1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 7680  8192 8192       -1     -1      -1        -1
+ 
+#######  op  datatype  layout  verify  init  log  repeat  M___ N___ K___  StrideA StrideB StrideC StrideC1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 1024  1024 1024     1024    1024    1024     1024
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 2048  2048 2048     2048    2048    2048     2048
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 4096  4096 4096     4096    4096    4096     4096
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 8192  8192 8192     8192    8192    8192     8192
+ 
+#######  op  datatype  layout  verify  init  log  repeat  M___ N___ K___  StrideA StrideB StrideC StrideC1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 1024  1024 1024     1056    1056    1056     1056
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 2048  2048 2048     2080    2080    2080     2080
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 4096  4096 4096     4128    4128    4128     4128
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 8192  8192 8192     8224    8224    8224     8224
+ 
+#######  op  datatype  layout  verify  init  log  repeat  M___ N___ K___  StrideA StrideB StrideC StrideC1
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 1024  1024 1024     1088    1088    1088     1088
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 2048  2048 2048     2112    2112    2112     2112
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 4096  4096 4096     4160    4160    4160     4160
+$DRIVER $OP $DATATYPE $LAYOUT $VERIFY $INIT $LOG $REPEAT 8192  8192 8192     8256    8256    8256     8256
