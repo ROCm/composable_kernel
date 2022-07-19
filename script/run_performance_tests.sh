906340e61ec00008e1c7a738fd429b7cc0a5e904
illsilin <Illia.Silin@amd.com>
2022-07-19 16:36:47 -0700

    try to fix git call

--- script/run_performance_tests.sh
+++ script/run_performance_tests.sh
@@ -18,13 +18,14 @@ echo 'GPU architecture: ' $gpu_arch
 
 function print_log_header(){
 	rm -f $1;
-	git status | grep -e 'On branch' > $1;
-	echo -n 'Node name: ' >>$1; hostname >> $1;
+	GIT='git --git-dir='/composable_kernel'/.git'
+	$GIT status | grep -e 'On branch' &> $1;
+	echo -n 'Node name: ' >> $1; hostname >> $1;
 	#get GPU_arch and number of compute units from rocminfo
 	echo -n "GPU_arch: " >> $1; rocminfo | grep "Name:" | grep "gfx" >> $1;
 	rocminfo | grep "Compute Unit:" >> $1;
 	hipcc --version | grep -e 'HIP version'  >> $1;
-	echo 'Environment type: ' $2 >>$1;
+	echo 'Environment type: ' $2 >> $1;
 	/opt/rocm/bin/amdclang++ --version | grep -e 'InstalledDir' >> $1;
 }
 #run gemm tests
