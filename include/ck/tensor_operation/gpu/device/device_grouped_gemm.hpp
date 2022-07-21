cdb627bf1b977ad3aa6d0913359679a48d9227c0
zjing14 <zhangjing14@gmail.com>
2022-07-21 10:07:01 -0500

    Grouped Gemm device with multiD grid (#319)
    * replace gridwise_v2r3 with multiD
    
    * adjust parameters
    
    * add instances
    
    * fixed test_grouped_gemm
    
    * fix standalone softmax race condition around blockwise reduction
    
    * fixed ci
    
    * fixed comment: remove redundant workspace
    
    * use instanceFactory
    
    * add test layout
    
    * add empty Ds
    
    * add bias example
    
    * use array
    
    * sperate examples
    
    Co-authored-by: Anthony Chang <ac.chang@outlook.com>
    
    [ROCm/composable_kernel commit: 7959dad5666918bc403eb01064fa9a697ae1b473]

---
+++ include/ck/tensor_operation/gpu/device/device_grouped_gemm.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+#include "device_base.hpp"
+
+namespace ck {
+namespace tensor_operation {
+namespace device {
+
+struct GemmDesc
+{
+    ck::index_t M_, N_, K_;
+    ck::index_t stride_A_, stride_B_, stride_C_;
+
+    std::vector<ck::index_t> stride_Ds_;
+};
+
+template <typename ALayout,
+          typename BLayout,
+          typename DELayout,
+          typename ADataType,
+          typename BDataType,
+          typename DsDataType,
+          typename EDataType,
+          typename AElementwiseOperation,
+          typename BElementwiseOperation,
+          typename CElementwiseOperation>
+struct DeviceGroupedGemm : public BaseOperator
+{
+    static constexpr index_t NumDTensor = DsDataType::Size();
+
+    virtual std::unique_ptr<BaseArgument>
+    MakeArgumentPointer(std::vector<const void*>& p_a,
+                        std::vector<const void*>& p_b,
+                        std::vector<std::array<const void*, NumDTensor>>& p_ds,
+                        std::vector<void*>& p_e,
+                        std::vector<GemmDesc>& gemm_desc,
+                        AElementwiseOperation a_element_op,
+                        BElementwiseOperation b_element_op,
+                        CElementwiseOperation c_element_op) = 0;
+
+    virtual std::unique_ptr<BaseInvoker> MakeInvokerPointer() = 0;
+};
+
+template <typename ALayout,
+          typename BLayout,
+          typename DELayout,
+          typename ADataType,
+          typename BDataType,
+          typename DsDataType,
+          typename EDataType,
+          typename AElementwiseOperation,
+          typename BElementwiseOperation,
+          typename CElementwiseOperation>
+using DeviceGroupedGemmPtr = std::unique_ptr<DeviceGroupedGemm<ALayout,
+                                                               BLayout,
+                                                               DELayout,
+                                                               ADataType,
+                                                               BDataType,
+                                                               DsDataType,
+                                                               EDataType,
+                                                               AElementwiseOperation,
+                                                               BElementwiseOperation,
+                                                               CElementwiseOperation>>;
+
+} // namespace device
+} // namespace tensor_operation
+} // namespace ck
