1cb3339efb8cb34d3a7a63522f579e2e4d2cd56c
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-03 03:05:40 -0400

    Remove unused include directive

--- library/include/ck/library/utility/literals.hpp
+++ library/include/ck/library/utility/literals.hpp
@@ -3,9 +3,7 @@
 
 #pragma once
 
-#include <type_traits>
-
-// Literal Suffix for (signed) size_t (C++23)
+// [P0330] Literal Suffix for (signed) size_t (C++23)
 // ref: https://wg21.link/p0330r8
 constexpr std::size_t operator""_uz(unsigned long long size)
 {
