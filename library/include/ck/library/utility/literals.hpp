17ac1c10aa1873f00005e53d1345d03f86b0a196
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-03 03:43:38 -0400

    Add 'inline' specifier to literal functions

--- library/include/ck/library/utility/literals.hpp
+++ library/include/ck/library/utility/literals.hpp
@@ -5,12 +5,12 @@
 
 // [P0330] Literal Suffix for (signed) size_t (C++23)
 // ref: https://wg21.link/p0330r8
-constexpr std::size_t operator""_uz(unsigned long long size)
+inline constexpr std::size_t operator""_uz(unsigned long long size)
 {
     return static_cast<std::size_t>(size);
 }
 
-constexpr std::size_t operator""_zu(unsigned long long size)
+inline constexpr std::size_t operator""_zu(unsigned long long size)
 {
     return static_cast<std::size_t>(size);
 }
