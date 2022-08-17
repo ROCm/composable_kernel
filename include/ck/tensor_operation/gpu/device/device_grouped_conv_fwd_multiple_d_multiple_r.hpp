f77601b403bf90eab0a51f6759e0e92ce1b080fd
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-17 12:01:55 -0400

    Update DeviceGroupedConvFwdMultipleDMultipleR definition

--- include/ck/tensor_operation/gpu/device/device_grouped_conv_fwd_multiple_d_multiple_r.hpp
+++ include/ck/tensor_operation/gpu/device/device_grouped_conv_fwd_multiple_d_multiple_r.hpp
@@ -16,29 +16,38 @@ namespace device {
 //   input : weight B[G, K, C, Y, X],
 //   input : D0[G, N, K, Ho, Wo], D1[G, N, K, Ho, Wo], ...
 //   output : output image E[G, N, K, Ho, Wo]
+//   output : R0[G], R1[G], ...
 //   C = a_op(A) * b_op(B)
 //   E = cde_op(C, D0, D1, ...)
+//   Q0 = reduce0(q_op0(E)), Q1 = reduce1(q_op0(E)), ...
+//   R0 = r_op0(Q0), R1 = r_op1(Q1), ...
+// Assume:
+//   D0, D1, ... and E have the same layout
 template <index_t NDimSpatial,
           typename ALayout,
           typename BLayout,
-          typename DsLayout,
-          typename ELayout,
+          typename DELayout,
           typename ADataType,
           typename BDataType,
           typename DsDataType,
           typename EDataType,
+          typename RsDataType,
           typename AElementwiseOperation,
           typename BElementwiseOperation,
-          typename CDEElementwiseOperation>
+          typename CDEElementwiseOperation,
+          typename QsElementwiseOperation,
+          typename RsElementwiseOperation>
 struct DeviceGroupedConvFwdMultipleDMultipleR : public BaseOperator
 {
     static constexpr index_t NumDTensor = DsDataType::Size();
+    static constexpr index_t NumRTensor = RsDataType::Size();
 
     virtual std::unique_ptr<BaseArgument> MakeArgumentPointer(
         const void* p_a,
         const void* p_b,
         const std::array<const void*, NumDTensor>& p_ds,
         void* p_e,
+        std::array<void*, NumRTensor> p_rs,
         const std::array<index_t, NDimSpatial + 3>& a_g_n_c_wis_lengths,
         const std::array<index_t, NDimSpatial + 3>& a_g_n_c_wis_strides,
         const std::array<index_t, NDimSpatial + 3>& b_g_k_c_xs_lengths,
@@ -53,7 +62,9 @@ struct DeviceGroupedConvFwdMultipleDMultipleR : public BaseOperator
         const std::array<index_t, NDimSpatial>& input_right_pads,
         const AElementwiseOperation& a_element_op,
         const BElementwiseOperation& b_element_op,
-        const CDEElementwiseOperation& cde_element_op) = 0;
+        const CDEElementwiseOperation& cde_element_op,
+        const QsElementwiseOperation& qs_element_op,
+        const RsElementwiseOperation& rs_element_op) = 0;
 
     virtual std::unique_ptr<BaseInvoker> MakeInvokerPointer() = 0;
 };
