51d0c6794c80e21a4ebc03dc2094e810b4daa7eb
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-19 05:31:04 -0400

    Remove constraint of Tensor<>::CopyAsType()

--- library/include/ck/library/utility/host_tensor.hpp
+++ library/include/ck/library/utility/host_tensor.hpp
@@ -254,7 +254,7 @@ struct Tensor
     Tensor(const HostTensorDescriptor& desc) : mDesc(desc), mData(mDesc.GetElementSpaceSize()) {}
 
     template <typename OutT>
-    std::enable_if_t<std::is_convertible_v<T, OutT>, Tensor<OutT>> CopyAsType() const
+    Tensor<OutT> CopyAsType() const
     {
         Tensor<OutT> ret(mDesc);
         for(size_t i = 0; i < mData.size(); i++)
