3f190b0779ceedf7aaf0b380712fda0518de72c1
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-21 01:16:47 -0400

    Add new DeviceMem API to copy memory

--- library/src/utility/device_memory.cpp
+++ library/src/utility/device_memory.cpp
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
 
-#include "ck/host_utility/hip_check_error.hpp"
+#include <memory>
 
+#include "ck/host_utility/hip_check_error.hpp"
 #include "ck/library/utility/device_memory.hpp"
 
 DeviceMem::DeviceMem(std::size_t mem_size) : mMemSize(mem_size)
@@ -14,15 +15,37 @@ void* DeviceMem::GetDeviceBuffer() const { return mpDeviceBuf; }
 
 std::size_t DeviceMem::GetBufferSize() const { return mMemSize; }
 
-void DeviceMem::ToDevice(const void* p) const
+void DeviceMem::ToDeviceImpl(const void* p) const
 {
     hip_check_error(hipMemcpy(mpDeviceBuf, const_cast<void*>(p), mMemSize, hipMemcpyHostToDevice));
 }
 
-void DeviceMem::FromDevice(void* p) const
+void DeviceMem::FromDeviceImpl(void* p) const
 {
     hip_check_error(hipMemcpy(p, mpDeviceBuf, mMemSize, hipMemcpyDeviceToHost));
 }
+
+#ifdef CK_EXPERIMENTAL_BIT_INT_EXTENSION_INT4
+void DeviceMem::ToDevice(const ck::int4_t* host) const
+{
+    const std::size_t count = GetBufferSize();
+    const auto buffer       = std::make_unique<int8_t[]>(count);
+
+    std::copy_n(host, count, buffer.get());
+
+    ToDevice(buffer.get());
+}
+
+void DeviceMem::FromDevice(ck::int4_t* host) const
+{
+    const std::size_t count = GetBufferSize();
+    const auto buffer       = std::make_unique<int8_t[]>(count);
+
+    FromDevice(buffer.get());
+
+    std::copy_n(buffer.get(), count, host);
+}
+#endif
 
 void DeviceMem::SetZero() const { hip_check_error(hipMemset(mpDeviceBuf, 0, mMemSize)); }
 
