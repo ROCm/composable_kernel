a4f56e4d2d41fe9c698058ac3a118ed179d53eec
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-08-22 05:50:30 -0400

    Revert "Add new DeviceMem API to copy memory"
    This reverts commit 3f190b0779ceedf7aaf0b380712fda0518de72c1.

--- library/include/ck/library/utility/device_memory.hpp
+++ library/include/ck/library/utility/device_memory.hpp
@@ -3,11 +3,7 @@
 
 #pragma once
 
-#include <cassert>
-
 #include <hip/hip_runtime.h>
-
-#include "ck/utility/data_type.hpp"
 
 template <typename T>
 __global__ void set_buffer_value(T* p, T x, uint64_t buffer_element_size)
@@ -18,49 +14,18 @@ __global__ void set_buffer_value(T* p, T x, uint64_t buffer_element_size)
     }
 }
 
-class DeviceMem
+struct DeviceMem
 {
-    void ToDeviceImpl(const void* p) const;
-    void FromDeviceImpl(void* p) const;
-
-    public:
     DeviceMem() = delete;
     DeviceMem(std::size_t mem_size);
     void* GetDeviceBuffer() const;
     std::size_t GetBufferSize() const;
+    void ToDevice(const void* p) const;
+    void FromDevice(void* p) const;
     void SetZero() const;
     template <typename T>
     void SetValue(T x) const;
     ~DeviceMem();
-
-    template <typename T>
-    std::enable_if_t<!std::is_const_v<T> &&
-                     (std::is_same_v<T, int8_t> || std::is_same_v<T, int32_t> ||
-                      std::is_same_v<T, ck::bhalf_t> || std::is_same_v<T, ck::half_t> ||
-                      std::is_same_v<T, float> || std::is_same_v<T, double>)>
-    FromDevice(T* host) const
-    {
-        assert(device.GetBufferSize() % sizeof(T) == 0);
-
-        FromDeviceImpl(host);
-    }
-
-    template <typename T>
-    std::enable_if_t<std::is_same_v<T, int8_t> || std::is_same_v<T, int32_t> ||
-                     std::is_same_v<T, ck::bhalf_t> || std::is_same_v<T, ck::half_t> ||
-                     std::is_same_v<T, ck::half_t> || std::is_same_v<T, float> ||
-                     std::is_same_v<T, double>>
-    ToDevice(const T* host) const
-    {
-        assert(device.GetBufferSize() % sizeof(T) == 0);
-
-        ToDeviceImpl(host);
-    }
-
-#ifdef CK_EXPERIMENTAL_BIT_INT_EXTENSION_INT4
-    void ToDevice(const ck::int4_t* host) const;
-    void FromDevice(ck::int4_t* host) const;
-#endif
 
     void* mpDeviceBuf;
     std::size_t mMemSize;
