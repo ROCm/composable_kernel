183312116ded8f2f6b0bd5d8da1001e15c48e1c0
Anthony Chang <ac.chang@outlook.com>
2022-08-24 03:52:56 +0800

    Attention with output permutation (#370)
    * comment on specialization for TensorSpecialization::Packed
    
    * gemm_softmax_gemm with output permutation
    
    * scaling
    
    * refactor MatrixPadder; rename to GemmPadder
    
    * remove old sanity check
    
    * restore original gemm_softmax_gemm
    
    * revise comment in gemm_softmax_gemm example
    
    * use GetElementSpaceSize()
    
    * remove extra header
    
    * typo
    
    * remove archaic DeviceOpPtr
    
    [ROCm/composable_kernel commit: e0d8806ca1cd8611d387f1fb441d3c9e92174ec5]

--- include/ck/tensor_operation/gpu/grid/gridwise_batched_gemm_softmax_gemm_xdl_cshuffle_v1.hpp
+++ include/ck/tensor_operation/gpu/grid/gridwise_batched_gemm_softmax_gemm_xdl_cshuffle_v1.hpp
@@ -245,8 +245,6 @@ struct GridwiseBatchedGemmSoftmaxGemm_Xdl_CShuffle
             return false;
         }
 
-        assert(num_gemm1_k_outer_loop * num_gemm1_k_inner_loop == N / Gemm1KPerBlock);
-
         if(!block_2_ctile_map.CheckValidity(c_grid_desc_m_n))
         {
             return false;
