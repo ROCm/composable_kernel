83711bb69c6fd15f47bcee3799c13d14c6892a6f
Astha Rai <astha.rai713@gmail.com>
2022-08-28 02:33:01 +0000

    fixed formatting

--- example/35_tensor_permute/tensor_permute.cpp
+++ example/35_tensor_permute/tensor_permute.cpp
@@ -14,86 +14,90 @@ using F16 = ck::half_t;
 using F32 = float;
 
 using ADataType = F16;
-using BDataType  = F16;
+using BDataType = F16;
 
 using PassThrough = ck::tensor_operation::element_wise::PassThrough;
 using DeviceElementwisePermuteInstance =
     ck::tensor_operation::device::DeviceElementwise<ck::Tuple<ADataType>,
-                                                        ck::Tuple<BDataType>,
-							PassThrough,
-							4,
-							8,
-							ck::Sequence <8>,
-							ck::Sequence<8>>;
+                                                    ck::Tuple<BDataType>,
+                                                    PassThrough,
+                                                    4,
+                                                    8,
+                                                    ck::Sequence<8>,
+                                                    ck::Sequence<8>>;
 
 template <typename HostTensorA, typename HostTensorB, typename Functor>
-void host_elementwise4D(HostTensorB& B,const HostTensorA& A, const std::vector<std::size_t>& shape, Functor functor)
+void host_elementwise4D(HostTensorB& B,
+                        const HostTensorA& A,
+                        const std::vector<std::size_t>& shape,
+                        Functor functor)
 {
-	using btype = ck::remove_reference_t<decltype(B(0, 0, 0, 0))>;
-	for(std::size_t n = 0; n < shape[0]; ++n) 
-		for(std::size_t c = 0; c < shape[1]; ++c) 
-			for(std::size_t h = 0; h < shape[2]; ++h) 
-				for(std::size_t w = 0; w < shape[3]; ++w) { 
-					auto a_val  = A(n, c, h, w); 
-					btype b_val = 0; 
-					functor(b_val, a_val); 
-					B(n, h, w, c) = b_val; 
-				}
+    using btype = ck::remove_reference_t<decltype(B(0, 0, 0, 0))>;
+    for(std::size_t n = 0; n < shape[0]; ++n)
+        for(std::size_t c = 0; c < shape[1]; ++c)
+            for(std::size_t h = 0; h < shape[2]; ++h)
+                for(std::size_t w = 0; w < shape[3]; ++w)
+                {
+                    auto a_val  = A(n, c, h, w);
+                    btype b_val = 0;
+                    functor(b_val, a_val);
+                    B(n, h, w, c) = b_val;
+                }
 }
 
 int main()
-{ 
-	bool do_verification = true; 
-	bool time_kernel     = false;
+{
+    bool do_verification = true;
+    bool time_kernel     = false;
 
-	std::vector<std::size_t> nchw = {4, 16, 32, 32}; 
-	Tensor<ADataType> a(nchw);
-       	Tensor<BDataType> b(nchw); 
-	
-	a.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 1.0});
+    std::vector<std::size_t> nchw = {4, 16, 32, 32};
+    Tensor<ADataType> a(nchw);
+    Tensor<BDataType> b(nchw);
 
-	DeviceMem a_device_buf(sizeof(ADataType) * a.mDesc.GetElementSpaceSize());
-       	DeviceMem b_device_buf(sizeof(BDataType) * b.mDesc.GetElementSpaceSize());
+    a.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 1.0});
 
-	a_device_buf.ToDevice(a.mData.data());
+    DeviceMem a_device_buf(sizeof(ADataType) * a.mDesc.GetElementSpaceSize());
+    DeviceMem b_device_buf(sizeof(BDataType) * b.mDesc.GetElementSpaceSize());
 
-	std::array<const void*, 1> input = {a_device_buf.GetDeviceBuffer()};
-       	std::array<void*, 1> output      = {b_device_buf.GetDeviceBuffer()};
-       
-	std::array<ck::index_t, 4> ab_lengths;
-       	std::array<ck::index_t, 4> a_strides;
-       	std::array<ck::index_t, 4> b_strides;
+    a_device_buf.ToDevice(a.mData.data());
 
-	std::copy(nchw.begin(), nchw.end(), ab_lengths.begin());
-       	std::copy(a.mDesc.GetStrides().begin(), a.mDesc.GetStrides().end(), a_strides.begin());
-       	std::copy(b.mDesc.GetStrides().begin(), b.mDesc.GetStrides().end(), b_strides.begin());
+    std::array<const void*, 1> input = {a_device_buf.GetDeviceBuffer()};
+    std::array<void*, 1> output      = {b_device_buf.GetDeviceBuffer()};
 
-	auto broadcastPermute = DeviceElementwisePermuteInstance{};
-       	auto argument     = broadcastPermute.MakeArgumentPointer(
-		       	ab_lengths, {a_strides}, {c_strides}, input, output, PassThrough{});
-	
-	if(!broadcastPermute.IsSupportedArgument(argument.get()))
-       	{
-	       	throw std::runtime_error(
-			            "The runtime parameters seems not supported by the device instance, exiting!");
-	}; 
-	auto broadcastPermute_invoker_ptr = broadcastPermute.MakeInvokerPointer();
-       	float ave_time =
-	        broadcastPermute_invoker_ptr->Run(argument.get(), StreamConfig{nullptr, time_kernel}); 
-	
-	std::cout << "Perf: " << ave_time << " ms" << std::endl; 
-	
-	bool pass = true;    
+    std::array<ck::index_t, 4> ab_lengths;
+    std::array<ck::index_t, 4> a_strides;
+    std::array<ck::index_t, 4> b_strides;
 
-	if(do_verification)
-       	{
-	       	b_device_buf.FromDevice(b.mData.data());
-	       	Tensor<BDataType> host_b(nchw); 
-		host_elementwise4D<Tensor<ADataType>, Tensor<BDataType>, PassThrough>(
-			       	host_b, a, nchw, PassThrough{}); 
-		pass &=
-		       	ck::utils::check_err(b.mData, host_b.mData, "Error: Incorrect results b", 1e-3, 1e-3);
-       	} 
-	
-	return pass ? 0 : 1;
-} 
+    std::copy(nchw.begin(), nchw.end(), ab_lengths.begin());
+    std::copy(a.mDesc.GetStrides().begin(), a.mDesc.GetStrides().end(), a_strides.begin());
+    std::copy(b.mDesc.GetStrides().begin(), b.mDesc.GetStrides().end(), b_strides.begin());
+
+    auto broadcastPermute = DeviceElementwisePermuteInstance{};
+    auto argument         = broadcastPermute.MakeArgumentPointer(
+        ab_lengths, {a_strides}, {c_strides}, input, output, PassThrough{});
+
+    if(!broadcastPermute.IsSupportedArgument(argument.get()))
+    {
+        throw std::runtime_error(
+            "The runtime parameters seems not supported by the device instance, exiting!");
+    };
+    auto broadcastPermute_invoker_ptr = broadcastPermute.MakeInvokerPointer();
+    float ave_time =
+        broadcastPermute_invoker_ptr->Run(argument.get(), StreamConfig{nullptr, time_kernel});
+
+    std::cout << "Perf: " << ave_time << " ms" << std::endl;
+
+    bool pass = true;
+
+    if(do_verification)
+    {
+        b_device_buf.FromDevice(b.mData.data());
+        Tensor<BDataType> host_b(nchw);
+        host_elementwise4D<Tensor<ADataType>, Tensor<BDataType>, PassThrough>(
+            host_b, a, nchw, PassThrough{});
+        pass &=
+            ck::utils::check_err(b.mData, host_b.mData, "Error: Incorrect results b", 1e-3, 1e-3);
+    }
+
+    return pass ? 0 : 1;
+}
