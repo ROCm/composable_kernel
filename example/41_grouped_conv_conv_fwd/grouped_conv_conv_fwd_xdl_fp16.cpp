6c4041bc58cb45ddcae0bb03db83a0194b39affc
Chao Liu <lc.roy86@gmail.com>
2022-08-31 18:59:42 -0400

    adding int4/int8/fp16/bf16 for conv+conv and gemm+gemm

--- example/41_grouped_conv_conv_fwd/grouped_conv_conv_fwd_xdl_fp16.cpp
+++ example/41_grouped_conv_conv_fwd/grouped_conv_conv_fwd_xdl_fp16.cpp
@@ -1,11 +1,23 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
 
-#include "grouped_conv_conv_fwd_common.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <numeric>
+#include <type_traits>
 
+#include "ck/ck.hpp"
+#include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
 #include "ck/tensor_operation/gpu/device/device_batched_gemm_gemm_xdl_cshuffle.hpp"
+#include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
 
+#include "ck/library/utility/check_err.hpp"
+#include "ck/library/utility/device_memory.hpp"
+#include "ck/library/utility/host_tensor.hpp"
+#include "ck/library/utility/host_tensor_generator.hpp"
+#include "ck/library/utility/convolution_parameter.hpp"
 #include "ck/library/utility/convolution_host_tensor_descriptor_helper.hpp"
+#include "ck/library/reference_tensor_operation/cpu/reference_conv_fwd.hpp"
 
 using In0DataType       = ck::half_t;
 using Wei0DataType      = ck::half_t;
@@ -14,6 +26,9 @@ using Wei1DataType      = ck::half_t;
 using Acc1DataType      = float;
 using C1ShuffleDataType = float;
 using Out1DataType      = ck::half_t;
+
+// This is used for reference code
+using Out0DataType = ck::half_t;
 
 template <ck::index_t... Is>
 using S = ck::Sequence<Is...>;
@@ -88,117 +103,6 @@ using DeviceBatchedGemmGemmInstance =
         S<1, 32, 1, 8>, // CShuffleBlockTransferClusterLengths_MBlock_MPerBlock_NBlock_NPerBlock
         8>;             // CShuffleBlockTransferScalarPerVector_NPerBlock
 
-int main(int argc, char* argv[])
-{
-    bool do_verification = true;
-    int init_method      = 1;
-    bool time_kernel     = false;
+#include "run_grouped_conv_conv_fwd_example.inc"
 
-    ck::utils::conv::ConvParam conv0_param{
-        2, 1, 128, 512, 128, {1, 1}, {28, 28}, {1, 1}, {1, 1}, {0, 0}, {0, 0}};
-
-    ck::utils::conv::ConvParam conv1_param{
-        2, 1, 128, 128, 512, {1, 1}, {28, 28}, {1, 1}, {1, 1}, {0, 0}, {0, 0}};
-
-    if(argc == 1)
-    {
-        // use default case
-    }
-    else if(argc == 4)
-    {
-        do_verification = std::stoi(argv[1]);
-        init_method     = std::stoi(argv[2]);
-        time_kernel     = std::stoi(argv[3]);
-    }
-    else
-    {
-        printf("arg1: verification (0=no, 1=yes)\n");
-        printf("arg2: initialization (0=no init, 1=integer value, 2=decimal value)\n");
-        printf("arg3: time kernel (0=no, 1=yes)\n");
-        exit(0);
-    }
-
-    const auto in0_element_op  = In0ElementOp{};
-    const auto wei0_element_op = Wei0ElementOp{};
-    const auto wei1_element_op = Wei1ElementOp{};
-    const auto out0_element_op = Out0ElementOp{};
-    const auto out1_element_op = Out1ElementOp{};
-
-    const auto run = [&](auto ndim_spatial,
-                         auto in0_layout,
-                         auto wei0_layout,
-                         auto wei1_layout,
-                         auto out1_layout) {
-        constexpr ck::index_t ndim_spatial_value = ndim_spatial.value;
-
-        using In0Layout  = decltype(in0_layout);
-        using Wei0Layout = decltype(wei0_layout);
-        using Wei1Layout = decltype(wei1_layout);
-        using Out1Layout = decltype(out1_layout);
-
-        const auto in0_g_n_c_wis_desc =
-            ck::utils::conv::make_input_host_tensor_descriptor_g_n_c_wis_packed<In0Layout>(
-                conv0_param);
-
-        const auto wei0_g_k_c_xs_desc =
-            ck::utils::conv::make_weight_host_tensor_descriptor_g_k_c_xs_packed<Wei0Layout>(
-                conv0_param);
-
-        // out0 doesn't physical exist, any layout for host verification is OK
-        const auto out0_g_n_k_wos_desc =
-            ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<Out1Layout>(
-                conv0_param);
-
-        const auto wei1_g_k_c_xs_desc =
-            ck::utils::conv::make_weight_host_tensor_descriptor_g_k_c_xs_packed<Wei1Layout>(
-                conv1_param);
-
-        const auto out1_g_n_k_wos_desc =
-            ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<Out1Layout>(
-                conv1_param);
-
-        return run_grouped_conv_conv_fwd<ndim_spatial_value,
-                                         In0DataType,
-                                         Wei0DataType,
-                                         Acc0DataType,
-                                         Wei1DataType,
-                                         Out1DataType,
-                                         In0ElementOp,
-                                         Wei0ElementOp,
-                                         Out0ElementOp,
-                                         Wei1ElementOp,
-                                         Out1ElementOp,
-                                         DeviceBatchedGemmGemmInstance>(do_verification,
-                                                                        init_method,
-                                                                        time_kernel,
-                                                                        conv0_param,
-                                                                        conv1_param,
-                                                                        in0_g_n_c_wis_desc,
-                                                                        wei0_g_k_c_xs_desc,
-                                                                        out0_g_n_k_wos_desc,
-                                                                        wei1_g_k_c_xs_desc,
-                                                                        out1_g_n_k_wos_desc,
-                                                                        in0_element_op,
-                                                                        wei0_element_op,
-                                                                        wei1_element_op,
-                                                                        out0_element_op,
-                                                                        out1_element_op);
-    };
-
-    namespace ctc = ck::tensor_layout::convolution;
-
-    if(conv0_param.num_dim_spatial_ == 1)
-    {
-        run(ck::Number<1>{}, ctc::GNWC{}, ctc::GKXC{}, ctc::GKXC{}, ctc::GNWK{});
-    }
-    else if(conv0_param.num_dim_spatial_ == 2)
-    {
-        run(ck::Number<2>{}, ctc::GNHWC{}, ctc::GKYXC{}, ctc::GKYXC{}, ctc::GNHWK{});
-    }
-    else if(conv0_param.num_dim_spatial_ == 3)
-    {
-        run(ck::Number<3>{}, ctc::GNDHWC{}, ctc::GKZYXC{}, ctc::GKZYXC{}, ctc::GNDHWK{});
-    }
-
-    return 0;
-}
+int main(int argc, char* argv[]) { return run_grouped_conv_conv_fwd_example(argc, argv) ? 0 : 1; }
