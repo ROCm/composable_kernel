8c5c7a10b8ab1960b657b23811c1a366f624164f
carlushuang <carlus.huang@amd.com>
2022-09-05 11:34:31 +0000

    remove useless makefile

--- example/36_sparse_embedding/CMakeFiles/example_sparse_embedding3_forward_layernorm.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for example_sparse_embedding3_forward_layernorm.
-# This may be replaced when dependencies are built.
--- example/36_sparse_embedding/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carhuang/repo/composable_kernel/example/36_sparse_embedding
-# Build directory: /home/carhuang/repo/composable_kernel/example/36_sparse_embedding
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example_sparse_embedding3_forward_layernorm "/home/carhuang/repo/composable_kernel/bin/example_sparse_embedding3_forward_layernorm")
-set_tests_properties(example_sparse_embedding3_forward_layernorm PROPERTIES  _BACKTRACE_TRIPLES "/home/carhuang/repo/composable_kernel/example/CMakeLists.txt;12;add_test;/home/carhuang/repo/composable_kernel/example/36_sparse_embedding/CMakeLists.txt;1;add_example_executable;/home/carhuang/repo/composable_kernel/example/36_sparse_embedding/CMakeLists.txt;0;")
