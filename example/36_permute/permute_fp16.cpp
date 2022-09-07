b8abc3e33b6c7f1757844539e5a7f2aff37ca392
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-09-07 08:42:51 -0400

    Add comment to indicate template argument location

--- example/36_permute/permute_fp16.cpp
+++ example/36_permute/permute_fp16.cpp
@@ -6,8 +6,14 @@
 using ADataType = F16;
 using BDataType = F16;
 
-using DevicePermuteInstance =
-    ck::tensor_operation::device::DevicePermute<ADataType, BDataType, PassThrough, 4, 8, 8, 1>;
+// clang-format off
+using DevicePermuteInstance = ck::tensor_operation::device::DevicePermute
+// ######|    InData|   OutData| Elementwise| NumDim| MPerThread|  InScalar| OutScalar|
+// ######|      Type|      Type|   Operation|       |           | PerVector| PerVector|
+// ######|          |          |            |       |           |          |          |
+// ######|          |          |            |       |           |          |          |
+         < ADataType, BDataType, PassThrough,      4,          8,         8,         1>;
+// clang-format on
 
 #include "run_permute_example.inc"
 
