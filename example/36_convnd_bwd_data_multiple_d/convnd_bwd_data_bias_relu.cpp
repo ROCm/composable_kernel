0cbbeb2654041ce169a6793f983c2ef1aa3416a8
shaojiewang <wsjmessi@163.com>
2022-09-10 10:48:30 +0800

    format example file

--- example/36_convnd_bwd_data_multiple_d/convnd_bwd_data_bias_relu.cpp
+++ example/36_convnd_bwd_data_multiple_d/convnd_bwd_data_bias_relu.cpp
@@ -34,7 +34,7 @@ using DeviceConvNdBwdDataInstances = std::tuple<
                                       //############################################|            |           |            |            |            |                 |          |            |             |             |                   |      |      |      |      |   |     |     |     |     |                |               |               |               |               |               |          |                |               |               |              |               |               |          |            |            |                             |                |
         ck::tensor_operation::device::DeviceConvNdBwdDataNwcKxcNwkBiasActivation_Xdl< NDimSpatial, InDataType, WeiDataType, OutDataType, AccDataType, CShuffleDataType, DDataType, InElementOp, WeiElementOp, OutElementOp, ConvBwdDataDefault,   256,   256,   128,     4,  8,   32,   32,    4,    2,     S<4, 64, 1>,     S<1, 0, 2>,     S<1, 0, 2>,              2,              8,              8,      true,     S<4, 64, 1>,     S<2, 0, 1>,     S<0, 2, 1>,             1,              2,              8,      true, 1,           1,               S<1, 1, 32, 1, 1, 8>,              8>,
         ck::tensor_operation::device::DeviceConvNdBwdDataNwcKxcNwkBiasActivation_Xdl< NDimSpatial, InDataType, WeiDataType, OutDataType, AccDataType, CShuffleDataType, DDataType, InElementOp, WeiElementOp, OutElementOp, ConvBwdDataDefault,   256,   128,   256,     4,  8,   32,   32,    2,    4,     S<4, 64, 1>,     S<1, 0, 2>,     S<1, 0, 2>,              2,              8,              8,      true,     S<4, 64, 1>,     S<2, 0, 1>,     S<0, 2, 1>,             1,              4,              8,      true, 1,           1,               S<1, 1, 32, 1, 1, 8>,              8>
-    // clang-format-on
+    // clang-format on
     >;
 
 int main(int argc, char* argv[])
@@ -75,7 +75,7 @@ int main(int argc, char* argv[])
     const auto out_element_op = OutElementOp{};
 
     // bias: assume contiguous 1d vector
-    auto bias_c_desc = 
+    auto bias_c_desc =
         HostTensorDescriptor(std::vector<std::size_t>({static_cast<std::size_t>(conv_param.C_)}));
 
     if(conv_param.num_dim_spatial_ == 1)
@@ -98,28 +98,27 @@ int main(int argc, char* argv[])
             ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<OutLayout>(
                 conv_param);
 
-        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<1>>, 1>{}([&](auto i)
-        {
+        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<1>>, 1>{}([&](auto i) {
             const auto device_instance = std::get<i>(device_instances);
-            using EachInstance = ck::remove_cvref_t<decltype(device_instance)>;
+            using EachInstance         = ck::remove_cvref_t<decltype(device_instance)>;
             run_conv_bwd_data_bias_relu<1,
-                                 InDataType,
-                                 WeiDataType,
-                                 OutDataType,
-                                 InElementOp,
-                                 WeiElementOp,
-                                 OutElementOp,
-                                 EachInstance>(do_verification,
-                                                                 init_method,
-                                                                 time_kernel,
-                                                                 conv_param,
-                                                                 in_g_n_c_wis_desc,
-                                                                 wei_g_k_c_xs_desc,
-                                                                 out_g_n_k_wos_desc,
-                                                                 bias_c_desc,
-                                                                 in_element_op,
-                                                                 wei_element_op,
-                                                                 out_element_op);
+                                        InDataType,
+                                        WeiDataType,
+                                        OutDataType,
+                                        InElementOp,
+                                        WeiElementOp,
+                                        OutElementOp,
+                                        EachInstance>(do_verification,
+                                                      init_method,
+                                                      time_kernel,
+                                                      conv_param,
+                                                      in_g_n_c_wis_desc,
+                                                      wei_g_k_c_xs_desc,
+                                                      out_g_n_k_wos_desc,
+                                                      bias_c_desc,
+                                                      in_element_op,
+                                                      wei_element_op,
+                                                      out_element_op);
         });
     }
     else if(conv_param.num_dim_spatial_ == 2)
@@ -142,28 +141,27 @@ int main(int argc, char* argv[])
             ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<OutLayout>(
                 conv_param);
 
-        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<2>>, 1>{}([&](auto i)
-        {
+        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<2>>, 1>{}([&](auto i) {
             const auto device_instance = std::get<i>(device_instances);
-            using EachInstance = ck::remove_cvref_t<decltype(device_instance)>;
+            using EachInstance         = ck::remove_cvref_t<decltype(device_instance)>;
             run_conv_bwd_data_bias_relu<2,
-                                 InDataType,
-                                 WeiDataType,
-                                 OutDataType,
-                                 InElementOp,
-                                 WeiElementOp,
-                                 OutElementOp,
-                                 EachInstance>(do_verification,
-                                                                 init_method,
-                                                                 time_kernel,
-                                                                 conv_param,
-                                                                 in_g_n_c_wis_desc,
-                                                                 wei_g_k_c_xs_desc,
-                                                                 out_g_n_k_wos_desc,
-                                                                 bias_c_desc,
-                                                                 in_element_op,
-                                                                 wei_element_op,
-                                                                 out_element_op);
+                                        InDataType,
+                                        WeiDataType,
+                                        OutDataType,
+                                        InElementOp,
+                                        WeiElementOp,
+                                        OutElementOp,
+                                        EachInstance>(do_verification,
+                                                      init_method,
+                                                      time_kernel,
+                                                      conv_param,
+                                                      in_g_n_c_wis_desc,
+                                                      wei_g_k_c_xs_desc,
+                                                      out_g_n_k_wos_desc,
+                                                      bias_c_desc,
+                                                      in_element_op,
+                                                      wei_element_op,
+                                                      out_element_op);
         });
     }
     else if(conv_param.num_dim_spatial_ == 3)
@@ -186,28 +184,27 @@ int main(int argc, char* argv[])
             ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<OutLayout>(
                 conv_param);
 
-        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<3>>, 1>{}([&](auto i)
-        {
+        ck::static_for<0, std::tuple_size_v<DeviceConvNdBwdDataInstances<3>>, 1>{}([&](auto i) {
             const auto device_instance = std::get<i>(device_instances);
-            using EachInstance = ck::remove_cvref_t<decltype(device_instance)>;
+            using EachInstance         = ck::remove_cvref_t<decltype(device_instance)>;
             run_conv_bwd_data_bias_relu<3,
-                                 InDataType,
-                                 WeiDataType,
-                                 OutDataType,
-                                 InElementOp,
-                                 WeiElementOp,
-                                 OutElementOp,
-                                 EachInstance>(do_verification,
-                                                                 init_method,
-                                                                 time_kernel,
-                                                                 conv_param,
-                                                                 in_g_n_c_wis_desc,
-                                                                 wei_g_k_c_xs_desc,
-                                                                 out_g_n_k_wos_desc,
-                                                                 bias_c_desc,
-                                                                 in_element_op,
-                                                                 wei_element_op,
-                                                                 out_element_op);
+                                        InDataType,
+                                        WeiDataType,
+                                        OutDataType,
+                                        InElementOp,
+                                        WeiElementOp,
+                                        OutElementOp,
+                                        EachInstance>(do_verification,
+                                                      init_method,
+                                                      time_kernel,
+                                                      conv_param,
+                                                      in_g_n_c_wis_desc,
+                                                      wei_g_k_c_xs_desc,
+                                                      out_g_n_k_wos_desc,
+                                                      bias_c_desc,
+                                                      in_element_op,
+                                                      wei_element_op,
+                                                      out_element_op);
         });
     }
 
