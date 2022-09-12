e8b0e222c9c4e0167f4f09fa5e23172490687cc6
Po-Yen, Chen <PoYen.Chen@amd.com>
2022-09-12 10:59:23 -0400

    Avoid using too-large LDS in kernel code

--- example/37_permute/permute_HxWx4_fp16.cpp
+++ example/37_permute/permute_HxWx4_fp16.cpp
@@ -12,10 +12,13 @@ using DevicePermuteInstance = ck::tensor_operation::device::DevicePermute
 // ######|      Type|      Type|   Operation|       |  Size| Block| Block| LdsExtraW| ThreadClusterLengths| ThreadClusterArrangeOrder| VectorDim| VectorDim| ScalarPerVector| ScalarPerVector|
 // ######|          |          |            |       |      |      |      |          |                     |                          |          |          |                |                |
 // ######|          |          |            |       |      |      |      |          |                     |                          |          |          |                |                |
-         < ADataType, BDataType, PassThrough,      3,   256,   128,   128,         0,         S<1, 16, 16>,                S<0, 1, 2>,         2,         1,               1,               1>;
+         < ADataType, BDataType, PassThrough,      3,   256,    16,    16,         0,         S<1, 16, 16>,                S<0, 1, 2>,         2,         1,               1,               1>;
 // clang-format on
 
 #define NUM_ELEMS_IN_BUNDLE 4
 #include "run_permute_example.inc"
 
-int main(int argc, char* argv[]) { return !run_permute_example(argc, argv, {1, 6, 5}, {0, 2, 1}); }
+int main(int argc, char* argv[])
+{
+    return !run_permute_example(argc, argv, {1, 64, 32}, {0, 2, 1});
+}
