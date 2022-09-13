e096148f5730e3ba5f785fe1211fdc631a77ffd4
Adam Osewski <aosewski@amd.com>
2022-09-13 16:14:43 +0200

    Refactor fp32 and int8 unit tests.

--- test/softmax/test_softmax_int8.cpp
+++ test/softmax/test_softmax_int8.cpp
@@ -5,43 +5,32 @@ template <ck::index_t N>
 using I = ck::Number<N>;
 
 template <typename Tuple>
-class TestSoftmaxINT8 : public ck::TestSoftmax<Tuple>
+class TestSoftmaxINT8Reduce1Dim : public ck::TestSoftmax<Tuple>
 {
 };
-
-// clang-format off
-using KernelTypes = ::testing::Types<
-// InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<1>, I<256>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<16>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
-    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>
-    >;
-// clang-format on
-TYPED_TEST_SUITE(TestSoftmaxINT8, KernelTypes);
-TYPED_TEST(TestSoftmaxINT8, Test_INT8) { this->Run(); }
 
 template <typename Tuple>
-class TestSoftmaxINT8v2 : public ck::TestSoftmax<Tuple>
+class TestSoftmaxINT8Reduce1DInnerDims : public ck::TestSoftmax<Tuple>
+{
+};
+
+template <typename Tuple>
+class TestSoftmaxINT8Reduce2Dims : public ck::TestSoftmax<Tuple>
+{
+};
+
+template <typename Tuple>
+class TestSoftmaxINT8Reduce2DInnerDims : public ck::TestSoftmax<Tuple>
+{
+};
+
+template <typename Tuple>
+class TestSoftmaxINT8ReduceAllDims : public ck::TestSoftmax<Tuple>
 {
 };
 
 // clang-format off
-using KernelTypesReduceSingleDim = ::testing::Types<
+using KernelTypesReduce1Dim = ::testing::Types<
 // InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
     std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
     std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
@@ -50,17 +39,110 @@ using KernelTypesReduceSingleDim = ::testing::Types<
     std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
     std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>
     >;
-// clang-format on
-TYPED_TEST_SUITE(TestSoftmaxINT8v2, KernelTypesReduceSingleDim);
 
-TYPED_TEST(TestSoftmaxINT8v2, Test_INT8_reduce_inner_dim)
+using KernelTypesReduce1DInnerDims = ::testing::Types<
+// InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
+    std::tuple<int8_t, float, int8_t, I<3>, I<1>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<1>, I<1>>
+    >;
+
+using KernelTypesReduce2Dims = ::testing::Types<
+// InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>
+    >;
+
+using KernelTypesReduce2DInnerDims = ::testing::Types<
+// InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
+    std::tuple<int8_t, float, int8_t, I<3>, I<2>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<1>, I<1>>
+    >;
+
+using KernelTypesReduceAllDims = ::testing::Types<
+// InDataType, AccDataType, OutDataType, Rank, NumReduceDim, BlockSize, MThreadClusterSize, KThreadClusterSize, MThreadSliceSize, KThreadSliceSize, InSrcVectorDim, InSrcVectorSize, OutDstVectorSize>
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<8>, I<32>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<4>, I<64>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<2>, I<128>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<16>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<32>, I<1>, I<16>, I<16>>,
+    std::tuple<int8_t, float, int8_t, I<3>, I<3>, I<256>, I<1>, I<256>, I<1>, I<64>, I<1>, I<16>, I<16>>
+    >;
+// clang-format on
+
+TYPED_TEST_SUITE(TestSoftmaxINT8Reduce1Dim, KernelTypesReduce1Dim);
+TYPED_TEST_SUITE(TestSoftmaxINT8Reduce1DInnerDims, KernelTypesReduce1DInnerDims);
+TYPED_TEST_SUITE(TestSoftmaxINT8Reduce2Dims, KernelTypesReduce2Dims);
+TYPED_TEST_SUITE(TestSoftmaxINT8Reduce2DInnerDims, KernelTypesReduce2DInnerDims);
+TYPED_TEST_SUITE(TestSoftmaxINT8ReduceAllDims, KernelTypesReduceAllDims);
+
+TYPED_TEST(TestSoftmaxINT8Reduce1Dim, ReduceInnermostDim)
+{
+    std::vector<ck::index_t> reduce_dims{2};
+    this->Run(reduce_dims);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce1Dim, IncorrectReduceDims)
+{
+    std::vector<ck::index_t> reduce_dims{-1};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+    reduce_dims = std::vector<ck::index_t>{3};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce1Dim, ReduceMoreDims)
+{
+    std::vector<ck::index_t> reduce_dims{1, 2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+    reduce_dims = std::vector<ck::index_t>{0, 1, 2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce1DInnerDims, ReduceInnerDims)
 {
     std::vector<ck::index_t> reduce_dims{1};
     this->Run(reduce_dims);
-}
-
-TYPED_TEST(TestSoftmaxINT8v2, Test_INT8_reduce_outermost_dim)
-{
-    std::vector<ck::index_t> reduce_dims{0};
+    reduce_dims = std::vector<ck::index_t>{0};
     this->Run(reduce_dims);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce2Dims, Reduce2Dims)
+{
+    std::vector<ck::index_t> reduce_dims{1, 2};
+    this->Run(reduce_dims);
+    reduce_dims = std::vector<ck::index_t>{0, 2};
+    this->Run(reduce_dims);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce2Dims, ReduceTooLessDims)
+{
+    std::vector<ck::index_t> reduce_dims{2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+    reduce_dims = std::vector<ck::index_t>{1};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce2Dims, ReduceIncorrectDims)
+{
+    std::vector<ck::index_t> reduce_dims{2, 4};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+    reduce_dims = std::vector<ck::index_t>{2, -1};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce2Dims, ReduceTooManyDims)
+{
+    std::vector<ck::index_t> reduce_dims{0, 1, 2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+}
+TYPED_TEST(TestSoftmaxINT8Reduce2DInnerDims, ReduceInnerDims)
+{
+    std::vector<ck::index_t> reduce_dims{0, 1};
+    this->Run(reduce_dims);
+}
+TYPED_TEST(TestSoftmaxINT8ReduceAllDims, ReduceAllDims)
+{
+    std::vector<ck::index_t> reduce_dims{0, 1, 2};
+    this->Run(reduce_dims);
+}
+TYPED_TEST(TestSoftmaxINT8ReduceAllDims, ReduceTooLessDims)
+{
+    std::vector<ck::index_t> reduce_dims{2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
+    reduce_dims = std::vector<ck::index_t>{1, 2};
+    EXPECT_THROW(this->Run(reduce_dims), std::runtime_error);
 }
