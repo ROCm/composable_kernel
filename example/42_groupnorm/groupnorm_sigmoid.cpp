e5b9beb3e81441673976cc6a57f9b190be6ce148
rocking <chunyu.lai@amd.com>
2022-09-16 06:36:08 -0400

    Refine error message

--- example/42_groupnorm/groupnorm_sigmoid.cpp
+++ example/42_groupnorm/groupnorm_sigmoid.cpp
@@ -119,8 +119,7 @@ int main()
         ref_invoker.Run(ref_argument);
 
         y_dev.FromDevice(y.mData.data());
-        pass &=
-            ck::utils::check_err(y.mData, host_y.mData, "Error: Incorrect results d1", 1e-3, 1e-3);
+        pass &= ck::utils::check_err(y.mData, host_y.mData, "Error: Incorrect results", 1e-3, 1e-3);
     }
     return (pass ? 0 : 1);
 }
