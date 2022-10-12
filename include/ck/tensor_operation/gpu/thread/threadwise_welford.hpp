631843ceb7bbfaafc30717020445e069a04c4301
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2022-10-12 03:46:41 +0000

    Add GetActualVariance template parameter for ThreadwiseWelfordMerge

--- include/ck/tensor_operation/gpu/thread/threadwise_welford.hpp
+++ include/ck/tensor_operation/gpu/thread/threadwise_welford.hpp
@@ -75,7 +75,10 @@ struct ThreadwiseWelford
     int max_count_;
 };
 
-template <typename T, typename SrcMeanVarCountThreadDesc_M_K, typename DstMeanVarThreadDesc_M>
+template <typename T,
+          typename SrcMeanVarCountThreadDesc_M_K,
+          typename DstMeanVarThreadDesc_M,
+          bool GetActualVariance = false>
 struct ThreadwiseWelfordMerge
 {
     static constexpr auto src_thread_desc_m_k = SrcMeanVarCountThreadDesc_M_K{};
@@ -122,6 +125,11 @@ struct ThreadwiseWelfordMerge
                       src_var_buf[Number<src_offset>{}],
                       src_count_buf[Number<src_offset>{}]);
             });
+
+            if constexpr(GetActualVariance)
+            {
+                dst_var_buf(iM) = dst_var_buf[iM] / dst_count_buf[iM];
+            };
         });
     };
 };
