750861a9a06483d0fad896a6070915f8e20a890e
rocking <chunyu.lai@amd.com>
2022-10-18 02:28:56 +0000

    Support quantization for non piecewise linear function

--- include/ck/tensor_operation/gpu/element/quantization_operation.hpp
+++ include/ck/tensor_operation/gpu/element/quantization_operation.hpp
@@ -57,6 +57,30 @@ struct Add_Activation_Mul_Clamp
     Activation activationOp_;
 };
 
+// For Activation function which is non piecewise linear function, such as TanH, Sigmoid ...etc
+template <typename Activation>
+struct Add_Mul_Activation_Mul_Clamp
+{
+    Add_Mul_Activation_Mul_Clamp(float multiplier1, float multiplier2, Activation activationOp)
+        : multiplier1_(multiplier1), multiplier2_(multiplier2), activationOp_(activationOp)
+    {
+    }
+
+    __host__ __device__ constexpr void
+    operator()(int8_t& y, const int32_t& x1, const int32_t& x2) const
+    {
+        float y_fp32 = ck::type_convert<float>(x1 + x2);
+        y_fp32       = multiplier1_ * y_fp32;
+        activationOp_(y_fp32, y_fp32);
+        y_fp32 = math::clamp(multiplier2_ * y_fp32, -128.f, 127.f);
+        y      = ck::type_convert<int8_t>(y_fp32);
+    }
+
+    float multiplier1_;
+    float multiplier2_;
+    Activation activationOp_;
+};
+
 } // namespace element_wise
 } // namespace tensor_operation
 } // namespace ck
