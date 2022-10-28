790e21ecc7edeeff122e788ef8e36684b6c3b99b
aska-0096 <haocwang@amd.com>
2022-10-28 16:10:55 +0000

    Refactor + Add all type unit test(int4 compile failed)

--- include/ck/utility/amd_wmma.hpp
+++ include/ck/utility/amd_wmma.hpp
@@ -41,58 +41,51 @@ struct intrin_wmma_f32_16x16x16_bf16_w32<16, 16>
 };
 
 // src: fp16, dst: fp16
-template <index_t MPerWave, index_t NPerWave>
+template <index_t MPerWave, index_t NPerWave, index_t Opsel>
 struct intrin_wmma_f16_16x16x16_f16_w32;
 
-template <>
-struct intrin_wmma_f16_16x16x16_f16_w32<16, 16>
+template <index_t Opsel>
+struct intrin_wmma_f16_16x16x16_f16_w32<16, 16, Opsel>
 {
     template <class FloatC>
-    __device__ static void
-    Run(const half16_t& reg_a, const half16_t& reg_b, FloatC& reg_c, const bool opsel)
+    __device__ static void Run(const half16_t& reg_a, const half16_t& reg_b, FloatC& reg_c)
     {
         // opsel usage
         // false: D0.[0:15] = result
         // true : D0.[16:31]= result
         reg_c.template AsType<half16_t>()(Number<0>{}) = __builtin_amdgcn_wmma_f16_16x16x16_f16_w32(
-            reg_a, reg_b, reg_c.template AsType<half16_t>()[Number<0>{}], opsel);
+            reg_a, reg_b, reg_c.template AsType<half16_t>()[Number<0>{}], Opsel);
     }
 };
 
-// src: bf16, dst: bf32
-template <index_t MPerWave, index_t NPerWave>
+// src: bf16, dst: bf16
+template <index_t MPerWave, index_t NPerWave, index_t Opsel>
 struct intrin_wmma_bf16_16x16x16_bf16_w32;
 
-template <>
-struct intrin_wmma_bf16_16x16x16_bf16_w32<16, 16>
+template <index_t Opsel>
+struct intrin_wmma_bf16_16x16x16_bf16_w32<16, 16, Opsel>
 {
     template <class FloatC>
-    __device__ static void
-    Run(const bhalf16_t& reg_a, const bhalf16_t& reg_b, FloatC& reg_c, const bool opsel)
+    __device__ static void Run(const bhalf16_t& reg_a, const bhalf16_t& reg_b, FloatC& reg_c)
     {
         // opsel usage
         // false: D0.[0:15] = result
         // true : D0.[16:31]= result
         reg_c.template AsType<bhalf16_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_bf16_16x16x16_bf16_w32(
-                reg_a, reg_b, reg_c.template AsType<bhalf16_t>()[Number<0>{}], opsel);
+                reg_a, reg_b, reg_c.template AsType<bhalf16_t>()[Number<0>{}], Opsel);
     }
 };
 
 // src: iu8, dst: i32
-template <index_t MPerWave, index_t NPerWave>
+template <index_t MPerWave, index_t NPerWave, bool neg_a, bool neg_b, bool clamp>
 struct intrin_wmma_i32_16x16x16_iu8_w32;
 
-template <>
-struct intrin_wmma_i32_16x16x16_iu8_w32<16, 16>
+template <bool neg_a, bool neg_b, bool clamp>
+struct intrin_wmma_i32_16x16x16_iu8_w32<16, 16, neg_a, neg_b, clamp>
 {
     template <class FloatC>
-    __device__ static void Run(const bool neg_a,
-                               const int8x16_t& reg_a,
-                               const bool neg_b,
-                               const int8x16_t& reg_b,
-                               FloatC& reg_c,
-                               const bool clamp)
+    __device__ static void Run(const int8x16_t& reg_a, const int8x16_t& reg_b, FloatC& reg_c)
     {
         reg_c.template AsType<int32x8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_i32_16x16x16_iu8_w32(
@@ -107,19 +100,14 @@ struct intrin_wmma_i32_16x16x16_iu8_w32<16, 16>
 
 #ifdef CK_EXPERIMENTAL_BIT_INT_EXTENSION_INT4
 // src: iu4, dst: i32
-template <index_t MPerWave, index_t NPerWave>
+template <index_t MPerWave, index_t NPerWave, bool neg_a, bool neg_b, bool clamp>
 struct intrin_wmma_i32_16x16x16_iu4_w32;
 
-template <>
-struct intrin_wmma_i32_16x16x16_iu4_w32<16, 16>
+template <bool neg_a, bool neg_b, bool clamp>
+struct intrin_wmma_i32_16x16x16_iu4_w32<16, 16, neg_a, neg_b, clamp>
 {
     template <class FloatC>
-    __device__ static void Run(const bool neg_a,
-                               const int4x16_t& reg_a,
-                               const bool neg_b,
-                               const int4x16_t& reg_b,
-                               FloatC& reg_c,
-                               const bool clamp)
+    __device__ static void Run(const int4x16_t& reg_a, const int4x16_t& reg_b, FloatC& reg_c)
     {
         reg_c.template AsType<int32x8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_i32_16x16x16_iu4_w32(
--- test/wmma_op/wmma_op.cpp
+++ test/wmma_op/wmma_op.cpp
@@ -1,178 +1,66 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
 
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <numeric>
-#include <initializer_list>
-#include <cstdlib>
+#include <tuple>
+#include <vector>
 
 #include "ck/ck.hpp"
-#include "ck/utility/amd_wmma.hpp"
-#include "ck/library/utility/check_err.hpp"
-#include "ck/library/utility/device_memory.hpp"
-#include "ck/library/utility/host_tensor.hpp"
-#include "ck/library/utility/host_tensor_generator.hpp"
+#include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
+#include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
+#include "test/wmma_op/wmma_op_util.hpp"
 
-namespace ck {
-__global__ void matmul(const half_t* a, const half_t* b, float* c)
+template <typename SrcType,
+          typename DstType,
+          typename GPUAccType,
+          typename CPUAccType,
+          ck::index_t AccNum>
+bool run_test()
 {
-    const int lIdx = threadIdx.x;
+    using Row         = ck::tensor_layout::gemm::RowMajor;
+    using Col         = ck::tensor_layout::gemm::ColumnMajor;
+    using PassThrough = ck::tensor_operation::element_wise::PassThrough;
+    bool pass         = true;
 
-    // a and b fragments are stored in 8 VGPRs each, in packed format, so 16 elements each for a and
-    // b a_frag will store one column of the 16x16 matrix tile b_frag will store one row of the
-    // 16x16 matrix tile
-    half16_t a_frag = {};
-    half16_t b_frag = {};
-    // initialize c fragment to 0
-    StaticBufferTupleOfVector<AddressSpaceEnum::Vgpr, float, 1, 8, true> c_thread_buf_;
+    const auto matmul_default = ck::wmma_op_util::matmul<SrcType, DstType, GPUAccType, AccNum>;
+    const auto matmul_swizzle_a =
+        ck::wmma_op_util::matmul_swizzle_a<SrcType, DstType, GPUAccType, AccNum>;
 
-    // lane is (0-31) mod 16 instead of 0-31 due to matrix replication in gfx11
-    // see https://atlvsp3.amd.com/sp3_gfx11_5_instructions.pdf page 482
-    // TODO: remove this dependency in gfx12 https://ontrack-internal.amd.com/browse/DEGFXSP3-101
-    const int lane = lIdx % 16;
+    const auto wmma_kernel_container = std::make_tuple(matmul_default, matmul_swizzle_a);
 
-    for(int ele = 0; ele < 16; ++ele)
-    {
-        b_frag[ele] = b[16 * lane + ele];
-    }
-    // follow origin design
-    for(int ele = 0; ele < 16; ++ele)
-    {
-        a_frag[ele] = a[16 * lane + ele];
-    }
-
-    // sync threads, similar to mma_sync
-    __syncthreads();
-    intrin_wmma_f32_16x16x16_f16_w32<16, 16>::Run(
-        a_frag, b_frag, c_thread_buf_.GetVectorTypeReference(Number<0>{}));
-    __syncthreads();
-    // wait for results, similar to mma_sync
-    static_for<0, 8, 1>{}([&](auto ele) {
-        const int r = ele * 2 + (lIdx / 16);
-        // store results from unpacked c_thread_buf_ output
-        c[16 * r + lane] = c_thread_buf_[Number<ele>{}];
+    ck::static_for<0, 2, 1>{}([&](auto i) {
+        pass &=
+            ck::wmma_op_util::TestWmma<decltype(std::get<ck::Number<i>{}>(wmma_kernel_container)),
+                                       SrcType,
+                                       SrcType,
+                                       DstType,
+                                       GPUAccType,
+                                       CPUAccType,
+                                       decltype(Row{}),
+                                       decltype(Col{}),
+                                       decltype(Row{}),
+                                       PassThrough,
+                                       PassThrough,
+                                       PassThrough,
+                                       AccNum>{}(std::get<ck::Number<i>{}>(wmma_kernel_container));
     });
+
+    return pass ? 1 : 0;
 }
-
-__global__ void matmul_swizzle_a(const half_t* a, const half_t* b, float* c)
-{
-    const int lIdx = threadIdx.x;
-
-    half16_t a_frag = {};
-    half16_t b_frag = {};
-    StaticBufferTupleOfVector<AddressSpaceEnum::Vgpr, float, 1, 8, true> c_thread_buf_;
-
-    const int lane = lIdx % 16;
-
-    for(int ele = 0; ele < 16; ++ele)
-    {
-        b_frag[ele] = b[16 * lane + ele];
-    }
-
-    const int offset_m = (((lane & 1) << 3) | (lane >> 1));
-    for(int ele = 0; ele < 16; ++ele)
-    {
-        a_frag[ele] = a[16 * offset_m + ele];
-    }
-
-    __syncthreads();
-    intrin_wmma_f32_16x16x16_f16_w32<16, 16>::Run(
-        a_frag, b_frag, c_thread_buf_.GetVectorTypeReference(Number<0>{}));
-    __syncthreads();
-
-    static_for<0, 8, 1>{}([&](auto ele) {
-        const int blk                   = lIdx / 16;
-        const int r                     = ele;
-        c[16 * 8 * blk + 16 * r + lane] = c_thread_buf_[Number<ele>{}];
-    });
-}
-} // namespace ck
-
 int main(int, char*[])
 {
-    std::vector<float> host_a(16 * 16);
-    std::vector<float> host_b(16 * 16);
-    std::vector<float> host_c(16 * 16);
-    std::vector<float> wmma_c(16 * 16);
-    std::vector<float> wmma_c_swizzle_a(16 * 16);
-    uint64_t num_element = 256;
+    bool pass = true;
+    // clang-format off
+    //              |SrcType     |DstType     |GPUAccType  |CPUAccType |AccNum
+    pass &= run_test<ck::half_t,  float,       float,       float,      8     >();
+    pass &= run_test<ck::half_t,  ck::half_t,  ck::half_t,  ck::half_t, 16    >();
+    pass &= run_test<ck::bhalf_t, ck::bhalf_t, ck::bhalf_t, float,      16    >();
+    pass &= run_test<int8_t,      int8_t,      int32_t,     int32_t,    8     >();
+    // clang-format on
 
-    // generate matrix a
-    for(int i_m = 0; i_m < 16; i_m++)
-    {
-        for(int i_k = 0; i_k < 16; i_k++)
-        {
-            host_a[i_m * 16 + i_k] = float(i_m + 1) / 99.0 + (float(i_k + 1) / 100);
-            // host_a[i_m * 16 + i_k] = float(i_k);
-        }
-    }
-
-    // generate matrix b
-    for(int i_n = 0; i_n < 16; i_n++)
-    {
-        for(int i_k = 0; i_k < 16; i_k++)
-        {
-            host_b[i_n * 16 + i_k] = float(i_n + 1) / 98.0 + (float(i_k + 1) / 100);
-            // host_b[i_n * 16 + i_k] = 1.0;
-        }
-    }
-
-    // run mk_nk_mn gemm on cpu
-    for(int i_m = 0; i_m < 16; i_m++)
-    {
-        for(int i_n = 0; i_n < 16; i_n++)
-        {
-            for(int i_k = 0; i_k < 16; i_k++)
-            {
-                host_c[i_m * 16 + i_n] += host_a[i_m * 16 + i_k] * host_b[i_n * 16 + i_k];
-            }
-        }
-    }
-
-    DeviceMem device_a(sizeof(ck::half_t) * num_element);
-    DeviceMem device_b(sizeof(ck::half_t) * num_element);
-    DeviceMem device_c(sizeof(float) * num_element);
-
-    std::vector<ck::half_t> fp16_a(16 * 16);
-    std::vector<ck::half_t> fp16_b(16 * 16);
-    // convert fp32 a and b into fp16 on host
-    for(int i = 0; i < 16 * 16; i++)
-    {
-        fp16_a[i] = __float2half_rn(host_a[i]);
-        fp16_b[i] = __float2half_rn(host_b[i]);
-    }
-
-    device_a.ToDevice(fp16_a.data());
-    device_b.ToDevice(fp16_b.data());
-
-    // run single wave wmma on GPU
-    ck::matmul<<<1, 32>>>(static_cast<const ck::half_t*>(device_a.GetDeviceBuffer()),
-                          static_cast<const ck::half_t*>(device_b.GetDeviceBuffer()),
-                          static_cast<float*>(device_c.GetDeviceBuffer()));
-
-    device_c.FromDevice(wmma_c.data());
-
-    // run single wave wmma_swizzle_a on GPU
-    ck::matmul_swizzle_a<<<1, 32>>>(static_cast<const ck::half_t*>(device_a.GetDeviceBuffer()),
-                                    static_cast<const ck::half_t*>(device_b.GetDeviceBuffer()),
-                                    static_cast<float*>(device_c.GetDeviceBuffer()));
-    device_c.FromDevice(wmma_c_swizzle_a.data());
-
-    // result check
-    bool res           = true;
-    bool res_swizzle_a = true;
-    res                = ck::utils::check_err(wmma_c, host_c, "Error: Incorrect results!", 1e-2);
-    res_swizzle_a =
-        ck::utils::check_err(wmma_c_swizzle_a, host_c, "Error: Incorrect results!", 1e-2);
-
-    if(res && res_swizzle_a)
-    {
-        std::cout << "test single wave wmma: Pass" << std::endl;
-        return 0;
-    }
-    else
-    {
-        std::cout << "test single wave wmma: Fail" << std::endl;
-        return -1;
-    }
+    std::cout << "TestGemm ..... " << (pass ? "SUCCESS" : "FAILURE") << std::endl;
+    return pass ? 0 : 1;
 }
