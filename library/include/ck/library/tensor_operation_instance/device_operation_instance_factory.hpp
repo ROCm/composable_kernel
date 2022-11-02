6d8614ee503f6a7879b252f006d92458c11acae1
Adam Osewski <19374865+aosewski@users.noreply.github.com>
2022-11-02 23:46:08 +0100

    Softmax unit-test reduction across all and non innermost dims cases. (#406)
    * Add reduction across all dims cases.
    
    * host softmax: handle all reduce
    
    * Test cases when reduced dim is not innermost axis.
    
    * Fix syntax.
    
    * Test non innermost dim for fp32 and int8
    
    * Group test suites wrt NumReduceDim.
    
    * Additionally test failing cases.
    
    * Throw error when Rank or NumReduceDims doesn't match arguments.
    
    * Check reducedDims has correct values
    
    * Move don't reuse DeviceReduceMultiblock IsSupportedArgument method.
    Instead implement own. (in fact just get rid of one check to enable
    reduction across inner dimensions).
    
    * Reorganize unit tests to better cover use scenarios.
    
    * Test input validation
    * Test reduction of inner dimensions with custom op instances.
    
    * Refactor fp32 and int8 unit tests.
    
    * Fix FP32 instance template parameters.
    
    * Add more instances.
    
    * Instances with InSrcVectorDim=0.
    
    * Do not initialize and copy data when arg not supported.
    
    * ckProfiler Softmax use instance factory.
    
    * Refactor device softmax IsSupported.
    
    * Additionally add non-polymorphic api functions
    
    * Split softmax instances into multiple files.
    
    * Fix profiler.
    
    * Reorganize tests to reuse profiler and cover edge cases.
    
    * Clang-format
    
    * I8 Softmax instances along with UT.
    
    * Reuse type alias definitions from instance factory header.
    
    * Clean included headers
    
    * Fix variable names.
    
    * Add missing checks in Argument constructor.
    
    Co-authored-by: Adam Osewski <aosewski@amd.com>
    Co-authored-by: Anthony Chang <ac.chang@outlook.com>

--- library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
+++ library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
@@ -3,10 +3,10 @@
 
 #pragma once
 
-#include "ck/utility/data_type.hpp"
-#include "ck/utility/tuple.hpp"
 #include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
 #include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
+#include "ck/utility/data_type.hpp"
+#include "ck/utility/tuple.hpp"
 
 namespace ck {
 namespace tensor_operation {
