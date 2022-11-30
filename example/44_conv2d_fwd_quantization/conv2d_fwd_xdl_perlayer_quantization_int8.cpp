ad541ad6b9de9b0579d5254f82e9d5b86103d309
rocking5566 <ChunYu.Lai@amd.com>
2022-12-01 04:13:04 +0800

    gemm, conv perchannel quantization (#503)
    * Use gemm_multiple_D instead
    
    * Add gemm bias relu quantization example
    
    * Add pure gemm quantization example
    
    * Add quantization of perchannel conv + bias + relu example
    
    * Refine the code
    
    * Rename multiplier to requant_scale
    
    * Rename the folder
    
    * Remove redundant comment
    
    * Rename the file. Prepare to add perchannel
    
    * Add conv perchannel instance
    
    * Move to quantization folder
    
    * Add conv perchannel client example
    
    * Apply Rangify constructor of HostTensorDescriptor & Tensor<>
    
    * Fix merge error

--- example/44_conv2d_fwd_quantization/conv2d_fwd_xdl_perlayer_quantization_int8.cpp
+++ example/44_conv2d_fwd_quantization/conv2d_fwd_xdl_perlayer_quantization_int8.cpp
@@ -11,13 +11,13 @@
 #include "ck/library/utility/device_memory.hpp"
 #include "ck/library/utility/host_tensor.hpp"
 #include "ck/library/utility/host_tensor_generator.hpp"
+#include "ck/library/utility/literals.hpp"
 #include "ck/library/utility/convolution_parameter.hpp"
 #include "ck/library/utility/convolution_host_tensor_descriptor_helper.hpp"
 #include "ck/library/reference_tensor_operation/cpu/reference_conv_fwd.hpp"
 
 using InDataType       = int8_t;
 using WeiDataType      = int8_t;
-using BiasDataType     = int32_t;
 using AccDataType      = int32_t;
 using CShuffleDataType = int32_t;
 using OutDataType      = int8_t;
@@ -28,31 +28,27 @@ using S = ck::Sequence<Is...>;
 using PassThrough  = ck::tensor_operation::element_wise::PassThrough;
 using InElementOp  = PassThrough;
 using WeiElementOp = PassThrough;
-using ActivationOp = ck::tensor_operation::element_wise::Relu;
-using OutElementOp = ck::tensor_operation::element_wise::Add_Activation_Mul_Clamp<ActivationOp>;
+using ActivationOp = PassThrough;
+using OutElementOp = ck::tensor_operation::element_wise::Activation_Mul_Clamp<ActivationOp>;
 
 static constexpr auto ConvSpec =
     ck::tensor_operation::device::ConvolutionForwardSpecialization::Default;
 
 static constexpr auto GemmSpec = ck::tensor_operation::device::GemmSpecialization::MNKPadding;
 
-template <ck::index_t NDimSpatial,
-          typename InLayout,
-          typename WeiLayout,
-          typename BiasLayout,
-          typename OutLayout>
+template <ck::index_t NDimSpatial, typename InLayout, typename WeiLayout, typename OutLayout>
 using DeviceGroupedConvNDFwdInstance =
     ck::tensor_operation::device::DeviceGroupedConvFwdMultipleD_Xdl_CShuffle<
         NDimSpatial,
         InLayout,
         WeiLayout,
-        ck::Tuple<BiasLayout>,
+        ck::Tuple<>,
         OutLayout,
         InDataType,
         WeiDataType,
         AccDataType,
         CShuffleDataType,
-        ck::Tuple<BiasDataType>,
+        ck::Tuple<>,
         OutDataType,
         InElementOp,
         WeiElementOp,
@@ -87,7 +83,7 @@ using DeviceGroupedConvNDFwdInstance =
         1,
         1,
         S<1, 64, 1, 4>,
-        8>;
+        16>;
 
 template <ck::index_t NDimSpatial,
           typename InDataType,
@@ -102,7 +98,6 @@ bool run_grouped_conv_fwd(bool do_verification,
                           const ck::utils::conv::ConvParam& conv_param,
                           const HostTensorDescriptor& in_g_n_c_wis_desc,
                           const HostTensorDescriptor& wei_g_k_c_xs_desc,
-                          const HostTensorDescriptor& bias_g_k_desc,
                           const HostTensorDescriptor& out_g_n_k_wos_desc,
                           const InElementOp& in_element_op,
                           const WeiElementOp& wei_element_op,
@@ -110,34 +105,27 @@ bool run_grouped_conv_fwd(bool do_verification,
 {
     Tensor<InDataType> in(in_g_n_c_wis_desc);
     Tensor<WeiDataType> wei(wei_g_k_c_xs_desc);
-    Tensor<BiasDataType> bias(bias_g_k_desc);
     Tensor<OutDataType> out_host(out_g_n_k_wos_desc);
     Tensor<OutDataType> out_device(out_g_n_k_wos_desc);
 
     std::cout << "in: " << in.mDesc << std::endl;
     std::cout << "wei: " << wei.mDesc << std::endl;
-    std::cout << "bias: " << bias.mDesc << std::endl;
     std::cout << "out: " << out_host.mDesc << std::endl;
 
     in.GenerateTensorValue(GeneratorTensor_2<InDataType>{-5, 5});
     wei.GenerateTensorValue(GeneratorTensor_2<WeiDataType>{-5, 5});
-    bias.GenerateTensorValue(GeneratorTensor_2<BiasDataType>{-5, 5});
 
     DeviceMem in_device_buf(sizeof(InDataType) * in.mDesc.GetElementSpaceSize());
     DeviceMem wei_device_buf(sizeof(WeiDataType) * wei.mDesc.GetElementSpaceSize());
-    DeviceMem bias_device_buf(sizeof(BiasDataType) * bias.mDesc.GetElementSpaceSize());
     DeviceMem out_device_buf(sizeof(OutDataType) * out_device.mDesc.GetElementSpaceSize());
 
     in_device_buf.ToDevice(in.mData.data());
     wei_device_buf.ToDevice(wei.mData.data());
-    bias_device_buf.ToDevice(bias.mData.data());
 
     std::array<ck::index_t, NDimSpatial + 3> a_g_n_c_wis_lengths{};
     std::array<ck::index_t, NDimSpatial + 3> a_g_n_c_wis_strides{};
     std::array<ck::index_t, NDimSpatial + 3> b_g_k_c_xs_lengths{};
     std::array<ck::index_t, NDimSpatial + 3> b_g_k_c_xs_strides{};
-    std::array<ck::index_t, NDimSpatial + 3> d0_g_n_k_wos_lengths{};
-    std::array<ck::index_t, NDimSpatial + 3> d0_g_n_k_wos_strides{};
     std::array<ck::index_t, NDimSpatial + 3> e_g_n_k_wos_lengths{};
     std::array<ck::index_t, NDimSpatial + 3> e_g_n_k_wos_strides{};
     std::array<ck::index_t, NDimSpatial> conv_filter_strides{};
@@ -151,8 +139,6 @@ bool run_grouped_conv_fwd(bool do_verification,
     copy(in_g_n_c_wis_desc.GetStrides(), a_g_n_c_wis_strides);
     copy(wei_g_k_c_xs_desc.GetLengths(), b_g_k_c_xs_lengths);
     copy(wei_g_k_c_xs_desc.GetStrides(), b_g_k_c_xs_strides);
-    copy(bias_g_k_desc.GetLengths(), d0_g_n_k_wos_lengths);
-    copy(bias_g_k_desc.GetStrides(), d0_g_n_k_wos_strides);
     copy(out_g_n_k_wos_desc.GetLengths(), e_g_n_k_wos_lengths);
     copy(out_g_n_k_wos_desc.GetStrides(), e_g_n_k_wos_strides);
     copy(conv_param.conv_filter_strides_, conv_filter_strides);
@@ -163,26 +149,25 @@ bool run_grouped_conv_fwd(bool do_verification,
     // do Conv
     auto conv     = DeviceConvNDFwdInstance{};
     auto invoker  = conv.MakeInvoker();
-    auto argument = conv.MakeArgument(
-        in_device_buf.GetDeviceBuffer(),
-        wei_device_buf.GetDeviceBuffer(),
-        std::array<const void*, 1>{bias_device_buf.GetDeviceBuffer()},
-        out_device_buf.GetDeviceBuffer(),
-        a_g_n_c_wis_lengths,
-        a_g_n_c_wis_strides,
-        b_g_k_c_xs_lengths,
-        b_g_k_c_xs_strides,
-        std::array<std::array<ck::index_t, NDimSpatial + 3>, 1>{{d0_g_n_k_wos_lengths}},
-        std::array<std::array<ck::index_t, NDimSpatial + 3>, 1>{{d0_g_n_k_wos_strides}},
-        e_g_n_k_wos_lengths,
-        e_g_n_k_wos_strides,
-        conv_filter_strides,
-        conv_filter_dilations,
-        input_left_pads,
-        input_right_pads,
-        in_element_op,
-        wei_element_op,
-        out_element_op);
+    auto argument = conv.MakeArgument(in_device_buf.GetDeviceBuffer(),
+                                      wei_device_buf.GetDeviceBuffer(),
+                                      {},
+                                      out_device_buf.GetDeviceBuffer(),
+                                      a_g_n_c_wis_lengths,
+                                      a_g_n_c_wis_strides,
+                                      b_g_k_c_xs_lengths,
+                                      b_g_k_c_xs_strides,
+                                      {},
+                                      {},
+                                      e_g_n_k_wos_lengths,
+                                      e_g_n_k_wos_strides,
+                                      conv_filter_strides,
+                                      conv_filter_dilations,
+                                      input_left_pads,
+                                      input_right_pads,
+                                      in_element_op,
+                                      wei_element_op,
+                                      out_element_op);
 
     if(!conv.IsSupportedArgument(argument))
     {
@@ -205,38 +190,32 @@ bool run_grouped_conv_fwd(bool do_verification,
 
     if(do_verification)
     {
-        Tensor<CShuffleDataType> c_host(out_g_n_k_wos_desc);
-
         auto ref_conv = ck::tensor_operation::host::ReferenceConvFwd<NDimSpatial,
                                                                      InDataType,
                                                                      WeiDataType,
-                                                                     CShuffleDataType,
+                                                                     OutDataType,
                                                                      InElementOp,
                                                                      WeiElementOp,
-                                                                     PassThrough>();
+                                                                     OutElementOp>();
 
         auto ref_invoker  = ref_conv.MakeInvoker();
         auto ref_argument = ref_conv.MakeArgument(in,
                                                   wei,
-                                                  c_host,
+                                                  out_host,
                                                   conv_param.conv_filter_strides_,
                                                   conv_param.conv_filter_dilations_,
                                                   conv_param.input_left_pads_,
                                                   conv_param.input_right_pads_,
                                                   in_element_op,
                                                   wei_element_op,
-                                                  PassThrough{});
+                                                  out_element_op);
 
         ref_invoker.Run(ref_argument);
 
-        // TODO: implement elementwise operation for host
-        out_host.ForEach(
-            [&](auto&, auto idx) { out_element_op(out_host(idx), c_host(idx), bias(idx)); });
-
         out_device_buf.FromDevice(out_device.mData.data());
 
-        pass &= ck::utils::check_err(
-            out_device.mData, out_host.mData, "Error: incorrect results!", 1e-5f, 1e-4f);
+        pass &=
+            ck::utils::check_err(out_device, out_host, "Error: incorrect results!", 1e-5f, 1e-4f);
     }
 
     return (pass ? 0 : 1);
@@ -266,10 +245,9 @@ int main()
     const auto wei_element_op = WeiElementOp{};
     const auto out_element_op = OutElementOp{0.5f, ActivationOp{}};
 
-    using InLayout   = ck::tensor_layout::convolution::GNHWC;
-    using WeiLayout  = ck::tensor_layout::convolution::GKYXC;
-    using BiasLayout = ck::tensor_layout::convolution::G_K;
-    using OutLayout  = ck::tensor_layout::convolution::GNHWK;
+    using InLayout  = ck::tensor_layout::convolution::GNHWC;
+    using WeiLayout = ck::tensor_layout::convolution::GKYXC;
+    using OutLayout = ck::tensor_layout::convolution::GNHWK;
 
     const auto in_g_n_c_wis_desc =
         ck::utils::conv::make_input_host_tensor_descriptor_g_n_c_wis_packed<InLayout>(conv_param);
@@ -277,24 +255,8 @@ int main()
     const auto wei_g_k_c_xs_desc =
         ck::utils::conv::make_weight_host_tensor_descriptor_g_k_c_xs_packed<WeiLayout>(conv_param);
 
-    // TODO - make_bias_host_tensor_descriptor_g_n_k_wos_packed()
-    const auto bias_g_k_desc = HostTensorDescriptor({conv_param.G_,
-                                                     conv_param.N_,
-                                                     conv_param.K_,
-                                                     conv_param.output_spatial_lengths_[0],
-                                                     conv_param.output_spatial_lengths_[1]},
-                                                    {
-                                                        conv_param.K_, // g
-                                                        0,             // n
-                                                        1,             // k
-                                                        0,             // ho
-                                                        0              // wo
-                                                    });
-
     const auto out_g_n_k_wos_desc =
         ck::utils::conv::make_output_host_tensor_descriptor_g_n_k_wos_packed<OutLayout>(conv_param);
-
-    std::cout << out_g_n_k_wos_desc << std::endl;
 
     return run_grouped_conv_fwd<
         ndim_spatial,
@@ -304,13 +266,12 @@ int main()
         InElementOp,
         WeiElementOp,
         OutElementOp,
-        DeviceGroupedConvNDFwdInstance<ndim_spatial, InLayout, WeiLayout, BiasLayout, OutLayout>>(
+        DeviceGroupedConvNDFwdInstance<ndim_spatial, InLayout, WeiLayout, OutLayout>>(
         do_verification,
         time_kernel,
         conv_param,
         in_g_n_c_wis_desc,
         wei_g_k_c_xs_desc,
-        bias_g_k_desc,
         out_g_n_k_wos_desc,
         in_element_op,
         wei_element_op,
