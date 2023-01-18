8941136f23accc50ef76e44ad422002b7fdbc3d3
fsx950223 <fsx950223@outlook.com>
2023-01-18 13:16:27 +0800

    run ci cd

--- example/36_sparse_embedding/sparse_embedding3_forward_layernorm.cpp
+++ example/36_sparse_embedding/sparse_embedding3_forward_layernorm.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2022, Advanced Micro Devices, Inc. All rights reserved.
 
+
 #include <iostream>
 #include <numeric>
 #include <initializer_list>
