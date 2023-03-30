696991c923754f320d100a629299feb305fa1103
zjing14 <zhangjing14@gmail.com>
2023-03-30 13:30:43 -0500

    add fp64 instances (#658)
    Co-authored-by: root <root@ctr-ubbsmc15.amd.com>
    
    [ROCm/composable_kernel commit: fde6d2742bc7fe56bb86864ff960f8ea26082dfc]

--- library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
+++ library/include/ck/library/tensor_operation_instance/device_operation_instance_factory.hpp
@@ -26,6 +26,7 @@ using Empty_Tuple = ck::Tuple<>;
 using F16_Tuple     = ck::Tuple<F16>;
 using F16_F16_Tuple = ck::Tuple<F16, F16>;
 
+using F64_Tuple     = ck::Tuple<F64>;
 using F32_Tuple     = ck::Tuple<F32>;
 using I32_Tuple     = ck::Tuple<I32>;
 using I32_F32_Tuple = ck::Tuple<I32, F32>;
