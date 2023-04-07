e7a56ac6075f58ac8d145c5c4ee508786570091c
ltqin <letao.qin@amd.com>
2023-04-07 03:17:36 +0000

    fix head file name

--- client_example/08_fused_attention/fused_attention_bias.cpp
+++ client_example/08_fused_attention/fused_attention_bias.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 #include "ck/ck.hpp"
-#include "ck/library/tensor_operation_instance/gpu/batched_gemm_softmax_gemm_permute_general.hpp"
+#include "ck/library/tensor_operation_instance/gpu/batched_gemm_softmax_gemm_permute.hpp"
 #include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
 #include "ck/tensor_operation/gpu/device/device_batched_gemm_softmax_gemm_permute.hpp"
 #include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
