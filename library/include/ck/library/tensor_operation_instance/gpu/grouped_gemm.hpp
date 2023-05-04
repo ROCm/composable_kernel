3948d09b031664635d9ae0cb1a52033cbb0a4bc2
Adam Osewski <aosewski@amd.com>
2023-05-04 15:18:49 +0200

    Limit the number of instances to compile.

--- library/include/ck/library/tensor_operation_instance/gpu/grouped_gemm.hpp
+++ library/include/ck/library/tensor_operation_instance/gpu/grouped_gemm.hpp
@@ -16,83 +16,83 @@ namespace tensor_operation {
 namespace device {
 namespace instance {
 
-void add_device_grouped_gemm_xdl_f16_f16_f16_mk_kn_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
-                                                  Row,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_f16_f16_f16_mk_kn_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
+//                                                   Row,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
-void add_device_grouped_gemm_xdl_f16_f16_f16_mk_nk_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
-                                                  Col,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_f16_f16_f16_mk_nk_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
+//                                                   Col,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
-void add_device_grouped_gemm_xdl_f16_f16_f16_km_kn_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Col,
-                                                  Row,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_f16_f16_f16_km_kn_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Col,
+//                                                   Row,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
-void add_device_grouped_gemm_xdl_f16_f16_f16_km_nk_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Col,
-                                                  Col,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_f16_f16_f16_km_nk_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Col,
+//                                                   Col,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
-void add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
-                                                  Col,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
+//                                                   Col,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
-void add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_kn_mn_instances(
-    std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
-                                                  Row,
-                                                  Empty_Tuple,
-                                                  Row,
-                                                  F16,
-                                                  F16,
-                                                  Empty_Tuple,
-                                                  F16,
-                                                  PassThrough,
-                                                  PassThrough,
-                                                  PassThrough>>>& instances);
+// void add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_kn_mn_instances(
+//     std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
+//                                                   Row,
+//                                                   Empty_Tuple,
+//                                                   Row,
+//                                                   F16,
+//                                                   F16,
+//                                                   Empty_Tuple,
+//                                                   F16,
+//                                                   PassThrough,
+//                                                   PassThrough,
+//                                                   PassThrough>>>& instances);
 
 void add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_irregular_instances(
     std::vector<std::unique_ptr<DeviceGroupedGemm<Row,
@@ -160,28 +160,28 @@ struct DeviceOperationInstanceFactory<ck::tensor_operation::device::DeviceGroupe
             if constexpr(is_same_v<ALayout, Row> && is_same_v<BLayout, Row> &&
                          is_same_v<ELayout, Row>)
             {
-                add_device_grouped_gemm_xdl_f16_f16_f16_mk_kn_mn_instances(op_ptrs);
-                add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_kn_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_f16_f16_f16_mk_kn_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_kn_mn_instances(op_ptrs);
                 add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_kn_mn_irregular_instances(
                     op_ptrs);
             }
             else if constexpr(is_same_v<ALayout, Row> && is_same_v<BLayout, Col> &&
                               is_same_v<ELayout, Row>)
             {
-                add_device_grouped_gemm_xdl_f16_f16_f16_mk_nk_mn_instances(op_ptrs);
-                add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_f16_f16_f16_mk_nk_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_instances(op_ptrs);
                 add_device_grouped_gemm_xdl_splitk_f16_f16_f16_mk_nk_mn_irregular_instances(
                     op_ptrs);
             }
             else if constexpr(is_same_v<ALayout, Col> && is_same_v<BLayout, Row> &&
                               is_same_v<ELayout, Row>)
             {
-                add_device_grouped_gemm_xdl_f16_f16_f16_km_kn_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_f16_f16_f16_km_kn_mn_instances(op_ptrs);
             }
             else if constexpr(is_same_v<ALayout, Col> && is_same_v<BLayout, Col> &&
                               is_same_v<ELayout, Row>)
             {
-                add_device_grouped_gemm_xdl_f16_f16_f16_km_nk_mn_instances(op_ptrs);
+                // add_device_grouped_gemm_xdl_f16_f16_f16_km_nk_mn_instances(op_ptrs);
             }
         }
         return op_ptrs;
