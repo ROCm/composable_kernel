d3929cb051573dba83f549c1ecd4a8f9e47894d5
Rostyslav Geyyer <rosty.geyyer@amd.com>
2023-05-08 17:31:13 -0400

    Format

--- include/ck/utility/data_type.hpp
+++ include/ck/utility/data_type.hpp
@@ -1108,34 +1108,32 @@ inline __host__ __device__ constexpr bhalf_t bf16_convert_rtn<bhalf_t, half_t>(h
 
 // Pseudo random number generator
 // version for fp32
-template <typename T,
-          uint32_t seed,
-          std::enable_if_t<std::is_same<float, T>{}, bool> = false>
+template <typename T, uint32_t seed, std::enable_if_t<std::is_same<float, T>{}, bool> = false>
 __host__ __device__ uint32_t prand_generator(index_t id, T val)
 {
-    uint32_t       x         = reinterpret_cast<uint32_t&>(val);
+    uint32_t x         = reinterpret_cast<uint32_t&>(val);
     uint32_t drop_bits = uint32_t(x) & 0xFFFFu;
     drop_bits ^= x >> 16;
     drop_bits = ((drop_bits & 31) << 11) | (drop_bits >> 5);
     drop_bits *= 0x7000149;
     // NOTE: If id is in 64 bit, we are only using lower 32 bit.
-    //       So, it can have an effect of using same id for multiple elements when the id is very large!
+    //       So, it can have an effect of using same id for multiple elements when the id is very
+    //       large!
     uint32_t rng = (drop_bits ^ 0x13371337 ^ (id * 229791) ^ seed);
     return rng;
 }
 
 // version for fp16
-template <typename T,
-          uint32_t seed,
-          std::enable_if_t<std::is_same<half_t, T>{}, bool> = false>
+template <typename T, uint32_t seed, std::enable_if_t<std::is_same<half_t, T>{}, bool> = false>
 __host__ __device__ uint32_t prand_generator(index_t id, T val)
 {
-    uint16_t       x         = reinterpret_cast<uint16_t&>(val);
+    uint16_t x         = reinterpret_cast<uint16_t&>(val);
     uint32_t drop_bits = uint32_t(x) & 0xFFFFu;
-    drop_bits = ((drop_bits & 31) << 11) | (drop_bits >> 5);
+    drop_bits          = ((drop_bits & 31) << 11) | (drop_bits >> 5);
     drop_bits *= 0x7000149;
     // NOTE: If id is in 64 bit, we are only using lower 32 bit.
-    //       So, it can have an effect of using same id for multiple elements when the id is very large!
+    //       So, it can have an effect of using same id for multiple elements when the id is very
+    //       large!
     uint32_t rng = (drop_bits ^ 0x13371337 ^ (id * 229791) ^ seed);
     return rng;
 }
