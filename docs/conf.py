42dc134b528cda051e144fb842fc15fad5d47aca
Sam Wu <sam.wu2@amd.com>
2023-05-18 11:08:38 -0600

    Documentation Updates (#710)
    * update documentation dependencies
    
    add version number to docs
    
    rename doc config directories
    
    enable more doc formats on rtd
    
    add license section in docs
    
    [ROCm/composable_kernel commit: 3cff34042385ee7dadf9252fc667b1285d4cee38]

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,21 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import subprocess
+
 from rocm_docs import ROCmDocs
 
-docs_core = ROCmDocs("Composable Kernel Documentation")
-docs_core.run_doxygen()
+
+name = "Composable Kernel"
+get_version = r'sed -n -e "s/^rocm_setup_version(.* \([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt'
+version = subprocess.getoutput(get_version)
+if len(version) > 0:
+    name = f"{name} {version}"
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(f"{name} Documentation")
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/docBin/xml")
 docs_core.setup()
 
 mathjax3_config = {
