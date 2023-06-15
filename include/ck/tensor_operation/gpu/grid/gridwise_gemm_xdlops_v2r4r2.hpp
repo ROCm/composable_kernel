624a327663d636e6ca10220d7445ccabfc9b052a
Illia Silin <98187287+illsilin@users.noreply.github.com>
2023-06-15 08:20:59 -0700

    Enable gfx941 and gfx942 architectures. (#752)
    * enable gfx941/942 targets
    
    * fix clang format
    
    * fix the cmake logic for multiple targets
    
    * fix cmake syntax for looping over targets
    
    * add gfx941/942 support for gemm_xdl instances
    
    [ROCm/composable_kernel commit: 027e46ee82cdb8a02313985361333c78f281b887]

--- include/ck/tensor_operation/gpu/grid/gridwise_gemm_xdlops_v2r4r2.hpp
+++ include/ck/tensor_operation/gpu/grid/gridwise_gemm_xdlops_v2r4r2.hpp
@@ -31,7 +31,7 @@ __global__ void
                                              const Block2CTileMap& b2c_map)
 {
 #if(!defined(__HIP_DEVICE_COMPILE__) || defined(__gfx908__) || defined(__gfx90a__) || \
-    defined(__gfx940__))
+    defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__))
     constexpr index_t shared_size = GridwiseGemm::GetSharedMemoryNumberOfByte();
 
     __shared__ uint8_t p_shared[shared_size];
