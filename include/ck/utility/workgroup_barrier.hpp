50e573f5d8c46266b11e9876b3f6562fb917a0a7
carlushuang <carlus.huang@amd.com>
2023-06-19 18:14:28 +0000

    fix format

--- include/ck/utility/workgroup_barrier.hpp
+++ include/ck/utility/workgroup_barrier.hpp
@@ -3,10 +3,9 @@
 #include <stdint.h>
 
 namespace ck {
-struct workgroup_barrier {
-    __device__ workgroup_barrier(uint32_t * ptr) :
-        base_ptr(ptr)
-    {}
+struct workgroup_barrier
+{
+    __device__ workgroup_barrier(uint32_t* ptr) : base_ptr(ptr) {}
 
     __device__ uint32_t ld(uint32_t offset)
     {
@@ -29,48 +28,46 @@ struct workgroup_barrier {
 
     __device__ void wait_eq(uint32_t offset, uint32_t value)
     {
-        if(threadIdx.x == 0){
-            while(ld(offset) != value){}
+        if(threadIdx.x == 0)
+        {
+            while(ld(offset) != value) {}
         }
         __syncthreads();
     }
 
     __device__ void wait_lt(uint32_t offset, uint32_t value)
     {
-        if(threadIdx.x == 0){
-            while(ld(offset) < value){}
+        if(threadIdx.x == 0)
+        {
+            while(ld(offset) < value) {}
         }
         __syncthreads();
     }
 
     __device__ void wait_set(uint32_t offset, uint32_t compare, uint32_t value)
     {
-        if(threadIdx.x == 0){
-            while(atomicCAS(base_ptr + offset, compare, value) != compare){}
+        if(threadIdx.x == 0)
+        {
+            while(atomicCAS(base_ptr + offset, compare, value) != compare) {}
         }
         __syncthreads();
     }
 
     // enter critical zoon, assume buffer is zero when launch kernel
-    __device__ void aquire(uint32_t offset)
-    {
-        wait_set(offset, 0, 1);
-    }
+    __device__ void aquire(uint32_t offset) { wait_set(offset, 0, 1); }
 
     // exit critical zoon, assume buffer is zero when launch kernel
-    __device__ void release(uint32_t offset)
-    {
-        wait_set(offset, 1, 0);
-    }
+    __device__ void release(uint32_t offset) { wait_set(offset, 1, 0); }
 
     __device__ void inc(uint32_t offset)
     {
         __syncthreads();
-        if(threadIdx.x == 0){
+        if(threadIdx.x == 0)
+        {
             atomicAdd(base_ptr + offset, 1);
         }
     }
 
-    uint32_t * base_ptr;
+    uint32_t* base_ptr;
 };
-}
+} // namespace ck
