6ab0ace06e00b03275779d1633ee6729bb88885e
rocking <chunyu.lai@amd.com>
2023-06-27 15:26:18 -0400

    Add avgpool bwd reference code

---
+++ library/include/ck/library/reference_tensor_operation/cpu/reference_avgpool_bwd.hpp
@@ -0,0 +1,341 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+#include "ck/tensor_operation/gpu/device/device_base.hpp"
+
+#include "ck/library/utility/host_tensor.hpp"
+
+namespace ck {
+namespace tensor_operation {
+namespace host {
+
+// input descriptor in [N, C, Do, Ho, Wo] order
+// output descriptor in [N, C, Di, Hi, Wi] order
+// phyiscal layout is irrelavent
+template <ck::index_t NDimSpatial,
+          typename InDataType,
+          typename OutDataType,
+          typename InElementwiseOperation,
+          typename OutElementwiseOperation,
+          typename std::enable_if<NDimSpatial >= 1 && NDimSpatial <= 3, bool>::type = false>
+struct ReferenceAvgPoolBwd : public device::BaseOperator
+{
+    // Argument
+    struct Argument : public device::BaseArgument
+    {
+        Argument(Tensor<InDataType>& input,
+                 const Tensor<OutDataType>& output,
+                 std::vector<ck::index_t> window_spatial_lengths,
+                 std::vector<ck::index_t> window_strides,
+                 std::vector<ck::index_t> window_dilations,
+                 std::vector<ck::index_t> input_left_pads,
+                 std::vector<ck::index_t> input_right_pads,
+                 InElementwiseOperation in_element_op,
+                 OutElementwiseOperation out_element_op)
+            : input_{input},
+              output_{output},
+              window_spatial_lengths_{window_spatial_lengths},
+              conv_strides_{window_strides},
+              conv_dilations_{window_dilations},
+              in_left_pads_{input_left_pads},
+              in_right_pads_{input_right_pads},
+              in_element_op_{in_element_op},
+              out_element_op_{out_element_op}
+        {
+        }
+
+        Tensor<InDataType>& input_;
+        const Tensor<OutDataType>& output_;
+
+        std::vector<ck::index_t> window_spatial_lengths_;
+        std::vector<index_t> conv_strides_;
+        std::vector<index_t> conv_dilations_;
+        std::vector<index_t> in_left_pads_;
+        std::vector<index_t> in_right_pads_;
+
+        InElementwiseOperation in_element_op_;
+        OutElementwiseOperation out_element_op_;
+    };
+
+    // Invoker
+    struct Invoker : public device::BaseInvoker
+    {
+        using Argument = ReferenceAvgPoolBwd::Argument;
+
+        float Run(const Argument& arg)
+        {
+            if(!(arg.input_.GetNumOfDimension() == NDimSpatial + 2 &&
+                 arg.output_.GetNumOfDimension() == NDimSpatial + 2))
+            {
+                throw std::runtime_error("wrong! inconsistent dimension");
+            }
+
+            if constexpr(NDimSpatial == 1)
+            {
+                auto f_ncw = [&](auto n, auto c, auto wi) {
+                    std::size_t X  = arg.window_spatial_lengths_[0];
+                    std::size_t Wo = arg.output_.GetLengths()[2];
+
+                    float v_acc = 0;
+
+                    for(std::size_t x = 0; x < X; ++x)
+                    {
+                        auto w_tmp = static_cast<ck::long_index_t>(wi) +
+                                     static_cast<ck::long_index_t>(arg.in_left_pads_[0]) -
+                                     static_cast<ck::long_index_t>(x * arg.conv_dilations_[0]);
+
+                        if(w_tmp % arg.conv_strides_[0] == 0)
+                        {
+                            auto wo = static_cast<ck::long_index_t>(w_tmp) /
+                                      static_cast<ck::long_index_t>(arg.conv_strides_[0]);
+
+                            if(wo >= 0 && ck::type_convert<std::size_t>(wo) < Wo)
+                            {
+                                float v_out = 0;
+                                arg.out_element_op_(v_out,
+                                                    ck::type_convert<float>(arg.output_(n, c, wo)));
+
+                                v_acc += v_out;
+                            }
+                        }
+                    }
+                    v_acc /= ck::type_convert<float>(X);
+
+                    float v_in;
+                    arg.in_element_op_(v_in, v_acc);
+
+                    arg.input_(n, c, wi) = ck::type_convert<InDataType>(v_acc);
+                };
+
+                make_ParallelTensorFunctor(f_ncw,
+                                           arg.input_.GetLengths()[0],
+                                           arg.input_.GetLengths()[1],
+                                           arg.input_.GetLengths()[2])(
+                    std::thread::hardware_concurrency());
+
+                return 0;
+            }
+            else if constexpr(NDimSpatial == 2)
+            {
+                auto f_nchw = [&](auto n, auto c, auto hi, auto wi) {
+                    std::size_t Y = arg.window_spatial_lengths_[0];
+                    std::size_t X = arg.window_spatial_lengths_[1];
+
+                    std::size_t Ho = arg.output_.GetLengths()[2];
+                    std::size_t Wo = arg.output_.GetLengths()[3];
+
+                    float v_acc = 0;
+
+                    for(std::size_t y = 0; y < Y; ++y)
+                    {
+                        auto h_tmp = static_cast<ck::long_index_t>(hi) +
+                                     static_cast<ck::long_index_t>(arg.in_left_pads_[0]) -
+                                     static_cast<ck::long_index_t>(y * arg.conv_dilations_[0]);
+                        if(h_tmp % arg.conv_strides_[0] == 0)
+                        {
+                            auto ho = static_cast<ck::long_index_t>(h_tmp) /
+                                      static_cast<ck::long_index_t>(arg.conv_strides_[0]);
+                            if(ho >= 0 && ck::type_convert<std::size_t>(ho) < Ho)
+                            {
+                                for(std::size_t x = 0; x < X; ++x)
+                                {
+                                    auto w_tmp =
+                                        static_cast<ck::long_index_t>(wi) +
+                                        static_cast<ck::long_index_t>(arg.in_left_pads_[1]) -
+                                        static_cast<ck::long_index_t>(x * arg.conv_dilations_[1]);
+                                    if(w_tmp % arg.conv_strides_[1] == 0)
+                                    {
+                                        auto wo =
+                                            static_cast<ck::long_index_t>(w_tmp) /
+                                            static_cast<ck::long_index_t>(arg.conv_strides_[1]);
+                                        if(wo >= 0 && ck::type_convert<std::size_t>(wo) < Wo)
+                                        {
+                                            float v_out = 0;
+                                            arg.out_element_op_(
+                                                v_out,
+                                                ck::type_convert<float>(arg.output_(n, c, ho, wo)));
+
+                                            v_acc += v_out;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    v_acc /= ck::type_convert<float>(Y * X);
+
+                    float v_in;
+
+                    arg.in_element_op_(v_in, v_acc);
+
+                    arg.input_(n, c, hi, wi) = ck::type_convert<InDataType>(v_acc);
+                };
+
+                make_ParallelTensorFunctor(f_nchw,
+                                           arg.input_.GetLengths()[0],
+                                           arg.input_.GetLengths()[1],
+                                           arg.input_.GetLengths()[2],
+                                           arg.input_.GetLengths()[3])(
+                    std::thread::hardware_concurrency());
+
+                return 0;
+            }
+            else if constexpr(NDimSpatial == 3)
+            {
+                auto f_ncdhw = [&](auto n, auto c, auto di, auto hi, auto wi) {
+                    std::size_t Z = arg.window_spatial_lengths_[0];
+                    std::size_t Y = arg.window_spatial_lengths_[1];
+                    std::size_t X = arg.window_spatial_lengths_[2];
+
+                    std::size_t Do = arg.output_.GetLengths()[2];
+                    std::size_t Ho = arg.output_.GetLengths()[3];
+                    std::size_t Wo = arg.output_.GetLengths()[4];
+
+                    float v_acc = 0;
+
+                    for(std::size_t z = 0; z < Z; ++z)
+                    {
+                        auto d_tmp = static_cast<ck::long_index_t>(di) +
+                                     static_cast<ck::long_index_t>(arg.in_left_pads_[0]) -
+                                     static_cast<ck::long_index_t>(z * arg.conv_dilations_[0]);
+                        if(d_tmp % arg.conv_strides_[0] == 0)
+                        {
+                            auto do_ = static_cast<ck::long_index_t>(d_tmp) /
+                                       static_cast<ck::long_index_t>(arg.conv_strides_[0]);
+                            if(do_ >= 0 && ck::type_convert<std::size_t>(do_) < Do)
+                            {
+                                for(std::size_t y = 0; y < Y; ++y)
+                                {
+                                    auto h_tmp =
+                                        static_cast<ck::long_index_t>(hi) +
+                                        static_cast<ck::long_index_t>(arg.in_left_pads_[1]) -
+                                        static_cast<ck::long_index_t>(y * arg.conv_dilations_[1]);
+                                    if(h_tmp % arg.conv_strides_[1] == 0)
+                                    {
+                                        auto ho =
+                                            static_cast<ck::long_index_t>(h_tmp) /
+                                            static_cast<ck::long_index_t>(arg.conv_strides_[1]);
+                                        if(ho >= 0 && ck::type_convert<std::size_t>(ho) < Ho)
+                                        {
+                                            for(std::size_t x = 0; x < X; ++x)
+                                            {
+                                                auto w_tmp = static_cast<ck::long_index_t>(wi) +
+                                                             static_cast<ck::long_index_t>(
+                                                                 arg.in_left_pads_[2]) -
+                                                             static_cast<ck::long_index_t>(
+                                                                 x * arg.conv_dilations_[2]);
+
+                                                if(w_tmp % arg.conv_strides_[2] == 0)
+                                                {
+                                                    auto wo = static_cast<ck::long_index_t>(w_tmp) /
+                                                              static_cast<ck::long_index_t>(
+                                                                  arg.conv_strides_[2]);
+                                                    if(wo >= 0 &&
+                                                       ck::type_convert<std::size_t>(wo) < Wo)
+                                                    {
+                                                        float v_out = 0;
+                                                        arg.out_element_op_(
+                                                            v_out,
+                                                            ck::type_convert<float>(
+                                                                arg.output_(n, c, do_, ho, wo)));
+
+                                                        v_acc += v_out;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    v_acc /= ck::type_convert<float>(Z * Y * X);
+
+                    float v_in;
+
+                    arg.in_element_op_(v_in, v_acc);
+
+                    arg.input_(n, c, di, hi, wi) = ck::type_convert<InDataType>(v_acc);
+                };
+
+                make_ParallelTensorFunctor(f_ncdhw,
+                                           arg.input_.GetLengths()[0],
+                                           arg.input_.GetLengths()[1],
+                                           arg.input_.GetLengths()[2],
+                                           arg.input_.GetLengths()[3],
+                                           arg.input_.GetLengths()[4])(
+                    std::thread::hardware_concurrency());
+
+                return 0;
+            }
+        }
+
+        float Run(const device::BaseArgument* p_arg,
+                  const StreamConfig& /* stream_config */ = StreamConfig{}) override
+        {
+            return Run(*dynamic_cast<const Argument*>(p_arg));
+        }
+    };
+
+    static constexpr bool IsValidCompilationParameter()
+    {
+        // TODO: properly implement this check
+        return true;
+    }
+
+    bool IsSupportedArgument(const device::BaseArgument*) override { return true; }
+
+    static auto MakeArgument(Tensor<InDataType>& input,
+                             const Tensor<OutDataType>& output,
+                             std::vector<ck::index_t> window_spatial_lengths,
+                             std::vector<ck::index_t> window_strides,
+                             std::vector<ck::index_t> window_dilations,
+                             std::vector<ck::index_t> input_left_pads,
+                             std::vector<ck::index_t> input_right_pads,
+                             InElementwiseOperation in_element_op,
+                             OutElementwiseOperation out_element_op)
+    {
+        if(window_spatial_lengths.size() != NDimSpatial || window_strides.size() != NDimSpatial ||
+           window_dilations.size() != NDimSpatial || input_left_pads.size() != NDimSpatial ||
+           input_right_pads.size() != NDimSpatial)
+            throw std::runtime_error("dimension is incorrect");
+
+        return Argument{input,
+                        output,
+                        window_spatial_lengths,
+                        window_strides,
+                        window_dilations,
+                        input_left_pads,
+                        input_right_pads,
+                        in_element_op,
+                        out_element_op};
+    }
+
+    static auto MakeInvoker() { return Invoker{}; }
+
+    virtual std::unique_ptr<device::BaseInvoker> MakeInvokerPointer()
+    {
+        return std::make_unique<Invoker>(Invoker{});
+    }
+
+    std::string GetTypeString() const override
+    {
+        auto str = std::stringstream();
+
+        // clang-format off
+        str << "ReferenceAvgPoolBwd"
+            << std::endl;
+        // clang-format on
+
+        return str.str();
+    }
+};
+
+} // namespace host
+} // namespace tensor_operation
+} // namespace ck
