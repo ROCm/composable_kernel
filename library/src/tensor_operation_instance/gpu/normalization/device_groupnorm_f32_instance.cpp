759fb65188459083baf13c09cb4a34b9a47114e6
rocking <ChunYu.Lai@amd.com>
2023-08-12 01:31:31 +0800

    Add Normalization splitk instances (#829)
    * Add normalization splitK to layernorm and groupnorm instances
    
    * Fix bug of GetKPerThread()
    
    * Refine naming
    
    * clang format
    
    [ROCm/composable_kernel commit: 03b8119e2eb7c2312eda9d32772c67f598304d86]

--- library/src/tensor_operation_instance/gpu/normalization/device_groupnorm_f32_instance.cpp
+++ library/src/tensor_operation_instance/gpu/normalization/device_groupnorm_f32_instance.cpp
@@ -17,6 +17,8 @@ void add_device_normalization_rank_5_3_f32_instances(
     add_device_operation_instances(instances,
                                    device_normalization_f32_generic_instance<Pass, 5, 3>{});
     add_device_operation_instances(instances, device_normalization_f32_instances<Pass, 5, 3>{});
+    add_device_operation_instances(instances,
+                                   device_normalization_splitk_f32_instances<Pass, 5, 3>{});
 }
 
 } // namespace instance
--- library/src/tensor_operation_instance/gpu/normalization/device_groupnorm_swish_f32_instance.cpp
+++ library/src/tensor_operation_instance/gpu/normalization/device_groupnorm_swish_f32_instance.cpp
@@ -17,6 +17,8 @@ void add_device_normalization_rank_5_3_swish_f32_instances(
     add_device_operation_instances(instances,
                                    device_normalization_f32_generic_instance<Swish, 5, 3>{});
     add_device_operation_instances(instances, device_normalization_f32_instances<Swish, 5, 3>{});
+    add_device_operation_instances(instances,
+                                   device_normalization_splitk_f32_instances<Swish, 5, 3>{});
 }
 
 } // namespace instance
