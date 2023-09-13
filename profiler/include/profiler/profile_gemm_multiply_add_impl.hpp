a66d14edf25d34cf2c4b021f8c27f12e5267d566
zjing14 <zhangjing14@gmail.com>
2023-09-12 22:17:56 -0500

    fixed fp8 issues (#894)
    * fixed fp8 init; and reference gemm
    
    * Update host_tensor_generator.hpp
    
    * fixed convert
    
    * fixed reference gemm
    
    * fixed comments
    
    * fixed comments
    
    * fixed ci
    
    * fixed computeType
    
    ---------
    
    Co-authored-by: Jing Zhang <jizha@amd.com>

--- profiler/include/profiler/profile_gemm_multiply_add_impl.hpp
+++ profiler/include/profiler/profile_gemm_multiply_add_impl.hpp
@@ -83,8 +83,8 @@ bool profile_gemm_multiply_add_impl(int do_verification,
         d1_m_n.GenerateTensorValue(GeneratorTensor_2<D1DataType>{-1, 1});
         break;
     default:
-        a_m_k.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 1.0});
-        b_k_n.GenerateTensorValue(GeneratorTensor_3<BDataType>{-0.5, 0.5});
+        a_m_k.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 0.2});
+        b_k_n.GenerateTensorValue(GeneratorTensor_3<BDataType>{-0.1, 0.1});
         d0_m_n.GenerateTensorValue(GeneratorTensor_3<D0DataType>{0.0, 1.0});
         d1_m_n.GenerateTensorValue(GeneratorTensor_3<D1DataType>{0.0, 1.0});
     }
