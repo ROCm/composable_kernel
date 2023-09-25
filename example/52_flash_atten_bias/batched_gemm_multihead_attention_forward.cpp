6bc73d4196e899a2a4739b3019ab6b97a7884187
letaoqin <letaoqin@amd.com>
2023-09-25 17:06:11 +0000

    rename example file name

--- example/52_flash_atten_bias/batched_gemm_multihead_attention_forward.cpp
+++ example/52_flash_atten_bias/batched_gemm_multihead_attention_forward.cpp
@@ -156,6 +156,6 @@ using ReferenceGemm1Instance = ck::tensor_operation::host::ReferenceBatchedGemm<
                                                                                 B1ElementOp,
                                                                                 CElementOp>;
 
-#include "run_batched_mutihead_attention_forward.inc"
+#include "run_batched_multihead_attention_forward.inc"
 
 int main(int argc, char* argv[]) { return run(argc, argv); }
