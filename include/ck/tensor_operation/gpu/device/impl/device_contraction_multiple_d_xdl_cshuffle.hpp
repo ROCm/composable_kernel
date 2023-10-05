3b3782dd0746cf68839caef8e0be6098852f3148
Illia Silin <98187287+illsilin@users.noreply.github.com>
2023-10-05 14:58:23 -0700

    Revert "Add support for mixed precision in contraction scale and bilinear" (#967)
    * Revert "Add support for mixed precision in contraction scale and bilinear (#936)"
    
    This reverts commit f7aff936cb9d02dc8e53a8a3ea8648e1058253a2.
    
    * revert commits #957 and #960
    
    [ROCm/composable_kernel commit: 4daedf8ca56f3bd93481708bd9d762045839ec20]

--- include/ck/tensor_operation/gpu/device/impl/device_contraction_multiple_d_xdl_cshuffle.hpp
+++ include/ck/tensor_operation/gpu/device/impl/device_contraction_multiple_d_xdl_cshuffle.hpp
@@ -112,7 +112,6 @@ template <index_t NumDimM,
           typename CShuffleDataType,
           typename DsDataType,
           typename EDataType,
-          typename ComputeDataType,
           typename AElementwiseOperation,
           typename BElementwiseOperation,
           typename CDEElementwiseOperation,
@@ -157,8 +156,7 @@ struct DeviceContractionMultipleD_Xdl_CShuffle
                                         EDataType,
                                         AElementwiseOperation,
                                         BElementwiseOperation,
-                                        CDEElementwiseOperation,
-                                        ComputeDataType>
+                                        CDEElementwiseOperation>
 {
     using DeviceOp = DeviceContractionMultipleD_Xdl_CShuffle;
 
@@ -311,6 +309,8 @@ struct DeviceContractionMultipleD_Xdl_CShuffle
     using BGridDesc_N_K  = decltype(MakeBGridDescriptor_N_K({}, {}));
     using DsGridDesc_M_N = remove_cvref_t<decltype(MakeDsGridDescriptor_M_N({{}}, {{}}))>;
     using EGridDesc_M_N  = decltype(MakeEGridDescriptor_M_N({}, {}));
+
+    using ComputeDataType = ADataType;
 
     // GridwiseGemm
     using GridwiseGemm = GridwiseGemmMultipleD_xdl_cshuffle<
