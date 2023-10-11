7328e0a5e022652b8c92fb1276956be8a390460b
Adam Osewski <19374865+aosewski@users.noreply.github.com>
2023-10-11 05:21:15 +0200

    Grouped Gemm with looping over the tiles. (#788)
    * Introduce LocalBlockToCTileMap.
    
    * Change the signature of CalculateBottomIndex() function which now does
    not accept any argument. The B2C map which is already passed as an
    argument to the kernel Run function is calculating block's local id
    already outside at kernel entry point __global__ function.
    The LocalB2C map stores as members local block ID.
    
    * Use LocalBlockToCTile map in device ops.
    
    * First draft of tile loop work distribution.
    
    * Fix typo.
    
    * Simplify kernel arguments.
    
    Calculate descriptors & B2C maps on the device.
    
    * Use looping kernel.
    
    * Fix B2C constructor.
    
    * Fix Navi21 errors.
    
    * Calculate tile start/end in device kernel.
    
    * Change Run API to accept user provided workspace buffer.
    
    * Add new line at EOF.
    
    * Move Gemm KernelArguments to device op interface.
    
    * Remove unused code.
    
    * Update API.
    
    * Launch grid size which is min of occupancy vs tile count
    
    * Get back to use constant memory for gemm descriptors.
    
    * Remove unused code.
    
    * Add default virtual method implementation.
    
    * Update comments to conform with doxygen style.
    
    * Fix doc style and unused parameters.
    
    * Add thread cluster lengths to kernel name.
    
    * Remove old splitk impl and replace it with tile looping one.
    
    * Modify instances.
    
    * set KPerBlock to 64
    * maximize wherever possible vector load size.
    
    * Fix instances cluster lengths.
    
    * Change comment style.
    
    * Use 128b store where possible in instances.
    
    * Update test cases, since KPerBlock has doubled.
    
    * Update output stream operator for Sequence.
    
    * Add pipeline version to GroupedGEMM device op type string.
    
    * Fix pipeline version type logging.
    
    * Fix input tensors type after merge.
    
    * Fix compiler error.
    
    * Fix output stream operator for Pipeline version.
    
    * Store using 128b.
    
    * Set of instances with kpb 32/64
    
    * Limit number of instances
    
    * Remove commented out instances.
    
    * Fix function name.
    
    * Limit the number of instances.
    
    Add pipline version to the regular instances
    
    * Change thr cluster layout for reading B tensor.
    
    * disabled failed instances
    
    ---------
    
    Co-authored-by: Adam Osewski <aosewski@amd.com>
    Co-authored-by: zjing14 <zhangjing14@gmail.com>
    Co-authored-by: Jing Zhang <jizha@amd.com>
    
    [ROCm/composable_kernel commit: a4f72a314a85732ed67d5aa8d1088d207a7e0e61]

--- include/ck/tensor_operation/gpu/device/device_grouped_gemm_splitk.hpp
+++ include/ck/tensor_operation/gpu/device/device_grouped_gemm_splitk.hpp
@@ -8,6 +8,57 @@ namespace ck {
 namespace tensor_operation {
 namespace device {
 
+///
+/// @brief      Structure representing single GEMM problem arguments.
+///
+///             The pointer to the vector of those structures is passed
+///             to the GroupedGEMM entry point kernel.
+///
+struct GroupedGemmKernelArguments
+{
+    __host__ __device__ GroupedGemmKernelArguments(const void* p_a_grid_,
+                                                   const void* p_b_grid_,
+                                                   void* p_c_grid_,
+                                                   index_t M_,
+                                                   index_t N_,
+                                                   index_t K_,
+                                                   index_t StrideA_,
+                                                   index_t StrideB_,
+                                                   index_t StrideC_)
+        : p_a_grid{p_a_grid_},
+          p_b_grid{p_b_grid_},
+          p_c_grid{p_c_grid_},
+          M{M_},
+          N{N_},
+          K{K_},
+          StrideA{StrideA_},
+          StrideB{StrideB_},
+          StrideC{StrideC_}
+    {
+    }
+
+    const void* p_a_grid;
+    const void* p_b_grid;
+    void* p_c_grid;
+    index_t M;
+    index_t N;
+    index_t K;
+    index_t StrideA;
+    index_t StrideB;
+    index_t StrideC;
+
+    void Print() const
+    {
+        std::cout << "arg {"
+                  << "M:" << M << ", "
+                  << "N:" << N << ", "
+                  << "K:" << K << ", "
+                  << "SA:" << StrideA << ", "
+                  << "SB:" << StrideB << ", "
+                  << "SC:" << StrideC << "}" << std::endl;
+    }
+};
+
 template <typename ALayout,
           typename BLayout,
           typename DsLayout,
@@ -31,7 +82,28 @@ struct DeviceGroupedGemmSplitK : public DeviceGroupedGemm<ALayout,
                                                           BElementwiseOperation,
                                                           CElementwiseOperation>
 {
-    virtual void SetKBatchSize(BaseArgument* p_arg, index_t kbatch) const = 0;
+    //----------------------------------------------------------------------------------------------
+    /// @brief      Sets the k batch size.
+    ///
+    /// @param      p_arg   Pointer to the Argument we're going to change.
+    /// @param[in]  kbatch  The kbatch value.
+    ///
+    virtual void SetKBatchSize([[maybe_unused]] BaseArgument* p_arg,
+                               [[maybe_unused]] index_t kbatch) const
+    {
+    }
+
+    //----------------------------------------------------------------------------------------------
+    /// @brief      Sets the device kernel arguments pointer.
+    ///
+    /// @param      p_arg              The pointer to the Argument we're going to update.
+    /// @param[in]  p_dev_kernel_args  The pointer to the device memory which contains kernel
+    ///                                arguments.
+    ///
+    virtual void SetDeviceKernelArgs([[maybe_unused]] BaseArgument* p_arg,
+                                     [[maybe_unused]] const void* p_dev_kernel_args) const
+    {
+    }
 };
 
 } // namespace device
