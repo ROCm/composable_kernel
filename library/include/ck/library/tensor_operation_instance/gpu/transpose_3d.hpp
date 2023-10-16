77a60235bd2a0168130bf211f179ff3d91992806
Astha Rai <astha.rai713@gmail.com>
2023-10-16 23:27:23 +0000

    implemented client ex with device_elementwise.hpp and device_elementwise_3d_impl.hpp

--- library/include/ck/library/tensor_operation_instance/gpu/transpose_3d.hpp
+++ library/include/ck/library/tensor_operation_instance/gpu/transpose_3d.hpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <memory>
 #include "ck/ck.hpp"
-#include "ck/tensor_operation/gpu/device/impl/device_elementwise_3d_impl.hpp"
+#include "ck/tensor_operation/gpu/device/device_elementwise.hpp"
 #include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
 #include "ck/library/tensor_operation_instance/device_operation_instance_factory.hpp"
 
@@ -15,11 +15,18 @@ namespace tensor_operation {
 namespace device {
 namespace instance {
 
+using F16 = ck::half_t;
+using F32 = float;
+
+using PassThrough = ck::tensor_operation::element_wise::PassThrough;
+
 void add_device_transpose_f16_instances(
-    std::vector<std::unique_ptr<DeviceElementwise3dImpl<F16, F16, NCDHW, 3>>>& instances);
+    std::vector<std::unique_ptr<DeviceElementwise<ck::Tuple<F16>, ck::Tuple<F16>, PassThrough, 5>>>&
+        instances);
 
 void add_device_transpose_f32_instances(
-    std::vector<std::unique_ptr<DeviceElementwise3dImpl<F32, F32, NCDHW, 3>>>& instances);
+    std::vector<std::unique_ptr<DeviceElementwise<ck::Tuple<F32>, ck::Tuple<F32>, PassThrough, 5>>>&
+        instances);
 
 template <typename InDataTypeTuple,
           typename OutDataTypeTuple,
@@ -27,19 +34,10 @@ template <typename InDataTypeTuple,
           index_t NumDim>
 struct DeviceOperationInstanceFactory<
     ck::tensor_operation::device::
-        DeviceElementwise3dImpl<InDataTypeTuple, OutDataTypeTuple, ElementwiseOperation, NumDim>>
+        DeviceElementwise<InDataTypeTuple, OutDataTypeTuple, ElementwiseOperation, NumDim>>
 {
-    using DeviceOp = DeviceElementwise3dImpl<InDataTypeTuple,
-                                             OutDataTypeTuple,
-                                             ElementwiseOperation,
-                                             NumDim_m, // choose how to set dims
-                                             NumDim_n,
-                                             NumDim_k,
-                                             MPerThread,
-                                             NPerThread,
-                                             KPerThread,
-                                             InScalarPerVectorSeq,
-                                             OutScalarPerVectorSeq>;
+    using DeviceOp =
+        DeviceElementwise3dImpl<InDataTypeTuple, OutDataTypeTuple, ElementwiseOperation, NumDim>;
 
     static auto GetInstances()
     {
--- library/src/tensor_operation_instance/gpu/transpose/device_transpose_instances_3d.cpp
+++ library/src/tensor_operation_instance/gpu/transpose/device_transpose_instances_3d.cpp
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2023, Advanced Micro Devices, Inc. All rights reserved.
 
+#include "ck/ck.hpp"
 #include "ck/library/tensor_operation_instance/gpu/transpose/device_transpose_instance.hpp"
+#include "ck/tensor_operation/gpu/device/tensor_layout.hpp"
 #include "ck/library/tensor_operation_instance/add_device_operation_instance.hpp"
 
 namespace ck {
@@ -9,21 +11,27 @@ namespace tensor_operation {
 namespace device {
 namespace instance {
 
+using F16         = ck::half_t;
+using F32         = float;
+using PassThrough = ck::tensor_operation::element_wise::PassThrough;
+
 void add_device_transpose_f16_instances(
-    std::vector<std::unique_ptr<DeviceElementwise3dImpl<F16, F16, NCDHW, 3>>>& instances)
+    std::vector<std::unique_ptr<DeviceElementwise<ck::Tuple<F16>, ck::Tuple<F16>, PassThrough, 5>>>&
+        instances)
 {
 #ifdef CK_ENABLE_FP16
-    add_device_operation_instances(instances, device_transpose_f16_instances<F16, F16, NCDHW, 3>{});
+    add_device_operation_instances(instances, device_transpose_f16_instances{});
 #else
     ignore = instances;
 #endif
 }
 
 void add_device_transpose_f32_instances(
-    std::vector<std::unique_ptr<DeviceElementwise3dImpl<F32, F32, NCDHW, 3>>>& instances)
+    std::vector<std::unique_ptr<DeviceElementwise<ck::Tuple<F32>, ck::Tuple<F32>, PassThrough, 5>>>&
+        instances)
 {
 #ifdef CK_ENABLE_FP32
-    add_device_operation_instances(instances, device_transpose_f32_instances<F32, F32, NCDHW, 3>{});
+    add_device_operation_instances(instances, device_transpose_f32_instances{});
 #else
     ignore = instances;
 #endif
