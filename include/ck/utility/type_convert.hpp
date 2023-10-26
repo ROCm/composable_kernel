a6f34ca5f2c2472a88e2b4080e2845c6e0c0ba19
Rostyslav Geyyer <rosty.geyyer@amd.com>
2023-10-26 19:49:06 +0000

    Fix the fp8 conversion

--- include/ck/utility/type_convert.hpp
+++ include/ck/utility/type_convert.hpp
@@ -110,7 +110,15 @@ inline __host__ __device__ f8_t type_convert<f8_t, float>(float x)
     uint32_t ival = 0;
     ival       = __builtin_amdgcn_cvt_pk_fp8_f32(val.fval, val.fval, ival, false); // false -> WORD0
     val.i32val = ival;
-    return val.i8val[0];
+    const uint8_t nan_code = 0x80;
+    if(val.i8val[0] == nan_code)
+    {
+        return NumericLimits<f8_t>::Max();
+    }
+    else
+    {
+        return val.i8val[0];
+    }
 #else
     constexpr bool negative_zero_nan = true;
     constexpr bool clip              = true;
