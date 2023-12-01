e6f44a47ea297e5b19f867379199195138f809dd
rocking <chunyu.lai@amd.com>
2023-12-01 09:37:58 -0500

    Change the example to f16

--- example/53_layernorm2d_bwd/layernorm2d_bwd_fp32.cpp
+++ example/53_layernorm2d_bwd/layernorm2d_bwd_fp32.cpp
@@ -17,30 +17,40 @@
 #include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_normalization_bwd_x_impl.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_normalization_bwd_gamma_beta_impl.hpp"
-#include "ck/library/reference_tensor_operation/cpu/reference_groupnorm_bwd.hpp"
+#include "ck/library/reference_tensor_operation/cpu/reference_layernorm_bwd.hpp"
 
-using DYDataType         = ck::half_t;
-using XDataType          = ck::half_t;
-using GammaDataType      = ck::half_t;
+using DYDataType         = float;
+using XDataType          = float;
+using GammaDataType      = float;
 using MeanInvStdDataType = float;
-using DGammaDataType     = ck::half_t;
-using DBetaDataType      = ck::half_t;
-using DXDataType         = ck::half_t;
+using DGammaDataType     = float;
+using DBetaDataType      = float;
+using DXDataType         = float;
 using ComputeDataType    = float;
 
-constexpr int Rank         = 5;
-constexpr int NumReduceDim = 3;
+constexpr int Rank         = 2;
+constexpr int NumReduceDim = 1;
 
-// Grouprnorm
-// kernel 1:                  M    , K
-// dy:     N, H, W, G, C -> N * G, H * W * C
-// x:      N, H, W, G, C -> N * G, H * W * C
-// gamma:  1, 1, 1, G, C -> 1 * G, 1 * 1 * C
-// mean:   N, 1, 1, G, 1 -> N * G, 1 * 1 * 1
-// rstd:   N, 1, 1, G, 1 -> N * G, 1 * 1 * 1
+// Layernorm:
 
-// dx:     N, H, W, G, C -> N * G, H * W * C
+// Input shape
+// dy:        [M, N]
+// x:         [M, N]
+// mean:      [M, 1]
+// inv_std:   [M, 1]
 
+// Output shape
+// dx:     [M, N]
+// dgamma: [1, N]
+// dbeta:  [1, N]
+
+// dgamma = reduce_sum(dy * (x - mean) * inv_std, axis=0)
+// dbeta = reduce_sum(dy, axis=0)
+
+// [CAUSION]
+// In DeviceNormalizationBwdXImpl & DeviceNormalizationBwdGammaBetaImpl, M is invarient dimension, K
+// is reduced dimension Hence, M in this example and DeviceNormalizationBwdGammaBetaImpl is
+// different
 using XDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdXImpl<
     DYDataType,
     XDataType,
@@ -54,28 +64,17 @@ using XDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdXImp
     8,     // MThreadClusterSize
     32,    // KThreadClusterSize
     1,     // MThreadSliceSize
-    8,     // KThreadSliceSize
+    4,     // KThreadSliceSize
     true,  // IsDYFastestDimReduced
-    8,     // DYSrcVectorSize
+    4,     // DYSrcVectorSize
     true,  // IsXFastestDimReduced
-    8,     // XSrcVectorSize
+    4,     // XSrcVectorSize
     true,  // IsGammaFastestDimReduced
-    8,     // GammaSrcVectorSize
+    4,     // GammaSrcVectorSize
     false, // IsMeanInvStdFastestDimReduced
     1,     // MeanInvStdSrcVectorSize
     true,  // IsDXFastestDimReduced
-    8>;    // DXDstVectorSize
-
-// kernel 2:                  M    , K
-// dy:     N, H, W, G, C -> G * C, N * H * W
-// x:      N, H, W, G, C -> G * C, N * H * W
-// mean:   N, 1, 1, G, 1 -> G * 1, N * 1 * 1
-// rstd:   N, 1, 1, G, 1 -> G * 1, N * 1 * 1
-
-// dgamma: 1, 1, 1, G, C -> G * C
-// dbeta:  1, 1, 1, G, C -> G * C
-
-// reduced axis: 0, 1, 2
+    4>;    // DXDstVectorSize
 
 using GammaBetaDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdGammaBetaImpl<
     DYDataType,
@@ -87,38 +86,35 @@ using GammaBetaDeviceInstance = ck::tensor_operation::device::DeviceNormalizatio
     Rank,
     NumReduceDim,
     256,   // BlockSize
-    8,     // ClusterInvarient
-    32,    // ClusterReduce
-    8,     // SliceInvarient
-    1,     // SliceReduce
+    8,     // MThreadClusterSize
+    32,    // KThreadClusterSize
+    4,     // MThreadSliceSize
+    1,     // KThreadSliceSize
     false, // IsDYFastestDimReduced
-    8,     // DYSrcVectorSize
+    4,     // DYSrcVectorSize
     false, // IsXFastestDimReduced
-    8,     // XSrcVectorSize
-    false, // IsMeanInvStdFastestDimReduced
+    4,     // XSrcVectorSize
+    true,  // IsMeanInvStdFastestDimReduced
     1,     // MeanInvStdSrcVectorSize
-    8,     // DGammaDstVectorSize
-    8>;    // DBetaDstVectorSize
+    4,     // DGammaDstVectorSize
+    4>;    // DBetaDstVectorSize
 
 int main()
 {
     bool time_kernel = false;
 
-    ck::index_t N = 16;
-    ck::index_t H = 16;
-    ck::index_t W = 16;
-    ck::index_t G = 32;
-    ck::index_t C = 64;
+    ck::index_t M = 1024;
+    ck::index_t N = 512;
 
-    Tensor<DYDataType> dy({N, H, W, G, C});
-    Tensor<XDataType> x({N, H, W, G, C});
-    Tensor<GammaDataType> gamma({G, C});
-    Tensor<MeanInvStdDataType> mean({N, G});
-    Tensor<MeanInvStdDataType> inv_std({N, G});
+    Tensor<DYDataType> dy({M, N});
+    Tensor<XDataType> x({M, N});
+    Tensor<GammaDataType> gamma({N});
+    Tensor<MeanInvStdDataType> mean({M});
+    Tensor<MeanInvStdDataType> inv_std({M});
 
-    Tensor<DGammaDataType> dgamma({G, C});
-    Tensor<DBetaDataType> dbeta({G, C});
-    Tensor<DXDataType> dx({N, H, W, G, C});
+    Tensor<DGammaDataType> dgamma({N});
+    Tensor<DBetaDataType> dbeta({N});
+    Tensor<DXDataType> dx({M, N});
 
     dy.GenerateTensorValue(GeneratorTensor_3<DYDataType>{0.0, 1.0});
     x.GenerateTensorValue(GeneratorTensor_3<XDataType>{0.0, 1.0});
@@ -141,24 +137,17 @@ int main()
     mean_dev.ToDevice(mean.mData.data());
     inv_std_dev.ToDevice(inv_std.mData.data());
 
-    std::vector<ck::index_t> dyStrides{dy.mDesc.GetStrides().begin(), dy.mDesc.GetStrides().end()};
-    std::vector<ck::index_t> xStrides{x.mDesc.GetStrides().begin(), x.mDesc.GetStrides().end()};
-    std::vector<ck::index_t> gammaStrides  = {0, 0, 0, C, 1};
-    std::vector<ck::index_t> meanStrides   = {G, 0, 0, 1, 0};
-    std::vector<ck::index_t> invStdStrides = {G, 0, 0, 1, 0};
-    std::vector<ck::index_t> dxStrides{dx.mDesc.GetStrides().begin(), dx.mDesc.GetStrides().end()};
-
     // backward x
     auto x_device_instance = XDeviceInstance{};
 
-    auto x_argument_ptr = x_device_instance.MakeArgumentPointer({N, H, W, G, C}, // lengths
-                                                                dyStrides,       // dyStrides
-                                                                xStrides,        // xStrides
-                                                                gammaStrides,    // gammaStrides
-                                                                meanStrides,     // meanStrides
-                                                                invStdStrides,   // invStdStrides
-                                                                dxStrides,       // dxStrides
-                                                                {1, 2, 4},       // reduceDims
+    auto x_argument_ptr = x_device_instance.MakeArgumentPointer({M, N}, // lengths
+                                                                {N, 1}, // dyStrides
+                                                                {N, 1}, // xStrides
+                                                                {0, 1}, // gammaStrides
+                                                                {1, 0}, // meanStrides
+                                                                {1, 0}, // invStdStrides
+                                                                {N, 1}, // dxStrides
+                                                                {1},    // reduceDims
                                                                 dy_dev.GetDeviceBuffer(),
                                                                 x_dev.GetDeviceBuffer(),
                                                                 gamma_dev.GetDeviceBuffer(),
@@ -177,18 +166,17 @@ int main()
     x_invoker_ptr->Run(x_argument_ptr.get(), StreamConfig{nullptr, time_kernel});
 
     // backward gamma & beta
-
     auto gamma_beta_device_instance = GammaBetaDeviceInstance{};
     auto gamma_beta_argument_ptr =
-        gamma_beta_device_instance.MakeArgumentPointer({N, H, W, G, C}, // inLengths
-                                                       dyStrides,       // dyStrides
-                                                       xStrides,        // xStrides
-                                                       meanStrides,     // meanStrides
-                                                       invStdStrides,   // invStdStrides
-                                                       {G, C},          // outLengths
-                                                       {C, 1},          // dgammaStrides
-                                                       {C, 1},          // dbetaStrides
-                                                       {0, 1, 2},       // reduceDims
+        gamma_beta_device_instance.MakeArgumentPointer({M, N}, // inLengths
+                                                       {N, 1}, // dyStrides
+                                                       {N, 1}, // xStrides
+                                                       {1, 0}, // meanStrides
+                                                       {1, 0}, // invStdStrides
+                                                       {N},    // outLengths
+                                                       {1},    // dgammaStrides
+                                                       {1},    // dbetaStrides
+                                                       {0},    // reduceDims
                                                        dy_dev.GetDeviceBuffer(),
                                                        x_dev.GetDeviceBuffer(),
                                                        mean_dev.GetDeviceBuffer(),
@@ -208,11 +196,11 @@ int main()
 
     bool pass = true;
     {
-        Tensor<DGammaDataType> host_dgamma({G, C});
-        Tensor<DBetaDataType> host_dbeta({G, C});
-        Tensor<DXDataType> host_dx({N, H, W, G, C});
+        Tensor<DGammaDataType> host_dgamma({N});
+        Tensor<DBetaDataType> host_dbeta({N});
+        Tensor<DXDataType> host_dx({M, N});
         using ReferenceInstance =
-            ck::tensor_operation::host::ReferenceGroupnormBwd<DYDataType,
+            ck::tensor_operation::host::ReferenceLayernormBwd<DYDataType,
                                                               XDataType,
                                                               GammaDataType,
                                                               MeanInvStdDataType,
@@ -222,8 +210,8 @@ int main()
                                                               ComputeDataType>;
 
         ReferenceInstance ref;
-        auto ref_argument = ref.MakeArgument(
-            dy, x, gamma, mean, inv_std, host_dgamma, host_dbeta, host_dx, {N, H, W, G, C});
+        auto ref_argument =
+            ref.MakeArgument(dy, x, gamma, mean, inv_std, host_dgamma, host_dbeta, host_dx, {M, N});
         auto ref_invoker = ref.MakeInvoker();
         ref_invoker.Run(ref_argument);
 
--- example/54_groupnorm_bwd/groupnorm_bwd_fp32.cpp
+++ example/54_groupnorm_bwd/groupnorm_bwd_fp32.cpp
@@ -17,40 +17,30 @@
 #include "ck/tensor_operation/gpu/element/element_wise_operation.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_normalization_bwd_x_impl.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_normalization_bwd_gamma_beta_impl.hpp"
-#include "ck/library/reference_tensor_operation/cpu/reference_layernorm_bwd.hpp"
+#include "ck/library/reference_tensor_operation/cpu/reference_groupnorm_bwd.hpp"
 
-using DYDataType         = ck::half_t;
-using XDataType          = ck::half_t;
-using GammaDataType      = ck::half_t;
+using DYDataType         = float;
+using XDataType          = float;
+using GammaDataType      = float;
 using MeanInvStdDataType = float;
-using DGammaDataType     = ck::half_t;
-using DBetaDataType      = ck::half_t;
-using DXDataType         = ck::half_t;
+using DGammaDataType     = float;
+using DBetaDataType      = float;
+using DXDataType         = float;
 using ComputeDataType    = float;
 
-constexpr int Rank         = 2;
-constexpr int NumReduceDim = 1;
+constexpr int Rank         = 5;
+constexpr int NumReduceDim = 3;
 
-// Layernorm:
+// Grouprnorm
+// kernel 1:                  M    , K
+// dy:     N, H, W, G, C -> N * G, H * W * C
+// x:      N, H, W, G, C -> N * G, H * W * C
+// gamma:  1, 1, 1, G, C -> 1 * G, 1 * 1 * C
+// mean:   N, 1, 1, G, 1 -> N * G, 1 * 1 * 1
+// rstd:   N, 1, 1, G, 1 -> N * G, 1 * 1 * 1
 
-// Input shape
-// dy:        [M, N]
-// x:         [M, N]
-// mean:      [M, 1]
-// inv_std:   [M, 1]
+// dx:     N, H, W, G, C -> N * G, H * W * C
 
-// Output shape
-// dx:     [M, N]
-// dgamma: [1, N]
-// dbeta:  [1, N]
-
-// dgamma = reduce_sum(dy * (x - mean) * inv_std, axis=0)
-// dbeta = reduce_sum(dy, axis=0)
-
-// [CAUSION]
-// In DeviceNormalizationBwdXImpl & DeviceNormalizationBwdGammaBetaImpl, M is invarient dimension, K
-// is reduced dimension Hence, M in this example and DeviceNormalizationBwdGammaBetaImpl is
-// different
 using XDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdXImpl<
     DYDataType,
     XDataType,
@@ -64,17 +54,28 @@ using XDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdXImp
     8,     // MThreadClusterSize
     32,    // KThreadClusterSize
     1,     // MThreadSliceSize
-    8,     // KThreadSliceSize
+    4,     // KThreadSliceSize
     true,  // IsDYFastestDimReduced
-    8,     // DYSrcVectorSize
+    4,     // DYSrcVectorSize
     true,  // IsXFastestDimReduced
-    8,     // XSrcVectorSize
+    4,     // XSrcVectorSize
     true,  // IsGammaFastestDimReduced
-    8,     // GammaSrcVectorSize
+    4,     // GammaSrcVectorSize
     false, // IsMeanInvStdFastestDimReduced
     1,     // MeanInvStdSrcVectorSize
     true,  // IsDXFastestDimReduced
-    8>;    // DXDstVectorSize
+    4>;    // DXDstVectorSize
+
+// kernel 2:                  M    , K
+// dy:     N, H, W, G, C -> G * C, N * H * W
+// x:      N, H, W, G, C -> G * C, N * H * W
+// mean:   N, 1, 1, G, 1 -> G * 1, N * 1 * 1
+// rstd:   N, 1, 1, G, 1 -> G * 1, N * 1 * 1
+
+// dgamma: 1, 1, 1, G, C -> G * C
+// dbeta:  1, 1, 1, G, C -> G * C
+
+// reduced axis: 0, 1, 2
 
 using GammaBetaDeviceInstance = ck::tensor_operation::device::DeviceNormalizationBwdGammaBetaImpl<
     DYDataType,
@@ -86,35 +87,38 @@ using GammaBetaDeviceInstance = ck::tensor_operation::device::DeviceNormalizatio
     Rank,
     NumReduceDim,
     256,   // BlockSize
-    8,     // MThreadClusterSize
-    32,    // KThreadClusterSize
-    8,     // MThreadSliceSize
-    1,     // KThreadSliceSize
+    8,     // ClusterInvarient
+    32,    // ClusterReduce
+    4,     // SliceInvarient
+    1,     // SliceReduce
     false, // IsDYFastestDimReduced
-    8,     // DYSrcVectorSize
+    4,     // DYSrcVectorSize
     false, // IsXFastestDimReduced
-    8,     // XSrcVectorSize
-    true,  // IsMeanInvStdFastestDimReduced
+    4,     // XSrcVectorSize
+    false, // IsMeanInvStdFastestDimReduced
     1,     // MeanInvStdSrcVectorSize
-    8,     // DGammaDstVectorSize
-    8>;    // DBetaDstVectorSize
+    4,     // DGammaDstVectorSize
+    4>;    // DBetaDstVectorSize
 
 int main()
 {
     bool time_kernel = false;
 
-    ck::index_t M = 1024;
-    ck::index_t N = 512;
+    ck::index_t N = 16;
+    ck::index_t H = 16;
+    ck::index_t W = 16;
+    ck::index_t G = 32;
+    ck::index_t C = 64;
 
-    Tensor<DYDataType> dy({M, N});
-    Tensor<XDataType> x({M, N});
-    Tensor<GammaDataType> gamma({N});
-    Tensor<MeanInvStdDataType> mean({M});
-    Tensor<MeanInvStdDataType> inv_std({M});
+    Tensor<DYDataType> dy({N, H, W, G, C});
+    Tensor<XDataType> x({N, H, W, G, C});
+    Tensor<GammaDataType> gamma({G, C});
+    Tensor<MeanInvStdDataType> mean({N, G});
+    Tensor<MeanInvStdDataType> inv_std({N, G});
 
-    Tensor<DGammaDataType> dgamma({N});
-    Tensor<DBetaDataType> dbeta({N});
-    Tensor<DXDataType> dx({M, N});
+    Tensor<DGammaDataType> dgamma({G, C});
+    Tensor<DBetaDataType> dbeta({G, C});
+    Tensor<DXDataType> dx({N, H, W, G, C});
 
     dy.GenerateTensorValue(GeneratorTensor_3<DYDataType>{0.0, 1.0});
     x.GenerateTensorValue(GeneratorTensor_3<XDataType>{0.0, 1.0});
@@ -137,17 +141,24 @@ int main()
     mean_dev.ToDevice(mean.mData.data());
     inv_std_dev.ToDevice(inv_std.mData.data());
 
+    std::vector<ck::index_t> dyStrides{dy.mDesc.GetStrides().begin(), dy.mDesc.GetStrides().end()};
+    std::vector<ck::index_t> xStrides{x.mDesc.GetStrides().begin(), x.mDesc.GetStrides().end()};
+    std::vector<ck::index_t> gammaStrides  = {0, 0, 0, C, 1};
+    std::vector<ck::index_t> meanStrides   = {G, 0, 0, 1, 0};
+    std::vector<ck::index_t> invStdStrides = {G, 0, 0, 1, 0};
+    std::vector<ck::index_t> dxStrides{dx.mDesc.GetStrides().begin(), dx.mDesc.GetStrides().end()};
+
     // backward x
     auto x_device_instance = XDeviceInstance{};
 
-    auto x_argument_ptr = x_device_instance.MakeArgumentPointer({M, N}, // lengths
-                                                                {N, 1}, // dyStrides
-                                                                {N, 1}, // xStrides
-                                                                {0, 1}, // gammaStrides
-                                                                {1, 0}, // meanStrides
-                                                                {1, 0}, // invStdStrides
-                                                                {N, 1}, // dxStrides
-                                                                {1},    // reduceDims
+    auto x_argument_ptr = x_device_instance.MakeArgumentPointer({N, H, W, G, C}, // lengths
+                                                                dyStrides,       // dyStrides
+                                                                xStrides,        // xStrides
+                                                                gammaStrides,    // gammaStrides
+                                                                meanStrides,     // meanStrides
+                                                                invStdStrides,   // invStdStrides
+                                                                dxStrides,       // dxStrides
+                                                                {1, 2, 4},       // reduceDims
                                                                 dy_dev.GetDeviceBuffer(),
                                                                 x_dev.GetDeviceBuffer(),
                                                                 gamma_dev.GetDeviceBuffer(),
@@ -166,17 +177,18 @@ int main()
     x_invoker_ptr->Run(x_argument_ptr.get(), StreamConfig{nullptr, time_kernel});
 
     // backward gamma & beta
+
     auto gamma_beta_device_instance = GammaBetaDeviceInstance{};
     auto gamma_beta_argument_ptr =
-        gamma_beta_device_instance.MakeArgumentPointer({M, N}, // inLengths
-                                                       {N, 1}, // dyStrides
-                                                       {N, 1}, // xStrides
-                                                       {1, 0}, // meanStrides
-                                                       {1, 0}, // invStdStrides
-                                                       {N},    // outLengths
-                                                       {1},    // dgammaStrides
-                                                       {1},    // dbetaStrides
-                                                       {0},    // reduceDims
+        gamma_beta_device_instance.MakeArgumentPointer({N, H, W, G, C}, // inLengths
+                                                       dyStrides,       // dyStrides
+                                                       xStrides,        // xStrides
+                                                       meanStrides,     // meanStrides
+                                                       invStdStrides,   // invStdStrides
+                                                       {G, C},          // outLengths
+                                                       {C, 1},          // dgammaStrides
+                                                       {C, 1},          // dbetaStrides
+                                                       {0, 1, 2},       // reduceDims
                                                        dy_dev.GetDeviceBuffer(),
                                                        x_dev.GetDeviceBuffer(),
                                                        mean_dev.GetDeviceBuffer(),
@@ -196,11 +208,11 @@ int main()
 
     bool pass = true;
     {
-        Tensor<DGammaDataType> host_dgamma({N});
-        Tensor<DBetaDataType> host_dbeta({N});
-        Tensor<DXDataType> host_dx({M, N});
+        Tensor<DGammaDataType> host_dgamma({G, C});
+        Tensor<DBetaDataType> host_dbeta({G, C});
+        Tensor<DXDataType> host_dx({N, H, W, G, C});
         using ReferenceInstance =
-            ck::tensor_operation::host::ReferenceLayernormBwd<DYDataType,
+            ck::tensor_operation::host::ReferenceGroupnormBwd<DYDataType,
                                                               XDataType,
                                                               GammaDataType,
                                                               MeanInvStdDataType,
@@ -210,8 +222,8 @@ int main()
                                                               ComputeDataType>;
 
         ReferenceInstance ref;
-        auto ref_argument =
-            ref.MakeArgument(dy, x, gamma, mean, inv_std, host_dgamma, host_dbeta, host_dx, {M, N});
+        auto ref_argument = ref.MakeArgument(
+            dy, x, gamma, mean, inv_std, host_dgamma, host_dbeta, host_dx, {N, H, W, G, C});
         auto ref_invoker = ref.MakeInvoker();
         ref_invoker.Run(ref_argument);
 
