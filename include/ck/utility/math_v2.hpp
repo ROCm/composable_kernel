5a24af3815e04db4104f5881a1db9187726e48fb
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2023-12-01 16:35:23 +0000

    Disable exp() and log() overloading for half_t to support xformers C++ extension building

--- include/ck/utility/math_v2.hpp
+++ include/ck/utility/math_v2.hpp
@@ -312,11 +312,13 @@ inline __device__ float exp<float>(float x)
     return __expf(x);
 }
 
+/*
 template <>
 inline __device__ half_t exp<half_t>(half_t x)
 {
     return hexp(x);
 };
+*/
 
 template <>
 inline __device__ double exp<double>(double x)
@@ -346,11 +348,13 @@ inline __device__ T log(T x)
     return ck::type_convert<T>(__logf(ck::type_convert<float>(x)));
 };
 
+/*
 template <>
 inline __device__ half_t log<half_t>(half_t x)
 {
     return hlog(x);
 };
+*/
 
 template <>
 inline __device__ float log<float>(float x)
