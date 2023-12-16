c3ee4f92a3396ed61b534290f54b24ce7133ca83
Astha Rai <astha.rai713@gmail.com>
2023-12-16 09:58:35 +0000

    fixed transpose profiler errors

--- profiler/src/profile_transpose.cpp
+++ profiler/src/profile_transpose.cpp
@@ -18,6 +18,39 @@ enum struct DataType
 #define OP_NAME "transpose"
 #define OP_DESC "Transpose"
 
+struct TransposeArgParser
+{
+    std::unordered_map<std::string, std::vector<int>> long_opts = {{"lengths", {}}};
+
+    bool parse_opt(int argc, char* argv[], const std::string& key, int i)
+    {
+        if(std::string("--") + key == argv[i])
+        {
+            int pos = i;
+            while(++i < argc && argv[i][0] != '-') {}
+            int end = i;
+            for(int j = pos + 1; j < end; j++)
+            {
+                long_opts[key].push_back(std::stoi(argv[j]));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void operator()(int argc, char* argv[])
+    {
+        for(auto& kv : long_opts)
+        {
+            for(int i = 1; i < argc; i++)
+            {
+                if(parse_opt(argc, argv, kv.first, i))
+                    break;
+            }
+        }
+    }
+};
+
 int profile_transpose(int argc, char* argv[])
 {
     if(argc != 7)
@@ -28,21 +61,24 @@ int profile_transpose(int argc, char* argv[])
         printf("arg4: initialization (0: no init; 1: integer value; 2: decimal value)\n");
         printf("arg5: print tensor value (0: no; 1: yes)\n");
         printf("arg6: time kernel (0=no, 1=yes)\n");
-        printf("arg7: lengths: N, C, D, H, W\n");
+        printf("arg7: --lengths: N, C, D, H, W\n");
         exit(1);
     }
+    TransposeArgParser arg_parser;
 
-    const auto data_type         = static_cast<DataType>(std::stoi(argv[2]));
-    const bool do_verification   = std::stoi(argv[3]);
-    const int init_method        = std::stoi(argv[4]);
-    const bool do_log            = std::stoi(argv[5]);
-    const bool time_kernel       = std::stoi(argv[6]);
-    std::vector<index_t> lengths = std::stoi(argv[7]);
+    const auto data_type       = static_cast<DataType>(std::stoi(argv[2]));
+    const bool do_verification = std::stoi(argv[3]);
+    const int init_method      = std::stoi(argv[4]);
+    const bool do_log          = std::stoi(argv[5]);
+    const bool time_kernel     = std::stoi(argv[6]);
+    arg_parser(argc, argv);
+    const std::vector<ck::index_t> lengths = arg_parser.long_opts["lengths"];
+    // std::vector<ck::index_t> lengths = std::stoi(argv[7]);
 
     using F32 = float;
     using F16 = ck::half_t;
 
-    auto profile = [&](auto a_type, auto b_type, 5) {
+    auto profile = [&](auto a_type, auto b_type) {
         using ADataType = decltype(a_type);
         using BDataType = decltype(b_type);
 
@@ -52,13 +88,13 @@ int profile_transpose(int argc, char* argv[])
         return pass ? 0 : 1;
     };
 
-    if(data_type == GemmDataType::F32_F32_F32_F32_F32)
+    if(data_type == DataType::F32_F32_F32_F32_F32)
     {
-        return profile(F32{}, F32{}, 5);
+        return profile(F32{}, F32{});
     }
-    else if(data_type == GemmDataType::F16_F16_F16_F16_F16)
+    else if(data_type == DataType::F16_F16_F16_F16_F16)
     {
-        return profile(F16{}, F16{}, 5);
+        return profile(F16{}, F16{});
     }
     else
     {
