2e39167b38450a836600b676805655e1e72b99d9
illsilin <Illia.Silin@amd.com>
2024-01-02 12:59:38 -0800

    change the googletest cmake syntax for older cmake versions

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -10,7 +10,6 @@ FetchContent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG f8d7d77c06936315286eb55f8de22cd23c188571
-    SYSTEM
 )
 
 # Suppress ROCMChecks WARNING on GoogleTests
