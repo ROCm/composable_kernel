da1df0e5d2a65c91b0ec15958378e9dd2480afb9
letaoqin <letaoqin@amd.com>
2024-02-05 18:54:56 +0000

    close dropout

--- example/91_tile_program/fmha/fmha_fwd.cpp
+++ example/91_tile_program/fmha/fmha_fwd.cpp
@@ -74,7 +74,7 @@ auto create_args(int argc, char* argv[])
                 "11939",
                 "random seed used for initializing input tensors. 0 to use "
                 "non-deterministic random number as seed")
-        .insert("p_drop", "0.1", "0~1 probability of dropout")
+        .insert("p_drop", "0", "0~1 probability of dropout")
         .insert("drop_seed", "1", "seed for random number maker")
         .insert("drop_offset", "0", "offset for random number maker");
 
@@ -146,9 +146,9 @@ bool run(const ArgParser& arg_parser)
     float descale_k = arg_parser.get_float("descale_k");
     float descale_v = arg_parser.get_float("descale_v");
 
-    std::string vlayout = arg_parser.get_str("vlayout");
-    bool use_bias       = arg_parser.get_bool("bias");
-    bool lse            = arg_parser.get_bool("lse");
+    std::string vlayout  = arg_parser.get_str("vlayout");
+    bool use_bias        = arg_parser.get_bool("bias");
+    bool lse             = arg_parser.get_bool("lse");
     float p_drop         = arg_parser.get_float("p_drop");
     uint64_t drop_seed   = arg_parser.get_uint64("drop_seed");
     uint64_t drop_offset = arg_parser.get_uint32("drop_offset");
@@ -157,7 +157,6 @@ bool run(const ArgParser& arg_parser)
         std::cerr << "The value of p_drop should be 0~1" << std::endl;
         return false;
     }
-
 
     mask_info mask = mask_info::decode(arg_parser.get_str("mask"), seqlen_q, seqlen_k);
 
@@ -483,14 +482,16 @@ bool run(const ArgParser& arg_parser)
                 s_host_ref, p_host_ref);
         }
 
-        if(p_drop > 0){
+        if(p_drop > 0)
+        {
             Tensor<DropDataType> drop_host_result({nhead, real_seqlen_q, real_seqlen_k});
             drop_host_result.ForEach([&](auto& self, auto idx) {
                 self(idx) = drop_host(b, idx[0], idx[1] + query_offset, idx[2] + key_offset);
             });
-            reference_batched_dropout(p_host_ref, drop_host_result, p_dropout_in_uint8_t, rp_dropout);
+            reference_batched_dropout(
+                p_host_ref, drop_host_result, p_dropout_in_uint8_t, rp_dropout);
         }
-        
+
         reference_batched_gemm<PDataType, VDataType, OaccDataType, ODataType>(
             p_host_ref, v_host_ref, o_host_ref);
 
