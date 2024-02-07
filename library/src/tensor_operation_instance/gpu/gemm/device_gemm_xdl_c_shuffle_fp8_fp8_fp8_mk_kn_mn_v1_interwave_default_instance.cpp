5c5e17c640d3a51cca3f49e4d3da98b10884fc21
Illia Silin <98187287+illsilin@users.noreply.github.com>
2024-02-07 12:47:12 -0800

    Split-up instances to improve build times. (#1159)
    * split up splitk-gemm instances
    
    * clean up some unused variables
    
    * split the mk_kn_mn interwave splitk-gemm instances
    
    * split up f16_f16_f16 mk_nk_mn splitk gemm instances
    
    * fix clang format
    
    * fix function names
    
    * fix typo
    
    * split up the 2 largest fp16*fp8 splitk gemm instances
    
    * get rid of unused variables
    
    * split up the largest splitk-gemm fp8*fp16 instance file
    
    * split up the instances for xdl fp8 gemms
    
    * split the headers for f16 and i8 for wmmma convolution instances
    
    [ROCm/composable_kernel commit: 1b0fbaebbb7a9235cf44c4b32a2698b97509901d]

--- library/src/tensor_operation_instance/gpu/gemm/device_gemm_xdl_c_shuffle_fp8_fp8_fp8_mk_kn_mn_v1_interwave_default_instance.cpp
+++ library/src/tensor_operation_instance/gpu/gemm/device_gemm_xdl_c_shuffle_fp8_fp8_fp8_mk_kn_mn_v1_interwave_default_instance.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
 
-#include "ck/library/tensor_operation_instance/gpu/device_gemm_xdl_c_shuffle_fp8_fp8_fp8_mk_kn_mn_instance.hpp"
+#include "ck/library/tensor_operation_instance/gpu/device_gemm_xdl_c_shuffle_fp8_fp8_fp8_mk_kn_mn_v1_interwave_instance.hpp"
 
 #ifdef CK_ENABLE_FP8
 namespace ck {
@@ -9,14 +9,15 @@ namespace tensor_operation {
 namespace device {
 namespace instance {
 
-static constexpr auto MNKPadding = ck::tensor_operation::device::GemmSpecialization::MNKPadding;
+static constexpr auto GemmDefault = ck::tensor_operation::device::GemmSpecialization::Default;
 
-void add_device_gemm_xdl_c_shuffle_f8_f8_f8_mk_kn_mn_padded_instances(
+void add_device_gemm_xdl_c_shuffle_f8_f8_f8_mk_kn_mn_v1_interwave_default_instances(
     std::vector<std::unique_ptr<
         DeviceGemm<Row, Row, Row, F8, F8, F8, PassThrough, PassThrough, PassThrough>>>& instances)
 {
     add_device_operation_instances(
-        instances, device_gemm_xdl_c_shuffle_f8_f8_f8_mk_kn_mn_instances<MNKPadding>{});
+        instances,
+        device_gemm_xdl_c_shuffle_f8_f8_f8_mk_kn_mn_v1_interwave_instances<GemmDefault>{});
 }
 
 } // namespace instance
