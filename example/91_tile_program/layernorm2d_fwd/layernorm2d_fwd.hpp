3b39350c048d60a0e0b6a51e6424f068ae76ebbe
rocking <chunyu.lai@amd.com>
2024-02-22 14:04:23 -0500

    Refine naming

--- example/91_tile_program/layernorm2d_fwd/layernorm2d_fwd.hpp
+++ example/91_tile_program/layernorm2d_fwd/layernorm2d_fwd.hpp
@@ -3,6 +3,6 @@
 
 #pragma once
 
-#include "ck/tile_program/block_tile_pipeline/block_layernorm2d_fwd_pipeline_problem.hpp"
+#include "ck/tile_program/block_tile_pipeline/block_layernorm2d_fwd_problem.hpp"
 #include "ck/tile_program/tile/tile_layernorm2d_fwd_shape.hpp"
 #include "layernorm2d_fwd_kernel.hpp"
