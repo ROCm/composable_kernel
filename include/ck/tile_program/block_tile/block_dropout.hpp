c5de2d5e7824b772b5ec2a9d7595be4266ffebaf
letaoqin <letaoqin@amd.com>
2024-03-04 18:01:35 +0000

    add const to parameters

--- include/ck/tile_program/block_tile/block_dropout.hpp
+++ include/ck/tile_program/block_tile/block_dropout.hpp
@@ -17,12 +17,12 @@ namespace block {
 struct BlockDropout
 {
     __host__ __device__ BlockDropout(const long_index_t& global_idx_,
-                                     float& p_dropout_rescale_,
-                                     uint8_t& p_undrop_in_uint8_t_,
-                                     index_t& total_n_len_,
-                                     unsigned long long seed,
-                                     unsigned long long offset,
-                                     bool is_store_randval_)
+                                     const float p_dropout_rescale_,
+                                     const uint8_t& p_undrop_in_uint8_t_,
+                                     const index_t total_n_len_,
+                                     const unsigned long long& seed,
+                                     const unsigned long long& offset,
+                                     const bool& is_store_randval_)
         : global_idx(global_idx_),
           p_dropout_rescale(p_dropout_rescale_),
           p_undrop_in_uint8_t(p_undrop_in_uint8_t_),
