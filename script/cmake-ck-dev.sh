255fbc565259e99076d855e2b3edb6c83c16e03b
Jing Zhang <jizhan@amd.com>
2024-03-09 14:53:34 -0500

    fixed

--- script/cmake-ck-dev.sh
+++ script/cmake-ck-dev.sh
@@ -11,7 +11,7 @@ cmake                                                                           
 -D CMAKE_CXX_FLAGS="-std=c++17 -O3 -ftemplate-backtrace-limit=0  -fPIE  -Wno-gnu-line-marker"     \
 -D CMAKE_BUILD_TYPE=Release                                                                       \
 -D BUILD_DEV=OFF                                                                                   \
--D GPU_TARGETS="gfx1100"                                                             \
+-D GPU_TARGETS="gfx1200"                                                             \
 -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON                                                                 \
 -D USE_BITINT_EXTENSION_INT4=OFF                                                                  \
 ${MY_PROJECT_SOURCE}
