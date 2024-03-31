42866940dcfa272102b222e07715bd75d288734f
carlushuang <carlus.huang@amd.com>
2024-03-31 00:01:30 +0000

    remove mistake

--- include/ck_tile/core/config.hpp
+++ include/ck_tile/core/config.hpp
@@ -20,8 +20,6 @@
 #define CK_TILE_DEVICE_EXTERN
 #endif
 
-// minimal arch
-
 #ifndef CK_TILE_USE_CUSTOM_DATA_TYPE
 #define CK_TILE_USE_CUSTOM_DATA_TYPE 0 // custom data type will generate extra move/bfi code
 #endif
