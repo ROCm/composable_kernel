ea5cc2b6f7225ca25b970d21463f5dfc7b561c0e
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2024-04-01 16:42:03 +0000

    Adapt fmha fwd epilogue for xformers to latest implementation

--- example/91_tile_program/xformers_fmha/ck_tiled_fmha_fwd_epilogue.hpp
+++ example/91_tile_program/xformers_fmha/ck_tiled_fmha_fwd_epilogue.hpp
@@ -7,19 +7,23 @@
 #include <ck/tile_program/tile/store_tile.hpp>
 #include <ck/tile_program/tile/tile_elementwise.hpp>
 
-template <typename OaccDataType_, typename ODataType_>
+template <typename OaccDataType_, typename ODataType_, bool kPadSeqLenQ_, bool kPadHeadDimV_>
 struct FmhaFwdEpilogueProblem
 {
-    using OaccDataType = ck::remove_cvref_t<OaccDataType_>;
-    using ODataType    = ck::remove_cvref_t<ODataType_>;
+    using OaccDataType                 = ck::remove_cvref_t<OaccDataType_>;
+    using ODataType                    = ck::remove_cvref_t<ODataType_>;
+    static constexpr bool kPadSeqLenQ  = kPadSeqLenQ_;
+    static constexpr bool kPadHeadDimV = kPadHeadDimV_;
 };
 
 template <typename Problem_, typename Policy_ = void>
 struct FmhaFwdEpilogue
 {
-    using Problem      = ck::remove_cvref_t<Problem_>;
-    using OaccDataType = ck::remove_cvref_t<typename Problem::OaccDataType>;
-    using ODataType    = ck::remove_cvref_t<typename Problem::ODataType>;
+    using Problem                      = ck::remove_cvref_t<Problem_>;
+    using OaccDataType                 = ck::remove_cvref_t<typename Problem::OaccDataType>;
+    using ODataType                    = ck::remove_cvref_t<typename Problem::ODataType>;
+    static constexpr bool kPadSeqLenQ  = Problem::kPadSeqLenQ;
+    static constexpr bool kPadHeadDimV = Problem::kPadHeadDimV;
 
     __host__ __device__ static constexpr ck::index_t GetSmemSize() { return 0; }
 
@@ -29,6 +33,15 @@ struct FmhaFwdEpilogue
         using namespace ck;
         using namespace ck::tile_program;
 
-        store_tile(o_dram_window_tmp, cast_tile<ODataType>(o_acc_tile));
+        // TODO: this is ugly
+        if constexpr(kPadSeqLenQ || kPadHeadDimV)
+        {
+            store_tile_raw(o_dram_window_tmp, cast_tile<ODataType>(o_acc_tile));
+            buffer_store_fence();
+        }
+        else
+        {
+            store_tile(o_dram_window_tmp, cast_tile<ODataType>(o_acc_tile));
+        }
     }
 };
