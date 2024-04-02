9858010a9e48c2c344b33f72dc87be3af4a62d89
Bartlomiej Kocot <barkocot@amd.com>
2024-04-02 12:06:54 +0000

    Introduce refrence elementwise

--- example/44_elementwise_permute/elementwise_permute_4D_fp32_row.cpp
+++ example/44_elementwise_permute/elementwise_permute_4D_fp32_row.cpp
@@ -8,6 +8,8 @@
 #include "ck/tensor_operation/gpu/element/combined_element_wise_operation.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_elementwise_dynamic_vector_dims_impl.hpp"
 
+#include "ck/library/reference_tensor_operation/cpu/reference_elementwise.hpp"
+
 #include "ck/library/utility/algorithm.hpp"
 #include "ck/library/utility/check_err.hpp"
 #include "ck/library/utility/device_memory.hpp"
@@ -38,23 +40,6 @@ using DeviceElementwisePermuteInstance = ck::tensor_operation::device::DeviceEle
     ck::Sequence<8>,      // InScalarPerVectorSeq
     ck::Sequence<8>>;     // OutScalarPerVectorSeq
 
-template <typename HostTensorA, typename HostTensorB>
-void host_elementwise4D(HostTensorB& B_nhwc, const HostTensorA& A_nchw, float scale)
-{
-    auto scale_square = [&](auto& b, auto& a) {
-        b = ck::type_convert<BDataType>(scale * ck::type_convert<float>(a * a));
-    };
-
-    for(std::size_t n = 0; n < A_nchw.mDesc.GetLengths()[0]; ++n)
-        for(std::size_t c = 0; c < A_nchw.mDesc.GetLengths()[1]; ++c)
-            for(std::size_t h = 0; h < A_nchw.mDesc.GetLengths()[2]; ++h)
-                for(std::size_t w = 0; w < A_nchw.mDesc.GetLengths()[3]; ++w)
-                {
-                    auto a_val = A_nchw(n, c, h, w);
-                    scale_square(B_nhwc(n, h, w, c), a_val);
-                }
-}
-
 int main()
 {
     bool do_verification = true;
@@ -62,18 +47,6 @@ int main()
 
     std::vector<std::size_t> nchw = {16, 128, 32, 64};
     std::vector<std::size_t> nhwc = {16, 32, 64, 128};
-    Tensor<ADataType> a(nchw);
-    Tensor<BDataType> b(nhwc);
-    float scale = 2.f;
-    a.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 1.0});
-
-    DeviceMem a_device_buf(sizeof(ADataType) * a.mDesc.GetElementSpaceSize());
-    DeviceMem b_device_buf(sizeof(BDataType) * b.mDesc.GetElementSpaceSize());
-
-    a_device_buf.ToDevice(a.mData.data());
-
-    std::array<const void*, 1> input = {a_device_buf.GetDeviceBuffer()};
-    std::array<void*, 1> output      = {b_device_buf.GetDeviceBuffer()};
 
     std::array<ck::index_t, 4> ab_lengths;
     std::array<ck::index_t, 4> a_strides = {static_cast<int>(nchw[1] * nchw[2] * nchw[3]),
@@ -86,6 +59,20 @@ int main()
                                             static_cast<int>(nhwc[3])};
 
     ck::ranges::copy(nchw, ab_lengths.begin());
+
+    std::array<Tensor<ADataType>, 1> as = {Tensor<ADataType>(ab_lengths, a_strides)};
+    Tensor<ADataType>& a                = as[0];
+    Tensor<BDataType> b(ab_lengths, b_strides);
+    float scale = 2.f;
+    a.GenerateTensorValue(GeneratorTensor_3<ADataType>{0.0, 1.0});
+
+    DeviceMem a_device_buf(sizeof(ADataType) * a.mDesc.GetElementSpaceSize());
+    DeviceMem b_device_buf(sizeof(BDataType) * b.mDesc.GetElementSpaceSize());
+
+    a_device_buf.ToDevice(a.mData.data());
+
+    std::array<const void*, 1> input = {a_device_buf.GetDeviceBuffer()};
+    std::array<void*, 1> output      = {b_device_buf.GetDeviceBuffer()};
 
     auto broadcastPermute = DeviceElementwisePermuteInstance{};
     auto argument =
@@ -124,10 +111,17 @@ int main()
 
     if(do_verification)
     {
-        b_device_buf.FromDevice(b.mData.data());
-        Tensor<BDataType> host_b(nhwc);
-        host_elementwise4D(host_b, a, scale);
+        Tensor<BDataType> host_b(ab_lengths, b_strides);
+        using ReferenceElementwiseInstance = ck::tensor_operation::host::
+            ReferenceElementwise<1, ADataType, BDataType, UnaryScaleSquare>;
+        auto ref_elementwise = ReferenceElementwiseInstance{};
+        auto ref_invoker     = ref_elementwise.MakeInvoker();
 
+        auto ref_argument = ref_elementwise.MakeArgument(
+            as, host_b, UnaryScaleSquare{UnarySquare{}, UnaryScale{scale}});
+        ref_invoker.Run(ref_argument);
+
+        b_device_buf.FromDevice(b.mData.data());
         pass &=
             ck::utils::check_err(b.mData, host_b.mData, "Error: Incorrect results b", 1e-3, 1e-3);
     }
--- example/44_elementwise_permute/elementwise_trinary_4D_fp16.cpp
+++ example/44_elementwise_permute/elementwise_trinary_4D_fp16.cpp
@@ -8,6 +8,8 @@
 #include "ck/tensor_operation/gpu/element/combined_element_wise_operation.hpp"
 #include "ck/tensor_operation/gpu/device/impl/device_elementwise_dynamic_vector_dims_impl.hpp"
 
+#include "ck/library/reference_tensor_operation/cpu/reference_elementwise.hpp"
+
 #include "ck/library/utility/algorithm.hpp"
 #include "ck/library/utility/check_err.hpp"
 #include "ck/library/utility/device_memory.hpp"
@@ -46,44 +48,27 @@ using DeviceElementwisePermuteInstance = ck::tensor_operation::device::DeviceEle
     ck::Sequence<8, 8, 8>,                      // InScalarPerVectorSeq
     ck::Sequence<8>>;                           // OutScalarPerVectorSeq
 
-template <typename HostTensorA0, typename HostTensorA1, typename HostTensorA2, typename HostTensorB>
-void host_elementwise4D(HostTensorB& B,
-                        const HostTensorA0& A0,
-                        const HostTensorA1& A1,
-                        const HostTensorA2& A2,
-                        float alpha,
-                        float beta,
-                        float gamma)
-{
-    auto elementwise_op_function = [&](auto& b, auto& a0, auto& a1, auto& a2) {
-        const float a0_tmp = alpha * ck::type_convert<float>(a0) * ck::type_convert<float>(a0);
-        const float a1_tmp = beta * ck::type_convert<float>(a1) * ck::type_convert<float>(a1);
-        const float a2_tmp = gamma * ck::type_convert<float>(a2) * ck::type_convert<float>(a2);
-        b                  = ck::type_convert<BDataType>(a0_tmp + a1_tmp + a2_tmp);
-    };
-
-    for(std::size_t n = 0; n < A0.mDesc.GetLengths()[0]; ++n)
-        for(std::size_t c = 0; c < A0.mDesc.GetLengths()[1]; ++c)
-            for(std::size_t h = 0; h < A0.mDesc.GetLengths()[2]; ++h)
-                for(std::size_t w = 0; w < A0.mDesc.GetLengths()[3]; ++w)
-                {
-                    auto a0_val = A0(n, c, h, w);
-                    auto a1_val = A1(n, c, h, w);
-                    auto a2_val = A2(n, c, h, w);
-                    elementwise_op_function(B(n, c, h, w), a0_val, a1_val, a2_val);
-                }
-}
-
 int main()
 {
     bool do_verification = true;
     bool time_kernel     = true;
 
     std::vector<std::size_t> nchw = {16, 128, 32, 64};
-    Tensor<ADataType> a0(nchw);
-    Tensor<ADataType> a1(nchw);
-    Tensor<ADataType> a2(nchw);
-    Tensor<BDataType> b(nchw);
+    std::array<ck::index_t, 4> ab_lengths;
+    std::array<ck::index_t, 4> ab_strides = {static_cast<int>(nchw[1] * nchw[2] * nchw[3]),
+                                             static_cast<int>(nchw[2] * nchw[3]),
+                                             static_cast<int>(nchw[3]),
+                                             1};
+
+    ck::ranges::copy(nchw, ab_lengths.begin());
+
+    std::array<Tensor<ADataType>, 3> as = {Tensor<ADataType>(ab_lengths, ab_strides),
+                                           Tensor<ADataType>(ab_lengths, ab_strides),
+                                           Tensor<ADataType>(ab_lengths, ab_strides)};
+    Tensor<ADataType>& a0               = as[0];
+    Tensor<ADataType>& a1               = as[1];
+    Tensor<ADataType>& a2               = as[2];
+    Tensor<BDataType> b(ab_lengths, ab_strides);
     float alpha = 3.f;
     float beta  = 2.f;
     float gamma = 4.f;
@@ -104,14 +89,6 @@ int main()
                                          a1_device_buf.GetDeviceBuffer(),
                                          a2_device_buf.GetDeviceBuffer()};
     std::array<void*, 1> output       = {b_device_buf.GetDeviceBuffer()};
-
-    std::array<ck::index_t, 4> ab_lengths;
-    std::array<ck::index_t, 4> ab_strides = {static_cast<int>(nchw[1] * nchw[2] * nchw[3]),
-                                             static_cast<int>(nchw[2] * nchw[3]),
-                                             static_cast<int>(nchw[3]),
-                                             1};
-
-    ck::ranges::copy(nchw, ab_lengths.begin());
 
     auto broadcastPermute  = DeviceElementwisePermuteInstance{};
     auto unary_scale_op_a0 = UnaryScaleSquare{UnarySquare{}, UnaryScale{alpha}};
@@ -156,12 +133,21 @@ int main()
 
     if(do_verification)
     {
-        b_device_buf.FromDevice(b.mData.data());
-        Tensor<BDataType> host_b(nchw);
-        host_elementwise4D(host_b, a0, a1, a2, alpha, beta, gamma);
+        Tensor<BDataType> host_b(ab_lengths, ab_strides);
+        using ReferenceElementwiseInstance = ck::tensor_operation::host::
+            ReferenceElementwise<3, ADataType, BDataType, TrinaryAddUnaryScaleSquare>;
+        auto ref_elementwise = ReferenceElementwiseInstance{};
+        auto ref_invoker     = ref_elementwise.MakeInvoker();
+
+        auto ref_argument = ref_elementwise.MakeArgument(
+            as,
+            host_b,
+            TrinaryAddUnaryScaleSquare{
+                BinaryAdd{}, BinaryAdd{}, unary_scale_op_a0, unary_scale_op_a1, unary_scale_op_a2});
+        ref_invoker.Run(ref_argument);
 
         const double threshold = std::pow(2, -10) * 2;
-
+        b_device_buf.FromDevice(b.mData.data());
         pass &= ck::utils::check_err(
             b.mData, host_b.mData, "Error: Incorrect results b", threshold, threshold);
     }
