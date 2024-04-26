764164b488a9009842c0ce4b14aa74d49eec5e6a
Haocong WANG <haocwang@amd.com>
2024-04-27 01:56:07 +0800

    [GEMM] UniversalGemm update (#1262)
    * Add bf16 instances
    
    * Add bf16 gemm universal example
    
    * tempsave
    
    * Add guard to navi compilation
    
    * workground on a specific mixed gemm instance ( bring back it when compiler fix upload)
    
    * fix formatting condition statement issue
    
    * solve conflict
    
    ---------
    
    Co-authored-by: Jun Liu <Liu.Jun@amd.com>

--- library/src/tensor_operation_instance/gpu/gemm_universal/device_gemm_xdl_universal_bf16_bf16_bf16/device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_mnkpadding_instance.cpp
+++ library/src/tensor_operation_instance/gpu/gemm_universal/device_gemm_xdl_universal_bf16_bf16_bf16/device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_mnkpadding_instance.cpp
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
 
-#include "device_gemm_xdl_universal_f16_f16_f16_mk_kn_mn.hpp"
+#include "device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn.hpp"
 
 namespace ck {
 namespace tensor_operation {
 namespace device {
 namespace instance {
 
-void add_device_gemm_xdl_universal_f16_f16_f16_mk_kn_mn_mem_v2_mnkpadding_instances(
+void add_device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_mnkpadding_instances(
     std::vector<std::unique_ptr<
-        DeviceGemmV2<Row, Row, Row, F16, F16, F16, PassThrough, PassThrough, PassThrough>>>&
+        DeviceGemmV2<Row, Row, Row, BF16, BF16, BF16, PassThrough, PassThrough, PassThrough>>>&
         instances)
 {
     add_device_operation_instances(
         instances,
-        device_gemm_xdl_universal_f16_f16_f16_mk_kn_mn_mem_instances<Interwave, GemmMNKPadding>{});
+        device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_instances<GemmMNKPadding>{});
 }
 
 } // namespace instance
