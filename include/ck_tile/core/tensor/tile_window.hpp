8aa3f1be929465e87e95c74b0c185ab55f9bf8b2
Po Yen Chen <PoYen.Chen@amd.com>
2024-04-29 05:59:35 +0000

    Add lengths check in ctor of tile_window_with_static_distribution<>

--- include/ck_tile/core/tensor/tile_window.hpp
+++ include/ck_tile/core/tensor/tile_window.hpp
@@ -143,6 +143,16 @@ struct tile_window_with_static_distribution
           tile_dstr_{tile_distribution},
           pre_computed_coords_{}
     {
+        // check if window_lengths is matching the shape of tile_distribution
+        static_assert(WindowLengths::size() == TileDstr::NDimX);
+        static_for<0, TileDstr::NDimX, 1>{}([&](auto dim) {
+            if constexpr(is_number_v<decltype(window_lengths[dim])>)
+            {
+                constexpr index_t window_length = decltype(window_lengths[dim])::value;
+                static_assert(window_length == TileDstr::get_lengths()[dim]);
+            }
+        });
+
 #if 0 // debug
       // TODO: this use more register for FA, but less register for GEMM
       // need investigation
