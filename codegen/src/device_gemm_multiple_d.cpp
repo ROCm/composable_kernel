da070ebb89e395ffcf7b4201328fa1231260eb36
Astha Rai <astha.rai713@gmail.com>
2024-05-21 19:00:40 +0000

    fixed copyright in files

--- codegen/src/device_gemm_multiple_d.cpp
+++ codegen/src/device_gemm_multiple_d.cpp
@@ -1,6 +1,6 @@
 
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "ck/host/device_gemm_multiple_d/problem.hpp"
 #include "ck/host/device_gemm_multiple_d/operation.hpp"
--- codegen/src/device_grouped_conv_fwd_multiple_d.cpp
+++ codegen/src/device_grouped_conv_fwd_multiple_d.cpp
@@ -1,6 +1,6 @@
 
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "ck/host/device_grouped_conv_fwd_multiple_d/conv_fwd_problem.hpp"
 #include "ck/host/device_grouped_conv_fwd_multiple_d/conv_fwd_op.hpp"
