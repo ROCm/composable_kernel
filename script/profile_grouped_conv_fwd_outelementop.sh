6a0b9bebb02e7e647d177aa8f192dcd55e819101
Andriy Roshchenko <andriy.roshchenko@amd.com>
2024-06-20 22:28:44 +0000

    Add script for profiling grouped convolutions FWD with OUT element-wise operation.

--- script/profile_grouped_conv_fwd_outelementop.sh
+++ script/profile_grouped_conv_fwd_outelementop.sh
@@ -1,47 +1,20 @@
 #!/bin/bash
- 
+
 ## GPU visibility
 export HIP_VISIBLE_DEVICES=0
-DRIVER="gfx90a-release/bin/ckProfiler"
+DRIVER="../build/bin/ckProfiler"
 
-OP=grouped_conv_fwd_outelementop
-DATATYPE=$1
-OUTELEMENTOP=$2
-LAYOUT=1
-VERIFY=$3
-INIT=$4
-LOG=0
-TIME=1
+OP=$1
+DATATYPE=$2
+OUTELEMENTOP=$3
+LAYOUT=$4
+VERIFY=$5
+INIT=$6
+LOG=$7
+TIME=$8
 
- N=4
+N=$9
 
-echo $DRIVER $OP   $DATATYPE $OUTELEMENTOP $LAYOUT  $VERIFY  $INIT  $LOG  $TIME      3 32   $N   96    96   3   3   3   28  28   28   1   1   1   1   1   1        1      1       1        1       1        1
 #######  op    datatype  OUTELEMENTOP  layout   verify   init   log   time  Ndims  G    N    K     C   Z   Y   X   Di  Hi   Wi  Sz  Sy  Sx  Dz  Dy  Dx  Left Pz LeftPy  LeftPx  RightPz RightPy  RightPx
 $DRIVER $OP   $DATATYPE $OUTELEMENTOP $LAYOUT  $VERIFY  $INIT  $LOG  $TIME      3 32   $N   96    96   3   3   3   28  28   28   1   1   1   1   1   1        1      1       1        1       1        1
-#$DRIVER $OP   $DATATYPE $OUTELEMENTOP $LAYOUT  $VERIFY  $INIT  $LOG  $TIME      3 32   $N  192   192   3   3   3   28  28   28   1   1   1   1   1   1        1      1       1        1       1        1
-
-
-
-
-# Resnet50
-########  op  datatype $OUTELEMENTOP  layout  verify  init  log  time conv_dim G__ N__ K___ C___ Y X Hi__ Wi__ Strides Dilations LeftPads RightPads
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  256 1024 1 1   14   14     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  512 1024 1 1   14   14     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  128  128 3 3   28   28     1 1       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  512  128 1 1   28   28     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  128  128 3 3   56   56     2 2       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  512 2048 1 1    7    7     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N 1024  256 1 1   14   14     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  256  256 3 3   14   14     1 1       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  256  256 3 3   28   28     2 2       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  128  256 1 1   56   56     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N   64  256 1 1   56   56     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  512  512 3 3   14   14     2 2       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  128  512 1 1   28   28     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  256  512 1 1   28   28     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N 2048  512 1 1    7    7     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  512  512 3 3    7    7     1 1       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N  256   64 1 1   56   56     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N   64   64 1 1   56   56     1 1       1 1      0 0       0 0
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N   64   64 3 3   56   56     1 1       1 1      1 1       1 1
-#  $DRIVER $OP $DATATYPE $OUTELEMENTOP $LAYOUT $VERIFY $INIT $LOG $TIME        3   1  $N   64    3 7 7  224  224     2 2       1 1      3 3       3 3
+$DRIVER $OP   $DATATYPE $OUTELEMENTOP $LAYOUT  $VERIFY  $INIT  $LOG  $TIME      3 32   $N  192   192   3   3   3   28  28   28   1   1   1   1   1   1        1      1       1        1       1        1
