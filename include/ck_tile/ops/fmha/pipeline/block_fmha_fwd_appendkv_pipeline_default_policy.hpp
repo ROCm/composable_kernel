34a3ff849fd144466532488860fb8b7b2b67e729
PoYen, Chen <PoYen.Chen@amd.com>
2024-06-27 09:55:35 +0000

    Fix Vnew tile dstr for row major case

--- include/ck_tile/ops/fmha/pipeline/block_fmha_fwd_appendkv_pipeline_default_policy.hpp
+++ include/ck_tile/ops/fmha/pipeline/block_fmha_fwd_appendkv_pipeline_default_policy.hpp
@@ -94,31 +94,28 @@ struct BlockFmhaFwdAppendKVPipelineDefaultPolicy
         using VDataType = remove_cvref_t<typename Problem::VDataType>;
 
         constexpr index_t kBlockSize = Problem::kBlockSize;
+        constexpr index_t kNPerBlock = Problem::kTileSizeDv;
+        constexpr index_t kKPerBlock = Problem::kTileSizeSk;
 
         if constexpr(std::is_same_v<VLayout, ck_tile::tensor_layout::gemm::RowMajor>)
         {
-            constexpr index_t kNPerBlock = Problem::kTileSizeDv;
-            constexpr index_t kKPerBlock = Problem::kTileSizeSk;
 
-            constexpr index_t K1 = GetAlignmentV<Problem>();
-            constexpr index_t K0 = kKPerBlock / K1;
-            constexpr index_t N2 = get_warp_size() / K0;
-            constexpr index_t N1 = kBlockSize / get_warp_size();
-            constexpr index_t N0 = kNPerBlock / (N2 * N1);
+            constexpr index_t N1 = 16 / sizeof(VDataType);
+            constexpr index_t N0 = kNPerBlock / N1;
+            constexpr index_t K2 = get_warp_size() / N0;
+            constexpr index_t K1 = kBlockSize / get_warp_size();
+            constexpr index_t K0 = kKPerBlock / (K2 * K1);
 
             return make_static_tile_distribution(
                 tile_distribution_encoding<sequence<1>,
-                                           tuple<sequence<N0, N1, N2>, sequence<K0, K1>>,
-                                           tuple<sequence<1>, sequence<1, 2>>,
-                                           tuple<sequence<1>, sequence<2, 0>>,
+                                           tuple<sequence<N0, N1>, sequence<K0, K1, K2>>,
+                                           tuple<sequence<2>, sequence<1, 2>>,
+                                           tuple<sequence<1>, sequence<0, 2>>,
                                            sequence<1, 2>,
-                                           sequence<0, 1>>{});
+                                           sequence<1, 0>>{});
         }
         else
         {
-            constexpr index_t kNPerBlock = Problem::kTileSizeDv;
-            constexpr index_t kKPerBlock = Problem::kTileSizeSk;
-
             constexpr index_t K1 = 16 / sizeof(VDataType);
             constexpr index_t K0 = kKPerBlock / K1;
             constexpr index_t N2 = get_warp_size() / K0;
