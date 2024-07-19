9607c9187bde8c6b616364b0f89d45724cdabad2
letaoqin <letaoqin@amd.com>
2024-07-19 16:12:58 +0800

    remove googletest git method  address

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -9,7 +9,6 @@ endif()
 FetchContent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    #GIT_REPOSITORY git@github.com:google/googletest.git
     GIT_TAG f8d7d77c06936315286eb55f8de22cd23c188571
 )
 
