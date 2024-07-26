1864119969fea01b45273f88ac575335313e3756
illsilin <Illia.Silin@amd.com>
2024-07-26 14:41:30 -0700

    fix the ck_tile fmha log names and headers

--- example/ck_tile/01_fmha/script/run_full_test.sh
+++ example/ck_tile/01_fmha/script/run_full_test.sh
@@ -18,15 +18,16 @@ export host_name=$3
 echo 'Host name: ' $host_name
 export GPU_arch=$4
 echo 'GPU_arch: ' $GPU_arch
+
 function print_log_header(){
 	rm -f $1;
-	echo 'On branch ' $2 &> $1;
-	echo 'Node name: ' $3 >> $1;
+	echo 'On branch ' $3 &> $1;
+	echo 'Node name: ' $4 >> $1;
 	#get GPU_arch and number of compute units from rocminfo
 	echo -n "GPU_arch: " >> $1; rocminfo | grep "Name:" | grep "gfx" >> $1;
 	rocminfo | grep "Compute Unit:" >> $1;
 	hipcc --version | grep -e 'HIP version'  >> $1;
-	echo 'Environment type: ' $1 >> $1;
+	echo 'Environment type: ' $2 >> $1;
 	/opt/rocm/bin/amdclang++ --version | grep -e 'InstalledDir' >> $1;
 }
 
@@ -35,11 +36,11 @@ example/ck_tile/01_fmha/script/smoke_test_fwd.sh
 example/ck_tile/01_fmha/script/smoke_test_bwd.sh
 
 #run performance benchmarks
-export fmha_fwd_log="build/perf_fmha_fwd_${GPU_arch}.log"
+export fmha_fwd_log="build/perf_fmha_fwd_$GPU_arch.log"
 print_log_header $fmha_fwd_log $env_type $branch $host_name
 example/ck_tile/01_fmha/script/benchmark_fwd.sh 2>&1 | tee -a $fmha_fwd_log
 
-export fmha_bwd_log="build/perf_fmha_bwd_${GPU_arch}.log"
+export fmha_bwd_log="build/perf_fmha_bwd_$GPU_arch.log"
 print_log_header $fmha_bwd_log $env_type $branch $host_name
 example/ck_tile/01_fmha/script/benchmark_bwd.sh 2>&1 | tee -a $fmha_bwd_log
 
