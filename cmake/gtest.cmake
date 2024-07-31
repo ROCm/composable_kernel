0add4d7fe454e88ff5ef7bb139c174b9930b7a22
Jing Zhang <jizhan@fb.com>
2024-07-30 18:06:37 -0700

    clean

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -8,8 +8,7 @@ endif()
 
 FetchContent_Declare(
     GTest
-    GIT_REPOSITORY git@github.com:google/googletest.git
-    #GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG f8d7d77c06936315286eb55f8de22cd23c188571
 )
 
