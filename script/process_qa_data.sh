e377ca404bf9bb47a502d7f16984a3639a7e1337
Illia Silin <98187287+illsilin@users.noreply.github.com>
2024-08-07 08:18:26 -0700

    Run CK_TILE FMHA benchmarks and collect the performance data. (#1447)
    * run ck_tile benchmarks after the smoke tests and store logs
    
    * change the path of fmha benchmark logs
    
    * change the way of stashig ck_tile fmha logs
    
    * prevent the errors in stages where no logs are generated
    
    * fix the ck_tile fmha log names and headers
    
    * generate the fmha performance logs in the root folder
    
    * change jenkins scrip arguments format
    
    * use exact file names for stashing
    
    * modify scripts to process FMHA performance results
    
    * unstash FMHA logs before parsing them
    
    [ROCm/composable_kernel commit: 12c1f68dd9fc7fa6d8d9998b1373c2e601d1c501]

--- script/process_qa_data.sh
+++ script/process_qa_data.sh
@@ -21,3 +21,20 @@ python3 process_perf_data.py perf_gemm_bilinear.log
 python3 process_perf_data.py perf_reduction.log
 python3 process_perf_data.py perf_splitK_gemm.log
 python3 process_perf_data.py perf_onnx_gemm.log
+
+file=./perf_fmha_fwd_gfx942.log
+if [ -e "$file" ]; then
+    python3 process_perf_data.py perf_fmha_fwd_gfx942.log
+fi
+file=./perf_fmha_bwd_gfx942.log
+if [ -e "$file" ]; then
+    python3 process_perf_data.py perf_fmha_bwd_gfx942.log
+fi
+file=./perf_fmha_fwd_gfx90a.log
+if [ -e "$file" ]; then
+    python3 process_perf_data.py perf_fmha_fwd_gfx90a.log
+fi
+file=./perf_fmha_bwd_gfx90a.log
+if [ -e "$file" ]; then
+    python3 process_perf_data.py perf_fmha_bwd_gfx90a.log
+fi
