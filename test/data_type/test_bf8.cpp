86d23d71123f7cc81dd2f274f29c3d9746505a1f
Rostyslav Geyyer <rosty.geyyer@amd.com>
2024-08-14 22:28:38 +0000

    Fix the flag

--- test/data_type/test_bf8.cpp
+++ test/data_type/test_bf8.cpp
@@ -27,7 +27,7 @@ TEST(BF8, ConvertFP32Nearest)
     // convert 0 float to bf8 and back, check if holds
     ASSERT_NEAR(0.0f, type_convert<float>(f8_convert_rne<bf8_t>(0.0f)), abs_tol);
     // don't run the next test on gfx11 devices
-#ifdef CK_SKIP_FLAKY_F8_TEST
+#ifndef CK_SKIP_FLAKY_F8_TEST
     // convert minimal float to bf8 and back, check if holds
     ASSERT_NEAR(std::numeric_limits<float>::min(),
                 type_convert<float>(f8_convert_rne<bf8_t>(std::numeric_limits<float>::min())),
