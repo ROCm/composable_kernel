5e517c738637e82d9969122f709332ad32e3a423
danyao12 <danyao12@amd.com>
2024-08-22 18:39:15 +0000

    asm rtn

--- include/ck_tile/core/numeric/bfloat16.hpp
+++ include/ck_tile/core/numeric/bfloat16.hpp
@@ -105,7 +105,7 @@ using bf16_t     = bfloat16_t;
 using bf16_raw_t = uint16_t;
 #endif
 // round to nearest
-CK_TILE_HOST_DEVICE
+CK_TILE_HOST
 constexpr uint16_t float_to_bf16_rtn_raw(float f)
 {
     union
@@ -146,6 +146,62 @@ constexpr uint16_t float_to_bf16_rtn_raw(float f)
         u.int32 |= 0x10000; // Preserve signaling NaN
     }
     return uint16_t(u.int32 >> 16);
+}
+
+// CK_TILE_HOST_DEVICE
+// constexpr uint16_t float_to_bf16_rtn_raw(float f)
+// {
+//     union
+//     {
+//         float fp32;
+//         uint32_t int32;
+//     } u = {f};
+
+//     bool exponent_not_all1s = ~u.int32 & 0x7f800000;
+//     uint32_t rounded        = 0x7fff + ((u.int32 >> 16) & 1);
+//     rounded += u.int32;
+//     uint32_t res      = exponent_not_all1s ? rounded : u.int32;
+//     bool preserve_nan = u.int32 & 0xffff;
+//     uint32_t nan      = u.int32 | 0x10000;
+//     res               = (!exponent_not_all1s & preserve_nan) ? nan : res;
+
+//     return uint16_t(res >> 16);
+// }
+
+CK_TILE_DEVICE
+uint16_t float_to_bf16_rtn_raw(float f)
+{
+    union
+    {
+        float fp32;
+        uint32_t int32;
+    } u = {f};
+
+    static constexpr uint32_t FP32_NAN = 0x7fff0000;
+    static constexpr uint32_t ROUND_BIAS_FOR_BF16 = 0x7fff;
+    using uint32x2_t  = uint32_t __attribute__((ext_vector_type(2)));
+    uint32x2_t check_nan;
+    uint32_t tmp;
+    asm volatile("\n \
+            v_cmp_u_f32 %0, %2, %2 \n \
+            v_bfe_u32 %1, %2, 16, 1 \n \
+            v_add3_u32 %1, %2, %1, %3 \n \
+            v_cndmask_b32 %2, %1, %4, %0 \n \
+            v_lshrrev_b32 %2, 16, %2 \n \
+            "
+                 : "=s"(check_nan), "+v"(tmp), "+v"(u.fp32)
+                 : "v"(ROUND_BIAS_FOR_BF16), "v"(FP32_NAN));
+    // asm volatile("\n \
+    //         v_cmp_u_f32 vcc, %1, %1 \n \
+    //         v_bfe_u32 %0, %1, 16, 1 \n \
+    //         v_add3_u32 %0, %1, %0, %2 \n \
+    //         v_cndmask_b32 %1, %0, %3, vcc \n \
+    //         v_lshrrev_b32 %1, 16, %1 \n \
+    //         "
+    //              : "+v"(tmp), "+v"(u.fp32)
+    //              : "v"(ROUND_BIAS_FOR_BF16), "v"(FP32_NAN)
+    //              : "vcc");
+    return uint16_t(u.int32);
 }
 
 // Truncate instead of rounding, preserving SNaN
