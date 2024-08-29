c388b8e66a2a84480e98013928bd5e1f5fe789b2
root <root@localhost.localdomain>
2024-08-29 12:48:16 +0000

    added int4/int8 dequantization support

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -146,16 +146,18 @@ function(clang_tidy_check TARGET)
         if((NOT "${SOURCE}" MATCHES "(h|hpp|hxx)$") AND (NOT "${SOURCE}" MATCHES "TARGET_OBJECTS"))
             string(MAKE_C_IDENTIFIER "${SOURCE}" tidy_file)
             set(tidy_target tidy-target-${TARGET}-${tidy_file})
-            add_custom_target(${tidy_target}
-                # for some targets clang-tidy not able to get information from .clang-tidy
-                DEPENDS ${SOURCE}
-                COMMAND ${CLANG_TIDY_COMMAND} "-config=\{CheckOptions: \[\{key: bugprone-reserved-identifier.AllowedIdentifiers,value: __HIP_PLATFORM_HCC__\; __HIP_PLATFORM_AMD__\; __HIP_ROCclr__\}\]\}" ${SOURCE} "-export-fixes=${CLANG_TIDY_FIXIT_DIR}/${TARGET}-${tidy_file}.yaml"
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                COMMENT "clang-tidy: Running clang-tidy on target ${SOURCE}..."
-            )
-            add_dependencies(${tidy_target} ${TARGET})
-            add_dependencies(${tidy_target} tidy-base)
-            add_dependencies(tidy ${tidy_target})
+            if(NOT TARGET ${tidy_target})
+                add_custom_target(${tidy_target}
+                    # for some targets clang-tidy not able to get information from .clang-tidy
+                    DEPENDS ${SOURCE}
+                    COMMAND ${CLANG_TIDY_COMMAND} "-config=\{CheckOptions: \[\{key: bugprone-reserved-identifier.AllowedIdentifiers,value: __HIP_PLATFORM_HCC__\; __HIP_PLATFORM_AMD__\; __HIP_ROCclr__\}\]\}" ${SOURCE} "-export-fixes=${CLANG_TIDY_FIXIT_DIR}/${TARGET}-${tidy_file}.yaml"
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    COMMENT "clang-tidy: Running clang-tidy on target ${SOURCE}..."
+                )
+                add_dependencies(${tidy_target} ${TARGET})
+                add_dependencies(${tidy_target} tidy-base)
+                add_dependencies(tidy ${tidy_target})
+            endif()
         endif()
     endforeach()
 endfunction()
