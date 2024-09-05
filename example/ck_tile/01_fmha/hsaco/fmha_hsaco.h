2d9b53441c66410eb73913552080d18133d1f73c
Fang.Che <chefang2@amd.com>
2024-09-05 03:00:23 +0000

    Add the hsaco binary compiled from fmha assembly

---
+++ example/ck_tile/01_fmha/hsaco/fmha_hsaco.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
+
+#pragma once
+
+extern unsigned char bwd_a16[];
+extern unsigned char bwd_a32_bf16_m1k1[];
+extern unsigned char bwd_a32_bf16_noCoex_m1k1[];
+extern unsigned char bwd_a32_bf16_noCoex[];
+extern unsigned char bwd_a32_bf16[];
+extern unsigned char bwd_a32_m1k1[];
+extern unsigned char bwd_a32_noCoex_m1k1[];
+extern unsigned char bwd_a32_noCoex[];
+extern unsigned char bwd_a32[];
+extern unsigned char bwd_arg[];
