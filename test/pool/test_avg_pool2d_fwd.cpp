8aeb2afbe24e235af4633f5a507ce89d33afc948
jakpiase <jakub.piasecki@amd.com>
2024-09-11 15:21:00 +0200

    Rewrite pool2d fwd (#1462)
    * added pool2d fwd
    
    * add tests
    
    * add reviewers changes
    
    * Revert "Merge remote-tracking branch 'origin/develop' into jakpiase/pool2d_fwd_new"
    
    This reverts commit 6b2ba7ff8960b0a6ddbe30d8dac53eeb55a8597e, reversing
    changes made to 22c82bea0caf3e0f29399100c1bb67b8003fc042.
    
    * Revert "add reviewers changes"
    
    This reverts commit 22c82bea0caf3e0f29399100c1bb67b8003fc042.
    
    * added reviewers comments
    
    * revert some old files
    
    * add reviewers requests
    
    ---------
    
    Co-authored-by: Adam Osewski <19374865+aosewski@users.noreply.github.com>
    
    [ROCm/composable_kernel commit: e8d2887cb263644b2e778419ad1f892cea0b0466]

---
+++ test/pool/test_avg_pool2d_fwd.cpp
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+
+#include "gtest/gtest.h"
+#include "profiler/profile_pool2d_fwd_impl.hpp"
+#include "test_pool_fwd_common.hpp"
+
+template <typename Tuple>
+class TestAvgPool2dFwd : public ::testing::Test
+{
+    protected:
+    using InDataType      = std::tuple_element_t<0, Tuple>;
+    using OutDataType     = std::tuple_element_t<1, Tuple>;
+    using ComputeDataType = std::tuple_element_t<2, Tuple>;
+    using IndexDataType   = std::tuple_element_t<3, Tuple>;
+
+    std::vector<PoolingParam> params;
+
+    void Run()
+    {
+        for(auto param : params)
+        {
+            // avg pool
+            bool success =
+                ck::profiler::profile_pool2d_fwd_impl<InDataType,
+                                                      OutDataType,
+                                                      ComputeDataType,
+                                                      IndexDataType,
+                                                      ck::tensor_layout::convolution::NHWC,
+                                                      ck::tensor_layout::convolution::NHWC,
+                                                      ck::ReduceTensorOp::AVG,
+                                                      false,
+                                                      false>(true,
+                                                             2,
+                                                             false,
+                                                             false,
+                                                             param.length_,
+                                                             param.window_spatial_lengths_,
+                                                             param.window_strides_,
+                                                             param.window_dilations_,
+                                                             param.input_left_pads_,
+                                                             param.input_right_pads_);
+            EXPECT_TRUE(success);
+        }
+    }
+};
+
+using KernelTypes = std::conditional_t<
+    CK_ENABLE_FP16 && CK_ENABLE_BF16,
+    ::testing::Types<std::tuple<F16, F16, F32, I32>,
+                     std::tuple<F16, F16, F32, I32>,
+                     std::tuple<BF16, BF16, F32, I32>,
+                     std::tuple<BF16, BF16, F32, I32>,
+                     std::tuple<F32, F32, F32, I32>,
+                     std::tuple<F32, F32, F32, I32>>,
+    ::testing::Types<std::tuple<F32, F32, F32, I32>, std::tuple<F32, F32, F32, I32>>>;
+
+TYPED_TEST_SUITE(TestAvgPool2dFwd, KernelTypes);
+TYPED_TEST(TestAvgPool2dFwd, Test_Pool)
+{
+    // length, window_length, window_stride, window_dilation, left_pad, right_pad
+    this->params = {{{1, 1, 1, 1}, {1, 1}, {1, 1}, {1, 1}, {0, 0}, {0, 0}},
+                    {{2, 16, 64, 64}, {64, 64}, {1, 1}, {1, 1}, {0, 0}, {0, 0}},
+                    {{2, 16, 64, 64}, {4, 4}, {4, 4}, {2, 2}, {0, 0}, {0, 0}},
+                    {{2, 32, 30, 30}, {2, 2}, {2, 2}, {1, 1}, {1, 1}, {1, 1}}};
+
+    this->Run();
+}
