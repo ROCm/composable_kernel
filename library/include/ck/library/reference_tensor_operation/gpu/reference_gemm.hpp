c910bdc2e1caa9cb0c052d201fb00aecd7abe9eb
Rostyslav Geyyer <rosty.geyyer@amd.com>
2024-09-26 13:20:37 +0000

    Try even less threads per block

--- library/include/ck/library/reference_tensor_operation/gpu/reference_gemm.hpp
+++ library/include/ck/library/reference_tensor_operation/gpu/reference_gemm.hpp
@@ -135,7 +135,7 @@ struct ReferenceGemm : public device::BaseOperator
 
         float Run(const Argument& arg, const StreamConfig& stream_config = StreamConfig{})
         {
-            int block_size = 8;
+            int block_size = 4;
             dim3 block_dim(block_size, block_size, 1);
             dim3 grid_dim(
                 (arg.m_ + block_size - 1) / block_size, (arg.n_ + block_size - 1) / block_size, 1);
