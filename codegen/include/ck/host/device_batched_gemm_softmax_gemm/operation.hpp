23dd6a1f7a391bf3eb0caf061d29880fbff047da
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2024-10-02 11:23:00 +0000

    Introduce ck_host library and gemm_softmax_gemm.

--- codegen/include/ck/host/device_batched_gemm_softmax_gemm/operation.hpp
+++ codegen/include/ck/host/device_batched_gemm_softmax_gemm/operation.hpp
@@ -8,11 +8,11 @@
 #include <string>
 #include "ck/host/types.hpp"
 #include "ck/host/operation/gemm.hpp"
-#include "ck/host/device_gemm_multiple_d/problem.hpp"
+#include "ck/host/device_batched_gemm_softmax_gemm/problem.hpp"
 
 namespace ck {
 namespace host {
-namespace device_gemm_multiple_d {
+namespace device_batched_gemm_softmax_gemm {
 
 // defines all values need for an instance of fwd conv
 struct Operation_Xdl_CShuffle
@@ -25,24 +25,25 @@ struct Operation_Xdl_CShuffle
     CreateOperations(const Problem& prob, const std::string& prologue, const std::string& epilogue);
     TensorDesc A{};
     TensorDesc B{};
-    DataType acc               = DataType::Float;
-    DataType cs_type           = DataType::Half;
-    std::vector<TensorDesc> Ds = {};
-    TensorDesc E{};
+    TensorDesc B1{};
+    TensorDesc C{};
     std::string a_elem_op           = PassThrough;
     std::string b_elem_op           = PassThrough;
-    std::string cde_elem_op         = Bilinear;
+    std::string b1_elem_op          = PassThrough;
+    std::string c_elem_op           = PassThrough;
+    std::string acc_elem_op         = Scale;
     std::string prologue            = "";
     std::string epilogue            = "";
     std::string gemm_specialization = "ck::tensor_operation::device::GemmSpecialization::Default";
     // tuning parameters
-    operation::TileDesc tile_desc{};
+    operation::TileDescGemmSoftmaxGemm tile_desc{};
     operation::BlockTransferDesc a_block_transfer{};
-    operation::BlockTransferDesc b_block_transfer{};
+    operation::BlockTransferDesc b0_block_transfer{};
+    operation::BlockTransferDesc b1_block_transfer{};
     operation::CShuffleDesc cshuffle{};
     operation::CBlockTransferDesc c_block_transfer{};
-    LoopScheduler loop_scheduler{};
-    PipelineVersion pipeline_version{};
+
+    bool mask_out_upper_triangle = false;
 
     // functions to update fusion operators if provided
     void update_prologue(const std::string& prologue);
@@ -52,6 +53,6 @@ struct Operation_Xdl_CShuffle
     Solution ToSolution() const;
 };
 
-} // namespace device_gemm_multiple_d
+} // namespace device_batched_gemm_softmax_gemm
 } // namespace host
 } // namespace ck
