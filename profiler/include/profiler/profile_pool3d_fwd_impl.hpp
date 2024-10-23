4aafac55b97b4ca1d6fcaaf5e86ef6cb76e6fed3
Aleksander Dudek <aleksander.dudek@amd.com>
2024-10-23 14:26:33 +0000

    Generic threshold calculation - OutDataType in absolute error

--- profiler/include/profiler/profile_pool3d_fwd_impl.hpp
+++ profiler/include/profiler/profile_pool3d_fwd_impl.hpp
@@ -254,11 +254,6 @@ bool profile_pool3d_fwd_impl(PoolFwdInputParams& in_params, PoolFwdKernelParams&
             auto relative_error_threshold =
                 ck::utils::get_relative_threshold<ComputeDataType, OutDataType>();
 
-            std::cout << "DEBUG absolute_error_threshold: " << absolute_error_threshold
-                      << std::endl;
-            std::cout << "DEBUG relative_error_threshold: " << relative_error_threshold
-                      << std::endl;
-
             bool pass = ck::utils::check_err(out_n_c_do_ho_wo_device.mData,
                                              out_n_c_do_ho_wo_host.mData,
                                              "Error: Incorrect results",
