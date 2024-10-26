59e7fe3ac8ff5efdc3a4ac7ef141659e1595fc51
valarLip <103567126+valarLip@users.noreply.github.com>
2024-10-26 16:39:34 +0800

    add int8 gemm multiply multiply a8w8 (#1591)
    * add int8 gemm multiply multiply a8w8
    
    * uncomment
    
    * clang-format-12
    
    * Add example_gemm_multiply_multiply_xdl_int8
    
    * Remove shell scripts
    
    * update preprocess number for mi308; bring back printout in ckprofiler
    
    * format
    
    ---------
    
    Co-authored-by: chenjun <junchen2@amd.com>
    Co-authored-by: Haocong WANG <haocwang@amd.com>
    Co-authored-by: carlushuang <carlus.huang@amd.com>
    
    [ROCm/composable_kernel commit: 37f7afed1e2a19be8c04b7cd26d07db41c082e88]

--- profiler/src/profile_gemm_multiply_multiply.cpp
+++ profiler/src/profile_gemm_multiply_multiply.cpp
@@ -27,6 +27,7 @@ enum struct GemmDataType
     F16_F8_F16,     // 5
     F16_F16_F16_F8, // 6
     F8_F8_BF16,     // 7
+    INT8_INT8_BF16, // 8
 };
 
 #define OP_NAME "gemm_multiply_multiply"
@@ -39,7 +40,7 @@ int profile_gemm_multiply_multiply(int argc, char* argv[])
         printf("arg1: tensor operation (" OP_NAME ": " OP_DESC ")\n");
         printf("arg2: data type (0: fp32; 1: fp16; 2: bf16; 3: int8; 4: f8@f16; 5: f16@f8; 6: "
                "f16->f8; 7: f8->bf16, "
-               "comp f8)\n");
+               "comp f8; 8: int8->bf16)\n");
         printf("arg3: matrix layout (0: A[m, k] * B[k, n] = C[m, n];\n");
         printf("                     1: A[m, k] * B[n, k] = C[m, n];\n");
         printf("                     2: A[k, m] * B[k, n] = C[m, n];\n");
@@ -89,6 +90,8 @@ int profile_gemm_multiply_multiply(int argc, char* argv[])
     using F32  = float;
     using BF16 = ck::bhalf_t;
     using F8   = ck::f8_t;
+    using I8   = int8_t;
+    using I32  = int;
 
     using Row = ck::tensor_layout::gemm::RowMajor;
     using Col = ck::tensor_layout::gemm::ColumnMajor;
@@ -161,6 +164,11 @@ int profile_gemm_multiply_multiply(int argc, char* argv[])
     {
         return profile(
             F8{}, F8{}, F8{}, F32{}, F32{}, F32{}, BF16{}, Row{}, Col{}, Row{}, Col{}, Row{});
+    }
+    else if(data_type == GemmDataType::INT8_INT8_BF16 && layout == GemmMatrixLayout::MK_NK_MN)
+    {
+        return profile(
+            I8{}, I8{}, I8{}, I32{}, F32{}, F32{}, BF16{}, Row{}, Col{}, Row{}, Col{}, Row{});
     }
     else
     {
