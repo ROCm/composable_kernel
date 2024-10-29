590adbef5b57cea1676f82eda28e8505e70d0820
Adam Osewski <Adam.Osewski@amd.com>
2024-10-29 15:50:01 +0000

    Unblock tests.

--- test/ck_tile/gemm/test_gemm_mem_pipeline.cpp
+++ test/ck_tile/gemm/test_gemm_mem_pipeline.cpp
@@ -17,11 +17,10 @@ using Col = ck_tile::tensor_layout::gemm::ColumnMajor;
 // clang-format off
 using KernelTypes = ::testing::Types<
     //         ALayout, BLayout, CLayout, ADataType, BDataType, AccDataType, CDataType
-    std::tuple<    Row,     Col,     Row,       F16,       F16,         F32,      F16>
-    // TODO: fixme!
-    // std::tuple<    Col,     Row,     Row,       F16,       F16,         F32,      F16>,
-    // std::tuple<    Row,     Row,     Row,       F16,       F16,         F32,      F16>,
-    // std::tuple<    Col,     Col,     Row,       F16,       F16,         F32,      F16>
+    std::tuple<    Row,     Col,     Row,       F16,       F16,         F32,      F16>,
+    std::tuple<    Col,     Row,     Row,       F16,       F16,         F32,      F16>,
+    std::tuple<    Row,     Row,     Row,       F16,       F16,         F32,      F16>,
+    std::tuple<    Col,     Col,     Row,       F16,       F16,         F32,      F16>
     >;
 // clang-format on
 
