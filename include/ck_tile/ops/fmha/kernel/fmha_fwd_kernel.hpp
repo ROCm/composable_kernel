606fb4df2ca2251332f1092116df0d0e8e082809
Qianfeng <qianfeng.zhang@amd.com>
2024-10-30 14:03:16 +0800

    [CK_TILE] Add fmha fwd headdim96 support (#1608)
    * Add ceil_to_qualified_tile_length()
    
    * Rename kK0BlockLength to kQKHeaddim
    
    * Add kSubQKHeaddim concept to support headdim96
    
    * Fix in math.hpp to avoid using __half interfaces
    
    * Add LdsBufferSequence instance for headdim96
    
    * Update in fmha_fwd/fmha_fwd_splitkv codegen to support hd96 testing
    
    * Disable hd96 instance generation in codegen fmha_fwd and fmha_fwd_splitkv to save compiling time
    
    * Reformat one file
    
    * Fix text alignment in fmha_fwd_splitkv.py
    
    ---------
    
    Co-authored-by: Po Yen Chen <PoYen.Chen@amd.com>
    
    [ROCm/composable_kernel commit: 863222181477ff42e809d034428f9160490a63ba]

--- include/ck_tile/ops/fmha/kernel/fmha_fwd_kernel.hpp
+++ include/ck_tile/ops/fmha/kernel/fmha_fwd_kernel.hpp
@@ -82,10 +82,10 @@ struct FmhaFwdKernel
             if (kPadHeadDimV) n += "dv";
             return n.empty() ? n : std::string("p") + n; }();
         return
-            _SS_("fmha_fwd_d") + _TS_(bfs::kK0BlockLength) + "_" + _SS_(t2s<QDataType>::name) +
+            _SS_("fmha_fwd_d") + _TS_(bfs::kQKHeaddim) + "_" + _SS_(t2s<QDataType>::name) +
             "_" + (kIsGroupMode ? "group" : "batch") + "_" + _SS_(TilePartitioner::name) + "_"
             "b" + _TS_(bfs::kM0) + "x" + _TS_(bfs::kN0) + "x" + _TS_(bfs::kK0) + "x" +
-                    _TS_(bfs::kN1) + "x" + _TS_(bfs::kK1) + "x" + _TS_(bfs::kK0BlockLength) + "_" +
+                    _TS_(bfs::kN1) + "x" + _TS_(bfs::kK1) + "x" + _TS_(bfs::kQKHeaddim) + "_" +
             "r" + _TS_(g0br::at(ck_tile::number<0>{})) + "x" + _TS_(g0br::at(ck_tile::number<1>{})) + "x" + _TS_(g0br::at(ck_tile::number<2>{})) + "_" +
             "r" + _TS_(g1br::at(ck_tile::number<0>{})) + "x" + _TS_(g1br::at(ck_tile::number<1>{})) + "x" + _TS_(g1br::at(ck_tile::number<2>{})) + "_" +
             "w" + _TS_(gwt::at(ck_tile::number<0>{})) + "x" + _TS_(gwt::at(ck_tile::number<1>{})) + "x" + _TS_(gwt::at(ck_tile::number<2>{})) + "_" +
@@ -657,7 +657,7 @@ struct FmhaFwdKernel
             {
                 return pad_tensor_view(
                     q_dram_naive,
-                    make_tuple(number<FmhaPipeline::kM0>{}, number<FmhaPipeline::kK0BlockLength>{}),
+                    make_tuple(number<FmhaPipeline::kM0>{}, number<FmhaPipeline::kSubQKHeaddim>{}),
                     sequence<kPadSeqLenQ, kPadHeadDimQ>{});
             }
             else
@@ -724,7 +724,7 @@ struct FmhaFwdKernel
             [&]() {
                 if constexpr(FmhaPipeline::kQLoadOnce)
                     return make_tuple(number<FmhaPipeline::kM0>{},
-                                      number<FmhaPipeline::kK0BlockLength>{});
+                                      number<FmhaPipeline::kSubQKHeaddim>{});
                 else
                     return make_tuple(number<FmhaPipeline::kM0>{}, number<FmhaPipeline::kK0>{});
             }(),
