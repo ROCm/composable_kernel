0bc19e0978957048a1a1e526d36efe5b562142ba
Po Yen, Chen <PoYen.Chen@amd.com>
2024-10-31 09:41:46 +0000

    Re-generate smoothquant.hpp

--- include/ck_tile/ops/smoothquant.hpp
+++ include/ck_tile/ops/smoothquant.hpp
@@ -7,6 +7,7 @@
 #include "ck_tile/ops/smoothquant/kernel/smoothquant_shape.hpp"
 #include "ck_tile/ops/smoothquant/pipeline/smoothquant_pipeline_default_policy.hpp"
 #include "ck_tile/ops/smoothquant/pipeline/smoothquant_pipeline_one_pass.hpp"
-#include "ck_tile/ops/smoothquant/pipeline/smoothquant_pipeline_two_pass.hpp"
 #include "ck_tile/ops/smoothquant/pipeline/smoothquant_pipeline_problem.hpp"
+#include "ck_tile/ops/smoothquant/pipeline/smoothquant_pipeline_two_pass.hpp"
+#include "ck_tile/ops/common/generic_2d_block_shape.hpp"
 #include "ck_tile/ops/common/tensor_layout.hpp"
