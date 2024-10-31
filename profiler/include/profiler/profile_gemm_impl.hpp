6e6a3bc69657c73da607e457e71dd1c2193e7ff6
Andriy Roshchenko <andriy.roshchenko@amd.com>
2024-10-31 17:22:58 +0000

    Bugfixes on gfx1101 architecture.

--- profiler/include/profiler/profile_gemm_impl.hpp
+++ profiler/include/profiler/profile_gemm_impl.hpp
@@ -74,8 +74,8 @@ int profile_gemm_impl(int do_verification,
     switch(init_method)
     {
     case 0:
-        ck::utils::FillConstant<ADataType>{static_cast<ADataType>(1.f)}(a_m_k);
-        ck::utils::FillConstant<BDataType>{static_cast<BDataType>(1.f)}(b_k_n);
+        ck::utils::FillConstant<ADataType>{type_convert<ADataType>(1.f)}(a_m_k);
+        ck::utils::FillConstant<BDataType>{type_convert<BDataType>(1.f)}(b_k_n);
         break;
     case 1:
         ck::utils::FillUniformDistributionIntegerValue<ADataType>{-5.f, 5.f}(a_m_k);
