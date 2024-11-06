0bb6e25f3588c96316c1ce731281497104f3868a
Rostyslav Geyyer <rosty.geyyer@amd.com>
2024-11-06 19:40:43 +0000

    Add scale <-> float conversions

--- include/ck/utility/e8m0_utils.hpp
+++ include/ck/utility/e8m0_utils.hpp
@@ -8,4 +8,14 @@
 
 namespace ck::utils {
 
+__host__ __device__ inline float cast_to_float(e8m0_scale_t const scale)
+{
+    return std::pow(2, bit_cast<uint8_t>(scale) - NumericUtils<e8m0_scale_t>::bias);
+}
+
+__host__ __device__ inline e8m0_scale_t cast_from_float(float const scale)
+{
+    return static_cast<uint8_t>(std::log2(scale) + NumericUtils<e8m0_scale_t>::bias);
+}
+
 } // namespace ck::utils
