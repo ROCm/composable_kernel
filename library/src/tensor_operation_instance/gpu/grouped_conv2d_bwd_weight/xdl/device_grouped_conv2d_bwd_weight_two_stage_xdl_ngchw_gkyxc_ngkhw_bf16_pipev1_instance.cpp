783dc82064a1a1f5717238d5ac0bf7aa12cc0e1f
Bartłomiej Kocot <barkocot@amd.com>
2024-11-08 10:04:33 +0100

    Add generic instances for two stage conv bwd wei (#1643)
    * Add generic instances for two stage conv bwd wei
    
    * Update layout prefix
    
    [ROCm/composable_kernel commit: ea3640fdea4b11178c1657feff4849ad011e5d26]

--- library/src/tensor_operation_instance/gpu/grouped_conv2d_bwd_weight/xdl/device_grouped_conv2d_bwd_weight_two_stage_xdl_ngchw_gkyxc_ngkhw_bf16_pipev1_instance.cpp
+++ library/src/tensor_operation_instance/gpu/grouped_conv2d_bwd_weight/xdl/device_grouped_conv2d_bwd_weight_two_stage_xdl_ngchw_gkyxc_ngkhw_bf16_pipev1_instance.cpp
@@ -10,14 +10,14 @@ namespace device {
 namespace instance {
 
 // Compilation parameters for in[n, hi, wi, g, c] * wei[g, k, y, x, c] = out[n, ho, wo, g, k]
-void add_device_grouped_conv2d_bwd_weight_two_stage_xdl_nhwgc_gkyxc_nhwgk_f16_pipev2_instances(
+void add_device_grouped_conv2d_bwd_weight_two_stage_xdl_ngchw_gkyxc_ngkhw_bf16_pipev1_instances(
     std::vector<std::unique_ptr<DeviceGroupedConvBwdWeight<2,
-                                                           NHWGC,
+                                                           NGCHW,
                                                            GKYXC,
-                                                           NHWGK,
-                                                           F16,
-                                                           F16,
-                                                           F16,
+                                                           NGKHW,
+                                                           BF16,
+                                                           BF16,
+                                                           BF16,
                                                            PassThrough,
                                                            PassThrough,
                                                            PassThrough>>>& instances)
@@ -25,14 +25,14 @@ void add_device_grouped_conv2d_bwd_weight_two_stage_xdl_nhwgc_gkyxc_nhwgk_f16_pi
     // 1. Default
     add_device_operation_instances(
         instances,
-        device_grouped_conv_bwd_weight_two_stage_nhwgc_xdl_c_shuffle_f16_instances<
+        device_grouped_conv_bwd_weight_two_stage_ngchw_xdl_c_shuffle_bf16_generic_instances<
             2,
-            NHWGC,
+            NGCHW,
             GKYXC,
-            NHWGK,
+            NGKHW,
             ConvBwdWeightDefault,
             BlockGemmPipelineScheduler::Intrawave,
-            BlockGemmPipelineVersion::v2>{});
+            BlockGemmPipelineVersion::v1>{});
 }
 
 } // namespace instance
