fb80979eda7284eb04d2d8d829879f4a43c7b19e
Harisankar Sadasivan <hari.sadasivan@amd.com>
2024-11-18 18:51:18 +0000

    fp8 exclusion on pre-gfx94 for unievrsal_streamk

--- profiler/src/profile_gemm_universal_streamk.cpp
+++ profiler/src/profile_gemm_universal_streamk.cpp
@@ -86,8 +86,9 @@ int profile_gemm_universal_streamk(int argc, char* argv[])
     using F32 = float;
     using F16 = ck::half_t;
     // using BF16 = ck::bhalf_t;
-
+#if defined(CK_USE_FP8_ON_UNSUPPORTED_ARCH) || defined(CK_USE_GFX94)
     using F8 = ck::f8_t;
+#endif
 
     using Row = ck::tensor_layout::gemm::RowMajor;
     using Col = ck::tensor_layout::gemm::ColumnMajor;
@@ -146,7 +147,7 @@ int profile_gemm_universal_streamk(int argc, char* argv[])
     {
         return profile(F16{}, F16{}, F32{}, F16{}, Row{}, Col{}, Row{});
     }
-
+#if defined(CK_USE_FP8_ON_UNSUPPORTED_ARCH) || defined(CK_USE_GFX94)
     else if(data_type == GemmDataType::F16_F8_F16 && layout == GemmMatrixLayout::MK_KN_MN)
     {
         return profile(F16{}, F8{}, F32{}, F16{}, Row{}, Row{}, Row{});
@@ -163,7 +164,7 @@ int profile_gemm_universal_streamk(int argc, char* argv[])
     {
         return profile(F8{}, F16{}, F32{}, F16{}, Row{}, Col{}, Row{});
     }
-
+#endif
     else
     {
         std::cout << "this data_type & layout is not implemented" << std::endl;
