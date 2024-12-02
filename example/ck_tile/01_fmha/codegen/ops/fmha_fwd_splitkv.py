a8738c8c089a79b657b33ba2601169a760507dca
Po Yen Chen <PoYen.Chen@amd.com>
2024-12-02 08:22:37 +0000

    Remove blank lines

--- example/ck_tile/01_fmha/codegen/ops/fmha_fwd_splitkv.py
+++ example/ck_tile/01_fmha/codegen/ops/fmha_fwd_splitkv.py
@@ -249,7 +249,6 @@ float fmha_fwd_splitkv(fmha_fwd_splitkv_traits t, fmha_fwd_splitkv_args a, const
 
 FMHA_FWD_SPLITKV_API_INNER_DISPATCH="""            {F_if}((t.is_group_mode == {F_mode}) && (t.is_v_rowmajor == {F_vlayout}) && ({F_mask_check}) && (t.bias_type == {F_bias_check}) && (t.do_fp8_static_quant == {F_squant}) &&
                         ((a.block_table_ptr != nullptr) == {F_pagedkv}) && ({F_scheck}) && ({F_skcheck}) && ({F_dcheck}) && ({F_dvcheck})) {{
-
                 using traits_ = fmha_fwd_splitkv_traits_<{F_hdim}, {F_dtype}, {F_mode}, {F_bm0}, {F_bn0}, {F_bk0}, {F_bn1}, {F_bk1}, {F_bk0max}, {F_vlayout}, {F_pipeline_enum}, {F_mask}, {F_bias}, true, {F_squant}, {F_pagedkv}, {F_spad}, {F_skpad}, {F_dpad}, {F_dvpad}>;
                 if (t.has_lse) {{
                     if constexpr (std::is_same_v<{F_dtype}, ck_tile::fp8_t>) {{
