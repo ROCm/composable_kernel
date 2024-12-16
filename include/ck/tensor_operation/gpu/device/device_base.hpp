5f6eefa30ea529a0e67ca93bfaf71e824dd485ae
illsilin <Illia.Silin@amd.com>
2024-12-16 07:22:27 -0800

    fix clang format

--- include/ck/tensor_operation/gpu/device/device_base.hpp
+++ include/ck/tensor_operation/gpu/device/device_base.hpp
@@ -44,8 +44,8 @@ namespace device {
 
 struct BaseArgument
 {
-    BaseArgument()                               = default;
-    BaseArgument(const BaseArgument&)            = default;
+    BaseArgument()                    = default;
+    BaseArgument(const BaseArgument&) = default;
     BaseArgument& operator=(const BaseArgument&) = default;
 
     virtual ~BaseArgument() {}
@@ -55,8 +55,8 @@ struct BaseArgument
 
 struct BaseInvoker
 {
-    BaseInvoker()                              = default;
-    BaseInvoker(const BaseInvoker&)            = default;
+    BaseInvoker()                   = default;
+    BaseInvoker(const BaseInvoker&) = default;
     BaseInvoker& operator=(const BaseInvoker&) = default;
 
     virtual float Run(const BaseArgument*, const StreamConfig& = StreamConfig{})
@@ -69,8 +69,8 @@ struct BaseInvoker
 
 struct BaseOperator
 {
-    BaseOperator()                               = default;
-    BaseOperator(const BaseOperator&)            = default;
+    BaseOperator()                    = default;
+    BaseOperator(const BaseOperator&) = default;
     BaseOperator& operator=(const BaseOperator&) = default;
 
     virtual bool IsSupportedArgument(const BaseArgument*) { return false; }
