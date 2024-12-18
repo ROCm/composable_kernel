5a3a1706231f258471baba7feef0d8fcfbc947b9
ThomasNing <thomas.ning@amd.com>
2024-12-18 09:07:31 +0000

    Fixed Comment

--- example/ck_tile/03_gemm/script/benchmark_basic.sh
+++ example/ck_tile/03_gemm/script/benchmark_basic.sh
@@ -6,16 +6,16 @@ a_layouts=("R")
 b_layouts=("R" "C")
 c_layouts=("R")
 
-m_values=(64 512 1024 2048)
-n_values=(512 1024 2048)
-k_values=(64 512 1024 2048)
+matrix_dim_combinations_1=(64 512 1024 2048)
+matrix_dim_combinations_2=(512 1024 2048)
+
 
 for a in "${a_layouts[@]}"; do
     for b in "${b_layouts[@]}"; do
         for c in "${c_layouts[@]}"; do
-            for m in "${m_values[@]}"; do
-                for n in "${n_values[@]}"; do
-                    for k in "${k_values[@]}"; do
+            for m in "${matrix_dim_combinations_1[@]}"; do
+                for n in "${matrix_dim_combinations_2[@]}"; do
+                    for k in "${matrix_dim_combinations_1[@]}"; do
                         $EXE -prec=fp16 -b=1 -m=$m -n=$n -k=$k -a_layout="$a" -b_layout="$b" -c_layout="$c" -v=$VALID
                     done
                 done
