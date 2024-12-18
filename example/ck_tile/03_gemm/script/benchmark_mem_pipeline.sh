54fe505c5fe0a875553f9758b8aa84af1218315a
ThomasNing <thomas.ning@amd.com>
2024-12-18 09:05:03 +0000

    Fixed the comments

--- example/ck_tile/03_gemm/script/benchmark_mem_pipeline.sh
+++ example/ck_tile/03_gemm/script/benchmark_mem_pipeline.sh
@@ -2,11 +2,23 @@
 EXE="$(find . -name tile_example_gemm_universal -type f | head -n 1)"
 VALID=0
 
-for b_matrix in "R" "C"; do
-    for m in "64" "512" "1024" "2048"; do
-        for n in "512" "1024" "2048"; do
-            for k in "512" "1024" "2048"; do
-                $EXE -prec=fp16 -b=1 -m=$m -n=$n -k=$k -a_layout="R" -b_layout="$b_matrix" -c_layout="R" -v=$VALID
+a_layouts=("R")
+b_layouts=("R" "C")
+c_layouts=("R")
+
+m_values=(64 512 1024 2048)
+n_values=(512 1024 2048)
+k_values=(64 512 1024 2048)
+
+for a in "${a_layouts[@]}"; do
+    for b in "${b_layouts[@]}"; do
+        for c in "${c_layouts[@]}"; do
+            for m in "${m_values[@]}"; do
+                for n in "${n_values[@]}"; do
+                    for k in "${k_values[@]}"; do
+                        $EXE -prec=fp16 -b=1 -m=$m -n=$n -k=$k -a_layout="$a" -b_layout="$b" -c_layout="$c" -v=$VALID
+                    done
+                done
             done
         done
     done
