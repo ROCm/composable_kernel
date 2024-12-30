cce106f6a1984fef8e5fd8e083ef38ebcd0b80d4
Muhammed Emin Ozturk <meozturk@login1.hpcfund>
2024-12-29 23:41:27 -0600

    fix instance

--- library/src/tensor_operation_instance/gpu/gemm_universal_streamk/device_gemm_xdl_universal_streamk_bf16_bf16_bf16/device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_mnpadding_instance.cpp
+++ library/src/tensor_operation_instance/gpu/gemm_universal_streamk/device_gemm_xdl_universal_streamk_bf16_bf16_bf16/device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_mnpadding_instance.cpp
@@ -9,15 +9,9 @@ namespace device {
 namespace instance {
 
 void add_device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_mnpadding_instances(
-    std::vector<std::unique_ptr<DeviceGemm_Streamk_V2<Col,
-                                                      Row,
-                                                      Row,
-                                                      BF16,
-                                                      BF16,
-                                                      BF16,
-                                                      PassThrough,
-                                                      PassThrough,
-                                                      PassThrough>>>& instances)
+    std::vector<std::unique_ptr<
+    DeviceGemm_Streamk_V2<Col, Row, Row, BF16, BF16, BF16, PassThrough, PassThrough,PassThrough>>>& 
+    instances)
 {
     add_device_operation_instances(
         instances,
