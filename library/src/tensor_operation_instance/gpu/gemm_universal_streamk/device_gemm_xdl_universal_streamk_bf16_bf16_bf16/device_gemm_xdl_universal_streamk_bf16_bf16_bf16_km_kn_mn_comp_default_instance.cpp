e824fd381c7c299193b9cc1182777643406d00ff
Muhammed Emin Ozturk <meozturk@login1.hpcfund>
2024-12-31 18:42:47 -0600

    CopyRight Comment 2025

--- library/src/tensor_operation_instance/gpu/gemm_universal_streamk/device_gemm_xdl_universal_streamk_bf16_bf16_bf16/device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_default_instance.cpp
+++ library/src/tensor_operation_instance/gpu/gemm_universal_streamk/device_gemm_xdl_universal_streamk_bf16_bf16_bf16/device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_default_instance.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn.hpp"
 
@@ -9,19 +9,12 @@ namespace device {
 namespace instance {
 
 void add_device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_default_instances(
-    std::vector<std::unique_ptr<DeviceGemm_Streamk_V2<Col,
-                                                      Row,
-                                                      Row,
-                                                      BF16,
-                                                      BF16,
-                                                      BF16,
-                                                      PassThrough,
-                                                      PassThrough,
-                                                      PassThrough>>>& instances)
+    std::vector<std::unique_ptr<
+        DeviceGemm_Streamk_V2<Col, Row, Row, BF16, BF16, BF16, PassThrough, PassThrough, PassThrough>>>& 
+        instances)
 {
     add_device_operation_instances(
-        instances,
-        device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_instances<GemmDefault>{});
+        instances, device_gemm_xdl_universal_streamk_bf16_bf16_bf16_km_kn_mn_comp_instances<GemmDefault>{});
 }
 
 } // namespace instance
