fbfbd16f937ad555f7d86aa929949f5fb6146826
Po Yen, Chen <PoYen.Chen@amd.com>
2025-01-05 09:16:53 +0000

    Remove assumption of seqlen_q=1

--- example/ck_tile/01_fmha/fmha_fwd.hpp
+++ example/ck_tile/01_fmha/fmha_fwd.hpp
@@ -403,7 +403,8 @@ auto fmha_fwd_create_kargs_and_grids(fmha_fwd_args args)
     const ck_tile::index_t nhead =
         (FmhaKernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_k : args.nhead_q);
     const ck_tile::index_t max_seqlen_q =
-        (FmhaKernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : args.max_seqlen_q);
+        args.max_seqlen_q *
+        (FmhaKernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : 1);
 
     if constexpr(FmhaKernel::kIsGroupMode)
     {
@@ -517,7 +518,7 @@ auto fmha_fwd_splitkv_create_kargs_and_grids(fmha_fwd_splitkv_args args)
     const ck_tile::index_t nhead =
         (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_k : args.nhead_q);
     const ck_tile::index_t max_seqlen_q =
-        (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : args.max_seqlen_q);
+        args.max_seqlen_q * (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : 1);
 
     dim3 grids = Kernel::GridSize(args.batch, nhead, max_seqlen_q, args.hdim_v, args.num_splits);
 
@@ -579,7 +580,7 @@ auto fmha_fwd_splitkv_combine_create_kargs_and_grids(fmha_fwd_splitkv_args args)
     const ck_tile::index_t nhead =
         (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_k : args.nhead_q);
     const ck_tile::index_t max_seqlen_q =
-        (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : args.max_seqlen_q);
+        args.max_seqlen_q * (Kernel::kMergeNumHeadGroupsSeqLenQ ? args.nhead_q / args.nhead_k : 1);
 
     dim3 grids = Kernel::GridSize(args.batch, nhead, max_seqlen_q, args.hdim_v);
 
