b781628184e6fa55b2f7fe2f6a23da78e168279d
Mateusz Ozga <mateusz.ozga@amd.com>
2025-01-07 07:46:32 +0000

    Removed namespaces

--- include/ck_tile/host/host_tensor.hpp
+++ include/ck_tile/host/host_tensor.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #pragma once
 
@@ -684,13 +684,13 @@ auto host_tensor_descriptor(std::size_t row, std::size_t col, std::size_t stride
 {
     using namespace ck_tile::literals;
 
-    if constexpr(std::is_same_v<decltype(layout), ck_tile::tensor_layout::gemm::RowMajor>)
+    if constexpr(std::is_same_v<decltype(layout), tensor_layout::gemm::RowMajor>)
     {
-        return ck_tile::HostTensorDescriptor({row, col}, {stride, 1_uz});
+        return HostTensorDescriptor({row, col}, {stride, 1_uz});
     }
     else
     {
-        return ck_tile::HostTensorDescriptor({row, col}, {1_uz, stride});
+        return HostTensorDescriptor({row, col}, {1_uz, stride});
     }
 }
 template <typename TLayout>
@@ -698,7 +698,7 @@ auto get_default_stride(std::size_t row, std::size_t col, std::size_t stride, TL
 {
     if(stride == 0)
     {
-        if constexpr(std::is_same_v<decltype(layout), ck_tile::tensor_layout::gemm::RowMajor>)
+        if constexpr(std::is_same_v<decltype(layout), tensor_layout::gemm::RowMajor>)
         {
             return col;
         }
