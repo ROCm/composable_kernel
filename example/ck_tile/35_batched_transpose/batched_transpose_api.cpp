0c36b0beb0c958242792f1fed55109d52df691d4
ThruptiRajLakshmanaGowda <tlakshma@amd.com>
2025-01-15 22:44:09 +0000

    code cleanup

--- example/ck_tile/35_batched_transpose/batched_transpose_api.cpp
+++ example/ck_tile/35_batched_transpose/batched_transpose_api.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 #include "batched_transpose_api.hpp"
 
 #define BATCHED_TRANSPOSE_DISPATCH()                                                   \
