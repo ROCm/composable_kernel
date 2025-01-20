832f11e0c0c656a38bd48b8623a93b928d634c9e
Andriy Roshchenko <andriy.roshchenko@amd.com>
2025-01-20 23:29:27 +0000

    Cleanup

--- include/ck_tile/core/numeric/half.hpp
+++ include/ck_tile/core/numeric/half.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "ck_tile/core/config.hpp"
 #include "ck_tile/core/utility/bit_cast.hpp"
