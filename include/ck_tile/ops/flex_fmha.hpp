4007289ad0072ef39d0adbadd7416fa73fc40664
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-01-22 16:42:04 +0000

    copy over fmha example
    remove bwd related commands from cmakelists
    
    remove unused ops in the example;
    
    select only bf16/nodropout/nolse/batched
    
    pass validation in the example driver
    
    fork pipeline
    
    add a hardcoded score_mod
    
    fork the kernel
    
    abstract score_mod from a pipeline
    
    unhardcode score_mod and pass it as a cpp expression from codegen
    
    modify host attention impl accounting for score_mod
    
    use custom score for testing
    
    reorder score mod and scale in host verification
    
    use cmakelists as the single source of truth for score_mod function definition
    
    fix numeric mismatches
    
    run clang-format
    
    remove bwd related scripts
    
    edit test and benchmark scripts for the new example
    
    remove readme
    
    remove unused cases from smoke test
    
    re-add group-mode kernels
    
    Add pre_softmax fnctor (#1852)
    
    * Add pre_softmax fnctor
    
    * remove stray define:wq
    
    * Move op out of pipeline, adds it to refnc
    
    ---------
    
    Co-authored-by: root <root@splinter-126-wr-d1.aus.dcgpu>
    Co-authored-by: Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
    
    added flex_attention in Jenkins file
    
    fixing clang
    
    fixing clang
    
    space added
    
    fixed copyright  errors
    
    fixed even more clangformat
    
    formatting
    
    modified jenkins
    
    fixed typo
    
    added flex attention test for gfx90a and gfx942
    
    fixed typo
    
    fixed example name
    
    fixed example script name
    
    added perf logs for both gpu arch
    
    pipeline fixes for accuracy issues; disable pre-softmax function until its accuracy is fixed
    
    added stash and unstash for perf logs
    
    fixed typo in perf name
    
    print error message
    
    print success  message
    
    hardcoded perf files names
    
    flex attention jenkins switch off
    
    flex attention jenkins switch off from settings
    
    fixed typo
    
    add context to score-mod signature

---
+++ include/ck_tile/ops/flex_fmha.hpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
+
+#pragma once
+
+#include "ck_tile/ops/fmha/block/block_attention_bias_enum.hpp"
+#include "ck_tile/ops/fmha/block/block_dropout.hpp"
+#include "ck_tile/ops/fmha/block/block_masking.hpp"
+#include "ck_tile/ops/fmha/block/block_position_encoding.hpp"
+#include "ck_tile/ops/fmha/block/block_rotary_embedding.hpp"
+#include "ck_tile/ops/fmha/block/page_block_navigator.hpp"
+#include "ck_tile/ops/fmha/kernel/fmha_flex_fwd_kernel.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_enum.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_problem.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_flex_qr_ks_vs.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_flex_qr_ks_vs_async.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_async_default_policy.hpp"
+#include "ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_default_policy.hpp"
+#include "ck_tile/ops/fmha/pipeline/tile_fmha_shape.hpp"
+#include "ck_tile/ops/fmha/pipeline/tile_fmha_traits.hpp"
+#include "ck_tile/ops/common/generic_2d_block_shape.hpp"
+#include "ck_tile/ops/common/tensor_layout.hpp"
