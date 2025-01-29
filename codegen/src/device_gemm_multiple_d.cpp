76fec53fcbe1e4dbd2f6078b8e004565a993902e
Astha Rai <astha.rai713@gmail.com>
2025-01-29 18:23:03 +0000

    updated copyright headers

--- codegen/src/device_gemm_multiple_d.cpp
+++ codegen/src/device_gemm_multiple_d.cpp
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
--- codegen/test/gemm_multiple_d.cpp
+++ codegen/test/gemm_multiple_d.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "ck/host/device_gemm_multiple_d/problem.hpp"
 #include "ck/host/device_gemm_multiple_d/operation.hpp"
