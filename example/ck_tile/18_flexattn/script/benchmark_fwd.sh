60d0af7e1cbef837580f719badd6fcda36bbf055
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-01-31 01:54:22 +0000

    edit test and benchmark scripts for the new example

--- example/ck_tile/18_flexattn/script/benchmark_fwd.sh
+++ example/ck_tile/18_flexattn/script/benchmark_fwd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # TODO: run this script from CK root or build directory
-EXE="$(find . -name tile_example_fmha_fwd -type f | head -n 1)"
+EXE="$(find . -name tile_example_flexattn_fwd -type f | head -n 1)"
 VALID=0
 
-for prec in "fp16" "bf16" ; do
-for perm in 0 1 ; do
+for prec in "bf16" ; do
+for perm in 0 ; do
 for hdim in 64 128 256 ; do
 
 nhead=$((2048 / $hdim))     # follow fav2 setup
@@ -17,15 +17,4 @@ $EXE -prec=$prec -b=1  -h=$nhead -d=$hdim -s=16384 -iperm=$perm -operm=$perm -kn
 
 done
 done
-done
-
-for perm in 0 1 ; do
-
-$EXE -prec=fp8 -squant=1 -b=32 -h=16 -d=128 -s=512   -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-$EXE -prec=fp8 -squant=1 -b=16 -h=16 -d=128 -s=1024  -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-$EXE -prec=fp8 -squant=1 -b=8  -h=16 -d=128 -s=2048  -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-$EXE -prec=fp8 -squant=1 -b=4  -h=16 -d=128 -s=4096  -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-$EXE -prec=fp8 -squant=1 -b=2  -h=16 -d=128 -s=8192  -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-$EXE -prec=fp8 -squant=1 -b=1  -h=16 -d=128 -s=16384 -iperm=$perm -operm=$perm -vlayout=c -range_q=240 -range_k=240 -range_v=240 -range_p=240 -range_o=240 -kname=1 -v=$VALID ; sleep 3
-
 done
