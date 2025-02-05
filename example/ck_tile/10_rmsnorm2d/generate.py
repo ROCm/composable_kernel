014e9ecba92d7ab1f48da5167a7aa79fec651bf2
Jiming Ruan <Jiming.Ruan@amd.com>
2025-02-05 06:47:49 +0000

    adjust workload

--- example/ck_tile/10_rmsnorm2d/generate.py
+++ example/ck_tile/10_rmsnorm2d/generate.py
@@ -536,7 +536,9 @@ float rmsnorm2d_fwd(rmsnorm2d_fwd_traits t,
                                   h_traits('x', 'y', 'xs', 'ys', 1,  4,  1,1024, 2,  True,  False, False,   0,    0),
                                   h_traits('x', 'y', 'xs', 'ys', 1,  8,  1,1024, 1,  True,  False, False,   0,    0)],
                         'big'  :[ h_traits('x', 'y', 'xs', 'ys', 1,  1,  1,1024, 8,  True,  False,  True,   0,    0),
-                                  h_traits('x', 'y', 'xs', 'ys', 1, 20,  1, 256, 1,  True,  False,  True,   0,    0)]}
+                                  h_traits('x', 'y', 'xs', 'ys', 1,  4,  1, 256, 4,  True,  False,  True,   0,    0),
+                                  h_traits('x', 'y', 'xs', 'ys', 1, 12,  1, 256, 2,  True,  False,  True,   0,    0),
+                                  h_traits('x', 'y', 'xs', 'ys', 1, 16,  1, 256, 1,  True,  False,  True,   0,    0)]}
         total_blob = list()
         for hs_key in h_trait_dict:
             hs = h_trait_dict[hs_key]
