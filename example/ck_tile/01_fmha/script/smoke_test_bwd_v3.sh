f88ba67e1188d0bf5a11c7ddaaa1f26abc266a37
danyao12 <danyao12@amd.com>
2025-02-05 17:22:52 +0800

    smoke test update

--- example/ck_tile/01_fmha/script/smoke_test_bwd_v3.sh
+++ example/ck_tile/01_fmha/script/smoke_test_bwd_v3.sh
@@ -10,13 +10,15 @@ COMMON_ARGS='-v=1'
 set -x
 for prec in "fp16" "bf16" ; do
 for perm in 0 1 ; do
-for hdim in 128 ; do
+for hdim in 64 72 96 128 ; do
 for v3_atomic_fp32 in 0 1 ; do
+for v3_bf16_cvt in 0 1 2 ; do
 for mask in 0 1 ; do
 
-$EXE -prec=$prec -b=4 -h=2 -d=$hdim -s=512 -iperm=$perm -operm=$perm -mask=$mask -bwd_v3=1 -v3_atomic_fp32=$v3_atomic_fp32 -mode=0 -kname=$KNAME $COMMON_ARGS
-$EXE -prec=$prec -b=1 -h=3 -d=$hdim -s=768 -iperm=$perm -operm=$perm -mask=$mask -bwd_v3=1 -v3_atomic_fp32=$v3_atomic_fp32 -mode=0 -kname=$KNAME $COMMON_ARGS
+$EXE -prec=$prec -b=2 -h=4 -h_k=2 -d=$hdim -s=512 -iperm=$perm -operm=$perm -mask=$mask -bwd_v3=1 -v3_atomic_fp32=$v3_atomic_fp32 -v3_bf16_cvt=$v3_bf16_cvt -mode=0 -kname=$KNAME $COMMON_ARGS
+$EXE -prec=$prec -b=1 -h=3 -h_k=1 -d=$hdim -s=768 -iperm=$perm -operm=$perm -mask=$mask -bwd_v3=1 -v3_atomic_fp32=$v3_atomic_fp32 -v3_bf16_cvt=$v3_bf16_cvt -mode=0 -kname=$KNAME $COMMON_ARGS
 
+done
 done
 done
 done
