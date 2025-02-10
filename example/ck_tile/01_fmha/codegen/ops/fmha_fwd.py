ddfc27ea8c83b4b76091b40ea07b8980938a82e8
Andy Lugo <Andy.LugoReyes@amd.com>
2025-02-10 19:05:28 +0000

    Add receipt 4 option to codegen

--- example/ck_tile/01_fmha/codegen/ops/fmha_fwd.py
+++ example/ck_tile/01_fmha/codegen/ops/fmha_fwd.py
@@ -494,6 +494,20 @@ def get_fwd_blobs(kernel_filter : Optional[str], receipt, mask_impl) -> Tuple[Fm
                     cond &= pipeline.F_squant == 'f'
                     if not cond:
                         continue
+                if receipt == 3:
+                    cond = dtype in ['fp16', 'bf16']
+                    cond &= pipeline.F_vlayout == 'row'
+                    cond &= pipeline.F_bias in ['no', 'alibi']
+                    cond &= pipeline.F_squant == 'f'
+                    if not cond:
+                        continue
+                if receipt == 4:
+                    cond = dtype in ['fp16', 'bf16']
+                    cond &= pipeline.F_vlayout == 'row'
+                    cond &= pipeline.F_bias in ['no', 'bias']
+                    cond &= pipeline.F_squant == 'f'
+                    if not cond:
+                        continue
                 api_pool.register_traits(k.api_trait())
                 gen.append(k)
 
