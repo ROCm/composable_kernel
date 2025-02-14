9de1a520b9ab3eecf5c4db26a9091dae9f95dfe9
rocking <chunyu.lai@amd.com>
2025-02-14 05:05:24 -0500

    update receipt

--- example/ck_tile/01_fmha/codegen/ops/fmha_bwd.py
+++ example/ck_tile/01_fmha/codegen/ops/fmha_bwd.py
@@ -526,6 +526,7 @@ def get_bwd_dq_dk_dv_blobs(kernel_filter : Optional[str], receipt, mask_impl) ->
             elif receipt == 11:
                     cond = dtype in ['fp16', 'bf16']
                     cond &= mode == "group"
+                    cond &= bias in ['no', 'alibi']
                     cond &= dropout in ['no', 'dropout_wg32',  'dropout_wg16']
                     cond &= dpad == dvpad
                     cond &= deterministic == "f"
