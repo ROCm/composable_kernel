dfd15c220d48724a891da8f9c365912f00babcc6
jefyang1 <146495389+jefyang1@users.noreply.github.com>
2025-03-04 10:32:25 -0800

    Remove CK_USE_AMD_MFMA_GFX950 (#1935)
    * Add runtime check in example_gemm_xdl_streamk for gfx950
    
    * Add runtime check in grouped conv fwd examples for gfx950
    
    * Disable CK_USE_AMD_MFMA_GFX950
    
    * Add new instances for gfx950
    
    * Fix test_gemm_universal on gfx950
    
    [ROCm/composable_kernel commit: c95bda93bacd4b8f809becd7c4b9e8b743d0ded8]

--- library/src/tensor_operation_instance/gpu/gemm_universal_reduce/device_gemm_xdl_universal_bf16_bf16_bf16/device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_default_instance.cpp
+++ library/src/tensor_operation_instance/gpu/gemm_universal_reduce/device_gemm_xdl_universal_bf16_bf16_bf16/device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn_comp_default_instance.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "device_gemm_xdl_universal_bf16_bf16_bf16_mk_kn_mn.hpp"
+#include "ck/host_utility/device_prop.hpp"
 
 namespace ck {
 namespace tensor_operation {
@@ -24,6 +25,14 @@ void add_device_gemm_xdl_universal_reduce_bf16_bf16_bf16_mk_kn_mn_comp_default_i
     add_device_operation_instances(
         instances,
         device_gemm_xdl_universal_reduce_bf16_bf16_bf16_mk_kn_mn_comp_instances<GemmDefault>{});
+
+    if(ck::get_device_name() != "gfx950")
+    {
+        add_device_operation_instances(
+            instances,
+            device_gemm_xdl_universal_reduce_bf16_bf16_bf16_mk_kn_mn_comp_instances_part2<
+                GemmDefault>{});
+    }
 }
 
 } // namespace instance
