b92caa3d84af3cc4fb5a2e340f1d8bcac44c4f0e
Illia Silin <98187287+illsilin@users.noreply.github.com>
2025-03-11 10:40:18 -0700

    Revert "[CK_TILE] support hdim=192/128 pair for deepseekv3 (#1961)" (#1969)
    This reverts commit 45fbd9210a2e6b88ff289bc1c640476f225d216a.
    
    [ROCm/composable_kernel commit: 8cbcd3e0d07db65d85cd7f67aff973d5a28d83e5]

--- include/ck_tile/core.hpp
+++ include/ck_tile/core.hpp
@@ -8,8 +8,11 @@
 #include "ck_tile/core/algorithm/indexing_adaptor.hpp"
 #include "ck_tile/core/algorithm/space_filling_curve.hpp"
 #include "ck_tile/core/algorithm/static_encoding_pattern.hpp"
-#include "ck_tile/core/arch/amd_buffer_addressing.hpp"
+#if __clang_major__ >= 20
 #include "ck_tile/core/arch/amd_buffer_addressing_builtins.hpp"
+#else
+#include "ck_tile/core/arch/amd_buffer_addressing.hpp"
+#endif
 #include "ck_tile/core/arch/arch.hpp"
 #include "ck_tile/core/arch/generic_memory_space_atomic.hpp"
 #include "ck_tile/core/arch/utility.hpp"
