5b57aec5f9c8f480f4586fcd308bce802fea73d1
root <root@banff-cyxtera-s70-4.ctr.dcgpu>
2025-03-20 09:20:35 +0000

    gtest succesfully added

--- profiler/include/profiler/profile_gemm_universal_streamk_impl.hpp
+++ profiler/include/profiler/profile_gemm_universal_streamk_impl.hpp
@@ -26,6 +26,7 @@ namespace profiler {
 
 template <typename ADataType,
           typename BDataType,
+          typename ComputeDataType,
           typename AccDataType,
           typename CDataType,
           typename ALayout,
@@ -131,7 +132,8 @@ bool profile_gemm_universal_streamk_impl(int do_verification,
                                                                                 AccDataType,
                                                                                 AElementOp,
                                                                                 BElementOp,
-                                                                                CElementOp>;
+                                                                                CElementOp,
+                                                                                ComputeDataType>;
 
         auto ref_gemm    = ReferenceGemmInstance{};
         auto ref_invoker = ref_gemm.MakeInvoker();
