895c0f7fd900149fc27387794550903688d86ee8
Rostyslav Geyyer <rosty.geyyer@amd.com>
2025-03-24 17:02:48 +0000

    Add printf to fix intrinsic

--- include/ck/utility/type_convert.hpp
+++ include/ck/utility/type_convert.hpp
@@ -832,6 +832,7 @@ inline __host__ __device__ f4x2_t f4_convert_sr(float2_t x, float scale = 1.0f)
         f4x2_t f4x2_array[4];
     } value{0};
     // permute high bits and low bits to match the order of the original vector
+    printf("This printf is required to fix FP32 to FP4 conversion intrinsic \n");
     value.bitwise = __builtin_amdgcn_cvt_scalef32_sr_pk_fp4_f32(
         value.bitwise, float2_t{x[1], x[0]}, rng, scale, 0);
     return value.f4x2_array[0];
