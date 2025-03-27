018e77f7f0e2c33940fc86103b63f6ed25d2d796
coderfeli <coderfeli@163.com>
2025-03-27 11:58:22 +0000

    change cmake; use smaller shape for i4

--- example/65_gemm_multiply_multiply/moe_gemm1_xdl_pk_i4.cpp
+++ example/65_gemm_multiply_multiply/moe_gemm1_xdl_pk_i4.cpp
@@ -191,14 +191,14 @@ int main(int argc, char* argv[])
     // experts = 8
     // per expert:
     // GEMM shape
-    ck::index_t N               = 14336 * 2;
-    ck::index_t K               = 4096;
+    ck::index_t N               = 4096 * 2;
+    ck::index_t K               = 6144;
     ck::index_t experts         = 8;
     ck::index_t sorted_tile_num = 16;
     ck::index_t valid_tile_num  = 13;
     ck::index_t sorted_size     = sorted_tile_num * MPerBlock;
     ck::index_t valid_size      = valid_tile_num * MPerBlock;
-    ck::index_t tokens          = 64;
+    ck::index_t tokens          = 644;
     ck::index_t topk            = 2;
 
     if(argc == 1)
