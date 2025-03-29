a3c6ca1761f2fe3b7d1f3752f62b42a65bd73540
root <bobofang@amd.com>
2025-03-29 21:05:21 +0000

    [GEMM] fix MFMA configurations

--- example/ck_tile/99_toy_example/02_gemm/block_gemm_asmem_bsmem_creg_default_policy.hpp
+++ example/ck_tile/99_toy_example/02_gemm/block_gemm_asmem_bsmem_creg_default_policy.hpp
@@ -6,10 +6,10 @@
 #include "ck_tile/core.hpp"
 #include "ck_tile/ops/gemm/warp/warp_gemm.hpp"
 
-#define mfma_m32_n32_k16 1
 #define mfma_m32_n32_k8 0
+#define mfma_m32_n32_k16 0
 #define mfma_m16_n16_k16 0
-#define mfma_m16_n16_k32 0
+#define mfma_m16_n16_k32 1
 
 namespace ck_tile {
 
@@ -21,21 +21,6 @@ struct BlockGemmASmemBSmemCRegDefaultPolicy
     CK_TILE_HOST_DEVICE static constexpr auto GetWarpGemmMWarpNWarp()
     {
 #if mfma_m32_n32_k8
-#pragma message ("mfma m32 n32 k16")
->>>>>>> Stashed changes
-        if constexpr(std::is_same_v<typename Problem::ADataType, half_t> &&
-                     std::is_same_v<typename Problem::BDataType, half_t> &&
-                     std::is_same_v<typename Problem::CDataType, float>)
-        {
-            return make_tuple(WarpGemmMfmaF16F16F32M32N32K16TransposedCDistribution{}, 4, 1);
-        }
-        else if constexpr(std::is_same_v<typename Problem::ADataType, bf16_t> &&
-                          std::is_same_v<typename Problem::BDataType, bf16_t> &&
-                          std::is_same_v<typename Problem::CDataType, float>)
-        {
-            return make_tuple(WarpGemmMfmaBf16Bf16F32M32N32K16TransposedCDistribution{}, 4, 1);
-        }
-#elif mfma_m32_n32_k16
 #pragma message ("mfma m32 n32 k8")
         if constexpr(std::is_same_v<typename Problem::ADataType, half_t> &&
                      std::is_same_v<typename Problem::BDataType, half_t> &&
@@ -49,7 +34,20 @@ struct BlockGemmASmemBSmemCRegDefaultPolicy
         {
         return make_tuple(WarpGemmMfmaBf16Bf16F32M32N32K8TransposedCDistribution{}, 4, 1);
         }
-	
+#elif mfma_m32_n32_k16
+#pragma message ("mfma m32 n32 k16")
+        if constexpr(std::is_same_v<typename Problem::ADataType, half_t> &&
+                     std::is_same_v<typename Problem::BDataType, half_t> &&
+                     std::is_same_v<typename Problem::CDataType, float>)
+        {
+            return make_tuple(WarpGemmMfmaF16F16F32M32N32K16TransposedCDistribution{}, 4, 1);
+        }
+        else if constexpr(std::is_same_v<typename Problem::ADataType, bf16_t> &&
+                          std::is_same_v<typename Problem::BDataType, bf16_t> &&
+                          std::is_same_v<typename Problem::CDataType, float>)
+        {
+            return make_tuple(WarpGemmMfmaBf16Bf16F32M32N32K16TransposedCDistribution{}, 4, 1);
+        }
 #elif mfma_m16_n16_k16
 #pragma message("mfma m16 n16 k16")
         if constexpr(std::is_same_v<typename Problem::ADataType, half_t> &&
