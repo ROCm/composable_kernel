99682572a21c83054f003df4f60e4f4a8e2046e7
ThomasNing <thomas.ning@amd.com>
2025-04-03 00:19:43 -0500

    Fix the ck tile gemm on MI355

--- include/ck_tile/core/config.hpp
+++ include/ck_tile/core/config.hpp
@@ -248,11 +248,9 @@ CK_TILE_DECLARE_ENV_VAR_BOOL(CK_TILE_LOGGING)
 #define CK_TILE_USE_OCP_FP8 0
 #endif
 #elif defined(__gfx950__) || defined(__gfx12__) // for GPU code
-#ifdef CK_TILE_USE_OCP_FP8
 #define CK_TILE_USE_OCP_FP8 1
-#else
+#else // for GPU code
 #define CK_TILE_USE_OCP_FP8 0
-#endif
 #endif
 
 #ifndef CK_TILE_USE_BUFFER_ADDRESSING_BUILTIN
