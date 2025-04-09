1766e6d3be592d2068f55bbedd556ea342def3a0
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-04-09 10:34:37 +0000

    Update to the scripts and error thresholds

--- example/ck_tile/18_hstu_attention/benchmark_hstu_attention.sh
+++ example/ck_tile/18_hstu_attention/benchmark_hstu_attention.sh
@@ -4,13 +4,13 @@ BUILD=build
 EXE=$BUILD/bin/tile_example_hstu_attention
 
 for dtype in "fp16" "bf16"; do
+    set -x 
+
     ## jagged is true
-    cmd="$EXE -v=0 -prec=$dtype -b=80 -jagged=1 -nhead=8 -hdim_qk=128 -hdim_v=128 -seqlen=1000 -causal=1 -local_len=5 -context_len=8 -minfull_len=7 -targets=8 -perf=1"
-    echo $cmd
     $EXE -v=0 -prec=$dtype -b=80 -jagged=1 -nhead=8 -hdim_qk=128 -hdim_v=128 -seqlen=1000 -causal=1 -local_len=5 -context_len=8 -minfull_len=7 -targets=8 -perf=1
 
     ## jagged is false
-    cmd="$EXE -v=0 -prec=$dtype -b=80 -jagged=0 -nhead=8 -hdim_qk=128 -hdim_v=128 -seqlen=1000 -causal=1 -local_len=5 -context_len=8 -minfull_len=7 -targets=8 -perf=1"
-    echo $cmd
     $EXE -v=0 -prec=$dtype -b=80 -jagged=0 -nhead=8 -hdim_qk=128 -hdim_v=128 -seqlen=1000 -causal=1 -local_len=5 -context_len=8 -minfull_len=7 -targets=8 -perf=1
+
+    set +x
 done
