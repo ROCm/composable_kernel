5b1c397806486057102e539b3ba33d21fecf7e57
YC Lin <yc.lin@amd.com>
2025-04-10 14:22:22 +0000

    [GEMM] Refactor GetStaticLdsSize and remove GetSmemSize

--- example/ck_tile/99_toy_example/02_gemm/gemm.hpp
+++ example/ck_tile/99_toy_example/02_gemm/gemm.hpp
@@ -64,34 +64,12 @@ struct TileGemmShape
 template <bool kPadM_,
           bool kPadN_,
           bool kPadK_,
-          typename ALayout_,
-          typename BLayout_,
-          typename CLayout_>
-struct TileGemmTraits
-{
-    static constexpr bool kPadM = kPadM_;
-    static constexpr bool kPadN = kPadN_;
-    static constexpr bool kPadK = kPadK_;
-
-    // TODO this can't be hardcoded here! Should be in policy!
-    static constexpr int _VectorSize = 16;
-
-    using ALayout = ALayout_;
-    using BLayout = BLayout_;
-    using CLayout = CLayout_;
-
-    static constexpr bool TransposeC = false;
-};
-
-template <bool kPadM_,
-          bool kPadN_,
-          bool kPadK_,
           bool DoubleSmemBuffer_,
           typename ALayout_,
           typename BLayout_,
           typename CLayout_,
           bool TransposeC_ = false>
-struct TileGemmUniversalTraits
+struct TileGemmTraits
 {
     static constexpr bool kPadM = kPadM_;
     static constexpr bool kPadN = kPadN_;
@@ -284,22 +262,21 @@ struct Gemm
                                             PermuteA,
                                             PermuteB>;
 
-            using GemmUniversalTraits =
-                TileGemmUniversalTraits<kPadM,
-                                        kPadN,
-                                        kPadK,
-                                        DoubleSmemBuffer,
-                                        /* ALayout */ tensor_layout::gemm::RowMajor,
-                                        /* BLayout */ tensor_layout::gemm::ColumnMajor,
-                                        /* CLayout */ tensor_layout::gemm::RowMajor,
-                                        TransposeC>;
+            using GemmTraits = TileGemmTraits<kPadM,
+                                              kPadN,
+                                              kPadK,
+                                              DoubleSmemBuffer,
+                                              /* ALayout */ tensor_layout::gemm::RowMajor,
+                                              /* BLayout */ tensor_layout::gemm::ColumnMajor,
+                                              /* CLayout */ tensor_layout::gemm::RowMajor,
+                                              TransposeC>;
 
             using BlockGemmPipelineProblem_ =
                 BlockGemmPipelineProblem<ADataType,
                                         BDataType,
                                         AccDataType,
                                         GemmShape,
-                                        GemmUniversalTraits,
+                                        GemmTraits,
                                         GemmPipelineScheduler::Intrawave,
                                         /* Has hot loop */ true,
                                         TailNumber::Full>;
