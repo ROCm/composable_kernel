9e020272c455d47957550e9c2f730a708a84a741
YC Lin <yc.lin@amd.com>
2025-04-14 03:29:27 +0000

    [GEMM] Remove redundant GetBlockGemm

--- example/ck_tile/99_toy_example/02_gemm/block_gemm_pipeline_agmem_bgmem_creg_default_policy.hpp
+++ example/ck_tile/99_toy_example/02_gemm/block_gemm_pipeline_agmem_bgmem_creg_default_policy.hpp
@@ -350,34 +350,11 @@ struct BlockGemmPipelineAGmemBGmemCRegDefaultPolicy
     }
 #endif
 
-#if 0
     template <typename Problem>
     CK_TILE_HOST_DEVICE static constexpr auto GetBlockGemm()
     {
-        using BlockWarps      = typename Problem::BlockGemmShape::BlockWarps;
-        using WarpTile        = typename Problem::BlockGemmShape::WarpTile;
-        using WarpGemm        = WarpGemmMfmaDispatcher<typename Problem::ComputeDataType,
-                                                typename Problem::ComputeDataType,
-                                                typename Problem::CDataType,
-                                                WarpTile::at(I0),
-                                                WarpTile::at(I1),
-                                                WarpTile::at(I2),
-                                                Problem::TransposeC>;
-        using BlockGemmPolicy = BlockGemmASmemBSmemCRegV1CustomPolicy<typename Problem::ADataType,
-                                                                      typename Problem::BDataType,
-                                                                      typename Problem::CDataType,
-                                                                      BlockWarps,
-                                                                      WarpGemm>;
-        return BlockUniversalGemmAsBsCr<Problem, BlockGemmPolicy>{};
+        return BlockGemmASmemBSmemCReg<Problem>{};
     }
-#else
-    template <typename Problem>
-    CK_TILE_HOST_DEVICE static constexpr auto GetBlockGemm()
-    {
-        using Policy = BlockGemmASmemBSmemCRegDefaultPolicy;
-        return BlockGemmASmemBSmemCReg<Problem, Policy>{};
-    }
-#endif
 };
 
 } // namespace ck_tile
