8fa2c73628f575f05fe0101af1ba9f171d293899
letaoqin <letaoqin@amd.com>
2025-04-17 08:57:00 +0000

    add hdim=160 to codegen

---
+++ example/ck_tile/01_fmha/script/benchmark_fwd_moe5t.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# TODO: run this script from CK root or build directory
+EXE="$(find . -name tile_example_fmha_fwd -type f | head -n 1)"
+VALID=0
+prec="fp16"
+bsz=1
+hdim=160
+sk=16384
+
+#
+for nheadkv in 2 1 1 1;do
+for nhead in 40 20 10 5;do
+for seqlen in 4028 2048 1024 512; do
+$EXE -prec=$prec -b=$bsz -h=$nhead -h_k=$nheadkv -d=$hdim -s=$seqlen -s_k=$sk  -kname=1 -v=$VALID ; sleep 1
+done
+done
+done
