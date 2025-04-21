022ed3fd8a211cf8846a29a10f2e89cc8f85f837
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-04-21 14:41:51 +0000

    Back to use exp() instead of exp2() since exp() in ck_tile using fast __builtin_amdgcn_exp2f()

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline.hpp
@@ -247,7 +247,7 @@ struct HstuAttentionFwdPipelineQRKSVS
         const auto f_silu = [](CompDataType& x) {
             const auto one = ck_tile::type_convert<CompDataType>(1.0f);
 
-            return x = x / (one + exp2(-log2e_v<CompDataType> * x));
+            return x = x / (one + exp(-x));
         };
 
         using OaccBlockTileType = decltype(gemm_1.MakeCBlockTile());
