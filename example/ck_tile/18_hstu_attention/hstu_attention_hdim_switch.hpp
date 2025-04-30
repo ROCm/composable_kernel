17fd4dee877a6a2a371ddd2d37ec219b69e4723a
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-04-30 01:54:43 +0000

    Temporarily close the instance for hdim64 and hdim256 to save compiling time

--- example/ck_tile/18_hstu_attention/hstu_attention_hdim_switch.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_hdim_switch.hpp
@@ -10,7 +10,7 @@
     [&] {                                                              \
         if(HDIM_1 <= 64 && HDIM_2 <= 64)                               \
         {                                                              \
-            constexpr ck_tile::index_t CONST_NAME = 64;                \
+            constexpr ck_tile::index_t CONST_NAME = 128;               \
             __VA_ARGS__();                                             \
         }                                                              \
         else if(HDIM_1 <= 128 && HDIM_2 <= 128)                        \
@@ -20,7 +20,7 @@
         }                                                              \
         else if(HDIM_1 <= 256 && HDIM_2 <= 256)                        \
         {                                                              \
-            constexpr ck_tile::index_t CONST_NAME = 256;               \
+            constexpr ck_tile::index_t CONST_NAME = 128;               \
             __VA_ARGS__();                                             \
         }                                                              \
         else                                                           \
