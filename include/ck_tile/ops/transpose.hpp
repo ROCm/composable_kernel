26257e4cb4a670ddad5520069dce480fea16f993
joyeamd <John.Ye@amd.com>
2025-05-07 07:57:23 +0800

    move transpose ops to example directory

--- include/ck_tile/ops/transpose.hpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
-
-#pragma once
-
-#include "ck_tile/ops/transpose/batched_transpose_kernel.hpp"
-#include "ck_tile/ops/transpose/block_transpose.hpp"
-#include "ck_tile/ops/transpose/transpose_policy.hpp"
