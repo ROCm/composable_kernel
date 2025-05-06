297e7667a70e70e652b5b3923b4c4455bfb16ec2
ThruptiRajLakshmanaGowda <tlakshma@amd.com>
2025-05-06 13:15:44 +0000

    Addressing Review Comment

--- example/ck_tile/35_batched_transpose/batched_transpose_example.cpp
+++ example/ck_tile/35_batched_transpose/batched_transpose_example.cpp
@@ -162,7 +162,7 @@ bool run_batched_transpose(ck_tile::ArgParser args)
 
     x_dev.ToDevice(x_host.data());
 
-    x_dev.FromDevice(x_host.data());
+    // x_dev.FromDevice(x_host.data());
 
     auto trait = batched_transpose_trait{prec, layout_in};
 
