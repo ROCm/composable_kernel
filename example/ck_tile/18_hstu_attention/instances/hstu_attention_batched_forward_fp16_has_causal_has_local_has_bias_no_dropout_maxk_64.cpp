1d1dd8f1eb6c4da743a45f2b6d7cd0649f42b476
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-05-07 13:37:15 +0000

    Revert "Temporarily close the instance for hdim64 and hdim256 to save compiling time"
    This reverts commit 2972de4c88a3e11e63b2ec3b0014ce8aba911ad6.

---
+++ example/ck_tile/18_hstu_attention/instances/hstu_attention_batched_forward_fp16_has_causal_has_local_has_bias_no_dropout_maxk_64.cpp
@@ -0,0 +1,22 @@
+
+/*
+  Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * The file is automatically generated, don't modify!
+ * See the generator script
+ * `composable_kernel/example/ck_tile/18_hstu_attention/generate_instances.py`
+ */
+
+#include <ck_tile/core/numeric/bfloat16.hpp>
+#include "hstu_attention_batched_forward_dispatch.hpp"
+
+template void run_batched_forward_causal_local_bias_dropout_dispatch<
+    ck_tile::fp16_t,
+    true,
+    true,
+    true,
+    false,
+    64>(HstuAttentionFwdParams& param, hipStream_t stream);
