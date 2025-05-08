069b791fc34068fe1821ce61dc7afa06ed964a6f
joye <joye@amd.com>
2025-05-08 15:55:44 +0800

    update include file

--- include/ck_tile/core.hpp
+++ include/ck_tile/core.hpp
@@ -13,6 +13,7 @@
 #include "ck_tile/core/arch/arch.hpp"
 #include "ck_tile/core/arch/generic_memory_space_atomic.hpp"
 #include "ck_tile/core/arch/utility.hpp"
+#include "ck_tile/core/arch/amd_transpose_load_encoding.hpp"
 #include "ck_tile/core/config.hpp"
 #include "ck_tile/core/container/array.hpp"
 #include "ck_tile/core/container/container_helper.hpp"
