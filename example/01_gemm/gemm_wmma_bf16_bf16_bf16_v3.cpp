4b5a9acc95973023c3e75d8adde475bfa13aeed2
apoorva <apoorva@streamhpc.com>
2025-05-08 21:46:27 +0000

    Fixing clang format

--- example/01_gemm/gemm_wmma_bf16_bf16_bf16_v3.cpp
+++ example/01_gemm/gemm_wmma_bf16_bf16_bf16_v3.cpp
@@ -38,7 +38,7 @@ using DeviceGemmInstance = ck::tensor_operation::device::DeviceGemm_Wmma_CShuffl
         128, 128, 
         32, 8, 8,
         16,   16,
-        4,    2, 
+        4,    2,  
         S<4, 64, 1>,  S<0, 2, 1>, S<0, 2, 1>, 
         1, 1, 8, 1, 
         S<4, 64, 1>,  S<0, 2, 1>, S<0, 2, 1>,       
