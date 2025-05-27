f41e5986195040cccfbb0e453e2b17bc33c94c1f
Aviral Goel <aviral.goel@amd.com>
2025-05-27 18:01:00 +0000

    Suggestions for LWPCK-3170: elementwise kernel improvements

--- include/ck_tile/ops/elementwise.hpp
+++ include/ck_tile/ops/elementwise.hpp
@@ -7,7 +7,7 @@
 #include "ck_tile/ops/elementwise/pipeline/elementwise_operators.hpp"
 #include "ck_tile/ops/elementwise/pipeline/elementwise_pipeline_default_policy.hpp"
 #include "ck_tile/ops/elementwise/pipeline/elementwise_pipeline_problem.hpp"
-#include "ck_tile/ops/elementwise/pipeline/elementwise_traits.hpp"
+#include "ck_tile/ops/elementwise/pipeline/elementwise_shape.hpp"
 #include "ck_tile/ops/elementwise/unary_element_wise_operation.hpp"
 #include "ck_tile/ops/common/generic_2d_block_shape.hpp"
 #include "ck_tile/ops/common/tensor_layout.hpp"
