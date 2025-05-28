2d74123427aa6187d66fe8c9491db6a8ea764bab
John Shumway <jshumway@amd.com>
2025-05-28 15:52:04 -0400

    Shard several of the most costly targets.
    Introduces a filter_tuple_by_modulo to break up tuples.
    
    Drops build time of target from 21 minutes to under 14 minutes with 64
    build processes, or 11 minutes with 128 build processes.
    
    time ninja -j 64 device_grouped_conv3d_fwd_instance

---
+++ library/src/tensor_operation_instance/gpu/grouped_conv3d_fwd/xdl/mem/device_grouped_conv3d_fwd_xdl_ngcdhw_gkczyx_ngkdhw_f16_mem_intra_instance_8of10.cpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+
+#include "device_grouped_conv3d_fwd_xdl_ngcdhw_gkczyx_ngkdhw_f16_mem_intra_instance.inc"
+
+namespace ck::tensor_operation::device::instance {
+template
+void add_device_grouped_conv3d_fwd_xdl_ngcdhw_gkczyx_ngkdhw_f16_mem_intra_instances_sharded<10, 7>(
+    device_grouped_conv3d_fwd_xdl_ngcdhw_gkczyx_ngkdhw_f16_mem_intra_instances& instances);
+} // namespace ck::tensor_operation::device::instance
