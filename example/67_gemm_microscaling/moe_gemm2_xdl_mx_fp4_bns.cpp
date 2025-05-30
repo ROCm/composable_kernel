6cba96e510af18844569e0214bf694c1f7b4ff8b
Ding, Yi <yi.ding@amd.com>
2025-05-30 05:46:31 +0000

    Use v1 pipeline for example_moe_gemm2_xdl_mx_fp4_bns

--- example/67_gemm_microscaling/moe_gemm2_xdl_mx_fp4_bns.cpp
+++ example/67_gemm_microscaling/moe_gemm2_xdl_mx_fp4_bns.cpp
@@ -159,7 +159,7 @@ using DeviceOpInstance                     = ck::tensor_operation::device::Devic
     S<8, 32, 1>, S<1, 0, 2>, S<1, 0, 2>, 2, 16, 16, 0,
     S<8, 32, 1>, S<1, 0, 2>, S<1, 0, 2>, 2, 16, 16, 0,
     2,    2,   S<1, 32, 1, 8>, S<2, 1, 1, 1>,
-    ck::BlockGemmPipelineScheduler::Intrawave, ck::BlockGemmPipelineVersion::v3, 0, false, false, MulRoutedWeight, ck::index_t, A0DataType>;
+    ck::BlockGemmPipelineScheduler::Intrawave, ck::BlockGemmPipelineVersion::v1, 0, false, false, MulRoutedWeight, ck::index_t, A0DataType>;
 // clang-format on
 
 int main(int argc, char* argv[])
