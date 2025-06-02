bcf93e292c77e26fad1208bd024357a0ce39a455
Anton Gorenko <anton@streamhpc.com>
2025-06-02 15:09:31 +0500

    Prepare gemma_add tests for adding wmma

--- test/gemm_add/test_gemm_add_silu_xdl.cpp
+++ test/gemm_add/test_gemm_add_silu_xdl.cpp
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #include "gtest/gtest.h"
 #include "ck/ck.hpp"
 #include "profiler/profile_gemm_add_silu_impl.hpp"
-#include "test_gemm_add_xdl.hpp"
+#include "test_gemm_common.hpp"
 
 template <typename Tuple>
-class TestGemmAddSilu : public TestGemmAdd<Tuple>
+class TestGemmAddSilu : public TestGemmD0Common<Tuple>
 {
     private:
     using ADataType   = std::tuple_element_t<0, Tuple>;
