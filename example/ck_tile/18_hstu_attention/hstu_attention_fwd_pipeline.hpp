ee915c32cf836e826216b08300306f1091a1265f
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-06-06 14:20:41 +0000

    Update IsFulleTileInsideMask() for kUseLocal is true situtation

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_pipeline.hpp
@@ -406,21 +406,27 @@ struct HstuAttentionFwdPipelineQRKSVS
                 {
                     if constexpr(HstuMask::kUseLocal)
                     {
-                        constexpr auto s_spans = SaccBlockTileType::get_distributed_spans();
-                        sweep_tile_span(s_spans[number<0>{}], [&](auto idx0) {
-                            sweep_tile_span(s_spans[number<1>{}], [&](auto idx1) {
-                                const auto tile_idx = get_x_indices_from_distributed_indices(
-                                    sacc_tile.get_tile_distribution(), make_tuple(idx0, idx1));
+                        if(!mask.IsFullTileInsideMask(q_origin.at(number<0>{}),
+                                                      seqlen_k_curr,
+                                                      number<kK1>{},
+                                                      number<kM0>{}))
+                        {
+                            constexpr auto s_spans = SaccBlockTileType::get_distributed_spans();
+                            sweep_tile_span(s_spans[number<0>{}], [&](auto idx0) {
+                                sweep_tile_span(s_spans[number<1>{}], [&](auto idx1) {
+                                    const auto tile_idx = get_x_indices_from_distributed_indices(
+                                        sacc_tile.get_tile_distribution(), make_tuple(idx0, idx1));
 
-                                const auto row =
-                                    q_origin.at(number<0>{}) + tile_idx.at(number<0>{});
-                                const auto col         = seqlen_k_curr + tile_idx.at(number<1>{});
-                                constexpr auto i_j_idx = make_tuple(idx0, idx1);
+                                    const auto row =
+                                        q_origin.at(number<0>{}) + tile_idx.at(number<0>{});
+                                    const auto col = seqlen_k_curr + tile_idx.at(number<1>{});
+                                    constexpr auto i_j_idx = make_tuple(idx0, idx1);
 
-                                sacc_tile(i_j_idx) *= static_cast<GemmAccDataType>(
-                                    mask.IsTokenPairInsideMask(row, col));
+                                    sacc_tile(i_j_idx) *= static_cast<GemmAccDataType>(
+                                        mask.IsTokenPairInsideMask(row, col));
+                                });
                             });
-                        });
+                        }
                     }
                     else // kUseCausal=true, kUseLocal=false
                     {
