ca7667991ded1749757df28f0ac9cce8c60bbbfe
Aviral Goel <aviral.goel@amd.com>
2025-06-09 16:08:12 -0400

    Revert "Include necessary files"

--- test/scatter_gather/scatter_gather.cpp
+++ test/scatter_gather/scatter_gather.cpp
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2018-2024, Advanced Micro Devices, Inc. All rights reserved.
 
-#include <ctime>
 #include <vector>
 #include <iostream>
 #include <numeric>
