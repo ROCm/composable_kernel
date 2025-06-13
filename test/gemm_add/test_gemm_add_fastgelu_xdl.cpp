61b6e9a60639acc6e7d3c4c85da4cc62bed00be3
Zoltan Lakatos <zoltan.lakatos@streamhpc.com>
2025-06-13 09:37:37 +0000

    splitk xdl test fixes

--- test/gemm_add/test_gemm_add_fastgelu_xdl.cpp
+++ test/gemm_add/test_gemm_add_fastgelu_xdl.cpp
@@ -24,30 +24,6 @@ class TestGemmAddFastgelu : public TestGemmD0Common<Tuple>
             typename TestGemmD0Common<Tuple>::D0Layout,
             typename TestGemmD0Common<Tuple>::ELayout>;
     }
-
-    private:
-    using ADataType   = std::tuple_element_t<0, Tuple>;
-    using BDataType   = std::tuple_element_t<1, Tuple>;
-    using AccDataType = std::tuple_element_t<2, Tuple>;
-    using D0DataType  = std::tuple_element_t<3, Tuple>;
-    using EDataType   = std::tuple_element_t<4, Tuple>;
-    using ALayout     = std::tuple_element_t<5, Tuple>;
-    using BLayout     = std::tuple_element_t<6, Tuple>;
-    using D0Layout    = std::tuple_element_t<7, Tuple>;
-    using ELayout     = std::tuple_element_t<8, Tuple>;
-
-    constexpr static auto ProfileGemmAddFastgeluImpl =
-        ck::profiler::profile_gemm_add_fastgelu_impl<ADataType,
-                                                     BDataType,
-                                                     AccDataType,
-                                                     D0DataType,
-                                                     EDataType,
-                                                     ALayout,
-                                                     BLayout,
-                                                     D0Layout,
-                                                     ELayout>;
-
-    decltype(ProfileGemmAddFastgeluImpl) GetImpl() override { return ProfileGemmAddFastgeluImpl; }
 };
 
 using KernelTypes = ::testing::Types<std::tuple<F16, I8, F32, F16, F16, Row, Row, Row, Row>,
