92af6c85c8bef68c1f934e13ea7c611f3a50e401
Ville Pietilä <>
2025-06-17 09:05:03 +0000

    Profiler script improvements.

--- script/run_conv_profiler.py
+++ script/run_conv_profiler.py
@@ -4,7 +4,6 @@ import os
 import argparse
 import subprocess
 import sys
-import pandas as pd
 
 def parse_cli_args():
     """Parse command line arguments"""
@@ -13,6 +12,8 @@ def parse_cli_args():
     parser.add_argument("--log-to-stdout", action="store_true", help="Log profiler output to stdout instead of /dev/null.")
     parser.add_argument("--start", type=int, default=None, help="Start index for the commands to run (1-based).")
     parser.add_argument("--end", type=int, default=None, help="End index for the commands to run (1-based).")
+    parser.add_argument("--disabled-ops", type=str, default=None, help="Comma-separated list of disabled operations for the profiler.")
+    parser.add_argument("--run-id", type=str, default=None, help="Run ID for the profiler run.")
     
     args, unknown_args = parser.parse_known_args()
     
@@ -22,12 +23,18 @@ def parse_cli_args():
     
     return args
 
-def run_ck_profiler_cmd(cmd, log_to_stdout=False):
+def run_ck_profiler_cmd(cmd, disabled_ops, run_id, log_to_stdout=False):
     working_dir = os.path.dirname(os.path.abspath(__file__))
     pid = os.getpid()
     env_vars = os.environ.copy()
-    env_vars["CK_PROFILER_DISABLED_OPS"] = "DeviceGroupedConvBwdWeight_Xdl_CShuffleV3;DeviceGroupedConvBwdWeight_Dl;DeviceGroupedConvBwdWeightTwoStage_Xdl_CShuffle;DeviceGroupedConvBwdWeight_Explicit_Xdl"
-    env_vars["CK_PROFILER_OUTPUT_FILE"] = f"{working_dir}/conv_profiler_output_{pid}.csv"
+    disabled_ops_str = ""
+    for op in disabled_ops:
+      disabled_ops_str += op + ";"
+    disabled_ops_str = disabled_ops_str.strip(';')
+    env_vars["CK_PROFILER_DISABLED_OPS"] = disabled_ops_str
+    env_vars["CK_PROFILER_OUTPUT_DIR"] = working_dir
+    run_id_str = f"_{run_id}" if run_id else ""
+    env_vars["CK_PROFILER_OUTPUT_FILE"] = f"{working_dir}/conv_profiler_output{run_id_str}_{pid}.csv"
     if log_to_stdout:
       subprocess.run(cmd, env=env_vars) 
     else:
@@ -54,6 +61,11 @@ def main():
         end = min(args.end, end)
       profiler_commands = profiler_commands[args.start-1:end]
 
+  disabled_ops = []
+  if args.disabled_ops:
+      disabled_ops = args.disabled_ops.split(',')
+      disabled_ops = [op.strip() for op in disabled_ops if op.strip()]
+
   for i, cmd in enumerate(profiler_commands):
       cmd_concatenated_str = ""
       for arg in cmd:
@@ -62,7 +74,7 @@ def main():
       print(f"\n##################################################################################################################################")
       print(f"Running command {i + 1}/{len(profiler_commands)}: {cmd_concatenated_str}")
       print(f"##################################################################################################################################")
-      run_ck_profiler_cmd(cmd, args.log_to_stdout)
+      run_ck_profiler_cmd(cmd, disabled_ops, args.run_id, args.log_to_stdout)
   
 if __name__ == "__main__":
     main()
