d6cd6bbaf1310e3530f216fd21215c93b10786c9
AviralGoel <aviral.goel@amd.com>
2025-06-20 14:52:02 +0000

    p1 default instances add

--- library/include/ck/library/tensor_operation_instance/gpu/gemm_universal_preshuffle.hpp
+++ library/include/ck/library/tensor_operation_instance/gpu/gemm_universal_preshuffle.hpp
@@ -68,6 +68,8 @@ struct DeviceOperationInstanceFactory<
                     op_ptrs);
                 add_device_gemm_xdl_universal_preshuffle_f8_f8_bf16_mk_mfma32x32_mn_compute_instances(
                     op_ptrs);
+                add_device_gemm_xdl_universal_preshuffle_f8_f8_bf16_mk_mfma_mn_p1_instances(
+                    op_ptrs);
                 add_device_gemm_xdl_universal_preshuffle_f8_f8_bf16_mk_mfma16x16_nk_mn_comp_default_instances_part5(
                     op_ptrs);
                 add_device_gemm_xdl_universal_preshuffle_f8_f8_bf16_mk_mfma16x16_nk_mn_comp_default_instances_part6(
