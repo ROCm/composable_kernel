c87a2174750b58d17af0294f8c0e8da4a24921bb
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-06-22 15:20:47 +0000

    Update to test_ck_hstu_mask.sh and test_pytorch_hstu_mask.py to align their testings

--- example/ck_tile/18_hstu_attention/scripts/test_ck_hstu_mask.sh
+++ example/ck_tile/18_hstu_attention/scripts/test_ck_hstu_mask.sh
@@ -3,4 +3,8 @@
 BUILD=build
 EXE=$BUILD/bin/tile_example_hstu_attention
 
-$EXE -v=1 -prec=fp16 -b=3 -jagged=1 -nhead=1 -hdim_qk=128 -hdim_v=128 -seqlens=56,60,64 -causal=1 -local_len=4 -context_len=3 -minfull_len=0 -targets=4,5,6 -save_mask=1 
+$EXE -v=1 -prec=fp16 -b=3 -jagged=1 -nhead=1 -hdim_qk=128 -hdim_v=128 -seqlens=49,52,55 -causal=1 -local_len=4 -context_len=3 -minfull_len=0 -targets=4,5,6 -save_mask=1
+mv ck_hstu_mask.dat ck_hstu_mask_0.dat
+
+$EXE -v=1 -prec=fp16 -b=3 -jagged=1 -nhead=1 -hdim_qk=128 -hdim_v=128 -seqlens=49,52,55 -causal=1 -local_len=4 -context_len=3 -minfull_len=6 -targets=4,5,6 -save_mask=1
+mv ck_hstu_mask.dat ck_hstu_mask_1.dat
