71d65d42944309b250f6c78de3cc9478ee5b1f95
apoorva <apoorva@streamhpc.com>
2025-06-25 08:24:32 +0000

    Updated tests to ad BF16 instances as per review comment

--- test/gemm_add/test_gemm_add_wmma.cpp
+++ test/gemm_add/test_gemm_add_wmma.cpp
@@ -25,10 +25,9 @@ class TestGemmAdd : public TestGemmD0Common<Tuple>
     }
 };
 
-using KernelTypes = ::testing::Types<std::tuple<F16, F16, F32, F16, F16, Row, Row, Row, Row>,
-                                     std::tuple<F16, F16, F32, F16, F16, Row, Col, Row, Row>,
-                                     std::tuple<F16, F16, F32, F16, F16, Col, Row, Row, Row>,
-                                     std::tuple<F16, F16, F32, F16, F16, Col, Col, Row, Row>>;
+using KernelTypes =
+    ::testing::Types<std::tuple<F16, F16, F32, F16, F16, Row, Row, ck::Tuple<Row>, Row>,
+                     std::tuple<BF16, BF16, F32, BF16, BF16, Row, Row, ck::Tuple<Row>, Row>>;
 
 TYPED_TEST_SUITE(TestGemmAdd, KernelTypes);
 TYPED_TEST(TestGemmAdd, Test_BF16FP16) { this->Run(); }
