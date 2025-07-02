48fd4a9f0a4e4c2e7943067e2ad65d1388b05e97
Gino Lu <gino.lu@amd.com>
2025-07-02 01:58:52 -0500

    fix test indent

--- test/ck_tile/data_type/test_pk_fp4.cpp
+++ test/ck_tile/data_type/test_pk_fp4.cpp
@@ -54,24 +54,24 @@ TEST(PackedFp4, NumericBasic)
 TEST(PackedFp4, ConvertDevice)
 {
     constexpr bool is_device = true;
-	test_convert<fp32_t, fp32_t, is_device>(); // fp32 -> fp4 -> fp32
-	test_convert<fp16_t, fp16_t, is_device>();
-	test_convert<bf16_t, bf16_t, is_device>();
-	test_convert<fp32_t, fp16_t, is_device>();
-	test_convert<fp32_t, bf16_t, is_device>();
-	test_convert<fp16_t, fp32_t, is_device>();
-	test_convert<bf16_t, fp32_t, is_device>();
+    test_convert<fp32_t, fp32_t, is_device>(); // fp32 -> fp4 -> fp32
+    test_convert<fp16_t, fp16_t, is_device>();
+    test_convert<bf16_t, bf16_t, is_device>();
+    test_convert<fp32_t, fp16_t, is_device>();
+    test_convert<fp32_t, bf16_t, is_device>();
+    test_convert<fp16_t, fp32_t, is_device>();
+    test_convert<bf16_t, fp32_t, is_device>();
 }
 TEST(PackedFp4, ConvertHost)
 {
     constexpr bool is_device = false;
-	test_convert<fp32_t, fp32_t, is_device>(); // fp32 -> fp4 -> fp32
-	test_convert<fp16_t, fp16_t, is_device>();
-	test_convert<bf16_t, bf16_t, is_device>();
-	test_convert<fp32_t, fp16_t, is_device>();
-	test_convert<fp32_t, bf16_t, is_device>();
-	test_convert<fp16_t, fp32_t, is_device>();
-	test_convert<bf16_t, fp32_t, is_device>();
+    test_convert<fp32_t, fp32_t, is_device>(); // fp32 -> fp4 -> fp32
+    test_convert<fp16_t, fp16_t, is_device>();
+    test_convert<bf16_t, bf16_t, is_device>();
+    test_convert<fp32_t, fp16_t, is_device>();
+    test_convert<fp32_t, bf16_t, is_device>();
+    test_convert<fp16_t, fp32_t, is_device>();
+    test_convert<bf16_t, fp32_t, is_device>();
 }
 
 #define toF32(x) ck_tile::type_convert<float>(x)
@@ -90,47 +90,47 @@ struct SrcPkfp4Dst {
         using DSTx2_t = ck_tile::ext_vector_t<DST, 2>;
 
         ck_tile::static_for<0, N, 2>{}([&](auto i) {
-            const auto input2 = SRCx2_t{src[i], src[i+1]};
+                const auto input2 = SRCx2_t{src[i], src[i+1]};
 
-            if( i % 4 == 0) { 
+                if( i % 4 == 0) { 
                 // ex: fp32_t -> fp4 -> bf16_t
                 dst[i] = toDST(toPF4(src[i]));	
                 // ex: fp32x2_t -> pk_fp4 -> unpack<0> -> bf16_t
                 dst[i+1] = toDST(toPF4(toPF4(input2).unpack(number<1>{})));
-            }
-            else { 
+                }
+                else { 
                 // ex: fp32x2_t -> pk_fp4_t -> bf16x2_t
                 reinterpret_cast<DSTx2_t*>(dst)[i>>1] = toDSTx2(toPF4(input2));
-            }
-        });
+                }
+                });
     }
 };
 
 template<typename SRC, typename DST, bool is_device>
 CK_TILE_HOST void test_convert() {
-	const auto test_data = std::array{
-		0.f, 0.25f, 0.5f, 0.75f, 1.f, 1.25f, 1.5f, 1.75f,
-		-0.f, -0.25f, -0.5f, -0.75f, -1.f, -1.25f, -1.5f, -1.75f,
-		2.f, 2.5f, 3.f, 3.5f, 4.f, 5.f, 5.0625f, 6.f};
-	const auto ref_data = std::array{
-		0.f, 0.f, 0.5f, 1.f, 1.f, 1.f, 1.5f, 2.f,
-		-0.f, -0.f, -0.5f, -1.f, -1.f, -1.f, -1.5f, -2.f,
-		2.f, 2.f, 3.f, 4.f, 4.f, 4.f, 6.f, 6.f};
-		
-	static_assert(test_data.size() == ref_data.size());
+    const auto test_data = std::array{
+        0.f, 0.25f, 0.5f, 0.75f, 1.f, 1.25f, 1.5f, 1.75f,
+            -0.f, -0.25f, -0.5f, -0.75f, -1.f, -1.25f, -1.5f, -1.75f,
+            2.f, 2.5f, 3.f, 3.5f, 4.f, 5.f, 5.0625f, 6.f};
+    const auto ref_data = std::array{
+        0.f, 0.f, 0.5f, 1.f, 1.f, 1.f, 1.5f, 2.f,
+            -0.f, -0.f, -0.5f, -1.f, -1.f, -1.f, -1.5f, -2.f,
+            2.f, 2.f, 3.f, 4.f, 4.f, 4.f, 6.f, 6.f};
+
+    static_assert(test_data.size() == ref_data.size());
     static_assert(test_data.size() % 2 == 0);
 
-	constexpr int N = test_data.size();
-	std::array<SRC, N> in;
-	std::array<DST, N> ref, out;
-	
+    constexpr int N = test_data.size();
+    std::array<SRC, N> in;
+    std::array<DST, N> ref, out;
+
     // prepare input and ground truth in host
-	for(int i=0; i<N; ++i) {
-		in[i] = toSRC(test_data[i]);
-		ref[i] = toDST(ref_data[i]);
-		EXPECT_EQ(test_data[i], toF32(in[i]));
-		EXPECT_EQ(ref_data[i], toF32(ref[i]));
-	}
+    for(int i=0; i<N; ++i) {
+        in[i] = toSRC(test_data[i]);
+        ref[i] = toDST(ref_data[i]);
+        EXPECT_EQ(test_data[i], toF32(in[i]));
+        EXPECT_EQ(ref_data[i], toF32(ref[i]));
+    }
 
     using job = SrcPkfp4Dst<SRC, DST, N>;
 
@@ -140,16 +140,16 @@ CK_TILE_HOST void test_convert() {
         in_d -> ToDevice(in.data());
 
         MyKernel<job><<<1, 1>>> (
-            reinterpret_cast<const SRC*>(in_d -> GetDeviceBuffer()), 
-            reinterpret_cast<DST*>(out_d -> GetDeviceBuffer()));
-        
+                reinterpret_cast<const SRC*>(in_d -> GetDeviceBuffer()), 
+                reinterpret_cast<DST*>(out_d -> GetDeviceBuffer()));
+
         out_d -> FromDevice(out.data());
     }
     else {
         job{}(in.data(), out.data());
     }
-	
-	for(int i=0; i<N; ++i)
-		EXPECT_EQ(ref[i], out[i]) << "i:" << i;
+
+    for(int i=0; i<N; ++i)
+        EXPECT_EQ(ref[i], out[i]) << "i:" << i;
 }
 
