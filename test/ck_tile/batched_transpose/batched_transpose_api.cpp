79cc5ecc181ffa0ea082102c26fd9100a0aebd27
Emily Martins <emily.martins@amd.com>
2025-07-06 19:55:07 +0000

    update copyright and licensing in files

--- test/ck_tile/batched_transpose/batched_transpose_api.cpp
+++ test/ck_tile/batched_transpose/batched_transpose_api.cpp
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
 #include "batched_transpose.hpp"
 
 template <typename ts_type,
