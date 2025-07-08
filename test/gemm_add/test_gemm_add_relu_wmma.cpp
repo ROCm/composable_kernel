bdfdb0c11ec93c819d734afcc52945d8d43ffcd6
apoorva <apoorva@streamhpc.com>
2025-07-08 12:17:20 +0000

    Fixes applied according to review comments

--- test/gemm_add/test_gemm_add_relu_wmma.cpp
+++ test/gemm_add/test_gemm_add_relu_wmma.cpp
@@ -26,9 +26,8 @@ class TestGemmAddRelu : public TestGemmD0Common<Tuple>
     }
 };
 
-using KernelTypes =
-    ::testing::Types<std::tuple<F16, F16, F32, F16, F16, Row, Row, ck::Tuple<Row>, Row>,
-                     std::tuple<BF16, BF16, F32, BF16, BF16, Row, Row, ck::Tuple<Row>, Row>>;
+using KernelTypes = ::testing::Types<std::tuple<F16, F16, F32, F16, F16, Row, Row, Row, Row>,
+                                     std::tuple<BF16, BF16, F32, BF16, BF16, Row, Row, Row, Row>>;
 
 TYPED_TEST_SUITE(TestGemmAddRelu, KernelTypes);
 TYPED_TEST(TestGemmAddRelu, Test_BF16FP16) { this->Run(); }
