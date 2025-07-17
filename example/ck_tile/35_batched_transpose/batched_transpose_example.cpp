815062e9f618ae79643b1128574a6b73502eb847
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-07-17 13:37:31 -0500

    count both read and written bytes into bandwidth (x2 the number)

--- example/ck_tile/35_batched_transpose/batched_transpose_example.cpp
+++ example/ck_tile/35_batched_transpose/batched_transpose_example.cpp
@@ -187,18 +187,14 @@ bool run_batched_transpose(ck_tile::ArgParser args)
 
     auto ms = batched_transpose(trait, karg, sc);
 
-    std::size_t num_operations = N * C * H * (W - 1);
-    std::size_t num_bytes      = N * C * H * W * sizeof(Type);
+    std::size_t num_bytes      = N * C * H * W * sizeof(Type) * 2; // read + written
 
-    float ave_time   = ms * 1E-3;
     float gb_per_sec = num_bytes / ms * 1.E-6;
-    float tflops     = static_cast<float>(num_operations) / ms * 1.E-6;
 
     std::cout << "Run Batched Transpose kernel with N=" << N << ", C=" << C << ", H=" << H
               << ", W=" << W << ", layout_in=" << layout_in << ", layout_out=" << layout_out
               << " : " << std::endl
-              << ms << " ms (" << ave_time << " ave_time) " << std::endl
-              << tflops << " TFlops" << std::endl
+              << ms << " ms " << std::endl
               << gb_per_sec << " GB/s " << std::endl;
 
     printf("[%s]N:%d, C:%d, H:%d, W:%d, layout_in:%s, %f\n",
