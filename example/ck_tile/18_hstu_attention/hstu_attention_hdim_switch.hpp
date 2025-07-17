35c60f348ad445feffaa7343d703cb21022296f0
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-07-17 09:00:41 +0000

    Revert "Disable support of hdim64 amnd hdim256 for quick compiling and testing"
    This reverts commit ed062f93e2ad835d15bdc3d862213286e2c63b7b.

--- example/ck_tile/18_hstu_attention/hstu_attention_hdim_switch.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_hdim_switch.hpp
@@ -8,9 +8,19 @@
 
 #define HDIM_SWITCH(HDIM_1, HDIM_2, CONST_NAME, ...)                   \
     [&] {                                                              \
-        if(HDIM_1 <= 128 && HDIM_2 <= 128)                             \
+        if(HDIM_1 <= 64 && HDIM_2 <= 64)                               \
+        {                                                              \
+            constexpr ck_tile::index_t CONST_NAME = 64;                \
+            __VA_ARGS__();                                             \
+        }                                                              \
+        else if(HDIM_1 <= 128 && HDIM_2 <= 128)                        \
         {                                                              \
             constexpr ck_tile::index_t CONST_NAME = 128;               \
+            __VA_ARGS__();                                             \
+        }                                                              \
+        else if(HDIM_1 <= 256 && HDIM_2 <= 256)                        \
+        {                                                              \
+            constexpr ck_tile::index_t CONST_NAME = 256;               \
             __VA_ARGS__();                                             \
         }                                                              \
         else                                                           \
