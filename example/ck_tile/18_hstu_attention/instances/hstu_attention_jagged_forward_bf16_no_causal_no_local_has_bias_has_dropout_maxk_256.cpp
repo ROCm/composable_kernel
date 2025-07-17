fed1474e4f7d8109aba6c7ff06c80e357709b4b1
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-07-17 09:00:41 +0000

    Revert "Disable support of hdim64 amnd hdim256 for quick compiling and testing"
    This reverts commit ed062f93e2ad835d15bdc3d862213286e2c63b7b.

---
+++ example/ck_tile/18_hstu_attention/instances/hstu_attention_jagged_forward_bf16_no_causal_no_local_has_bias_has_dropout_maxk_256.cpp
@@ -0,0 +1,22 @@
+
+/*
+  Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * The file is automatically generated, don't modify!
+ * See the generator script
+ * `composable_kernel/example/ck_tile/18_hstu_attention/generate_instances.py`
+ */
+
+#include <ck_tile/core/numeric/half.hpp>
+#include "hstu_attention_jagged_forward_dispatch.hpp"
+
+template void run_jagged_forward_causal_local_bias_dropout_dispatch<
+    ck_tile::bf16_t,
+    false,
+    false,
+    true,
+    true,
+    256>(HstuAttentionFwdParams& param, hipStream_t stream);
