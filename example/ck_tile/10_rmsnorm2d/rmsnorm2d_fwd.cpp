1cb4149d8279e780683dc6dba3efd401f96ab9fb
Clement Lin <clement.lin@amd.com>
2025-07-30 13:00:03 +0800

    Fix clang format

--- example/ck_tile/10_rmsnorm2d/rmsnorm2d_fwd.cpp
+++ example/ck_tile/10_rmsnorm2d/rmsnorm2d_fwd.cpp
@@ -197,7 +197,7 @@ bool run(const ck_tile::ArgParser& arg_parser)
     {
         use_model_sensitive_rmsnorm = 0;
     }
-            
+
     std::cout << "[" << prec_str << "]"
               << " m:" << m << ", n:" << n << ", x_stride:" << x_stride
               << ", xr_stride:" << xr_stride << ", y_stride:" << y_stride
