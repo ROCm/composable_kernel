ec81649b329eeb8c0d9c515dee1c45a238fe79c3
ThruptiRajLakshmanaGowda <Thruptiraj.LakshmanaGowda@amd.com>
2025-08-05 19:24:23 +0000

    Working code before Unary element support

--- tile_engine/ops/gemm_multi_d/benchmark_gemm_multi_d.hpp
+++ tile_engine/ops/gemm_multi_d/benchmark_gemm_multi_d.hpp
@@ -91,7 +91,7 @@ void gemm_multi_d_host_reference(int verify,
                                            DsDataType,
                                            AccDataType,
                                            EDataType,
-                                           ck_tile::element_wise::PassThrough>(
+                                           MultiplyMultiply>(
             a_m_k, b_k_n, {d0_m_n, d1_m_n}, e_m_n_host_result);
     }
     std::cout << "Exiting gemm_multi_d_host_reference function" << std::endl;
