8acf78f25bff6f1f4ddba26f63bbd1b5caf0334e
illsilin_amdeng <Illia.Silin@amd.com>
2025-08-04 19:29:32 -0700

    fix clang format

--- example/02_gemm_bilinear/gemm_bilinear_wmma_int8.cpp
+++ example/02_gemm_bilinear/gemm_bilinear_wmma_int8.cpp
@@ -21,7 +21,7 @@
 
 struct AlphaBetaAdd
 {
-    AlphaBetaAdd(int alpha, int beta) : alpha_(alpha), beta_(beta) {};
+    AlphaBetaAdd(int alpha, int beta) : alpha_(alpha), beta_(beta){};
 
     template <typename E, typename C, typename D>
     __host__ __device__ constexpr void operator()(E& e, const C& c, const D& d) const;
