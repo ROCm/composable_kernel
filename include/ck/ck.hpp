108171ae8307593c7855afc9a2c965e211a99ac6
Slobodan Josic <127323561+slojosic-amd@users.noreply.github.com>
2025-08-12 21:38:10 +0200

    enable gfx115x support (#2065) (#2552)
    Co-authored-by: Illia Silin <98187287+illsilin@users.noreply.github.com>

--- include/ck/ck.hpp
+++ include/ck/ck.hpp
@@ -70,7 +70,8 @@ CK_DECLARE_ENV_VAR_BOOL(CK_LOGGING)
 #define __gfx103__
 #endif
 #if defined(__gfx1100__) || defined(__gfx1101__) || defined(__gfx1102__) || \
-    defined(__gfx1103__) || defined(__gfx11_generic__)
+    defined(__gfx1103__) || defined(__gfx1150__) || defined(__gfx1151__) || \
+    defined(__gfx1152__) || defined(__gfx11_generic__)
 #define __gfx11__
 #endif
 #if defined(__gfx1200__) || defined(__gfx1201__) || defined(__gfx12_generic__)
