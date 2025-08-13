461f129619b76df16611fcb3bccd0282dcad8f05
mirchen-amd <mirchen@amd.com>
2025-08-13 15:47:20 +0000

    added gemm stride checking to more profiler gemm operations

--- profiler/include/profiler/profile_gemm_reduce_impl.hpp
+++ profiler/include/profiler/profile_gemm_reduce_impl.hpp
@@ -15,6 +15,7 @@
 #include "ck/library/utility/host_tensor.hpp"
 #include "ck/library/utility/host_tensor_generator.hpp"
 #include "ck/library/utility/literals.hpp"
+#include "ck/library/utility/profiler_validation_common.hpp"
 #include "ck/library/reference_tensor_operation/cpu/reference_gemm.hpp"
 
 namespace ck {
@@ -87,6 +88,9 @@ bool profile_gemm_reduce_impl(int do_verification,
                 return HostTensorDescriptor({row, col}, {1_uz, stride});
             }
         };
+
+    ck::profiler::validate_gemm_strides_abc<ALayout, BLayout, CLayout>(
+        M, N, K, StrideA, StrideB, StrideC);
 
     Tensor<ADataType> a_m_k(f_host_tensor_descriptor(M, K, StrideA, ALayout{}));
     Tensor<BDataType> b_k_n(f_host_tensor_descriptor(K, N, StrideB, BLayout{}));
