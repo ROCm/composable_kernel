6a71babb9750cf537182751b1b0cac1cdc1a9ce3
joye <joye@amd.com>
2025-08-13 15:27:20 +0800

    fix ck_tile's elementwise test

--- test/ck_tile/elementwise/test_elementwise_1d.cpp
+++ test/ck_tile/elementwise/test_elementwise_1d.cpp
@@ -53,7 +53,7 @@ class TestCkTileElementwise : public ::testing::Test
     using BlockTile_        = std::tuple_element_t<5, Tuple>;
     using WarpTile_         = std::tuple_element_t<6, Tuple>;
     using TestElementWiseShape =
-        ck_tile::ElementWiseShape<BlockWarps_, BlockTile_, WarpTile_, ComputeDataType>;
+        ck_tile::ElementWiseShape<BlockWarps_, BlockTile_, WarpTile_, XDataType>;
     static constexpr int NumInputs = elementwise_op_traits<ElementwiseOpType>::num_inputs;
 
     void RunTest(ck_tile::index_t total_m_elements)
