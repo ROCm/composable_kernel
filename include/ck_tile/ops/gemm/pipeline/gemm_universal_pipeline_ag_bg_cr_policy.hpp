3002df2b0cdfeaafa7245c2b9e85ad5b65e17b6a
Sami Remes <samremes@amd.com>
2025-08-19 13:31:39 +0000

    disable layout for wave32

--- include/ck_tile/ops/gemm/pipeline/gemm_universal_pipeline_ag_bg_cr_policy.hpp
+++ include/ck_tile/ops/gemm/pipeline/gemm_universal_pipeline_ag_bg_cr_policy.hpp
@@ -57,50 +57,10 @@ struct UniversalGemmBasePolicy
         }
         else
         {
-            if constexpr(std::is_same_v<ALayout, ck_tile::tensor_layout::gemm::RowMajor>)
-            {
-                constexpr auto MLdsLayer = (32 * 4 / KPerBlock / DataTypeSize) < 1
-                                               ? 1
-                                               : (32 * 4 / KPerBlock / DataTypeSize);
-
-                constexpr auto a_lds_block_desc_0 = make_naive_tensor_descriptor(
-                    make_tuple(number<KPerBlock / KPack * MLdsLayer>{},
-                               number<MPerBlock / MLdsLayer>{},
-                               number<KPack>{}),
-                    make_tuple(number<KPack>{}, number<KPerBlock * MLdsLayer>{}, number<1>{}),
-                    number<KPack>{},
-                    number<1>{});
-
-                constexpr auto a_lds_block_desc_permuted = transform_tensor_descriptor(
-                    a_lds_block_desc_0,
-                    make_tuple(
-                        make_xor_transform(make_tuple(number<MPerBlock / MLdsLayer>{},
-                                                      number<KPerBlock / KPack * MLdsLayer>{})),
-                        make_pass_through_transform(number<KPack>{})),
-                    make_tuple(sequence<1, 0>{}, sequence<2>{}),
-                    make_tuple(sequence<1, 0>{}, sequence<2>{}));
-
-                constexpr auto a_lds_block_desc_xk0_mnldslayer_mn_xk1 = transform_tensor_descriptor(
-                    a_lds_block_desc_permuted,
-                    make_tuple(make_unmerge_transform(
-                                   make_tuple(number<MLdsLayer>{}, number<KPerBlock / KPack>{})),
-                               make_pass_through_transform(number<MPerBlock / MLdsLayer>{}),
-                               make_pass_through_transform(number<KPack>{})),
-                    make_tuple(sequence<0>{}, sequence<1>{}, sequence<2>{}),
-                    make_tuple(sequence<0, 2>{}, sequence<1>{}, sequence<3>{}));
-
-                constexpr auto a_lds_block_desc = transform_tensor_descriptor(
-                    a_lds_block_desc_xk0_mnldslayer_mn_xk1,
-                    make_tuple(make_merge_transform_v3_division_mod(make_tuple(
-                                   number<MPerBlock / MLdsLayer>{}, number<MLdsLayer>{})),
-                               make_merge_transform_v3_division_mod(
-                                   make_tuple(number<KPerBlock / KPack>{}, number<KPack>{}))),
-                    make_tuple(sequence<1, 0>{}, sequence<2, 3>{}),
-                    make_tuple(sequence<0>{}, sequence<1>{}));
-
-                return a_lds_block_desc;
-            }
-            else // A is in ColumnMajor
+            // Only use this ColumnMajor layout for Wave64 mode (gfx9)
+            constexpr auto Wave64 = get_warp_size() == 64;
+            if constexpr(Wave64 &&
+                         std::is_same_v<ALayout, ck_tile::tensor_layout::gemm::ColumnMajor>)
             {
                 // kfold and mpair dimension is not always required.
                 // more dimension in merge_transform increase the difficulty of generating immarg
@@ -215,6 +175,49 @@ struct UniversalGemmBasePolicy
 
                 return a_lds_block_desc_ak0_m_ak1;
             }
+            else // A is in RowMajor
+            {
+                constexpr auto MLdsLayer = (32 * 4 / KPerBlock / DataTypeSize) < 1
+                                               ? 1
+                                               : (32 * 4 / KPerBlock / DataTypeSize);
+
+                constexpr auto a_lds_block_desc_0 = make_naive_tensor_descriptor(
+                    make_tuple(number<KPerBlock / KPack * MLdsLayer>{},
+                               number<MPerBlock / MLdsLayer>{},
+                               number<KPack>{}),
+                    make_tuple(number<KPack>{}, number<KPerBlock * MLdsLayer>{}, number<1>{}),
+                    number<KPack>{},
+                    number<1>{});
+
+                constexpr auto a_lds_block_desc_permuted = transform_tensor_descriptor(
+                    a_lds_block_desc_0,
+                    make_tuple(
+                        make_xor_transform(make_tuple(number<MPerBlock / MLdsLayer>{},
+                                                      number<KPerBlock / KPack * MLdsLayer>{})),
+                        make_pass_through_transform(number<KPack>{})),
+                    make_tuple(sequence<1, 0>{}, sequence<2>{}),
+                    make_tuple(sequence<1, 0>{}, sequence<2>{}));
+
+                constexpr auto a_lds_block_desc_xk0_mnldslayer_mn_xk1 = transform_tensor_descriptor(
+                    a_lds_block_desc_permuted,
+                    make_tuple(make_unmerge_transform(
+                                   make_tuple(number<MLdsLayer>{}, number<KPerBlock / KPack>{})),
+                               make_pass_through_transform(number<MPerBlock / MLdsLayer>{}),
+                               make_pass_through_transform(number<KPack>{})),
+                    make_tuple(sequence<0>{}, sequence<1>{}, sequence<2>{}),
+                    make_tuple(sequence<0, 2>{}, sequence<1>{}, sequence<3>{}));
+
+                constexpr auto a_lds_block_desc = transform_tensor_descriptor(
+                    a_lds_block_desc_xk0_mnldslayer_mn_xk1,
+                    make_tuple(make_merge_transform_v3_division_mod(make_tuple(
+                                   number<MPerBlock / MLdsLayer>{}, number<MLdsLayer>{})),
+                               make_merge_transform_v3_division_mod(
+                                   make_tuple(number<KPerBlock / KPack>{}, number<KPack>{}))),
+                    make_tuple(sequence<1, 0>{}, sequence<2, 3>{}),
+                    make_tuple(sequence<0>{}, sequence<1>{}));
+
+                return a_lds_block_desc;
+            }
         }
     }
 
@@ -245,50 +248,9 @@ struct UniversalGemmBasePolicy
         }
         else
         {
-            if constexpr(std::is_same_v<BLayout, ck_tile::tensor_layout::gemm::ColumnMajor>)
-            {
-                constexpr index_t KPack     = GetSmemPackB<Problem>();
-                constexpr auto BK0          = number<KPerBlock / KPack>{};
-                constexpr auto DataTypeSize = sizeof(BDataType);
-                constexpr auto NLdsLayer    = (32 * 4 / KPerBlock / DataTypeSize) < 1
-                                                  ? 1
-                                                  : (32 * 4 / KPerBlock / DataTypeSize);
-
-                constexpr auto b_lds_block_desc_0 = make_naive_tensor_descriptor(
-                    make_tuple(BK0 * number<NLdsLayer>{},
-                               number<NPerBlock / NLdsLayer>{},
-                               number<KPack>{}),
-                    make_tuple(number<KPack>{}, number<KPerBlock * NLdsLayer>{}, number<1>{}),
-                    number<KPack>{},
-                    number<1>{});
-
-                constexpr auto b_lds_block_desc_permuted = transform_tensor_descriptor(
-                    b_lds_block_desc_0,
-                    make_tuple(make_xor_transform(make_tuple(number<NPerBlock / NLdsLayer>{},
-                                                             BK0 * number<NLdsLayer>{})),
-                               make_pass_through_transform(number<KPack>{})),
-                    make_tuple(sequence<1, 0>{}, sequence<2>{}),
-                    make_tuple(sequence<1, 0>{}, sequence<2>{}));
-
-                constexpr auto b_lds_block_desc_bk0_nldslayer_n_bk1 = transform_tensor_descriptor(
-                    b_lds_block_desc_permuted,
-                    make_tuple(make_unmerge_transform(make_tuple(number<NLdsLayer>{}, BK0)),
-                               make_pass_through_transform(number<NPerBlock / NLdsLayer>{}),
-                               make_pass_through_transform(number<KPack>{})),
-                    make_tuple(sequence<0>{}, sequence<1>{}, sequence<2>{}),
-                    make_tuple(sequence<0, 2>{}, sequence<1>{}, sequence<3>{}));
-
-                constexpr auto b_lds_block_desc = transform_tensor_descriptor(
-                    b_lds_block_desc_bk0_nldslayer_n_bk1,
-                    make_tuple(
-                        make_merge_transform_v3_division_mod(
-                            make_tuple(number<NPerBlock / NLdsLayer>{}, number<NLdsLayer>{})),
-                        make_merge_transform_v3_division_mod(make_tuple(BK0, number<KPack>{}))),
-                    make_tuple(sequence<1, 0>{}, sequence<2, 3>{}),
-                    make_tuple(sequence<0>{}, sequence<1>{}));
-                return b_lds_block_desc;
-            }
-            else // B is Row Major
+            // Only use this RowMajor layout for Wave64 mode (gfx9)
+            constexpr auto Wave64 = get_warp_size() == 64;
+            if constexpr(Wave64 && std::is_same_v<BLayout, ck_tile::tensor_layout::gemm::RowMajor>)
             {
                 constexpr index_t BlockSize   = Problem::kBlockSize;
                 constexpr index_t VecLoadSize = GetVectorSizeB<Problem>();
@@ -403,6 +365,50 @@ struct UniversalGemmBasePolicy
 
                 return b_lds_block_desc_nk;
             }
+            else
+            {
+                constexpr index_t KPack     = GetSmemPackB<Problem>();
+                constexpr auto BK0          = number<KPerBlock / KPack>{};
+                constexpr auto DataTypeSize = sizeof(BDataType);
+                constexpr auto NLdsLayer    = (32 * 4 / KPerBlock / DataTypeSize) < 1
+                                                  ? 1
+                                                  : (32 * 4 / KPerBlock / DataTypeSize);
+
+                constexpr auto b_lds_block_desc_0 = make_naive_tensor_descriptor(
+                    make_tuple(BK0 * number<NLdsLayer>{},
+                               number<NPerBlock / NLdsLayer>{},
+                               number<KPack>{}),
+                    make_tuple(number<KPack>{}, number<KPerBlock * NLdsLayer>{}, number<1>{}),
+                    number<KPack>{},
+                    number<1>{});
+
+                constexpr auto b_lds_block_desc_permuted = transform_tensor_descriptor(
+                    b_lds_block_desc_0,
+                    make_tuple(make_xor_transform(make_tuple(number<NPerBlock / NLdsLayer>{},
+                                                             BK0 * number<NLdsLayer>{})),
+                               make_pass_through_transform(number<KPack>{})),
+                    make_tuple(sequence<1, 0>{}, sequence<2>{}),
+                    make_tuple(sequence<1, 0>{}, sequence<2>{}));
+
+                constexpr auto b_lds_block_desc_bk0_nldslayer_n_bk1 = transform_tensor_descriptor(
+                    b_lds_block_desc_permuted,
+                    make_tuple(make_unmerge_transform(make_tuple(number<NLdsLayer>{}, BK0)),
+                               make_pass_through_transform(number<NPerBlock / NLdsLayer>{}),
+                               make_pass_through_transform(number<KPack>{})),
+                    make_tuple(sequence<0>{}, sequence<1>{}, sequence<2>{}),
+                    make_tuple(sequence<0, 2>{}, sequence<1>{}, sequence<3>{}));
+
+                constexpr auto b_lds_block_desc = transform_tensor_descriptor(
+                    b_lds_block_desc_bk0_nldslayer_n_bk1,
+                    make_tuple(
+                        make_merge_transform_v3_division_mod(
+                            make_tuple(number<NPerBlock / NLdsLayer>{}, number<NLdsLayer>{})),
+                        make_merge_transform_v3_division_mod(make_tuple(BK0, number<KPack>{}))),
+                    make_tuple(sequence<1, 0>{}, sequence<2, 3>{}),
+                    make_tuple(sequence<0>{}, sequence<1>{}));
+                return b_lds_block_desc;
+            }
+            else // B is Row Major
         }
     }
 
