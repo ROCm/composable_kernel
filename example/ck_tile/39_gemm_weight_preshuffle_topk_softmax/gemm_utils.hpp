7bc1c41f464266f6e7be6b398968f70d9a37bed9
huizzhan <huizzhan@amd.com>
2025-08-22 07:12:19 +0000

    pw gemm+softmax 0.43ms error 5e-5

--- example/ck_tile/39_gemm_weight_preshuffle_topk_softmax/gemm_utils.hpp
+++ example/ck_tile/39_gemm_weight_preshuffle_topk_softmax/gemm_utils.hpp
@@ -238,8 +238,8 @@ struct GemmConfigPreshuffle_2 : public GemmConfigBase
     static constexpr ck_tile::index_t N_Tile = 256;
     static constexpr ck_tile::index_t K_Tile = 128 / sizeof(PrecType);
 
-    static constexpr ck_tile::index_t M_Warp = 1;
-    static constexpr ck_tile::index_t N_Warp = 4;
+    static constexpr ck_tile::index_t M_Warp = 4;
+    static constexpr ck_tile::index_t N_Warp = 1;
     static constexpr ck_tile::index_t K_Warp = 1;
 
     static constexpr ck_tile::index_t M_Warp_Tile = 16;
