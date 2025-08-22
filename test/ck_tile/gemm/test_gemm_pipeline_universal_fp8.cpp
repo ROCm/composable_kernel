a20d044362dba59065941a30bd3f01eecdc6d560
Sami Aario <samaario@amd.com>
2025-08-22 07:30:27 +0000

    Remove unnecessary include statements in universal GEMM tests

--- test/ck_tile/gemm/test_gemm_pipeline_universal_fp8.cpp
+++ test/ck_tile/gemm/test_gemm_pipeline_universal_fp8.cpp
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
-#include <cstddef>
-#include <hip/hip_runtime.h>
-
-#include <cstring>
-#include <iostream>
-#include <string>
-
 #include "ck_tile/host.hpp"
 #include "test_gemm_pipeline_smoke_util.hpp"
 #include "test_gemm_pipeline_smoke_run_test.inc"
