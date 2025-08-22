7e831ed66236a91e0ac56c4d5c6a08fcb8c38a3c
Sami Aario <samaario@amd.com>
2025-08-22 07:48:03 +0000

    Improve compilation time of basic GEMM tests by only compiling the precision variants that we need

--- test/ck_tile/gemm/test_gemm_pipeline_basic_bf8.cpp
+++ test/ck_tile/gemm/test_gemm_pipeline_basic_bf8.cpp
@@ -2,4 +2,4 @@
 // Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 #include "test_gemm_pipeline_basic_run_test.inc"
 
-int main() { return run_gemm_combinations("bf8"); }
+int main() { return run_gemm_combinations<ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t>(); }
