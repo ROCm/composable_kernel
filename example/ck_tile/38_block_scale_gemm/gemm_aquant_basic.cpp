15968723105e5af329b4dce2d0734363e31da4bc
Cong Ma <congma13@amd.com>
2025-08-26 12:05:19 -0500

    [CK TILE Block Scale] Rename GemmConfig used in block scale
        - Remove unused GemmConfig
        - Rename GemmConfig used in block scale

--- example/ck_tile/38_block_scale_gemm/gemm_aquant_basic.cpp
+++ example/ck_tile/38_block_scale_gemm/gemm_aquant_basic.cpp
@@ -225,4 +225,4 @@ int run_gemm_example(int argc, char* argv[])
     }
 }
 
-int main(int argc, char* argv[]) { return !run_gemm_example<GemmConfigComputeV3>(argc, argv); }
+int main(int argc, char* argv[]) { return !run_gemm_example<GemmConfigDecode>(argc, argv); }
--- example/ck_tile/38_block_scale_gemm/gemm_aquant_preshuffle.cpp
+++ example/ck_tile/38_block_scale_gemm/gemm_aquant_preshuffle.cpp
@@ -225,4 +225,7 @@ int run_gemm_example(int argc, char* argv[])
     }
 }
 
-int main(int argc, char* argv[]) { return !run_gemm_example<GemmConfigPreshufle_AQ>(argc, argv); }
+int main(int argc, char* argv[])
+{
+    return !run_gemm_example<GemmConfigPreshuffleQuant>(argc, argv);
+}
