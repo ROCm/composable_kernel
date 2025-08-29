bad7262507641389549e41ccf70d1807a71d943e
Vijay Krish <vjkrish@meta.com>
2025-08-28 23:43:02 -0700

    ck_tile kernel for gemm with groupwise quantized B tensor. (#2663)
    * This change introduces new pipelines with Intrawave scheduler and block gemm primitives that loads the scale tensor to registers to perform dequantization post MFMA on C tensor in registers.
    
    Scale tensor data, BQ is spliced across threads in registers and not stored in LDS.
    
    Current support is for the following combinations, but it should be fairly straightforward to extend support to more formats.
    
    fp8, fp8 -> f32
    bf8, bf8 -> f32
    fp8, i4 -> f32
    bf8, i4 -> f32
    Group size can go down to as low as K length of underlying WarpGemm primitive.
    
    * Solve merge conflict
    
    * [CK TILE] Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Vijay Krishnamoorthy <vjkrish@fb.com>
    Co-authored-by: ThomasNing <thomas.ning@amd.com>
    Co-authored-by: Cong Ma <congma13@amd.com>
    
    [ROCm/composable_kernel commit: 4208e2898818362735e1ae9980a4cc2fea607ab4]

--- include/ck_tile/ops/gemm_group_quant/pipeline/gemm_quant_pipeline_problem.hpp
+++ include/ck_tile/ops/gemm_group_quant/pipeline/gemm_quant_pipeline_problem.hpp
@@ -121,4 +121,107 @@ using GemmAQuantPipelineProblem = GemmAQuantPipelineProblemBase<ADataType_,
                                                                 HasHotLoop_,
                                                                 TailNum_>;
 
+template <typename ADataType_,
+          typename BDataType_,
+          typename BQDataType_,
+          typename CDataType_,
+          typename BlockGemmShape_,
+          typename Traits_,
+          uint32_t QuantGroupSize_,
+          typename ComputeDataType_        = ADataType_,
+          GemmPipelineScheduler Scheduler_ = GemmPipelineScheduler::Intrawave,
+          bool HasHotLoop_                 = true,
+          TailNumber TailNum_              = TailNumber::Full>
+struct GemmBQuantPipelineProblemBase : public GemmPipelineProblemBase<ADataType_,
+                                                                      BDataType_,
+                                                                      CDataType_,
+                                                                      BlockGemmShape_,
+                                                                      Traits_,
+                                                                      ComputeDataType_>
+{
+    using Base = GemmPipelineProblemBase<ADataType_,
+                                         BDataType_,
+                                         CDataType_,
+                                         BlockGemmShape_,
+                                         Traits_,
+                                         ComputeDataType_>;
+
+    using Traits = typename Base::Traits;
+
+    using typename Base::ADataType;
+    using typename Base::BDataType;
+    using typename Base::CDataType;
+    using typename Base::ComputeDataType;
+    using BQDataType = remove_cvref_t<BQDataType_>;
+
+    using BlockGemmShape = typename Base::BlockGemmShape;
+
+    using typename Base::ALayout;
+    using typename Base::BLayout;
+    using typename Base::CLayout;
+
+    static constexpr bool TransposeC = Traits::TransposeC;
+
+    using Base::kBlockSize;
+
+    using Base::kPadK;
+    using Base::kPadM;
+    using Base::kPadN;
+
+    using Base::DoubleSmemBuffer;
+    using Base::VectorLoadSize;
+
+    using BQLayout = remove_cvref_t<typename Traits::BQLayout>;
+
+    static constexpr uint32_t kQuantGroupSize = QuantGroupSize_;
+    static constexpr auto Scheduler           = Scheduler_;
+    static constexpr auto HasHotLoop          = HasHotLoop_;
+    static constexpr auto TailNum             = TailNum_;
+
+    static_assert(BlockGemmShape::kK % kQuantGroupSize == 0);
+    static_assert(Scheduler == GemmPipelineScheduler::Intrawave);
+
+    [[nodiscard]] CK_TILE_HOST static const std::string GetName()
+    {
+        // clang-format off
+        return concat('_', "gemm_bquant_problem",
+                      concat('x', VectorLoadSize, kBlockSize),
+                      concat('x', kPadM, kPadN, kPadK),
+                      Scheduler,
+                      "QuantGroupSize",
+                      kQuantGroupSize);
+        // clang-format on
+    }
+
+    CK_TILE_HOST_DEVICE static constexpr auto GetAlignmentBQ()
+    {
+        return VectorLoadSize / sizeof(BQDataType);
+    }
+
+    static constexpr index_t VectorSizeBQ = []() { return kPadK ? 1 : GetAlignmentBQ(); }();
+};
+
+template <typename ADataType_,
+          typename BDataType_,
+          typename BQDataType_,
+          typename CDataType_,
+          typename BlockGemmShape_,
+          typename Traits_,
+          uint32_t QuantGroupSize_,
+          typename ComputeDataType_        = ADataType_,
+          GemmPipelineScheduler Scheduler_ = GemmPipelineScheduler::Intrawave,
+          bool HasHotLoop_                 = true,
+          TailNumber TailNum_              = TailNumber::Full>
+using GemmBQuantPipelineProblem = GemmBQuantPipelineProblemBase<ADataType_,
+                                                                BDataType_,
+                                                                BQDataType_,
+                                                                CDataType_,
+                                                                BlockGemmShape_,
+                                                                Traits_,
+                                                                QuantGroupSize_,
+                                                                ComputeDataType_,
+                                                                Scheduler_,
+                                                                HasHotLoop_,
+                                                                TailNum_>;
+
 } // namespace ck_tile
