061fb06eef74e258da0217fd05e6e62589d8230f
John Shumway <jshumway@amd.com>
2025-09-01 21:54:41 +0000

    Add thread block info to factory.
    Now we can set the thread block size and submatrix shape for the builder.

--- experimental/builder/include/ck_tile/builder/conv_algorithm.hpp
+++ experimental/builder/include/ck_tile/builder/conv_algorithm.hpp
@@ -1,94 +1,47 @@
 #pragma once
 
 #include <type_traits>
+#include <concepts>
 
 namespace ck_tile::builder {
 
-enum class GemmImplementationType
-{
-    XDL,
-    WMMA,
-    DL
+// Convenience struct for a tuple of m, n, and k values.    
+template <typename T>
+struct MNK {
+    T m{};
+    T n{};
+    T k{};
 };
 
-enum class ConvolutionDirection
-{
-    Forward,
-    BackwardData,
-    BackwardWeight
+
+// Concept for thread block info for a GEMM problem.
+template <typename T>
+concept ThreadBlockInfo = requires(T t) {
+    { t.block_size } -> std::convertible_to<int>;
+    { t.sub_matrix.m } -> std::convertible_to<int>;
+    { t.sub_matrix.n } -> std::convertible_to<int>;
+    { t.sub_matrix.k } -> std::convertible_to<int>;
 };
 
-enum class UniversalGemmSupport
-{
-    Supported,
-    NotSupported
+
+// Describe a thread block for a GEMM.
+struct ThreadBlock {
+    // Thread block size.
+    int block_size;
+    // Size of the submatrix problem in a thread block.
+    MNK<int> sub_matrix;
 };
 
-enum class SplitKSupport
-{
-    Supported,
-    SupportedTwoStage,
-    NotSupported
+static_assert(ThreadBlockInfo<ThreadBlock>);
+
+// Concept to check if struct provides thread block info.
+template <typename T>
+concept HasThreadBlockInfo = requires {
+    { T::THREAD_BLOCK } -> ThreadBlockInfo;
 };
 
-enum class DepthwiseOptimization
-{
-    X16,
-    X8,
-    X4,
-    X2,
-    NotSupported
-};
 
-enum class LargeTensorSupport
-{
-    Supported,
-    SplitBatch,
-    NotSupported
-};
-
-enum class ImplementationType
-{
-    ExplicitDefault,
-    ExplicitMPadding,
-    ExplicitNPadding,
-    ExplicitKPadding,
-    ExplicitMNPadding,
-    ExplicitMKPadding,
-    ExplicitNKPadding,
-    ExplicitMNKPadding,
-    Implicit
-};
-
-enum class GemmPipelineVersion
-{
-    Naive,
-    ComputeFriendly,
-    MemFriendly,
-    ComputeFriendlyDoubleLDS,
-    ComputeFriendlyDoubleGlobalPrefetch
-};
-
-enum class GemmPipelineScheduler
-{
-    Intrawave,
-    Interwave
-};
-
-enum class ConvolutionSpecialization
-{
-    Default,
-    Filter1x1Pad0,
-    Filter1x1Stride1Pad0,
-    Filter3x3
-};
-
-enum class MFMAInstructionSize
-{
-    M16N16,
-    M32N32
-};
-
+// No requirements yet for a ConvAlogorithm concept.
 template <typename T>
 concept ConvAlgorithm = std::is_class_v<T>;
 
