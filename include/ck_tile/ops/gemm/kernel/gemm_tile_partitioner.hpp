e0032e588aac1f2d4eda339ff23eca294c472127
Astha Rai <astha.rai713@gmail.com>
2025-09-02 19:46:51 +0000

    added documentation for template parameters

--- include/ck_tile/ops/gemm/kernel/gemm_tile_partitioner.hpp
+++ include/ck_tile/ops/gemm/kernel/gemm_tile_partitioner.hpp
@@ -372,6 +372,14 @@ struct GemmSpatiallyLocalTilePartitioner
  * for the Stream-K algorithm which decomposes the GEMM problem
  * into smaller work units and distributes them more evenly across available blocks,
  * improving load balancing especially for cases where the K dimension is large.
+ *
+ *  @tparam BlockGemmShapeType  A class providing basic GEMM parameters.
+ *  @tparam StreamKReductionStrategy  A class that defines the reduction strategy for the results in
+ * the C Tensor.
+ *  @tparam TileSwizzleSubM  A value that defines the size of the swizzle group along the m
+ * dimension, where the swizzle group denotes consecutive tiles down a column. For instance a
+ * swizzle group of 8 denotes tiles 0, 1, ..., 7, map to tiles [0,0], [1,0], ..., [7,0] in the C
+ * tensor.
  */
 template <typename BlockGemmShapeType,
           StreamKReductionStrategy ReductionStrategy = ck_tile::StreamKReductionStrategy::Atomic,
