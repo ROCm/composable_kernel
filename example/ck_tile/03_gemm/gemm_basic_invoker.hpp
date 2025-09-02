52fbfe641263bcea36a08d3706e7bebcb01e89cc
Sami Aario <samaario@amd.com>
2025-09-02 13:15:23 +0000

    Add back CK_TILE_USE_WMMA related changes

--- example/ck_tile/03_gemm/gemm_basic_invoker.hpp
+++ example/ck_tile/03_gemm/gemm_basic_invoker.hpp
@@ -28,6 +28,15 @@ struct BasicInvoker
         constexpr ck_tile::index_t N_Tile = 256;
         constexpr ck_tile::index_t K_Tile = 64;
 
+#if CK_TILE_USE_WMMA
+        constexpr ck_tile::index_t M_Warp = 4;
+        constexpr ck_tile::index_t N_Warp = 2;
+        constexpr ck_tile::index_t K_Warp = 1;
+
+        constexpr ck_tile::index_t M_Warp_Tile = 16;
+        constexpr ck_tile::index_t N_Warp_Tile = 16;
+        constexpr ck_tile::index_t K_Warp_Tile = 16;
+#else
         constexpr ck_tile::index_t M_Warp = 2;
         constexpr ck_tile::index_t N_Warp = 2;
         constexpr ck_tile::index_t K_Warp = 1;
@@ -35,6 +44,7 @@ struct BasicInvoker
         constexpr ck_tile::index_t M_Warp_Tile = 32;
         constexpr ck_tile::index_t N_Warp_Tile = 32;
         constexpr ck_tile::index_t K_Warp_Tile = 16;
+#endif
 
         using CodegenGemmShape =
             ck_tile::TileGemmShape<ck_tile::sequence<M_Tile, N_Tile, K_Tile>,
