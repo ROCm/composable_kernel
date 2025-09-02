e05879fbd685f5d96a97fb73cdd57e68df72f74f
John Shumway <jshumway@amd.com>
2025-09-02 23:08:32 +0000

    Add block transfer paramters to builder.
    These are very hard to test in the kernel class, so just test the values in the factory.

--- experimental/builder/test/test_conv_builder.cpp
+++ experimental/builder/test/test_conv_builder.cpp
@@ -36,16 +36,31 @@ struct FwdConvAlgorithm
 {
     ckb::ThreadBlock thread_block;
     ckb::ConvTuningParams tuning_params;
+    struct BlockTransfer
+    {
+        ckb::BlockATransferLengthsInfo thread_cluster_lengths_a;
+        ckb::BlockBTransferLengthsInfo thread_cluster_lengths_b;
+        ckb::BlockCTransferLengthsInfo thread_cluster_lengths_c;
+    } block_transfer;
 };
 static_assert(ckb::ConvAlgorithm<FwdConvAlgorithm>);
 static_assert(ckb::HasThreadBlockInfo<FwdConvAlgorithm>);
 static_assert(ckb::HasConvTuningInfo<FwdConvAlgorithm>);
+static_assert(ckb::HasABlockTransferInfo<FwdConvAlgorithm>);
+static_assert(ckb::HasBBlockTransferInfo<FwdConvAlgorithm>);
+static_assert(ckb::HasCBlockTransferInfo<FwdConvAlgorithm>);
 
 TEST(ConvBuilderTest, TestConvFwdXdlBf16CompInstances2xInstance0)
 {
     static constexpr FwdConvAlgorithm algorithm{
         .thread_block{.block_size = 256, .sub_matrix = {.m = 256, .n = 128, .k = 64}},
-        .tuning_params{.ak1 = 16, .bk1 = 16, .m_xdl_per_wave = 2, .n_xdl_per_wave = 2}};
+        .tuning_params{.ak1 = 16, .bk1 = 16, .m_xdl_per_wave = 2, .n_xdl_per_wave = 2},
+        .block_transfer{
+            .thread_cluster_lengths_a = {.k0 = 4, .m = 64, .k1 = 1},
+            .thread_cluster_lengths_b = {.k0 = 4, .n = 64, .k1 = 1},
+            .thread_cluster_lengths_c =
+                {.m_block = 1, .m_wave_per_xdl = 32, .n_block = 1, .n_wave_per_xdl = 8},
+        }};
     using Builder = ckb::ConvBuilder<FwdConvSignature, algorithm, API_VERSION>;
     EXPECT_EQ(
         Builder::Instance::TypeString(),
@@ -53,12 +68,28 @@ TEST(ConvBuilderTest, TestConvFwdXdlBf16CompInstances2xInstance0)
         "8, 8, 8, 1, 1, BlkGemmPipelineScheduler: Intrawave, BlkGemmPipelineVersion: v4>");
     EXPECT_EQ(Builder::factory::TUNING.ak1, 16);
     EXPECT_EQ(Builder::factory::TUNING.bk1, 16);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[0], 4);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[1], 64);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[0], 4);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[1], 64);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[0], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[1], 32);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[3], 8);
 }
 TEST(ConvBuilderTest, GroupedConvFwdXdlBf16CompInstance0)
 {
     static constexpr FwdConvAlgorithm algorithm{
         .thread_block{.block_size = 256, .sub_matrix = {.m = 256, .n = 256, .k = 32}},
-        .tuning_params{.ak1 = 8, .bk1 = 8, .m_xdl_per_wave = 4, .n_xdl_per_wave = 4}};
+        .tuning_params{.ak1 = 8, .bk1 = 8, .m_xdl_per_wave = 4, .n_xdl_per_wave = 4},
+        .block_transfer{
+            .thread_cluster_lengths_a = {.k0 = 4, .m = 64, .k1 = 1},
+            .thread_cluster_lengths_b = {.k0 = 4, .n = 64, .k1 = 1},
+            .thread_cluster_lengths_c =
+                {.m_block = 1, .m_wave_per_xdl = 32, .n_block = 1, .n_wave_per_xdl = 8},
+        }};
     using Builder = ckb::ConvBuilder<FwdConvSignature, algorithm, API_VERSION>;
     EXPECT_EQ(
         Builder::Instance::TypeString(),
@@ -66,6 +97,16 @@ TEST(ConvBuilderTest, GroupedConvFwdXdlBf16CompInstance0)
         "8, 8, 8, 1, 1, BlkGemmPipelineScheduler: Intrawave, BlkGemmPipelineVersion: v4>");
     EXPECT_EQ(Builder::factory::TUNING.ak1, 8);
     EXPECT_EQ(Builder::factory::TUNING.bk1, 8);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[0], 4);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[1], 64);
+    EXPECT_EQ(Builder::factory::A_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[0], 4);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[1], 64);
+    EXPECT_EQ(Builder::factory::B_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[0], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[1], 32);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[2], 1);
+    EXPECT_EQ(Builder::factory::C_BLOCK_TRANSFER.thread_cluster_lengths[3], 8);
 };
 
 } // namespace
