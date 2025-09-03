2946645cd51c6c91500317daef8440943de14cf1
Sami Aario <samaario@amd.com>
2025-09-03 11:26:50 +0000

    Separate the clearing of the GEMM output memory from the cache flushing in the universal GEMM example

--- example/ck_tile/03_gemm/universal_gemm_invoker.hpp
+++ example/ck_tile/03_gemm/universal_gemm_invoker.hpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
 #pragma once
 
+#include <functional>
 #include "gemm_utils.hpp"
 
 struct UniversalInvoker
@@ -130,6 +131,17 @@ struct UniversalInvoker
                           << ", blocks: {" << blocks.x << ", " << blocks.y << ", " << blocks.z
                           << "}" << std::endl;
             }
+
+            // Declare rotating_mem_ptr here so it stays in scope until it is needed
+            std::unique_ptr<ck_tile::RotatingMemWrapper<ADataType, BDataType>> rotating_mem_ptr;
+            std::function<void()> preprocess;
+
+            auto clear_gemm_output = [&]() {
+                if(args.k_batch > 1)
+                    hipGetErrorString(hipMemsetAsync(
+                        args.e_ptr, 0, args.M * args.N * sizeof(CDataType), s.stream_id_));
+            };
+
             if(s.flush_cache_)
             {
                 std::cout << "Flushing cache..." << std::endl;
@@ -142,36 +154,26 @@ struct UniversalInvoker
                 auto size_a_buffer = a_m.get_element_space_size_in_bytes();
                 auto size_b_buffer = b_n.get_element_space_size_in_bytes();
 
-                ck_tile::RotatingMemWrapper<ADataType, BDataType> rotating_mem(kargs.as_ptr[0],
-                                                                               kargs.bs_ptr[0],
-                                                                               s.rotating_count_,
-                                                                               size_a_buffer,
-                                                                               size_b_buffer);
-                rotating_mem.Print();
+                rotating_mem_ptr = std::make_unique<ck_tile::RotatingMemWrapper<ADataType, BDataType>>(
+                    kargs.as_ptr[0], kargs.bs_ptr[0], s.rotating_count_, size_a_buffer, size_b_buffer);
+                rotating_mem_ptr->Print();
 
-                auto run_flush_cache = [&]() {
-                    // flush icache
+                preprocess = [&]() {
                     ck_tile::flush_icache();
-                    // rotating mem
-                    rotating_mem.Next();
-                    // clear c mem
-                    if(args.k_batch > 1)
-                        hipGetErrorString(hipMemsetAsync(
-                            args.e_ptr, 0, args.M * args.N * sizeof(CDataType), s.stream_id_));
+                    rotating_mem_ptr->Next();
+                    clear_gemm_output();
                 };
-                ave_time =
-                    ck_tile::launch_kernel_time_mask(s,
-                                                     run_flush_cache,
-                                                     ck_tile::make_kernel<GemmConfig::kBlockPerCu>(
-                                                         Kernel{}, grids, blocks, 0, kargs));
             }
             else
             {
-                ave_time = ck_tile::launch_kernel(s,
-                                                  ck_tile::make_kernel<GemmConfig::kBlockPerCu>(
-                                                      Kernel{}, grids, blocks, 0, kargs));
+                preprocess = clear_gemm_output;
             }
-            return ave_time;
+
+            return
+                ck_tile::launch_kernel_time_mask(s,
+                                                 preprocess,
+                                                 ck_tile::make_kernel<GemmConfig::kBlockPerCu>(
+                                                     Kernel{}, grids, blocks, 0, kargs));
         };
 
         const auto RunSplitk = [&](const auto has_hot_loop_, const auto tail_number_) {
