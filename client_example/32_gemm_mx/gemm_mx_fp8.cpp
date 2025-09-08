e07b664da862c7ce320043d200ca0ad1217bf7cd
AviralGoelAMD <aviral.goel@amd.com>
2025-09-08 20:55:25 +0000

    style(client_example): update copyright header to match the new template

--- client_example/32_gemm_mx/gemm_mx_fp8.cpp
+++ client_example/32_gemm_mx/gemm_mx_fp8.cpp
@@ -1,5 +1,6 @@
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
-// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+
 #include <iomanip>
 #include <vector>
 #include <iostream>
