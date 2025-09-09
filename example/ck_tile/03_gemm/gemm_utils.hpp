f7ffd111ee62ffbae532e033cee25d46f908ca3f
Cong Ma <142121551+CongMa13@users.noreply.github.com>
2025-09-09 17:40:52 -0600

    [CK TILE] Support fp8/fp16 with pk_int4_t as data types for tensors A and B (#2805)
    - Add support for tensor A/B in both fp16+pk_int4_t and fp8+pk_int4_t formats
    - Implement A(bf8) B(i4) support in universal GEMM
    - Use new implementation for i4 to fp8 conversion in Block Scale
    
    [ROCm/composable_kernel commit: 82890192ddab0dd34938f1e01f9b58239951a822]

--- example/ck_tile/03_gemm/gemm_utils.hpp
+++ example/ck_tile/03_gemm/gemm_utils.hpp
@@ -345,6 +345,24 @@ struct GemmTypeConfig<ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t>
 };
 
 template <>
+struct GemmTypeConfig<ck_tile::fp8_t, ck_tile::pk_int4_t, ck_tile::half_t>
+{
+    using ADataType   = ck_tile::fp8_t;
+    using BDataType   = ck_tile::pk_int4_t;
+    using AccDataType = float;
+    using CDataType   = ck_tile::half_t;
+};
+
+template <>
+struct GemmTypeConfig<ck_tile::bf8_t, ck_tile::pk_int4_t, ck_tile::half_t>
+{
+    using ADataType   = ck_tile::bf8_t;
+    using BDataType   = ck_tile::pk_int4_t;
+    using AccDataType = float;
+    using CDataType   = ck_tile::half_t;
+};
+
+template <>
 struct GemmTypeConfig<ck_tile::half_t, ck_tile::pk_int4_t, ck_tile::half_t>
 {
     using ADataType   = ck_tile::half_t;
