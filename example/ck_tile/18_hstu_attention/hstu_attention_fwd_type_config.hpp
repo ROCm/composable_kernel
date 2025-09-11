8313b34543c81aa72cd6079a260d0f5e091ef579
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-09-11 10:09:37 +0000

    Unify the license statements on all the source files

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_type_config.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_type_config.hpp
@@ -1,9 +1,6 @@
-/*
- * Copyright (c) 2023-2024, Advanced Micro Devices, Inc. All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
+
 #pragma once
 
 #include <ck_tile/core.hpp>
