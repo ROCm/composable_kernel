63e2dbece7a2c3f993a7d56a8523daabc9c8a279
Enrico Degregori <enrico@streamhpc.com>
2025-09-11 08:25:59 +0000

    Fix batched gemm for multiABD gridwise implementation

--- include/ck/tensor_operation/gpu/grid/gridwise_gemm_wmma_cshuffle_v3.hpp
+++ include/ck/tensor_operation/gpu/grid/gridwise_gemm_wmma_cshuffle_v3.hpp
@@ -327,6 +327,8 @@ struct GridwiseGemm_wmma_cshuffle_v3
     using typename Base::AsGridPointer;
     using typename Base::BsGridPointer;
     using typename Base::DsGridPointer;
+    using AsDataType_ = AsDataType;
+    using BsDataType_ = BsDataType;
 
     struct Problem
     {
