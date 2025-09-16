cfa1cd84f042bd903ed75c0b811ef806760c411b
Sami Aario <samaario@amd.com>
2025-09-16 15:21:15 +0000

    I4 support introduced more failing test cases that mirror the existing ones for F8

--- test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_kernel_types.hpp
+++ test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_kernel_types.hpp
@@ -24,18 +24,19 @@ using Default = ck_tile::integral_constant<ck_tile::GemmPipelineScheduler,
 using WeightPreshuffle =
     ck_tile::integral_constant<GemmPipelineType, GemmPipelineType::WeightPreshuffle>;
 
-// Adding alias for the F8 parameters to facilitate skipping tests.
-// This alias can be removed once test failures are fixed.
-using F8Types = std::tuple<Row, Col, Row, F8, F8, F32, F16, Default, WeightPreshuffle>;
+// Adding aliases for the F8 parameters to facilitate skipping tests.
+// These aliases can be removed once test failures are fixed.
+using F8F8Types = std::tuple<Row, Col, Row, F8, F8, F32, F16, Default, WeightPreshuffle>;
+using F8I4Types = std::tuple<Row, Col, Row, F8, I4, F32, F16, Default, WeightPreshuffle>;
 
 // clang-format off
 
 using KernelTypesWeightPreshuffle = ::testing::Types<
      std::tuple<    Row,     Col,     Row,       F16,       F16,         F32,       F16,             Default,        WeightPreshuffle>,
-     std::tuple<    Row,     Col,     Row,       BF16,      BF16,        F32,       BF16,            Default,        WeightPreshuffle>,
-     std::tuple<    Row,     Col,     Row,       F8,        I4,          F32,       F16,             Default,        WeightPreshuffle>
+     std::tuple<    Row,     Col,     Row,       BF16,      BF16,        F32,       BF16,            Default,        WeightPreshuffle>
 #if !CK_TILE_USE_WMMA || CK_TILE_USE_OCP_FP8     
-     , F8Types
+     , F8F8Types,
+     F8I4Types
 #endif     
      >;
 
