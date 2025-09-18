645f5c4b7e943ed7a7cb8abf85341fb606f0f491
ThomasNing <thomas.ning@amd.com>
2025-09-18 06:14:16 +0000

    Revert "have a workable version for atomic add"
    This reverts commit 792377a590c26cfff9c8f545d9a9e8484a7422eb.

--- include/ck_tile/core/arch/amd_buffer_addressing_builtins.hpp
+++ include/ck_tile/core/arch/amd_buffer_addressing_builtins.hpp
@@ -2660,53 +2660,6 @@ __device__ auto amd_transpose_load_to_vgpr(const T* __restrict__ in_ptr)
 }
 #endif
 
-// amd_wave_read_first_lane is the SGPR function from AMD GPU device to load 1 or a series of the memory to the SGPR registers.
-__device__ inline uint32_t amd_wave_read_first_lane(uint16_t v) {
-    return __builtin_amdgcn_readfirstlane(static_cast<uint32_t>(v));
-}
-
-__device__ inline uint32_t amd_wave_read_first_lane(uint8_t v) {
-    return __builtin_amdgcn_readfirstlane(static_cast<uint32_t>(v));
-}
-
-__device__ inline uint32_t amd_wave_read_first_lane(uint32_t value)
-{
-    return __builtin_amdgcn_readfirstlane(value);
-}
-
-__device__ inline int32_t amd_wave_read_first_lane(int32_t value)
-{
-    return __builtin_amdgcn_readfirstlane(value);
-}
-
-template <typename Object,
-          std::enable_if_t<std::is_trivially_copyable_v<T>, int> = 0>
-__device__ inline auto amd_wave_read_first_lane(const Object& obj)
-{
-    constexpr size_t ObjectSize = sizeof(Object);
-    constexpr size_t SGPR_size = 4;
-
-    const unsigned char* src = reinterpret_cast<const unsigned char*>(&obj);
-    alignas(Object) unsigned char dst[ObjectSize];
-
-
-    for (size_t offset = 0; offset + SGPR_size <= ObjectSize; offset += SGPR_size) {
-        uint32_t read_src;
-        __builtin_memcpy(&read_src, src + offset, SGPR_size);
-        read_src = __builtin_amdgcn_readfirstlane(read_src);
-        __builtin_memcpy(dst + off, &read_src, SGPR_size);
-    }
-    if constexpr (ObjectSize % SGPR_size) {
-        uint32_t tail = 0;
-        __builtin_memcpy(&tail, src + offset, ObjectSize%SGPR_size);
-        tail = __builtin_amdgcn_readfirstlane(tail);
-        __builtin_memcpy(dst + offset, &tail, ObjectSize % SGPR_size);
-    }
-    Object out;
-    __builtin_memcpy(&out, dst, ObjectSize);
-    return out;
-}
-
 } // namespace ck_tile
 
 #endif // CK_TILE_USE_BUFFER_ADDRESSING_BUILTIN
