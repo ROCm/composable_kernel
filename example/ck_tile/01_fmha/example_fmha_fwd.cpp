ee43f0f0bebb3565cdeaac58c8834800d9b87faf
Illia Silin <98187287+illsilin@users.noreply.github.com>
2025-09-19 08:15:02 -0700

    Revert "[CK_TILE] Add sequence padding and variable length support in fmha (a…" (#2883)
    This reverts commit 7ede589f4b2fc5e439c68f8deb7ec75ea9a09da2.
    
    [ROCm/composable_kernel commit: b765fe78f37c85a9ca10c24fec6b7247a170034f]

--- example/ck_tile/01_fmha/example_fmha_fwd.cpp
+++ example/ck_tile/01_fmha/example_fmha_fwd.cpp
@@ -33,10 +33,6 @@ auto create_args(int argc, char* argv[])
                 "0",
                 "seqlen_k for new key/value, 0 means not to use this at all; "
                 "-1 to choose s_knew in [1, s] randomly.")
-        .insert("s_qpad",
-                "-1",
-                "seqlen_q stride between 2 batches (group-mode optional).\n"
-                "Provide positive strides per-batch to simulate physical padding on Q.")
         .insert("s_kpad",
                 "-1",
                 "seqlen_k stride between 2 batches, currently used in group-mode only\n"
@@ -111,15 +107,7 @@ auto create_args(int argc, char* argv[])
         .insert("warmup", "5", "number of iterations before benchmark the kernel")
         .insert("repeat", "20", "number of iterations to benchmark the kernel")
         .insert("json", "0", "0: No Json, 1: Dump Results in Json format")
-        .insert("jsonfile", "fmha_fwd.json", "json file name to dump results")
-        .insert("q_eff_lens",
-                "",
-                "Batch-mode only: per-batch effective seqlen for Q (exclude PAD).\n"
-                "Comma-separated list of length 'b'. If empty, no override.")
-        .insert("kv_eff_lens",
-                "",
-                "Batch-mode only: per-batch effective seqlen for KV (exclude PAD).\n"
-                "Comma-separated list of length 'b'. If empty, no override.");
+        .insert("jsonfile", "fmha_fwd.json", "json file name to dump results");
 
     bool result = arg_parser.parse(argc, argv);
     return std::make_tuple(result, arg_parser);
@@ -139,9 +127,6 @@ auto run(const ck_tile::ArgParser& arg_parser)
     ck_tile::index_t hdim_v          = arg_parser.get_int("d_v");
     ck_tile::index_t seqlen_knew     = arg_parser.get_int("s_knew");
     auto seqlen_kpads                = arg_parser.get_int_vec("s_kpad");
-    auto seqlen_qpads                = arg_parser.get_int_vec("s_qpad");
-    auto q_eff_lens_per_batch        = arg_parser.get_int_vec("q_eff_lens");
-    auto kv_eff_lens_per_batch       = arg_parser.get_int_vec("kv_eff_lens");
     ck_tile::index_t rotary_dim      = arg_parser.get_int("rotary_dim");
     bool i_perm                      = arg_parser.get_bool("iperm");
     bool o_perm                      = arg_parser.get_bool("operm");
@@ -189,10 +174,7 @@ auto run(const ck_tile::ArgParser& arg_parser)
                                         hdim_q,
                                         hdim_v,
                                         seqlen_knew,
-                                        seqlen_qpads,
                                         seqlen_kpads,
-                                        q_eff_lens_per_batch,
-                                        kv_eff_lens_per_batch,
                                         rotary_dim,
                                         i_perm,
                                         o_perm,
