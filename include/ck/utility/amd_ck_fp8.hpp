3f870e5f099a5ba7da60456001d32133855c3613
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-09-24 18:56:10 +0000

    format

--- include/ck/utility/amd_ck_fp8.hpp
+++ include/ck/utility/amd_ck_fp8.hpp
@@ -35,7 +35,7 @@ namespace ck {
 
 struct f8_fnuz_t
 {
-    using data_type = unsigned char;
+    using data_type  = unsigned char;
     data_type m_data = data_type{};
     __host__ __device__ explicit constexpr f8_fnuz_t(data_type in_data) : m_data(in_data) {}
     __host__ __device__ explicit constexpr f8_fnuz_t() = default;
@@ -48,7 +48,7 @@ struct f8_fnuz_t
 
 struct bf8_fnuz_t
 {
-    using data_type = unsigned char;
+    using data_type  = unsigned char;
     data_type m_data = data_type{};
     __host__ __device__ explicit constexpr bf8_fnuz_t(data_type in_data) : m_data(in_data) {}
     __host__ __device__ explicit constexpr bf8_fnuz_t() = default;
