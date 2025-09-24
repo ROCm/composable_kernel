1ad2c9a1039bbad6b496f458806378bc9173611f
AviralGoelAMD <aviral.goel@amd.com>
2025-09-24 21:42:48 +0000

    feat(grouped_gemm_multi_d): add new example that integrates grouped_gemm and multi_d_gemm feature

--- include/ck_tile/ops/gemm.hpp
+++ include/ck_tile/ops/gemm.hpp
@@ -34,6 +34,7 @@
 #include "ck_tile/ops/gemm/kernel/gemm_multi_abd_kernel.hpp"
 #include "ck_tile/ops/gemm/kernel/gemm_tile_partitioner.hpp"
 #include "ck_tile/ops/gemm/kernel/grouped_gemm_kernel.hpp"
+#include "ck_tile/ops/gemm/kernel/grouped_gemm_multi_d.hpp"
 #include "ck_tile/ops/gemm/kernel/streamk_gemm_kernel.hpp"
 #include "ck_tile/ops/gemm/kernel/universal_gemm_kernel.hpp"
 #include "ck_tile/ops/gemm/pipeline/gemm_pipeline_ag_bg_cr_base.hpp"
