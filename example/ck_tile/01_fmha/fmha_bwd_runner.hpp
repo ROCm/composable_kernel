4dd820b4e29bba1ca702b4e2024a77a26729d7c3
Anton Gorenko <anton@streamhpc.com>
2025-09-24 12:38:24 +0500

    Fix condition that dq_acc must be set to zero when mask is used
    The change was introduced in #2799

--- example/ck_tile/01_fmha/fmha_bwd_runner.hpp
+++ example/ck_tile/01_fmha/fmha_bwd_runner.hpp
@@ -786,7 +786,7 @@ bwd_result fmha_bwd_run(mode_enum mode,
         // non-deterministic kernels use atomic add to write dq
         // Some block may be skipped with causal mask and dq are not set to zeros
         // In these cases thus we need to zero out it first
-        if(!deterministic || mask.type == mask_enum::no_mask)
+        if(!deterministic || mask.type != mask_enum::no_mask)
             dq_acc_buf.SetZero();
 
         ck_tile::stream_config stream_config_v{nullptr, true, 0, 0, 1};
