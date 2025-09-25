bb5eeef2afa073d457b63fdea4e401cf8cc5102d
Khushbu Agarwal <khuagarw@amd.com>
2025-09-25 10:32:42 -0700

    Fix for Add the API to load SGPR (#2913)
    * Revert "Revert "[CK-Tile] Add the API to load SGPR  (#2878)" (#2904)"
    
    This reverts commit 5cc40c160f28f5dc0fcf08d505ab72e2808d7b0e.
    
    * Fix: sgpr minor issue
    
    * cyclic dependency resolved
    
    * clang formatted
    
    * removing unused variable
    
    * clang formatted
    
    ---------
    
    Co-authored-by: Illia Silin <98187287+illsilin@users.noreply.github.com>
    
    [ROCm/composable_kernel commit: b56e5d1d79b42569f0de5e48b2ae415921464955]

--- include/ck_tile/ops/smoothquant/kernel/moe_smoothquant_kernel.hpp
+++ include/ck_tile/ops/smoothquant/kernel/moe_smoothquant_kernel.hpp
@@ -138,7 +138,7 @@ struct MoeSmoothquant
         const index_t i_topk  = blockIdx.x;
         const index_t i_token = blockIdx.y * Block_M;
         const index_t i_token_in_thrd =
-            __builtin_amdgcn_readfirstlane(threadIdx.x / Problem::BlockShape::ThreadPerBlock_N);
+            amd_wave_read_first_lane(threadIdx.x / Problem::BlockShape::ThreadPerBlock_N);
 
         const index_t i_expert = reinterpret_cast<const index_t*>(
             kargs.p_topk_ids)[(i_token + i_token_in_thrd) * kargs.topk + i_topk];
