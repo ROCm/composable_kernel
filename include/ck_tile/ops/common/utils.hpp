3c08ce1e64f228438d96d89c31e882e57fa47ec9
Ville Pietilä <ville.pietila@amd.com>
2025-10-15 11:02:21 +0000

    Improve the grouped conv kernel name generation in CK Tile.

--- include/ck_tile/ops/common/utils.hpp
+++ include/ck_tile/ops/common/utils.hpp
@@ -19,6 +19,12 @@ template <> struct typeToStr<fp8_t> { static constexpr const char * name = "fp8"
 template <> struct typeToStr<bf8_t> { static constexpr const char * name = "bf8"; };
 template <> struct typeToStr<int8_t> { static constexpr const char * name = "int8"; };
 template <> struct typeToStr<pk_int4_t> { static constexpr const char * name = "pk_int4"; };
+
+template <memory_operation_enum MemOp> struct memOpToStr;
+template <> struct memOpToStr<memory_operation_enum::set> { static constexpr const char * name = "set"; };
+template <> struct memOpToStr<memory_operation_enum::atomic_add> { static constexpr const char * name = "atomic_add"; };
+template <> struct memOpToStr<memory_operation_enum::atomic_max> { static constexpr const char * name = "atomic_max"; };
+template <> struct memOpToStr<memory_operation_enum::add> { static constexpr const char * name = "add"; };
 // clang-format on
 
 template <typename ADataType_, typename BDataType_>
@@ -30,6 +36,12 @@ std::string gemm_prec_str()
         base_str += "_" + std::string(typeToStr<BDataType_>::name);
     }
     return base_str;
+}
+
+template <memory_operation_enum MemOp_>
+std::string mem_op_string()
+{
+    return std::string(memOpToStr<MemOp_>::name);
 }
 
 } // namespace ck_tile
