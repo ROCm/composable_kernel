0fa9b47ee5fdb7bb153f8c7ff691ca9c2c2ddc69
AviralGoelAMD <aviral.goel@amd.com>
2025-10-21 14:39:08 +0000

    feat: add bf8 support

--- example/ck_tile/17_grouped_gemm/quant_grouped_gemm.hpp
+++ example/ck_tile/17_grouped_gemm/quant_grouped_gemm.hpp
@@ -39,6 +39,14 @@ struct GemmTypeConfig<ck_tile::fp8_t>
     using AccDataType = float;
     using CDataType   = ck_tile::half_t;
 };
+template <>
+struct GemmTypeConfig<ck_tile::bf8_t>
+{
+    using ADataType   = ck_tile::bf8_t;
+    using BDataType   = ck_tile::bf8_t;
+    using AccDataType = float;
+    using CDataType   = ck_tile::half_t;
+};
 
 struct GemmConfigBase
 {
