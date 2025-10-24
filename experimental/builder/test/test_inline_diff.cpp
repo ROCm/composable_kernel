73c395049f789265f3f94fd85116f5a4f2b8b1a4
Kevin Abraham <kevin.abraham@streamhpc.com>
2025-10-24 18:36:08 +0000

    renamed test_builder_utils to test_inline_diff, renamed tests, removed builder_uitls, added non-color tests

--- experimental/builder/test/test_inline_diff.cpp
+++ experimental/builder/test/test_inline_diff.cpp
@@ -2,37 +2,13 @@
 // Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 #include <gtest/gtest.h>
-#include <ck/utility/sequence.hpp>
-#include <array>
-#include <type_traits>
 
-#include <ck_tile/builder/builder_utils.hpp>
 #include "testing_utils.hpp"
 
 namespace ck_tile::builder {
 namespace {
 
-class BuilderUtils : public ::testing::Test
-{
-};
-
-TEST(BuilderUtils, ToSequence)
-{
-    static constexpr std::array<int, 4> test_arr{1, 2, 8, 16};
-    using result_seq   = to_sequence_v<test_arr>;
-    using expected_seq = ck::Sequence<1, 2, 8, 16>;
-    EXPECT_TRUE((std::is_same_v<result_seq, expected_seq>));
-}
-
-TEST(BuilderUtils, ToSequenceEmpty)
-{
-    static constexpr std::array<int, 0> test_arr{};
-    using result_seq   = to_sequence_v<test_arr>;
-    using expected_seq = ck::Sequence<>;
-    EXPECT_TRUE((std::is_same_v<result_seq, expected_seq>));
-}
-
-TEST(BuilderUtils, StringLiteral)
+TEST(InlineDiff, simpleColorDiff)
 {
     std::string str1{"hello"};
     std::string str2{"hello"};
@@ -43,6 +19,22 @@ TEST(BuilderUtils, StringLiteral)
     EXPECT_THAT(test::inlineDiff(str1, str2, true), "hello");
     EXPECT_THAT(test::inlineDiff(str1, str3, true),
                 "[\x1B[36mwor\x1B[0m|\x1B[35mhel\x1B[0m]l[\x1B[36md\x1B[0m|\x1B[35mo\x1B[0m]");
+}
+
+TEST(InlineDiff, noColorDiff)
+{
+    std::string str1{"hello"};
+    std::string str2{"hello"};
+    std::string str3{"world"};
+
+    // some easy tests
+    // you can veryfy the ungodly strings are meaningful by running echo -e "<string>"
+    EXPECT_THAT(test::inlineDiff(str1, str2, false), "hello");
+    EXPECT_THAT(test::inlineDiff(str1, str3, false), "[wor|hel]l[d|o]");
+}
+
+TEST(InlineDiff, complexColorDiff)
+{
 
     // now something more interesting
     std::string str4{"this part has changed, this part has been left out, this part, this part has "
