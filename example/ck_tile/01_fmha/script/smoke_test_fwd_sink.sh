0b634abbda4445802d9a2e2803c39926b0a403ed
LJ-underdog <Jun.Lin@amd.com>
2025-10-28 15:27:56 +0800

    update test script
    Signed-off-by: LJ-underdog <Jun.Lin@amd.com>

--- example/ck_tile/01_fmha/script/smoke_test_fwd_sink.sh
+++ example/ck_tile/01_fmha/script/smoke_test_fwd_sink.sh
@@ -16,7 +16,7 @@ set -x
 COMMON_ARGS='-v=1 -warmup=0 -repeat=1'
 
 
-$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=c -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=t:2,0,2
+$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=r -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=t:2,0,2
 
 # window_size[2,0], sink_size = 2
 
@@ -31,7 +31,7 @@ $EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=8 -bias=n -lse=0 -iper
 #    * * * * * 1 1 1           1 1 * * * 1 1 1
 #    l=2/r=0(tl)               l=2/r=0/s=2(tl)
 
-$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=c -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=t:0,3,2 #-mask=b:3,0,2
+$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=r -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=t:0,3,2 #-mask=b:3,0,2
 
 #    x=4/y=1                   
 #    1 1 1 1 * * * *           1 1 1 1 * * * * 
@@ -43,7 +43,7 @@ $EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iper
 #    l=3/r=0(br)               l=3/r=0/s=2(br)  
 
 
-$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=c -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:1,0,2 
+$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=r -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:1,0,2 
 
 #    x=4/y=-1          
 #    * * 1 1 * * * *           1 1 1 1 * * * * 
@@ -54,7 +54,7 @@ $EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=5 -s_k=8 -bias=n -lse=0 -iper
 #    l=1/r=0(br)               l=1/r=0/s=2(br)
 
 
-$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=6 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=c -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:2,0,2
+$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=6 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=r -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:2,0,2
 
 #    x=-1/y=5 
      
@@ -69,7 +69,7 @@ $EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=6 -bias=n -lse=0 -iper
 #    l=2/r=0(br)               l=2/r=0/s=2(br)
 
 
-$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=5 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=c -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:-1,1,2
+$EXE -prec=fp16 -mode=1 -b=1 -h=1 -d=16 -d_v=32 -s=8 -s_k=5 -bias=n -lse=0 -iperm=0 -operm=0 -vlayout=r -num_splits=1 -page_block_size=0 -cache_batch_idx=0  -kname=1 -v=1 -warmup=0 -repeat=1 -mask=b:-1,1,2
 #      x=-1/y=8
 #    * * * * *               * * * * *    
 #    * * * * *               * * * * * 
