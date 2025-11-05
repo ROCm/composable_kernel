66780cf2e4ce1aad3ecce7f9f6acfaa30edfaa3b
Adam Osewski <Adam.Osewski@amd.com>
2025-11-05 15:07:33 +0000

    Fix handling union based conv direction.

--- experimental/builder/include/ck_tile/builder/conv_signature_predicates.hpp
+++ experimental/builder/include/ck_tile/builder/conv_signature_predicates.hpp
@@ -33,30 +33,35 @@ concept ConvDirectionIsBackwardWeight = (Sig.direction == ConvDirection::BACKWAR
 // Predicate for DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3 operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3 =
+    ConvDirectionIsForward<Sig> &&
     (Sig.device_operation._fwd ==
      FwdGroupConvDeviceOperation::DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3);
 
 // Predicate for DeviceGroupedConvFwdDlMultipleD_NHWC_KYXC_NHWK operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvFwdDlMultipleD_NHWC_KYXC_NHWK =
+    ConvDirectionIsForward<Sig> &&
     (Sig.device_operation._fwd ==
      FwdGroupConvDeviceOperation::DeviceGroupedConvFwdDlMultipleD_NHWC_KYXC_NHWK);
 
 // Predicate for DeviceGroupedConvFwdMultipleD_Wmma_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvFwdMultipleD_Wmma_CShuffle =
+    ConvDirectionIsForward<Sig> &&
     (Sig.device_operation._fwd ==
      FwdGroupConvDeviceOperation::DeviceGroupedConvFwdMultipleD_Wmma_CShuffle);
 
 // Predicate for DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle =
+    ConvDirectionIsForward<Sig> &&
     (Sig.device_operation._fwd ==
      FwdGroupConvDeviceOperation::DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle);
 
 // Predicate for DeviceGroupedConvFwdMultipleD_Xdl_CShuffle_Large_Tensor operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvFwdMultipleD_Xdl_CShuffle_Large_Tensor =
+    ConvDirectionIsForward<Sig> &&
     (Sig.device_operation._fwd ==
      FwdGroupConvDeviceOperation::DeviceGroupedConvFwdMultipleD_Xdl_CShuffle_Large_Tensor);
 
@@ -76,48 +81,56 @@ concept ConvDeviceOpIsForward =
 // Predicate for DeviceGroupedConvBwdWeight operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeight =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeight);
 
 // Predicate for DeviceGroupedConvBwdWeightMultipleD_Xdl_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeightMultipleD_Xdl_CShuffle =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeightMultipleD_Xdl_CShuffle);
 
 // Predicate for DeviceGroupedConvBwdWeight_Xdl_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeight_Xdl_CShuffle =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeight_Xdl_CShuffle);
 
 // Predicate for DeviceGroupedConvBwdWeightTwoStage_Xdl_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeightTwoStage_Xdl_CShuffle =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeightTwoStage_Xdl_CShuffle);
 
 // Predicate for DeviceGroupedConvBwdWeight_Wmma_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeight_Wmma_CShuffle =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeight_Wmma_CShuffle);
 
 // Predicate for DeviceGroupedConvBwdWeight_Xdl_CShuffleV3 operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeight_Xdl_CShuffleV3 =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeight_Xdl_CShuffleV3);
 
 // Predicate for DeviceGroupedConvBwdWeightMultipleD operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeightMultipleD =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeightMultipleD);
 
 // Predicate for DeviceGroupedConvBwdWeight_Dl operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdWeight_Dl =
+    ConvDirectionIsBackwardWeight<Sig> &&
     (Sig.device_operation._bwd_weight ==
      BwdWeightGroupConvDeviceOperation::DeviceGroupedConvBwdWeight_Dl);
 
@@ -140,18 +153,21 @@ concept ConvDeviceOpIsBackwardWeight =
 // Predicate for DeviceGroupedConvBwdDataMultipleD_Xdl_CShuffle_v1 operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdDataMultipleD_Xdl_CShuffle_v1 =
+    ConvDirectionIsBackwardData<Sig> &&
     (Sig.device_operation._bwd_data ==
      BwdDataGroupConvDeviceOperation::DeviceGroupedConvBwdDataMultipleD_Xdl_CShuffle_v1);
 
 // Predicate for DeviceGroupedConvBwdDataMultipleD operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdDataMultipleD =
+    ConvDirectionIsBackwardData<Sig> &&
     (Sig.device_operation._bwd_data ==
      BwdDataGroupConvDeviceOperation::DeviceGroupedConvBwdDataMultipleD);
 
 // Predicate for DeviceGroupedConvBwdDataMultipleD_Wmma_CShuffle operation.
 template <auto Sig>
 concept ConvDeviceOpIs_DeviceGroupedConvBwdDataMultipleD_Wmma_CShuffle =
+    ConvDirectionIsBackwardData<Sig> &&
     (Sig.device_operation._bwd_data ==
      BwdDataGroupConvDeviceOperation::DeviceGroupedConvBwdDataMultipleD_Wmma_CShuffle);
 
