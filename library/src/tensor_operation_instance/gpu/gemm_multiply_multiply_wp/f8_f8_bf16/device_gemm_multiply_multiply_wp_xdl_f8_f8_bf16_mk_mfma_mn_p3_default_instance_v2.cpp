25f356a104b1e6a4974bc6c2405c29d9e4fe4b9c
jefyang1 <jeffreyj.yang@amd.com>
2025-11-11 02:03:57 +0000

    Fix test_gemm_multiply_multiply_wp_xdl_fp8 on gfx950

--- library/src/tensor_operation_instance/gpu/gemm_multiply_multiply_wp/f8_f8_bf16/device_gemm_multiply_multiply_wp_xdl_f8_f8_bf16_mk_mfma_mn_p3_default_instance_v2.cpp
+++ library/src/tensor_operation_instance/gpu/gemm_multiply_multiply_wp/f8_f8_bf16/device_gemm_multiply_multiply_wp_xdl_f8_f8_bf16_mk_mfma_mn_p3_default_instance_v2.cpp
@@ -26,6 +26,11 @@ void add_device_gemm_multiply_multiply_weight_preshuffle_xdl_f8_f8_bf16_mk_mfma_
         device_gemm_multiply_multiply_weight_preshuffle_xdl_f8_f8_bf16_mk_mfma_mn_p3_instances<
             v2,
             GemmDefault>{});
+    add_device_operation_instances(
+        instances,
+        device_gemm_multiply_multiply_weight_preshuffle_xdl_f8_f8_bf16_mk_mfma_mn_p3_instances_part2<
+            v2,
+            GemmDefault>{});
 }
 
 } // namespace instance
