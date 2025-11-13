3b9fb5a00ff884c0a462f4804a2978474afb9d40
Khushbu Agarwal <khuagarw@amd.com>
2025-11-12 23:44:12 -0800

    fixing ambiguous shuffle definitions (#3175)
    Co-authored-by: Thomas Ning <Thomas.Ning@amd.com>

--- test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_util.hpp
+++ test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_util.hpp
@@ -9,6 +9,7 @@
 #include "ck_tile/host.hpp"
 #include "ck_tile/host/kernel_launch.hpp"
 #include "ck_tile/host/permute_pk_int4.hpp"
+#include "ck_tile/host/tensor_shuffle_utils.hpp"
 #include "ck_tile/ops/epilogue.hpp"
 #include "ck_tile/ops/gemm.hpp"
 
@@ -259,48 +260,6 @@ class TestCkTileGemmPipeline : public ::testing::Test
         k_batches_ = {1};
     }
 
-    template <typename GemmConfig, typename T>
-    auto shuffle_b(const ck_tile::HostTensor<T>& t)
-    {
-        assert(t.get_lengths().size() == 2);
-        int n_ = t.get_lengths()[1];
-        int k_ = t.get_lengths()[0];
-
-        if(ck_tile::is_gfx12_supported())
-        {
-            constexpr int divisor      = 2;
-            constexpr int kABK1PerLane = 8;
-            constexpr int kABK0PerLane = GemmConfig::K_Warp_Tile / divisor / kABK1PerLane;
-            ck_tile::HostTensor<T> t_view({n_ / GemmConfig::N_Warp_Tile,
-                                           GemmConfig::N_Warp_Tile,
-                                           k_ / GemmConfig::K_Warp_Tile,
-                                           kABK0PerLane,
-                                           divisor,
-                                           kABK1PerLane});
-            std::copy(t.begin(), t.end(), t_view.begin());
-            return ck_tile::reference_permute(t_view, {0, 2, 4, 1, 3, 5});
-        }
-        else
-        {
-            int divisor = 1;
-            if(ck_tile::is_gfx11_supported())
-            {
-                divisor = 1;
-            }
-            else
-            {
-                assert(is_wave32() == false);
-                divisor = GemmConfig::N_Warp_Tile == 32 ? 2 : 4;
-            }
-            ck_tile::HostTensor<T> t_view({n_ / GemmConfig::N_Warp_Tile,
-                                           GemmConfig::N_Warp_Tile,
-                                           k_ / GemmConfig::K_Warp_Tile,
-                                           divisor,
-                                           GemmConfig::K_Warp_Tile / divisor});
-            std::copy(t.begin(), t.end(), t_view.begin());
-            return ck_tile::reference_permute(t_view, {0, 2, 3, 1, 4});
-        }
-    }
     template <bool PadM = true, bool PadN = true, bool PadK = true, bool Preshuffle = false>
     void Run(const int M,
              const int N,
