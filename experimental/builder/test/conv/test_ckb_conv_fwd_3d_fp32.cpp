91f7d4ac750b2e95bf56a586eaf09d94f8f5e178
Ville Pietilä <188998872+vpietila-amd@users.noreply.github.com>
2025-11-13 18:47:25 +0200

    [CK_BUILDER] Forward convolution builder improvements (#3179)
    Proposed changes
    Improve the forward convolution builder implementation and addressed leftover feedback left from PR #3138. Main changes
    
    Refactored tests such that they reflect better the builder pattern. The templates and types for the convolution algorithm concepts are created via factory that facilitates programmatic creation of the device op instances.
    Moved tests into anonymous namespace.
    The convolution factory had lot of if-else constructs when CK Builder types were converted into CK library types. I had initially trouble in using static_assert in the default branch of switch as the static_assert was evaluated at compile time even for valid types. However, if we change the static_assert to throw "<error message>", it will result in a compile-time error only if the default branch is actually hit. This assumes that the function is consteval. Hence, changed all conversions in the convolution factory to use switch, which is more intuitive.
    Removed the explicit device op definition from convolution signature and the corresponding predicate file. The device ops are defined by the corresponding concepts. This allowed to remove lot of boilerplate code from the convolution factory.
    Adde inheritance and convolution algorithm specialization to handle device ops that are specialization of a more generic ones. The large tensor support is more naturally expressed by this pattern.
    Added support for the FP8 data type.
    
    * WIP: Builder for expected test results.
    
    * Improve ckb fwd conv instance tests.
    
    * clang-format
    
    * Change if-else statements into switch in conv factory.
    
    * Fix clang-formatting.
    
    * Removed unnecessary includes.
    
    * Added missing copyright.
    
    * Remove explicit device op flag from from convolution signature.
    
    * Add missing concept.
    
    * Fix build.
    
    * clang-format
    
    * Add test for building conv fwd FP8 instances.
    
    * Add missing header to instance traits.
    
    * Clean-up recently added instances.
    
    * Introduce inheritance and specialization.
    
    * Use builder to build conv algorithm templates and types.
    
    * clang-format
    
    * Fix conv description tests.
    
    ---------
    
    Co-authored-by: John Shumway <john.shumwayjr@gmail.com>
    
    [ROCm/composable_kernel commit: 7d57bc169f8206f06bc516a7f930f388def32347]

--- experimental/builder/test/conv/test_ckb_conv_fwd_3d_fp32.cpp
+++ experimental/builder/test/conv/test_ckb_conv_fwd_3d_fp32.cpp
@@ -1,33 +1,39 @@
 // Copyright (C) Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
 
-#include "utils/ckb_conv_test_common.hpp"
+#include "utils/ckb_conv_test_configs.hpp"
+#include "utils/ckb_conv_test_utils.hpp"
+
+namespace {
 
 using namespace ck_tile::builder::test_utils;
-
-namespace ck_tile::builder::testing {
 
 // 3D FP32 NGCDHW (channels-first) with Pipeline V1 and FILTER_1X1_PAD0
 TEST(FwdConvInstances,
      Create_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3_Instance_3D_FP32_ChannelsFirst)
 {
-    constexpr ConvSignature FwdConvSignature{
-        .spatial_dim           = 3,
-        .direction             = ConvDirection::FORWARD,
-        .layout                = GroupConvLayout3D::NGCDHW_GKCZYX_NGKDHW,
-        .data_type             = DataType::FP32,
-        .elementwise_operation = ElementwiseOperation::PASS_THROUGH,
-        .device_operation =
-            FwdGroupConvDeviceOperation::DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3};
+    constexpr ConvSignature FwdConvSignature{.spatial_dim = 3,
+                                             .direction   = ConvDirection::FORWARD,
+                                             .layout      = GroupConvLayout3D::NGCDHW_GKCZYX_NGKDHW,
+                                             .data_type   = DataType::FP32,
+                                             .elementwise_operation =
+                                                 ElementwiseOperation::PASS_THROUGH};
 
-    constexpr ThreadBlock FwdThreadBlock{.block_size = 256,
-                                         .tile_size  = {.m = 128, .n = 128, .k = 32}};
+    constexpr auto FwdConvAlgorithm =
+        ConvAlgorithm_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3{}
+            .with_thread_block(FwdThreadBlock_256_256x256x32)
+            .with_gemm_config(FwdGemmParams_Xdl_4x4_per_wave)
+            .with_block_transfer(FwdBlockTransfer_4x64x1)
+            .with_specializations(ConvFwdSpecialization::FILTER_1X1_PAD0,
+                                  GemmSpecialization::MNKPadding)
+            .with_block_gemm(BlockGemmDesc_v1_intrawave);
 
-    run_test_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3<
-        FwdConvSignature,
-        FwdThreadBlock,
-        PipelineVersion::V1,
-        ConvFwdSpecialization::FILTER_1X1_PAD0>();
+    using Builder = ConvBuilder<FwdConvSignature, FwdConvAlgorithm>;
+    run_test<Builder>({"DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_V3",
+                       "256, 256, 256, 32",
+                       "Filter1x1Pad0",
+                       "BlkGemmPipelineScheduler: Intrawave",
+                       "BlkGemmPipelineVersion: v1"});
 }
 
-} // namespace ck_tile::builder::testing
+} // namespace
