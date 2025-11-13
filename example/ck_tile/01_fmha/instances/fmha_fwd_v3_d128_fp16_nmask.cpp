f4d92f1b04d3b2675cef1903852ceeebfa9c63e2
PoYen, Chen <PoYen.Chen@amd.com>
2025-11-13 01:49:59 -0600

    Remove fmha fwd v3 example

--- example/ck_tile/01_fmha/instances/fmha_fwd_v3_d128_fp16_nmask.cpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
-
-#include "fmha_fwd_v3.hpp"
-#include "fmha_fwd_v3_impl.hpp"
-
-using kernel_traits = ck_tile::fmha_fwd_v3_kernel_traits<FmhaFwdFp16, false, false>;
-
-INST_FMHA_FWD_V3_DISPATCH(kernel_traits)
