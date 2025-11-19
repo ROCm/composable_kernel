b314fb0d950150e005018b2a0843504211df601b
AviralGoelAMD <aviral.goel@amd.com>
2025-11-19 21:41:17 +0000

    chore(copyright): update copyright header for test directory

--- test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_util.hpp
+++ test/ck_tile/gemm_weight_preshuffle/test_gemm_pipeline_util.hpp
@@ -1,7 +1,7 @@
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
-#pragma once
 
+#pragma once
 #include <sstream>
 #include <gtest/gtest.h>
 
