b9ee41c660fd4172803d6187e54408d701c6a5e4
kabrahamAMD <kevin.abraham@amd.com>
2025-11-19 21:31:05 +0100

    [CK_Builder ]fixed accidental drop of get_elementwise_operation during merge and added usage of get_elementwise_operation() to other builder instances (#3238)
    Fixed issues encountered during merge of #3192
    
    * fixed accidental drop of get_elementwise_operation during merge and added call to get_elementwise_op to 4 other builders
    
    * run clang-format
    
    ---------
    
    Co-authored-by: Kevin Abraham <kevin.abraham@streamhpc.com>
    
    [ROCm/composable_kernel commit: 964f8e1f60395a4fd8ecbfe8907bff1b8d881314]

---
+++ experimental/builder/include/ck_tile/builder/conv_signature_utils.hpp
@@ -0,0 +1,47 @@
+// Copyright (C) Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+#include "ck_tile/builder/types.hpp"
+
+namespace ck_tile::builder {
+/**********************************************
+ * constexpr helper functions for optional parameters
+ **********************************************/
+
+template <auto Sig>
+concept ProvidesElementwiseOperation = requires { Sig.elementwiseOperation; };
+
+template <auto Sig>
+concept ProvidesConvolutionDirection = requires { Sig.direction; };
+
+template <auto Sig>
+constexpr auto get_elementwise_operation()
+{
+    if constexpr(ProvidesElementwiseOperation<Sig>)
+    {
+        return Sig.elementwise_operation;
+    }
+    else
+    {
+        return ElementwiseOperation::PASS_THROUGH;
+    }
+}
+
+template <auto Sig>
+constexpr auto get_conv_direction()
+{
+    if constexpr(ProvidesConvolutionDirection<Sig>)
+    {
+        return Sig.direction;
+    }
+    else
+    {
+        return ConvDirection::FORWARD;
+    }
+}
+} // namespace ck_tile::builder
