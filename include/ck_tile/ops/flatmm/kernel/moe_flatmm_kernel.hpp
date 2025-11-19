5b4ee5518571c0775cb87b7001b9fac3082c2487
mohsen saffari <msaffari@amd.com>
2025-11-19 15:32:39 +0100

    correct clang-format

--- include/ck_tile/ops/flatmm/kernel/moe_flatmm_kernel.hpp
+++ include/ck_tile/ops/flatmm/kernel/moe_flatmm_kernel.hpp
@@ -1264,7 +1264,7 @@ struct MoeFlatmmKernel
                         scatter_token_id =
                             scatter_token_id * kargs.TopK + (fused_token >> token_id_offset);
                     c_scatter_offsets[mIter][m0] = scatter_token_id * kargs.stride_C;
-                    c_scatter_valids[mIter][m0] = (scatter_token_id < kargs.NumTokens);
+                    c_scatter_valids[mIter][m0]  = (scatter_token_id < kargs.NumTokens);
                 });
             });
 
