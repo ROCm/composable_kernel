69c61552f6715be533e0922b076f9b05512435de
Gavin Zhao <gavinzhaojw@protonmail.com>
2025-11-20 13:45:57 -0500

    Add support for RDNA1 GPUs (#3220)
    * Allow compilation for RDNA1 (__gfx101__)
    
    Signed-off-by: Gavin Zhao <git@gzgz.dev>
    
    * More RDNA1 changes
    
    Signed-off-by: Gavin Zhao <git@gzgz.dev>
    
    * Even more RDNA1 changes
    
    Signed-off-by: Gavin Zhao <git@gzgz.dev>
    
    * cmake: skip build quantization for unsupported arches
    
    * add gfx10-1-generic support as well
    
    * add gfx1013 and complete gfx10-1-generic
    
    * fix clang format
    
    * enable DL kernels on gfx101x
    
    ---------
    
    Signed-off-by: Gavin Zhao <git@gzgz.dev>
    Co-authored-by: illsilin_amdeng <Illia.Silin@amd.com>
    Co-authored-by: Illia Silin <98187287+illsilin@users.noreply.github.com>

--- include/ck/tensor_operation/gpu/grid/gridwise_tensor_rearrange.hpp
+++ include/ck/tensor_operation/gpu/grid/gridwise_tensor_rearrange.hpp
@@ -36,7 +36,7 @@ __launch_bounds__(CK_MAX_THREAD_PER_BLOCK, CK_MIN_BLOCK_PER_CU)
                             const ComputePtrOffsetOfStridedBatch compute_ptr_offset_of_batch)
 {
 #if(defined(__gfx906__) || defined(__gfx908__) || defined(__gfx90a__) || defined(__gfx94__) || \
-    defined(__gfx103__) || defined(__gfx11__) || defined(__gfx12__))
+    defined(__gfx101__) || defined(__gfx103__) || defined(__gfx11__) || defined(__gfx12__))
     GridwiseTensorRearrangeKernel::Run(in_grid_desc,
                                        p_in_global,
                                        out_grid_desc,
