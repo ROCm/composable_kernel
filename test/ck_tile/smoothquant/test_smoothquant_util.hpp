9f80d9a0b108a26b0d383eb1379a94f6f204f5d0
AviralGoelAMD <aviral.goel@amd.com>
2025-11-21 14:32:39 +0000

    chore(copyright): update copyright header for test directory

--- test/ck_tile/smoothquant/test_smoothquant_util.hpp
+++ test/ck_tile/smoothquant/test_smoothquant_util.hpp
@@ -1,5 +1,5 @@
-// Copyright © Advanced Micro Devices, Inc., or its affiliates.
-// SPDX-License-Identifier:  MIT
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier: MIT
 
 #include "ck_tile/host.hpp"
 #include "smoothquant.hpp"
