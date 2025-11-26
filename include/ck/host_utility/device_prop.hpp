20e9b275da8b1213ea79c07dcd76b6e92ddfd6a5
Matthias Gehre <matthias.gehre@xilinx.com>
2025-11-26 16:38:44 +0100

    Add support for gfx1153

--- include/ck/host_utility/device_prop.hpp
+++ include/ck/host_utility/device_prop.hpp
@@ -62,7 +62,7 @@ inline bool is_gfx11_supported()
     return ck::get_device_name() == "gfx1100" || ck::get_device_name() == "gfx1101" ||
            ck::get_device_name() == "gfx1102" || ck::get_device_name() == "gfx1103" ||
            ck::get_device_name() == "gfx1150" || ck::get_device_name() == "gfx1151" ||
-           ck::get_device_name() == "gfx1152";
+           ck::get_device_name() == "gfx1152" || ck::get_device_name() == "gfx1153";
 }
 
 inline bool is_xdl_supported()
