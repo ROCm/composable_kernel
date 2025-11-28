a4252c48cef00ed36bdddbb78e27998134579870
Ville Pietilä <>
2025-11-28 13:36:46 +0000

    Make signature definition look nice in the test code.

--- experimental/builder/test/conv/test_ckb_conv_fwd_2d_bf16_scaleadd_relu.cpp
+++ experimental/builder/test/conv/test_ckb_conv_fwd_2d_bf16_scaleadd_relu.cpp
@@ -12,48 +12,21 @@ using namespace ck_tile::builder::test_utils;
 TEST(FwdConvInstances,
      Create_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle_Instance_2D_BF16_scale_add_relu)
 {
-    constexpr auto G_K = BiasLayout::G_K_strided;
-    constexpr auto NHWGK = ConvOutputLayout2D::NHWGK; 
-
-    // Input
-    constexpr auto inputConfig = TensorConfig{
-        .layout             = ConvInputLayout2D::NHWGC
-    };
-    constexpr auto inputTensor = ConvolutionTensor{
-        .config      = inputConfig
-    };
-
-    // Weight
-    constexpr auto weight = TensorConfig{
-        .layout             = ConvWeightLayout2D::GKYXC
-    };
-    constexpr auto weightTensor = ConvolutionTensor{
-        .config      = weight
-    };
-
-    // Output with elementwise ops
-    constexpr auto output = TensorConfig{
-        .layout             = ConvOutputLayout2D::NHWGK
-    };
-    constexpr auto output_op = TensorOperation<> 
-        {
-            .elementwise_operation = ElementwiseOperation::SCALEADD_SCALEADD_RELU
-        }
-    .with_auxiliary_operand_configs<NHWGK, G_K>();
-
-    constexpr auto outputTensor = ConvolutionTensor{
-        .config      = output,
-        .operation   = output_op
-    };
-
-    constexpr ConvSignature FwdConvSignature{.spatial_dim = 2,
-                                             .direction   = ConvDirection::FORWARD,
-                                             .data_type   = DataType::BF16,
-                                             .accumulation_data_type = DataType::FP32,
-                                             .input    = inputTensor,
-                                             .weight   = weightTensor,
-                                             .output   = outputTensor
-                                            };
+    constexpr ConvSignature FwdConvSignature
+        { 
+            .spatial_dim = 2,
+            .direction   = ConvDirection::FORWARD,
+            .data_type   = DataType::BF16,
+            .accumulation_data_type = DataType::FP32,
+            .input    = ConvolutionTensor { .config = { .layout = ConvInputLayout2D::NHWGC } },
+            .weight   = ConvolutionTensor { .config = { .layout = ConvWeightLayout2D::GKYXC } },
+            .output   = ConvolutionTensor
+                            {
+                                .config = { .layout = ConvOutputLayout2D::NHWGK },
+                                .operation = TensorOperation<> { .elementwise_operation = ElementwiseOperation::SCALEADD_SCALEADD_RELU }
+                                                .with_auxiliary_operand_configs<ConvOutputLayout2D::NHWGK, BiasLayout::G_K_strided>()
+                            }
+        };
 
     constexpr auto FwdConvAlgorithm =
         ConvAlgorithm_DeviceGroupedConvFwdMultipleABD_Xdl_CShuffle{}
