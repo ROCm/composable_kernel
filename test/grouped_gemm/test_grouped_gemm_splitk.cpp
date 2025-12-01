90bebdb065a7f2c55aa7343ad862846343b4e5b3
Erwin Terpstra <erwinterpstra@gmail.com>
2025-12-02 00:32:10 +0100

    Add grouped gemm instances for RDNA4 (#3237)
    * wip: grouped_gemm implementation based on wmma kernel + example for fp16
    
    * chore: clean up grouped_gem_wmma_splitk_fp16 example
    
    * chore: add cmake options to fully disable XDL or WMMA kernels
    
    * feat: add tests for grouped gemma wmma instances for f16 and bf16 (all layouts)
    
    * chore: add grouped gemm wmma bf16 example
    
    * refactor: reuse more code between instance factory functions
    
    * chore: turn test failure if not all batch sizes are supported into a warning
    
    * chore: made failing of test on unsupported instances conditional to not break old tests
    
    * chore: add log message to failure case where AK1/BK1/KBatch is too high for K value
    
    * fix: issue with new overloads of GridwiseGemm_wmma_cshuffle_v3::Run()
    
    * fix: stray comma after parameter list
    
    * fix: compilation issues on RDNA3 and tests failing due to unsupported problems still being ran
    
    * chore: update copyright in header comments
    
    * nit: minor feebdack
    
    * refactor: unified XDL / wma tests
    
    * fix: properly disable FP8 instances when ONLY targeting gfx11
    
    * refactor: add v3 suffix to grouped_gemm device struct name
    
    * fix: small typos in example code
    
    * fix: fully exclude xdl/wmma instances when using the corresponding cmake flags
    
    * chore: remove unused destructor and added pipeline support checks to remove unnecessary paths
    
    * fix: make sure to not add instance library to group if library was skipped
    
    * fix: make sure xdl grouped gemm doesnt fail the new test
    
    * fix: explicitly exclude test if no xdl/wmma support, as pattern matching fails in this case
    
    * fix: examples not working since dependent types and functions were moved to ck namespace in develop
    
    * fix: tests failing when compiling for just gfx11 due to trying to run unsupported instances
    
    * chore: replace/add copyright headers with new format
    
    [ROCm/composable_kernel commit: 46f1d740f03d11bc2a78fce60a95cd0933b9dd4d]

--- test/grouped_gemm/test_grouped_gemm_splitk.cpp
+++ test/grouped_gemm/test_grouped_gemm_splitk.cpp
@@ -24,21 +24,48 @@ using Col = ck::tensor_layout::gemm::ColumnMajor;
 template <typename Tuple>
 class TestGroupedGemm : public ck::test::TestGroupedGemm<Tuple>
 {
+    public:
+    void SetUp() override
+    {
+        ck::test::TestGroupedGemm<Tuple>::SetUp();
+
+#if defined(CK_USE_WMMA)
+        // The old XDL tests didn't fail if instances were not supported, so we want to keep that
+        // behaviour When compiling WMMA instances and WMMA is supported, then we'll fail if a
+        // specific case is not supported
+        this->fail_if_no_supported_instances_ =
+            ck::is_gfx11_supported() || ck::is_gfx12_supported();
+#endif
+    }
 };
 
 // clang-format off
 using KernelTypes = ::testing::Types<
+
+#if defined(CK_USE_WMMA)
+    // WWMA only. No reason to not have it for XDL, but the instance was not defined and it was not in the original test.
+    std::tuple<     Col, Col, Row, BF16, BF16, BF16>,
+#endif
+    
+#if defined(CK_USE_XDL) && defined(__gfx9__)
+    // XDL only at the moment, instances for WMMA not defined
+    std::tuple<     Row, Row, Row, BF16, I8, BF16>,
+    std::tuple<     Row, Col, Row, BF16, I8, BF16>,
+#endif
+
+#if (defined(CK_USE_XDL) && (defined(__gfx9__) || defined(__gfx12__))) || (defined(CK_USE_WMMA) && defined(__gfx12__))
+    std::tuple<     Row, Row, Row, F8, F16, F16>,
+    std::tuple<     Row, Row, Row, F16, F8, F16>,
+#endif
+
     std::tuple<     Row, Row, Row, F16, F16, F16>,
     std::tuple<     Row, Col, Row, F16, F16, F16>,
     std::tuple<     Col, Row, Row, F16, F16, F16>,
     std::tuple<     Col, Col, Row, F16, F16, F16>,
+
     std::tuple<     Row, Row, Row, BF16, BF16, BF16>,
     std::tuple<     Row, Col, Row, BF16, BF16, BF16>,
-    std::tuple<     Col, Row, Row, BF16, BF16, BF16>,
-    std::tuple<     Row, Row, Row, BF16, I8, BF16>,
-    std::tuple<     Row, Col, Row, BF16, I8, BF16>,
-    std::tuple<     Row, Row, Row, F16, F8, F16>,
-    std::tuple<     Row, Row, Row, F8, F16, F16>
+    std::tuple<     Col, Row, Row, BF16, BF16, BF16>
     >;
 // clang-format on
 
