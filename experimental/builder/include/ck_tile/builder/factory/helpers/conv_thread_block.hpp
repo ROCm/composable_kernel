424618c2b4d54a9d03f949da082a5d4c24c9a946
John Shumway <jshumway@amd.com>
2025-12-01 12:12:21 -0500

    Address reviewer comments.
    Cleanup names and comments based on reviewer feedback.

--- experimental/builder/include/ck_tile/builder/factory/helpers/conv_thread_block.hpp
+++ experimental/builder/include/ck_tile/builder/factory/helpers/conv_thread_block.hpp
@@ -7,26 +7,31 @@
 
 namespace ck_tile::builder::factory::internal {
 
-// Block info for a convolution.
-struct MNK
+/// @brief Data tile dimensions processed by a workgroup.
+/// @details This struct defines the M, N, and K dimensions of the data tile
+/// that a single workgroup (thread block) is responsible for processing in the
+/// underlying GEMM computation.
+struct DataTileInfo
 {
-    size_t m{};
-    size_t n{};
-    size_t k{};
+    int m; ///< M dimension of the tile processed by the workgroup (MPerBlock).
+    int n; ///< N dimension of the tile processed by the workgroup (NPerBlock).
+    int k; ///< K dimension of the tile processed by the workgroup (KPerBlock).
 };
 
 struct ConvBlock
 {
-    size_t block_size = 0;
-    MNK per_block     = {};
+    size_t block_size      = 0;
+    DataTileInfo per_block = {};
 };
 
 template <ConvAlgorithmDescriptor auto ALGORITHM>
 constexpr ConvBlock SetThreadBlockInfo()
 {
     constexpr auto& TB = ALGORITHM.thread_block;
-    return ConvBlock{.block_size = TB.block_size,
-                     .per_block  = {.m = TB.tile_size.m, .n = TB.tile_size.n, .k = TB.tile_size.k}};
+    return ConvBlock{
+        .block_size = TB.block_size,
+        .per_block  = {.m = TB.tile_size.m, .n = TB.tile_size.n, .k = TB.tile_size.k},
+    };
 }
 
 } // namespace ck_tile::builder::factory::internal
