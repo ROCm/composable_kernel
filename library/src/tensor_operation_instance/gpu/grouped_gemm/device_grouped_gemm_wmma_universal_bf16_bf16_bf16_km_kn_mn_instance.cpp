328a733e0e7d6b36dbed022811f6ca39eb93fb0f
Erwin Terpstra <erwinterpstra@gmail.com>
2025-12-02 00:32:10 +0100

    Add grouped gemm instances for RDNA4 (#3237)
    * wip: grouped_gemm implementation based on wmma kernel + example for fp16
    
    * chore: clean up grouped_gem_wmma_splitk_fp16 example
    
    * chore: add cmake options to fully disable XDL or WMMA kernels
    
    * feat: add tests for grouped gemma wmma instances for f16 and bf16 (all layouts)
    
    * chore: add grouped gemm wmma bf16 example
    
    * refactor: reuse more code between instance factory functions
    
    * chore: turn test failure if not all batch sizes are supported into a warning
    
    * chore: made failing of test on unsupported instances conditional to not break old tests
    
    * chore: add log message to failure case where AK1/BK1/KBatch is too high for K value
    
    * fix: issue with new overloads of GridwiseGemm_wmma_cshuffle_v3::Run()
    
    * fix: stray comma after parameter list
    
    * fix: compilation issues on RDNA3 and tests failing due to unsupported problems still being ran
    
    * chore: update copyright in header comments
    
    * nit: minor feebdack
    
    * refactor: unified XDL / wma tests
    
    * fix: properly disable FP8 instances when ONLY targeting gfx11
    
    * refactor: add v3 suffix to grouped_gemm device struct name
    
    * fix: small typos in example code
    
    * fix: fully exclude xdl/wmma instances when using the corresponding cmake flags
    
    * chore: remove unused destructor and added pipeline support checks to remove unnecessary paths
    
    * fix: make sure to not add instance library to group if library was skipped
    
    * fix: make sure xdl grouped gemm doesnt fail the new test
    
    * fix: explicitly exclude test if no xdl/wmma support, as pattern matching fails in this case
    
    * fix: examples not working since dependent types and functions were moved to ck namespace in develop
    
    * fix: tests failing when compiling for just gfx11 due to trying to run unsupported instances
    
    * chore: replace/add copyright headers with new format
    
    [ROCm/composable_kernel commit: 46f1d740f03d11bc2a78fce60a95cd0933b9dd4d]

---
+++ library/src/tensor_operation_instance/gpu/grouped_gemm/device_grouped_gemm_wmma_universal_bf16_bf16_bf16_km_kn_mn_instance.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier: MIT
+
+#include <cstdlib>
+
+#include "ck/library/tensor_operation_instance/add_device_operation_instance.hpp"
+#include "ck/library/tensor_operation_instance/gpu/grouped_gemm/device_grouped_gemm_wmma_splitk_instance.hpp"
+
+namespace ck {
+namespace tensor_operation {
+namespace device {
+namespace instance {
+
+void add_device_grouped_gemm_wmma_universal_bf16_bf16_bf16_km_kn_mn_instances(
+    std::vector<std::unique_ptr<DeviceGroupedGemm<Col,
+                                                  Row,
+                                                  DsLayout,
+                                                  ELayout,
+                                                  BF16,
+                                                  BF16,
+                                                  DsDataType,
+                                                  BF16,
+                                                  AElementOp,
+                                                  BElementOp,
+                                                  CDEElementOp>>>& instances)
+{
+    add_device_grouped_gemm_wmma_universal_instances<
+        BF16,
+        Col,
+        Row,
+        device_grouped_gemm_wmma_universal_km_kn_mn_instances>(instances);
+}
+
+} // namespace instance
+} // namespace device
+} // namespace tensor_operation
+} // namespace ck
