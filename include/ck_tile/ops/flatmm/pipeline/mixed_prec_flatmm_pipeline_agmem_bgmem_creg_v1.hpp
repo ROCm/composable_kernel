b2b6fa1aa969d65e74eaa34b172fa15b762a4618
yadaish <yadai@amd.com>
2025-12-01 05:44:21 +0000

    update

--- include/ck_tile/ops/flatmm/pipeline/mixed_prec_flatmm_pipeline_agmem_bgmem_creg_v1.hpp
+++ include/ck_tile/ops/flatmm/pipeline/mixed_prec_flatmm_pipeline_agmem_bgmem_creg_v1.hpp
@@ -288,7 +288,7 @@ struct F16xMXF4FlatmmPipelineAGmemBGmemCRegV1
             uint32_t uscale = uint32_t(scale.data) << float_mantissa;
             */
 
-            float scale_f32 = type_cast<float>(scale.data);
+            float scale_f32 = type_convert<float>(scale.data);
 
             using ComputeV2Type =
                 std::conditional_t<std::is_same_v<ComputeType, half_t>, fp16x2_t, bf16x2_t>;
