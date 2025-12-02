915e6cae86af0c13b97c79c811ed4542e97e3264
Ville Pietilä <>
2025-12-02 11:56:26 +0000

    Remove accidental edits.

--- experimental/builder/test/impl/conv_algorithm_types.hpp
+++ experimental/builder/test/impl/conv_algorithm_types.hpp
@@ -339,14 +339,6 @@ struct ConvAlgorithmTemplate : Components...
         result.transfer = t;
         return result;
     }
-
-    template <typename OP>
-    constexpr auto with_elementwise_operation(const OP& op) const
-    {
-        auto result       = *this;
-        result.element_op = op;
-        return result;
-    }
 };
 
 // Algorithm types
