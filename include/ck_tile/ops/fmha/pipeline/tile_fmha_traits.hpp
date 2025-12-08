7fc9dbf5d9f287cbfee8f6b236e05c20492c587f
Linjun-AMD <Jun.Lin@amd.com>
2025-12-08 16:43:40 +0800

    Update tile_fmha_traits.hpp

--- include/ck_tile/ops/fmha/pipeline/tile_fmha_traits.hpp
+++ include/ck_tile/ops/fmha/pipeline/tile_fmha_traits.hpp
@@ -20,7 +20,7 @@ template <bool kPadSeqLenQ_ /* padding for seqlen_q */,
           bool kStoreLSE_,
           bool kHasDropout_,
           BlockAttentionQuantScaleEnum QScaleEnum_,
-          index_t kBlockPerCu_  = -1, /* overwrite occupancy if not -1 */
+          index_t kBlockPerCu_  = -1,    /* overwrite occupancy if not -1 */
           bool kSkipMinSeqlenQ_ = false, /* skip min seqlen q while chunked prefill */
           bool kHasSink_        = false>
 struct TileFmhaTraits
