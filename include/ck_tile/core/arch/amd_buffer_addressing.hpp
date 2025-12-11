d5645ff481a92a4c2fb4a68724d047530c375a84
eliotwang <46883838+eliotwang@users.noreply.github.com>
2025-12-11 23:20:29 +0800

    Bf16*fp4 gemm (#2801)
    * support bf16*mxfp4 gemm
    
    * rebase bf16*fp4 example to develop branch
    
    * Clean up commented debug code in GEMM kernel
    
    * rename example folder
    
    * support bf16*mxfp4 gemm
    
    * rebase bf16*fp4 example to develop branch
    
    * Clean up commented debug code in GEMM kernel
    
    * rename example folder
    
    * rebase to new develop
    
    * fix clang format
    
    * update code according to reviewer's comment
    
    * Update README.md
    
    * update code according to reviewer's comment
    
    * update code according to reviewer's comment
    
    * Update CMakeLists.txt
    
    * Update README.md
    
    * Update CMakeLists.txt
    
    * Delete files
    
    * Delete files
    
    * Add unit tests
    
    * Update test_gemm_quant_base.hpp
    
    * merge bf16*fp4 example to develop branch
    
    * fix clang format
    
    * fix clang format
    
    * Update CMakeLists.txt
    
    * fix ci test
    
    * fix clang format
    
    * resolve conflicts
    
    ---------
    
    Co-authored-by: eliotwang <charyang@smci355-ccs-aus-m10-29.cs-aus.dcgpu>
    Co-authored-by: ShaoChunLee <Shao-Chun.Lee@amd.com>
    Co-authored-by: Illia Silin <98187287+illsilin@users.noreply.github.com>
    Co-authored-by: illsilin_amdeng <Illia.Silin@amd.com>
    Co-authored-by: Thomas Ning <Thomas.Ning@amd.com>
    
    [ROCm/composable_kernel commit: 715671e419cbbebe72109ceeeed9d582cca34d02]

--- include/ck_tile/core/arch/amd_buffer_addressing.hpp
+++ include/ck_tile/core/arch/amd_buffer_addressing.hpp
@@ -1550,9 +1550,10 @@ CK_TILE_DEVICE thread_buffer<T, N> amd_buffer_load_impl(int32x4_t src_wave_buffe
             (std::is_same<T, int8_t>::value && (N == 1 || N == 2 || N == 4 || N == 8 || N == 16)) ||
             (std::is_same<T, e8m0_t>::value && (N == 1 || N == 2 || N == 4 || N == 8 || N == 16)) ||
             (std::is_same<T, pk_int4_t>::value &&
-                 (N == 1 || N == 2 || N == 4 || N == 8 || N == 16 || N == 32) ||
-             (std::is_same<T, pk_fp4_t>::value &&
-              (N == 1 || N == 2 || N == 4 || N == 8 || N == 16))),
+             (N == 1 || N == 2 || N == 4 || N == 8 || N == 16 || N == 32)) ||
+            (std::is_same<T, pk_fp4_raw_t>::value &&
+             (N == 1 || N == 2 || N == 4 || N == 8 || N == 16)) ||
+            (std::is_same<T, pk_fp4_t>::value && (N == 1 || N == 2 || N == 4 || N == 8 || N == 16)),
         "wrong! not implemented");
 
     using rtn_type = thread_buffer<T, N>;
