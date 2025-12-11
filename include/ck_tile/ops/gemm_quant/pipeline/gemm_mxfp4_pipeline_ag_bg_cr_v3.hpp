112b5ecf6b18f00d5b9d6e371dab6d2fe1c23b4a
KenSCLin <lshyhchy@amd.com>
2025-12-11 17:42:06 +0000

    fix pre-commit error

--- include/ck_tile/ops/gemm_quant/pipeline/gemm_mxfp4_pipeline_ag_bg_cr_v3.hpp
+++ include/ck_tile/ops/gemm_quant/pipeline/gemm_mxfp4_pipeline_ag_bg_cr_v3.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
 
 #pragma once
