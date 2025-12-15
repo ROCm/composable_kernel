483e0bf9c070b4f0e45777b1ce40f9f9f0ef71b3
Andrew Clark <andrew.clark@amd.com>
2025-12-15 11:21:17 -0500

    Adding copyright info

--- script/monitor_sccache_during_build.sh
+++ script/monitor_sccache_during_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier: MIT
+
 # Continuous monitoring script for sccache during builds
 # Usage: ./monitor_sccache_during_build.sh [log_prefix] &
 
