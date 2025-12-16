53ad2bbae1973f09f6c0d0f521b490080fa5e346
Erwin Terpstra <erwin.terpstra@streamhpc.com>
2025-12-16 11:24:03 +0000

    chore: set grouped gemm example M/N warp tile to 16

--- example/ck_tile/17_grouped_gemm/quant_grouped_gemm_config.hpp
+++ example/ck_tile/17_grouped_gemm/quant_grouped_gemm_config.hpp
@@ -64,8 +64,8 @@ struct GemmConfigComputeV3_2 : public GemmConfigBase<Persistent>
     static constexpr ck_tile::index_t N_Warp = 2;
     static constexpr ck_tile::index_t K_Warp = 1;
 
-    static constexpr ck_tile::index_t M_Warp_Tile = 32;
-    static constexpr ck_tile::index_t N_Warp_Tile = 32;
+    static constexpr ck_tile::index_t M_Warp_Tile = 16;
+    static constexpr ck_tile::index_t N_Warp_Tile = 16;
     static constexpr ck_tile::index_t K_Warp_Tile =
         ck_tile::get_k_warp_tile<PrecType, M_Warp_Tile>();
 };
