f75cc7c41582d1d237e3641e0c04975dff9461b2
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-12-21 15:13:14 +0000

    Update to the non-whole-k-prefetch path in the whoke_k_prefetch pipeline

--- include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_whole_k_prefetch.hpp
+++ include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_qr_ks_vs_whole_k_prefetch.hpp
@@ -505,10 +505,12 @@ struct BlockFmhaPipelineQRKSVSWholeKPrefetch
                         move_tile_window(k_dram_window, {kN0Sub, 0});
                     };
 
-                    if constexpr(i_n0 < NumPrefetchV)
+                    if constexpr(i_n0 == n0_loops - 1)
                     {
-                        v_tiles[i_n0] = load_tile(v_dram_window);
-                        move_tile_window(v_dram_window, {0, kK1});
+                        static_for<0, NumPrefetchV, 1>{}([&](auto i_k1) {
+                            v_tiles[i_k1] = load_tile(v_dram_window);
+                            move_tile_window(v_dram_window, {0, kK1});
+                        });
                     };
 
                     __builtin_amdgcn_sched_barrier(0x00000001);
