29bda92c78a78ab677cb4fba92068e57870f4480
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2026-01-02 19:08:35 -0600

    remove AtomicAddRequiresKBatchGreaterThanOne test since the property is removed from the kernel scope

--- test/ck_tile/grouped_conv/test_ck_tile_grouped_conv_bwd_weight.cpp
+++ test/ck_tile/grouped_conv/test_ck_tile_grouped_conv_bwd_weight.cpp
@@ -210,25 +210,6 @@ TEST_F(GroupedConvBwdWeightIsSupportedArgumentTest, InvalidKBatchLessThanOne)
     EXPECT_FALSE(Kernel::IsSupportedArgument(kargs));
 }
 
-TEST_F(GroupedConvBwdWeightIsSupportedArgumentTest, AtomicAddRequiresKBatchGreaterThanOne)
-{
-    using Kernel = typename BuildKernel<half_t,
-                                        TestConvConfig,
-                                        tensor_layout::convolution::NHWGC,
-                                        tensor_layout::convolution::GKYXC,
-                                        tensor_layout::convolution::NHWGK>::type;
-
-    // k_batch = 1 should fail with atomic_add
-    auto host_args_kbatch_1 = create_2d_host_args(1);
-    auto kargs_1 = typename Kernel::GroupedConvBwdWeightKernelArgsSpecialized(host_args_kbatch_1);
-    EXPECT_FALSE(Kernel::IsSupportedArgument(kargs_1));
-
-    // k_batch = 2 should pass
-    auto host_args_kbatch_2 = create_2d_host_args(2);
-    auto kargs_2 = typename Kernel::GroupedConvBwdWeightKernelArgsSpecialized(host_args_kbatch_2);
-    EXPECT_TRUE(Kernel::IsSupportedArgument(kargs_2));
-}
-
 TEST_F(GroupedConvBwdWeightIsSupportedArgumentTest, K0KBatchLimitation)
 {
     using Kernel = typename BuildKernel<half_t,
