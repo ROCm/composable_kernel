a47bef99b5071641a15b8d4ce3594add38b88fe3
John Shumway <jshumway@amd.com>
2026-01-06 12:50:42 -0500

    Create a non-templated conv_traits struct

--- experimental/builder/include/ck_tile/builder/reflect/conv_describe.hpp
+++ experimental/builder/include/ck_tile/builder/reflect/conv_describe.hpp
@@ -7,7 +7,7 @@
 #pragma once
 
 #include "ck_tile/builder/reflect/conv_description.hpp"
-#include "ck_tile/builder/reflect/conv_traits.hpp"
+#include "ck_tile/builder/reflect/instance_to_conv_traits.hpp"
 
 namespace ck_tile::reflect {
 
@@ -17,31 +17,31 @@ namespace ck_tile::reflect {
 template <conv::HasConvTraits Instance>
 conv::ConvDescription describe()
 {
-    using Traits = conv::ConvTraits<Instance>;
+    const auto traits = conv::instance_to_conv_traits<Instance>();
 
     return conv::ConvDescription(
         conv::ConvSignatureInfo{
-            .spatial_dim       = Traits::spatial_dim,
-            .direction         = Traits::direction,
-            .input_layout      = Traits::layout[0],
-            .weight_layout     = Traits::layout[1],
-            .output_layout     = Traits::layout[2],
-            .data_type         = Traits::data_type,
-            .input_element_op  = Traits::input_element_op,
-            .weight_element_op = Traits::weight_element_op,
-            .output_element_op = Traits::output_element_op,
+            .spatial_dim       = traits.spatial_dim,
+            .direction         = traits.direction,
+            .input_layout      = traits.layout[0],
+            .weight_layout     = traits.layout[1],
+            .output_layout     = traits.layout[2],
+            .data_type         = traits.data_type,
+            .input_element_op  = traits.input_element_op,
+            .weight_element_op = traits.weight_element_op,
+            .output_element_op = traits.output_element_op,
         },
         conv::GemmAlgorithmInfo{
-            .thread_block_size   = Traits::thread_block_size,
-            .tile_dims           = Traits::tile_dims,
-            .warp_gemm           = Traits::warp_gemm,
-            .a_tile_transfer     = Traits::a_tile_transfer,
-            .b_tile_transfer     = Traits::b_tile_transfer,
-            .c_tile_transfer     = Traits::c_tile_transfer,
-            .pipeline_version    = Traits::pipeline_version,
-            .pipeline_scheduler  = Traits::pipeline_scheduler,
-            .conv_specialization = Traits::conv_specialization,
-            .padding             = Traits::gemm_padding,
+            .thread_block_size   = traits.thread_block_size,
+            .tile_dims           = traits.tile_dims,
+            .warp_gemm           = traits.warp_gemm,
+            .a_tile_transfer     = traits.a_tile_transfer,
+            .b_tile_transfer     = traits.b_tile_transfer,
+            .c_tile_transfer     = traits.c_tile_transfer,
+            .pipeline_version    = traits.pipeline_version,
+            .pipeline_scheduler  = traits.pipeline_scheduler,
+            .conv_specialization = traits.conv_specialization,
+            .padding             = traits.gemm_padding,
         },
         []() { return reflect::instance_string<Instance>(); });
 }
