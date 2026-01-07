acf98936bc4572109667fb87207652d344a55b8c
Johannes Graner <johannes.graner@amd.com>
2026-01-07 16:30:57 +0100

    [CI, CK examples] Disable time_kernel for CI tests and examples (#3464)
    * Disable kernel timing in tests
    
    * default time_kernel = false in old CK examples
    
    [ROCm/composable_kernel commit: 0a474aa62f6dd3f4b95bb405f0a8f1d457a4c0eb]

--- example/65_gemm_multiply_multiply/moe_gemm1_xdl_fp8_blockscale.cpp
+++ example/65_gemm_multiply_multiply/moe_gemm1_xdl_fp8_blockscale.cpp
@@ -193,7 +193,7 @@ int main(int argc, char* argv[])
 {
     bool do_verification = true;
     int init_method      = 1;
-    bool time_kernel     = true;
+    bool time_kernel     = false;
 #if 1
     // GEMM shape
     ck::index_t N       = 4096;
