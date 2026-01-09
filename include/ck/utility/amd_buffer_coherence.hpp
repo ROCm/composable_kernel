c9c68adadf12dc59765ddcbcd2eac5c2735990f7
yadaish <yadai@amd.com>
2026-01-09 10:14:43 +0800

    revert any change in amd_buffer_coherence

--- include/ck/utility/amd_buffer_coherence.hpp
+++ include/ck/utility/amd_buffer_coherence.hpp
@@ -82,6 +82,7 @@ enum struct AmdBufferCoherenceEnum
 // SC[1:0] System Cache level: 0=wave, 1=group, 2=device, 3=system
 // NT Non-Temporal: 0=expect temporal reuse; 1=do not expect temporal reuse
 #elif defined(__gfx942__) || defined(__gfx950__)
+
     WAVE   = 0,
     GROUP  = 1,
     DEVICE = 16,
