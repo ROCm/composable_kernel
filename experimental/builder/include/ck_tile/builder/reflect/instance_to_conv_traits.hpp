ef0227d255c8104dc170f4fa9530f22f10eb0b7b
Kevin Abraham <kevin.abraham@streamhpc.com>
2026-01-15 16:55:52 +0000

    added reflection for grouped_conv_bwd weight_cshuffleV3

--- experimental/builder/include/ck_tile/builder/reflect/instance_to_conv_traits.hpp
+++ experimental/builder/include/ck_tile/builder/reflect/instance_to_conv_traits.hpp
@@ -9,6 +9,7 @@
 #include "ck_tile/builder/reflect/conv_traits_device_grouped_conv_fwd_multiple_d_xdl_large_tensor_cshuffle.hpp"
 
 #include "ck_tile/builder/reflect/conv_traits_device_grouped_conv_bwd_weight_xdl_cshuffle.hpp"
+#include "ck_tile/builder/reflect/conv_traits_device_grouped_conv_bwd_weight_xdl_cshuffle_v3.hpp"
 
 // Wmma instances
 #include "ck_tile/builder/reflect/conv_traits_device_grouped_conv_fwd_multiple_d_wmma_cshuffle.hpp"
