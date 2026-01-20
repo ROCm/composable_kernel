85c574149268334fe077fd6967c204626e37b00c
Bartłomiej Kocot <barkocot@amd.com>
2026-01-20 06:29:01 +0100

    [CK_BUILDER] Add grouped conv fwd ck tile profiler (#3518)
    * [BULDER] Add grouped conv fwd ck tile profiler
    
    * [CK TILE] Fix grouped conv kernels splitk and double lds
    
    * Updates
    
    * Fixes
    
    * Move to ckProfiler
    
    * Fixes
    
    * fix
    
    * fix
    
    * Change instances to empty list by default
    
    * fix
    
    * fix
    
    * Update grouped_convolution_signatures.hpp
    
    * Update grouped_convolution_forward_tile_algs.hpp
    
    * [CK TILE] Add grouped convolution forward tests (#3556)
    
    * [CK TILE] Add grouped convolution forward tests
    
    * fix jenkins
    
    * fixes
    
    * comments fixes
    
    * unit test
    
    * unit test fix
    
    * Move instances outside builder
    
    * fix includes
    
    * clang format fix
    
    * readme fix
    
    * fix includes
    
    * fixes
    
    [ROCm/composable_kernel commit: 0727e85e523aac7a1e82af00f44081cc67f5cde0]

--- experimental/builder/include/ck_tile/builder/testing/tensor_descriptor.hpp
+++ experimental/builder/include/ck_tile/builder/testing/tensor_descriptor.hpp
@@ -418,6 +418,10 @@ struct TensorDescriptor
         size_t x = 1;
         for(size_t i = 0; i < RANK; ++i)
         {
+            if(lengths[indices[i]] == 1)
+            {
+                continue;
+            }
             if(strides[indices[i]] != x)
                 return false;
 
@@ -441,6 +445,15 @@ struct TensorDescriptor
         ck_tile::builder::test::Extent<1> lengths = {this->get_element_space_size()};
         ck_tile::builder::test::Extent<1> strides = {1};
         return TensorDescriptor<DT, 1>(lengths, strides);
+    }
+
+    /// @brief Print tensor descriptor details.
+    ///
+    /// Print tensor descriptor details - lengths and strides.
+    friend std::ostream& operator<<(std::ostream& os, const TensorDescriptor<DT, RANK>& tensor_desc)
+    {
+        os << tensor_desc.inner_descriptor_;
+        return os;
     }
 
     private:
