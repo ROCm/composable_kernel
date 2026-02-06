683250e41bbcef0687605bca4c2d0a538a9beeff
Matti Eskelinen <matti.eskelinen@amd.com>
2026-02-06 09:33:33 +0000

    fix buffer size for output

--- test/ck_tile/sinkhorn/test_sinkhorn_impl.hpp
+++ test/ck_tile/sinkhorn/test_sinkhorn_impl.hpp
@@ -38,9 +38,8 @@ class TestCkTileSinkHorn : public ::testing::Test
 
         ck_tile::FillUniformDistribution<XDataType>{-5.f, 5.f}(h_x);
 
-        auto buffer_size = h_x.get_element_space_size_in_bytes();
-        ck_tile::DeviceMem d_x_mem(buffer_size);
-        ck_tile::DeviceMem d_y_mem(buffer_size);
+        ck_tile::DeviceMem d_x_mem(h_x.get_element_space_size_in_bytes());
+        ck_tile::DeviceMem d_y_mem(h_y.get_element_space_size_in_bytes());
 
         ck_tile::SinkhornKnoppArgs args{static_cast<void*>(d_y_mem.GetDeviceBuffer()),
                                         static_cast<void*>(d_x_mem.GetDeviceBuffer()),
