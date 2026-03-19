ac9a142e638c8b77d19796fe34847ec41b161354
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2026-03-19 10:39:37 +0000

    Enable run-time selection of MTile sizes according to the predicted CU utilization ratio

--- example/ck_tile/18_hstu_attention/hstu_attention_util.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_util.hpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <stdexcept>
 
+#include "ck_tile/host/hip_check_error.hpp"
+
 #define HSTU_CHECK(COND, ERR)                  \
     if(!(COND))                                \
     {                                          \
@@ -15,3 +17,16 @@
         ostr << "'" #COND "' failed: " << ERR; \
         throw std::runtime_error(ostr.str());  \
     }
+
+static inline int get_number_of_cu()
+{
+    int device;
+
+    HIP_CHECK_ERROR(hipGetDevice(&device));
+
+    hipDeviceProp_t props;
+
+    HIP_CHECK_ERROR(hipGetDeviceProperties(&props, device));
+
+    return props.multiProcessorCount;
+}
