e912220433441e65080c3158ae8cd1873ab15a53
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2026-04-27 10:15:14 +0000

    Use include <...> format to refer to header files from ck_tile

--- example/ck_tile/18_hstu_attention/hstu_attention_epilogue.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_epilogue.hpp
@@ -3,9 +3,9 @@
 
 #pragma once
 
-#include "ck_tile/core.hpp"
-#include "ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp"
-#include "ck_tile/ops/common/tensor_layout.hpp"
+#include <ck_tile/core.hpp>
+#include <ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp>
+#include <ck_tile/ops/common/tensor_layout.hpp>
 
 namespace ck_tile {
 
