789ef3809386a08ec24d2a129bd4684429cb7c67
Illia Silin <98187287+illsilin@users.noreply.github.com>
2026-06-12 18:19:31 +0000

    [rocm-libraries] ROCm/rocm-libraries#8333 (commit 69b3fc1)
    Revert "[CK_TILE] Implement RTC API for a subset of FMHA
     functionality for MGX" (#8333)
    
    Reverts ROCm/rocm-libraries#6086
    Need to revert as the codegen test for fmha is failing due to including
    std header:
    
    2026-06-11T22:36:03.673Z] In file included from
    /tmp/comgr-953928-0-473822/include/ck/host/device_fmha_fwd/fmha_fwd_wrapper.hpp:8:
    [2026-06-11T22:36:03.673Z] In file included from
    /bin/../lib/gcc/x86_64-linux-gnu/13/../../../../include/c++/13/cmath:49:
    [2026-06-11T22:36:03.673Z] In file included from
    /bin/../lib/gcc/x86_64-linux-gnu/13/../../../../include/c++/13/bits/std_abs.h:38:
    [2026-06-11T22:36:03.673Z] /usr/include/stdlib.h:32:10: fatal error:
    'stddef.h' file not found
    [2026-06-11T22:36:03.673Z]    32 | #include <stddef.h>
    [2026-06-11T22:36:03.673Z]       |          ^~~~~~~~~~
    
    The ck_tile headers were never prepped for hiprtc compilation.

--- codegen/src/headers.cpp
+++ codegen/src/headers.cpp
@@ -2,10 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include "ck/host/headers.hpp"
-#include "ck/host/ck_tile_headers_preprocessor.hpp"
 #include "ck_headers.hpp"
-#include "ck_tile_headers.hpp"
-#include "ck_codegen_headers.hpp"
 
 namespace ck {
 namespace host {
@@ -24,30 +21,6 @@ std::unordered_map<std::string_view, std::string_view> GetHeaders()
     auto headers = ck_headers();
     headers.insert(std::make_pair("ck/config.h", config_header));
     return headers;
-}
-
-std::unordered_map<std::string, std::string> GetTileHeaders()
-{
-    auto tile_hdrs    = ck_tile_headers();
-    auto codegen_hdrs = ck_codegen_headers();
-
-    std::unordered_map<std::string, std::string> result;
-    result.reserve(tile_hdrs.size() + codegen_hdrs.size());
-
-    for(auto& [name, content] : tile_hdrs)
-    {
-        if(name == "ck_tile/core/utility/env.hpp")
-        {
-            result.emplace(std::string(name), "");
-            continue;
-        }
-        result.emplace(std::string(name), strip_host_bodies(content));
-    }
-
-    for(auto& [name, content] : codegen_hdrs)
-        result.emplace(std::string(name), std::string(content));
-
-    return result;
 }
 
 } // namespace host
