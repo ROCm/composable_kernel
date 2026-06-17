5bebfd460f9a68e18c3c3218e1a3c5d5adb15023
damien-lejeune <31985270+damien-lejeune@users.noreply.github.com>
2026-06-17 06:22:26 +0000

    [rocm-libraries] ROCm/rocm-libraries#8492 (commit 46b6a06)
    Add tile size for FMHA batch prefill bf16 for MI308X
    
    ## Motivation
    
    Adding a tile size adapted to MI308X, for the FMHA Batch Prefill BF16
    input type case
    
    ## Technical Details
    
    N/A
    
    ## Test Plan
    
    Benchmarking from the Aiter side with:
    
    ```
    python3 op_tests/test_batch_prefill.py  -s 8000 -p 1 -q 4 -k 1 --head_dim 256 -c true -d bf16 --input_dtype bf16 --quant_method none --kv_layout linear -t sglang -l 0.0 --return_lse false --profile
    ```
    
    ## Test Result
    
    We see an improvement with the new tile size on MI308X (both with PLT
    mode OFF and ON)
    
    ## Submission Checklist
    
    - [X] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Co-authored-by: Damien Lejeune <damien.lejeune@amd.com>

--- example/ck_tile/01_fmha/codegen/ops/fmha_batch_prefill.py
+++ example/ck_tile/01_fmha/codegen/ops/fmha_batch_prefill.py
@@ -677,7 +677,10 @@ class KernelComponentFactory:
         if dtype in ["fp16", "bf16"]:
             return {
                 128 : [FmhaFwdTileSize(128, 128, 32, 128, 32,  128,  4, 1, 1,  4, 1, 1,  32, 32, 16,  32, 32, 16,  -1)],
-                256 : [FmhaFwdTileSize(128, 128, 32, 256, 32,  256,  4, 1, 1,  4, 1, 1,  32, 32, 16,  32, 32, 16,  -1)],
+                256 : [
+                    FmhaFwdTileSize(128,  32, 16, 256, 16,  256,  4, 1, 1,  4, 1, 1,  32, 32, 16,  32, 32, 16, 2, CppConstraint("num_cus < 128")),
+                    FmhaFwdTileSize(128, 128, 32, 256, 32,  256,  4, 1, 1,  4, 1, 1,  32, 32, 16,  32, 32, 16,  -1),
+                ],
             }  # fmt: skip
         elif dtype in ["fp8bf16"]:
             return {
