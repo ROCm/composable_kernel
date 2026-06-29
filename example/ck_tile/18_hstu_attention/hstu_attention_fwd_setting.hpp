36fa507ab5bc3a0f5035f2bc3f989cda0df43845
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2026-06-29 10:21:47 +0000

    Add max_seqlen_kv as host API parameter and adjust the rules for using splitkv for cross-attention

--- example/ck_tile/18_hstu_attention/hstu_attention_fwd_setting.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_fwd_setting.hpp
@@ -465,22 +465,3 @@ template struct HstuAttentionWithSoftmaxFwdTileSetting<256, 64>;
 template struct HstuAttentionWithSoftmaxFwdTileSetting<256, 128>;
 
 #endif
-
-static int get_hstu_attention_fwd_mtile(int num_batches, int num_heads, int max_seqlen_q)
-{
-    int num_CUs  = get_number_of_cu();
-    auto ceildiv = [](int a, int b) { return (a + b - 1) / b; };
-
-    if(max_seqlen_q <= 64)
-        return 64;
-
-    int nbatch_nhead_mblocks = num_batches * num_heads * ceildiv(max_seqlen_q, 128);
-
-    // assuming each CU is assigned two work-groups
-    if(nbatch_nhead_mblocks >= static_cast<int>(0.85f * num_CUs * 2.0f))
-        return 128;
-
-    // currently, only hdim-128 actually uses mtile-64, for other hdim, the settings for
-    // mtile-64 can be added through tuning/verification
-    return 64;
-};
