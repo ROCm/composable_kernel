8f2c4a5db58201312065b28acd99c4b3516a7b29
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2026-07-08 15:06:52 +0000

    Remove the name string definition in each pipeline since it is not used

--- example/ck_tile/18_hstu_attention/hstu_attention_no_softmax_fwd_trload_pipeline.hpp
+++ example/ck_tile/18_hstu_attention/hstu_attention_no_softmax_fwd_trload_pipeline.hpp
@@ -102,8 +102,6 @@ struct HstuAttentionNoSoftmaxFwdPipelineQRKSVSTrLoad
         }
     }();
 
-    static constexpr const char* name = "qr_hstu";
-
     using DropoutType = std::conditional_t<kHasDropout, BlockDropout, NullBlockDropout>;
 
     CK_TILE_DEVICE static constexpr ck_tile::index_t GetSmemSize()
